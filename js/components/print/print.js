bc2708e217090cf720eb39b1dfcb473a73664b5b
Raitis <raitisbe@gmail.com>
2014-09-12 14:19:29 +0200

    Initial print functionality

--- js/components/print/print.js
+++ js/components/print/print.js
@@ -4,14 +4,23 @@ angular.module('hs.print', [])
             templateUrl: 'js/components/print/partials/printdialog.html'
         };
     })
-    .directive('printbutton', function() {
-        return {
-            templateUrl: 'js/components/print/partials/printbutton.html'
-        };
-    })
 
 .controller('Print', ['$scope',
     function($scope) {
-
+        $scope.canvas_serialized = "";
+        $scope.print_visible = false;
+        
+        $scope.showPrint = function(){
+            $scope.print_visible = !$scope.print_visible;
+        }
+        
+        $scope.print = function(){
+                var canvas = canvas=document.getElementsByTagName("canvas")[0];
+                 var win=window.open();
+                 win.document.write("<h2>"+$scope.title+"</h2>");
+                 win.document.write("<br><img src='"+canvas.toDataURL()+"'/>");
+                 win.print();
+                 win.location.reload();
+        }
     }
 ]);
