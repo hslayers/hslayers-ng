48caa0399e41122ee22018c9377bfc4f789f0294
Raitis <raitisbe@gmail.com>
2014-09-29 18:03:01 +0300

    toolbar component

---
+++ js/components/toolbar/toolbar.js
@@ -0,0 +1,24 @@
+angular.module('hs.toolbar', ['hs.map'])
+    .directive('toolbar', function() {
+        return {
+            templateUrl: 'js/components/toolbar/partials/toolbar.html'
+        };
+    }).service("ToolbarService", ['$rootScope',
+        function($rootScope) {
+            var me = {
+                mainpanel:"",
+                setMainPanel : function(which) {
+                    me.mainpanel = which; 
+                    $rootScope.$broadcast( 'toolbar.mainpanel_changed' );//Not used now, but could be useful
+                }
+            };
+            
+            return me;
+        }
+    ])
+
+.controller('Toolbar', ['$scope', 'OlMap',
+    function($scope, OlMap) {
+        
+    }
+]);
