92fae44f0db141be5234683b63f51a068247ecb7
Raitis <raitisbe@gmail.com>
2014-12-30 05:06:16 +0200

    follow geolocation styles for geolocation show speed and altitude on screen

--- components/geolocation/geolocation.js
+++ components/geolocation/geolocation.js
@@ -17,75 +17,10 @@ define(['angular', 'ol'],
                 };
             }])
 
-        .service('Geolocation', ['OlMap',
-            function(OlMap) {
-                var geolocation = new ol.Geolocation({
-                    projection: OlMap.map.getView().getProjection()
-                });
-                //var track = new ol.dom.Input(document.getElementById('track'));
-                //track.bindTo('checked', geolocation, 'tracking');
-
-                geolocation.on('change', function() {
-                    accuracy = geolocation.getAccuracy() + ' [m]';
-                    altitude = geolocation.getAltitude() + ' [m]';
-                    altitudeAccuracy = geolocation.getAltitudeAccuracy() + ' [m]';
-                    heading = geolocation.getHeading() + ' [rad]';
-                    speed = geolocation.getSpeed() + ' [m/s]';
-                });
-
-                // handle geolocation error.
-                geolocation.on('error', function(error) {
-                    var info = document.getElementById('info');
-                    info.innerHTML = error.message;
-                    info.style.display = '';
-                });
-
-                var accuracyFeature = new ol.Feature();
-                accuracyFeature.bindTo('geometry', geolocation, 'accuracyGeometry');
-
-                var positionFeature = new ol.Feature();
-                positionFeature.bindTo('geometry', geolocation, 'position')
-                    .transform(function() {}, function(coordinates) {
-                        return coordinates ? new ol.geom.Point(coordinates) : null;
-                    });
-
-                var style = new ol.style.Style({
-                    fill: new ol.style.Fill({
-                        color: [0xbb, 0xbb, 0xbb, 0.2]
-                    }),
-                    stroke: new ol.style.Stroke({
-                        color: '#3399FF'
-                    })
-                });
-
-                var style2 = new ol.style.Style({
-                    stroke: new ol.style.Stroke({
-                        color: '#3399FF'
-                    })
-                });
-
-                var featuresOverlay = new ol.FeatureOverlay({
-                    map: OlMap.map,
-                    features: [],
-                    style: style
-                });
-
-                OlMap.map.on("pointermove", function(event) {
-                    var mouseCoordInMapPixels = [event.originalEvent.offsetX, event.originalEvent.offsetY];
-
-                    //detect feature at mouse coords
-                    var hit = OlMap.map.forEachFeatureAtPixel(mouseCoordInMapPixels, function(feature, layer) {
-                        return (feature == accuracyFeature || feature == positionFeature);
-                    });
-                    if (hit) {
-                        featuresOverlay.setStyle(style2)
-                    } else {
-                        featuresOverlay.setStyle(style);
-                    }
-                })
-
-                return {
-                    geolocation: geolocation,
+        .service('Geolocation', ['OlMap', '$rootScope',
+            function(OlMap, $rootScope) {
+                var me = {
+                    geolocation: null,
                     toggleFeatures: function(visible) {
                         if (visible) {
                             featuresOverlay.addFeature(accuracyFeature);
@@ -96,9 +31,77 @@ define(['angular', 'ol'],
 
                         }
                     }
-                }
+                };
+                me.geolocation = new ol.Geolocation({
+                    projection: OlMap.map.getView().getProjection()
+                });
+                //var track = new ol.dom.Input(document.getElementById('track'));
+                //track.bindTo('checked', geolocation, 'tracking');
+
+                me.geolocation.on('change', function() {
+                    if (!me.geolocation.getTracking()) return;
+                    var p = me.geolocation.getPosition();
+                    me.accuracy = me.geolocation.getAccuracy() + ' [m]';
+                    me.altitude = me.geolocation.getAltitude() ? me.geolocation.getAltitude() + ' [m]' : '-';
+                    me.altitudeAccuracy = me.geolocation.getAltitudeAccuracy() ? '+/- ' + me.geolocation.getAltitudeAccuracy() + ' [m]' : '';
+                    me.heading = me.geolocation.getHeading() + ' [rad]';
+                    me.speed = me.geolocation.getSpeed() ? me.geolocation.getSpeed() + ' [m/s]' : '-';
+                    if (!positionFeature.getGeometry())
+                        positionFeature.setGeometry(new ol.geom.Point(p));
+                    else
+                        positionFeature.getGeometry().setCoordinates(p);
+                    OlMap.map.getView().setCenter(p);
+                    $rootScope.$broadcast('geolocation.updated');
+                });
+
+                // handle geolocation error.
+                me.geolocation.on('error', function(error) {
+                    var info = document.getElementById('info');
+                    info.innerHTML = error.message;
+                    info.style.display = '';
+                });
+
+                var style = new ol.style.Style({
+                    image: new ol.style.Circle({
+                        fill: new ol.style.Fill({
+                            color: [242, 121, 0, 0.7]
+                        }),
+                        stroke: new ol.style.Stroke({
+                            color: [0xbb, 0x33, 0x33, 0.7]
+                        }),
+                        radius: 5
+                    }),
+                    fill: new ol.style.Fill({
+                        color: [0xbb, 0xbb, 0xbb, 0.2]
+                    }),
+                    stroke: new ol.style.Stroke({
+                        color: [0x66, 0x66, 0x00, 0.8]
+                    })
+                });
+
+                var accuracyFeature = new ol.Feature();
+                accuracyFeature.bindTo('geometry', me.geolocation, 'accuracyGeometry');
+
+                var positionFeature = new ol.Feature();
+
+                accuracyFeature.setStyle(style);
+                positionFeature.setStyle(style);
+
+
+                var featuresOverlay = new ol.FeatureOverlay({
+                    map: OlMap.map,
+                    features: []
+                });
+
+
+                return me;
             }
-        ]).run(function(Geolocation) { // instance-injector
-            //Gets executed after service is loaded
-        });
+        ]).controller('Geolocation', ['$scope', 'Geolocation', function($scope, Geolocation) {
+            $scope.$on('geolocation.updated', function(event) {
+                $scope.speed = Geolocation.speed;
+                $scope.alt = Geolocation.altitude;
+                $scope.altitudeAccuracy = Geolocation.altitudeAccuracy;
+                if (!$scope.$$phase) $scope.$digest();
+            });
+        }]);
     })
