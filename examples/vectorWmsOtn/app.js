dbc7a077acbc33cafba98d07ac9defc7b07627d8
Raitis <raitisbe@gmail.com>
2015-02-26 14:41:07 +0200

    vectorWfs layer example for otn project

---
+++ examples/vectorWmsOtn/app.js
@@ -0,0 +1,78 @@
+'use strict';
+
+define(['angular', 'ol', 'toolbar', 'layermanager', 'WfsSource', 'core', 'map', 'query', 'search', 'print', 'permalink', 'measure', 'geolocation'],
+
+    function(angular, ol, toolbar, layermanager, WfsSource) {
+        var module = angular.module('hs', [
+            'hs.core',
+            'hs.toolbar',
+            'hs.layermanager',
+            'hs.map',
+            'hs.query',
+            'hs.search', 'hs.print', 'hs.permalink',
+            'hs.geolocation'
+        ]);
+
+        module.directive('hs', ['OlMap', 'Core', function(OlMap, Core) {
+            return {
+                templateUrl: hsl_path + 'hslayers.html',
+                link: function(scope, element) {
+                    Core.fullscreenMap(element);
+                }
+            };
+        }]);
+        
+        module.value('box_layers', []);
+       
+            var style = new ol.style.Style({
+                    image: new ol.style.Circle({
+                        fill: new ol.style.Fill({
+                            color: [242, 121, 0, 0.7]
+                        }),
+                        stroke: new ol.style.Stroke({
+                            color: [0xbb, 0x33, 0x33, 0.7]
+                        }),
+                        radius: 5
+                    }),
+                    fill: new ol.style.Fill({
+                        color: "rgba(139, 189, 214, 0.3)",
+                    }),
+                    stroke: new ol.style.Stroke({
+                        color: '#112211',
+                        width: 1
+                    })
+                })
+        
+        module.value('default_layers', [            
+            new ol.layer.Tile({
+                source: new ol.source.OSM(),
+                title: "Base layer",
+                box_id: 'osm',
+                base: true
+            }),
+            new ol.layer.Vector({
+                title: "Accidents",
+                source: new WfsSource({url:'http://gis.lesprojekt.cz/cgi-bin/mapserv?map=/home/dima/maps/accidents_wfs.map', typename:'accidents', projection:'EPSG:3857'}),
+                style: style
+            })
+        ]);
+
+        module.value('default_view', new ol.View({
+            center: ol.proj.transform([17.474129, 52.574000], 'EPSG:4326', 'EPSG:3857'), //Latitude longitude    to Spherical Mercator
+            zoom: 4,
+            units: "m"
+        }));
+
+        module.controller('Main', ['$scope', 'Core', 'InfoPanelService',
+            function($scope, Core, InfoPanelService) {
+                if (console) console.log("Main called");
+                $scope.hsl_path = hsl_path; //Get this from hslayers.js file
+                $scope.Core = Core;
+
+                $scope.$on('infopanel.updated', function(event) {
+                });
+            }
+        ]);
+
+        return module;
+    });
---
+++ examples/vectorWmsOtn/hslayers.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var hsl_path = '../../';
+//https://github.com/tnajdek/angular-requirejs-seed
+require.config({
+    paths: {
+        angular: hsl_path+'bower_components/angular/angular',
+        ol: hsl_path+'lib/ol3/ol',
+        toolbar: hsl_path+'components/toolbar/toolbar',
+        layermanager: hsl_path+'components/layermanager/layermanager',
+        map: hsl_path+'components/map/map',
+        ows: hsl_path+'components/ows/ows',
+        'ows.wms': hsl_path+'components/ows/ows_wms',
+        'ows.nonwms': hsl_path+'components/ows/ows_nonwms',
+        'ows.wmsprioritized': hsl_path+'components/ows/ows_wmsprioritized',
+        query: hsl_path+'components/query/query',
+        search: hsl_path+'components/search/search',
+        print: hsl_path+'components/print/print',
+        permalink: hsl_path+'components/permalink/permalink',
+        lodexplorer: hsl_path+'components/lodexplorer/lodexplorer',
+        geolocation: hsl_path+'components/geolocation/geolocation',
+        measure: hsl_path+'components/measure/measure',
+        legend: hsl_path+'components/legend/legend',
+        app: 'app',
+        xml2json: hsl_path+'lib/xml2json.min',
+        panoramio: hsl_path+'components/panoramio/panoramio',
+        drag: hsl_path+'components/drag/drag',
+        core: hsl_path+'components/core/core',
+        WfsSource: hsl_path+'extensions/hs.source.Wfs',
+        'angular-sanitize': hsl_path+'bower_components/angular-sanitize/angular-sanitize'
+    },
+    shim: {
+        'angular': {
+            'exports': 'angular'
+        },
+        'angular-sanitize': { 
+            deps: ['angular'],
+        }
+    },
+    priority: [
+        "angular"
+    ]
+});
+
+//http://code.angularjs.org/1.2.1/docs/guide/bootstrap#overview_deferred-bootstrap
+window.name = "NG_DEFER_BOOTSTRAP!";
+
+require([
+    'angular',
+    'ol',
+    'app'
+], function(angular, ol, app) {
+    var $html = angular.element(document.getElementsByTagName('html')[0]);
+    angular.element().ready(function() {
+        angular.resumeBootstrap([app['name']]);
+    });
+    
+});
