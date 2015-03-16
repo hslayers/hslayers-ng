fc1d939bd8514c69e01c6f169d65982881e28770
Jānis <jajanka3@gmail.com>
2015-03-16 02:58:31 +0200

    Crossfilter and chart drawing

--- components/feature_crossfilter/f_crossfilter.js
+++ components/feature_crossfilter/f_crossfilter.js
@@ -10,7 +10,29 @@ define(['angular', 'ol', 'dc', 'map'],
                     }
                 };
             })
-
+        .service('feature_crossfilter', [function(){
+            return {
+                makeCrossfilterDimensions : function(source, attributes){
+                    console.log('asd');
+                    var facts = crossfilter(source.getFeatures());
+                    var tmp = [];
+                    for (var attr_i in attributes) {
+                        var attr = attributes[attr_i];    
+                        var total = facts.dimension( function(feature) { 
+                            console.log(feature.get(attr)+"s");
+                            return feature.get(attr);
+                        });
+                        var groupsByTotal = total.group().reduceCount( function(feature) { 
+                            return feature.get(attr); 
+                        });
+                        tmp.push({dimension: total, grouping: groupsByTotal});
+                    }
+                    
+                    return tmp;
+                    // caur konsoli: var a = angular.element('*[ng-app]').injector().get('hsService');
+                }
+            };
+        }])
         .controller('FeatureCrossfilter', ['$scope', 'OlMap', 'feature_crossfilter',
             function($scope, OlMap, feature_crossfilter) {
                 var map = OlMap.map;
@@ -18,9 +40,20 @@ define(['angular', 'ol', 'dc', 'map'],
                 $scope.ajax_loader = hsl_path + 'components/lodexplorer/ajax-loader.gif';
                 $scope.loading = false;
                 $scope.groupings = [];
+                $scope.groupings.push({name: "Category"});
+                if (!$scope.$$phase) $scope.$digest();
                 
                 setTimeout(function(){
-                    feature_crossfilter.makeCrossfilterDimensions(OlMap.map.getLayers().item(2).getSource(), ["http://gis.zcu.cz/poi#category_os"]);
+                    var dimAndGroups = feature_crossfilter.makeCrossfilterDimensions(OlMap.map.getLayers().item(2).getSource(), ["http://gis.zcu.cz/poi#category_osm"]);
+                    var pies = [];
+                    
+                    var chart = dc.rowChart('#fc_chart'+'Category');
+                                chart.width($("#f_crossfilter_data_panel").width() - 35)
+                                    .height(dimAndGroups[0].grouping.size() * 23 + 40).labelOffsetY(12)
+                                    .dimension(dimAndGroups[0].dimension) // set dimension
+                                    .group(dimAndGroups[0].grouping) // set group
+                                pies.push(chart);
+                    dc.renderAll();
                 }, 4000);
                 
                 $scope.$on('infopanel.updated', function(event) {});
