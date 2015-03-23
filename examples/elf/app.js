f586b3e7913d1732c5cdb214d9a2a60a4a371e66
Raitis <raitisbe@gmail.com>
2015-03-23 17:24:36 +0200

    Wfs layers 2.0.0 version support and basic auth

---
+++ examples/elf/app.js
@@ -0,0 +1,88 @@
+'use strict';
+
+define(['angular', 'ol', 'toolbar', 'layermanager', 'WfsSource', 'core', 'map', 'query', 'search', 'print', 'permalink', 'measure', 'geolocation', 'api', 'angular-gettext', 'translations'],
+
+    function(angular, ol, toolbar, layermanager, WfsSource) {
+        var module = angular.module('hs', [
+            'hs.core',
+            'hs.toolbar',
+            'hs.layermanager',
+            'hs.map',
+            'hs.query',
+            'hs.search', 'hs.print', 'hs.permalink',
+            'hs.geolocation',
+            'hs.api',
+            'gettext'
+        ]);
+
+        module.directive('hs', ['OlMap', 'Core', function(OlMap, Core) {
+            return {
+                templateUrl: hsl_path + 'hslayers.html',
+                link: function(scope, element) {
+                    Core.fullscreenMap(element);
+                }
+            };
+        }]);
+
+        module.value('box_layers', []);
+
+        var style = new ol.style.Style({
+            image: new ol.style.Circle({
+                fill: new ol.style.Fill({
+                    color: [242, 121, 0, 0.7]
+                }),
+                stroke: new ol.style.Stroke({
+                    color: [0xbb, 0x33, 0x33, 0.7]
+                }),
+                radius: 5
+            }),
+            fill: new ol.style.Fill({
+                color: "rgba(139, 189, 214, 0.3)",
+            }),
+            stroke: new ol.style.Stroke({
+                color: '#112211',
+                width: 1
+            })
+        })
+
+        module.value('default_layers', [
+            new ol.layer.Tile({
+                source: new ol.source.OSM(),
+                title: "Base layer",
+                box_id: 'osm',
+                base: true
+            }),
+            new ol.layer.Vector({
+                title: "NUTS points",
+                source: new WfsSource({
+                    url: 'https://54.247.162.180/wss/service/CZ-AD/httpauth',
+                    typename: 'AD:Address',
+                    projection: 'EPSG:3857',
+                    version: '2.0.0',
+                    hsproxy: false,
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader("Authorization", "Basic " + btoa("WRLS" + ":" + "WRLSELFx1")); 
+                    }
+                }),
+                style: style
+            })
+        ]);
+
+        module.value('default_view', new ol.View({
+            center: ol.proj.transform([17.474129, 52.574000], 'EPSG:4326', 'EPSG:3857'), //Latitude longitude    to Spherical Mercator
+            zoom: 4,
+            units: "m"
+        }));
+
+        module.controller('Main', ['$scope', 'Core', 'InfoPanelService',
+            function($scope, Core, InfoPanelService) {
+                if (console) console.log("Main called");
+                $scope.hsl_path = hsl_path; //Get this from hslayers.js file
+                $scope.Core = Core;
+
+                $scope.$on('infopanel.updated', function(event) {});
+            }
+        ]);
+
+        return module;
+    });
--- extensions/hs.source.Wfs.js
+++ extensions/hs.source.Wfs.js
@@ -1,19 +1,23 @@
 define(function(require) {
     var ol = require('ol');
     return function(options) {
+        if(typeof options.version == 'undefined') options.version = '1.0.0';
+        if(typeof options.hsproxy == 'undefined') options.hsproxy = true;
+        if(typeof options.beforeSend == 'undefined') options.beforeSend = function (xhr) {};
         var src = new ol.source.ServerVector({
             format: new ol.format.GeoJSON(),
             loader: function(extent, resolution, projection) {
                 src.clear();
                 var p = options.url + (options.url.indexOf('?') > 0 ? '&' : '?') +
                     'service=WFS&TYPENAME=' + options.typename + '&request=GetFeature&' +
-                    'version=1.0.0&' +
+                    'version='+options.version+'&' +
                     'SRSNAME=' + options.projection + '&outputFormat=geojson&' +
                     'bbox=' + extent.join(',') + ',urn:ogc:def:crs:EPSG:6.3:3857';
-                var url = "/cgi-bin/hsproxy.cgi?toEncoding=utf-8&url=" + window.escape(p);
+                var url = options.hsproxy ? "/cgi-bin/hsproxy.cgi?toEncoding=utf-8&url=" + window.escape(p) : p;
 
                 $.ajax({
-                        url: url
+                        url: url,
+                        beforeSend: options.beforeSend
                     })
                     .done(function(response) {
                         src.addFeatures(src.readFeatures(response));
