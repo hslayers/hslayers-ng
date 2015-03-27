ed3ed41853341575098efaf990864de1dd3c0fd5
Raitis <raitisbe@gmail.com>
2015-03-27 12:14:47 +0200

    gml3 support

--- examples/elf/app.js
+++ examples/elf/app.js
@@ -59,7 +59,7 @@ define(['angular', 'ol', 'toolbar', 'layermanager', 'WfsSource', 'core', 'map', 
                     typename: 'AD:Address',
                     projection: 'EPSG:3857',
                     version: '2.0.0',
-                    format: new ol.format.GML(),
+                    format: new ol.format.GML3(),
                     hsproxy: true,
                     beforeSend: function (xhr) {
                         xhr.setRequestHeader("Authorization", "Basic " + btoa("WRLS" + ":" + "WRLSELFx1")); 
