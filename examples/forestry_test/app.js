9537c0d8d929eb1f15094b2f55c24542b65aa892
Přemysl Vohnout <vohnout@ccss.cz>
2015-09-08 15:46:05 +0200

    forestry_test update

--- examples/forestry_test/app.js
+++ examples/forestry_test/app.js
@@ -3,7 +3,7 @@
 define(['ol', 'dc', 'toolbar', 'layermanager', 'SparqlJsonForestry', 'query', 'search', 'print', 'permalink', 'measure', 'geolocation', 'legend', 'bootstrap', 'bootstrap', 'api'],
 
     function(ol, dc, toolbar, layermanager, SparqlJsonForestry) {
-        proj4.defs('EPSG:5514', '+title=S-JTSK Krovak +proj=krovak +lat_0=49.5 +lon_0=24.83333333333333 +alpha=30.28813975277778 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +units=m +towgs84=570.8,85.7,462.8,4.998,1.587,5.261,3.56 no_defs <>');             
+        proj4.defs('EPSG:5514', '+title=S-JTSK Krovak +proj=krovak +lat_0=49.5 +lon_0=24.83333333333333 +alpha=30.28813975277778 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +units=m +towgs84=570.8,85.7,462.8,4.998,1.587,5.261,3.56 no_defs <>');
         var jtsk = ol.proj.get('EPSG:5514');
         var jtskExtent = [-925000.000000000000, -1444353.535999999800, -400646.464000000040, -920000.000000000000];
         //var jtskExtent = [-905000.000000000000, -1228000.000000000000, -430000.000000000000, -934000.000000000000];
@@ -160,47 +160,50 @@ define(['ol', 'dc', 'toolbar', 'layermanager', 'SparqlJsonForestry', 'query', 's
         module.value('box_layers', [new ol.layer.Group({
             title: '',
             layers: [
-            new ol.layer.Tile({
-                        title: "Forests",
-                        source: new ol.source.TileWMS({
-                            url: 'http://gis.bnhelp.cz/cgi-bin/crtopo',
-                            params: {
-                                LAYERS: 'kraje',
-                                INFO_FORMAT: undefined,
-                                FORMAT: "image/png"
-                            },
-                            crossOrigin: null
-                        }),
+                new ol.layer.Tile({
+                    title: "BaseMap",
+                    source: new ol.source.WMTS({
+                        extent: jtskExtent,
+                        format: "image/jpeg",
+                        layer: "zm",
+                        matrixSet: "jtsk:epsg:5514",
+                        projection: jtsk,
+                        style: 'inspire_common:DEFAULT',
+                        tileGrid: tileGrid,
+                        url: "http://geoportal.cuzk.cz/WMTS_ZM/WMTService.aspx",
+                        version: "1.0.0"
                     }),
-            
-            new ol.layer.Tile({
-                        title: "Ortofoto",
-                        source: new ol.source.WMTS({
-                            extent: jtskExtent,
-                            format: "image/jpeg",
-                            layer: "orto",
-                            matrixSet: "jtsk:epsg:5514",
-                            projection: jtsk,
-                            style: 'inspire_common:DEFAULT',
-                            tileGrid: tileGrid,
-                            url: "http://geoportal.cuzk.cz/WMTS_ORTOFOTO/WMTService.aspx",
-                            version: "1.0.0"
-                        }),
-                        visible: true
-                    }),
-                    createOneDimensionLayer('Forest cover', 'http://nil.uhul.cz/lod/forest_cover/forest_cover.rdf', 'forest_cover'),
-                    createOneDimensionLayer('Average growing stock per hectare', 'http://nil.uhul.cz/lod/average_growing_stock_per_hectare/average_growing_stock_per_hectare.rdf', 'average_growing_stock_per_hectare'),
-                    createOneDimensionLayer('Total forest area', 'http://nil.uhul.cz/lod/total_forest_area/total_forest_area.rdf', 'total_forest_area'),
-                    createOneDimensionLayer('Total growing stock', 'http://nil.uhul.cz/lod/total_growing_stock/total_growing_stock.rdf', 'total_growing_stock')
-                ].concat(specie_lyrs).concat(zakoni_lyrs).concat(create1DimensionLayers(silvicultural, 'Silvicultural system', 'http://nil.uhul.cz/lod/silvicultural_system/silvicultural_system.rdf', 'silvicultural_system'))
-                .concat(create1DimensionLayers({
-                    '"les nízký"@cs': 'Les nízký vzniká systematicky se opakující vegetativní obnovou pařezovými nebo kořenovými výmladky. ',
-                    '"tvar lesa nehodnocen"@cs': 'Tvar lesa nehodnocen.',
-                    '"les vysoký"@cs': 'Vysoký les je charakterizován původem výhradně generativním (síje, sadba, přirozené zmlazení).',
-                    '"les střední"@cs': 'Les střední je kombinací lesa vysokého a nízkého - je tvořen spodní výmladkovou etáží doplněnou etáží z generativně založených jedinců.'
-                }, 'Stand richness', 'http://nil.uhul.cz/lod/stand_richness/stand_richness.rdf', 'stand_richness'))
+                    visible: true
+                }),
 
-                .concat(create1DimensionLayers({
+                new ol.layer.Tile({
+                    title: "Ortofoto",
+                    source: new ol.source.WMTS({
+                        extent: jtskExtent,
+                        format: "image/jpeg",
+                        layer: "orto",
+                        matrixSet: "jtsk:epsg:5514",
+                        projection: jtsk,
+                        style: 'inspire_common:DEFAULT',
+                        tileGrid: tileGrid,
+                        url: "http://geoportal.cuzk.cz/WMTS_ORTOFOTO/WMTService.aspx",
+                        version: "1.0.0"
+                    }),
+                    visible: true
+                }),
+                createOneDimensionLayer('Forest cover', 'http://nil.uhul.cz/lod/forest_cover/forest_cover.rdf', 'forest_cover'),
+                createOneDimensionLayer('Average growing stock per hectare', 'http://nil.uhul.cz/lod/average_growing_stock_per_hectare/average_growing_stock_per_hectare.rdf', 'average_growing_stock_per_hectare'),
+                createOneDimensionLayer('Total forest area', 'http://nil.uhul.cz/lod/total_forest_area/total_forest_area.rdf', 'total_forest_area'),
+                createOneDimensionLayer('Total growing stock', 'http://nil.uhul.cz/lod/total_growing_stock/total_growing_stock.rdf', 'total_growing_stock')
+            ].concat(specie_lyrs).concat(zakoni_lyrs).concat(create1DimensionLayers(silvicultural, 'Silvicultural system', 'http://nil.uhul.cz/lod/silvicultural_system/silvicultural_system.rdf', 'silvicultural_system'))
+            .concat(create1DimensionLayers({
+                '"les nízký"@cs': 'Les nízký vzniká systematicky se opakující vegetativní obnovou pařezovými nebo kořenovými výmladky. ',
+                '"tvar lesa nehodnocen"@cs': 'Tvar lesa nehodnocen.',
+                '"les vysoký"@cs': 'Vysoký les je charakterizován původem výhradně generativním (síje, sadba, přirozené zmlazení).',
+                '"les střední"@cs': 'Les střední je kombinací lesa vysokého a nízkého - je tvořen spodní výmladkovou etáží doplněnou etáží z generativně založených jedinců.'
+            }, 'Stand richness', 'http://nil.uhul.cz/lod/stand_richness/stand_richness.rdf', 'stand_richness'))
+
+            .concat(create1DimensionLayers({
                 '"věkový stupeň 15"@cs': 'Věkový stupeň 15 – věk 141 až 150 let',
                 '"věkový stupeň 1"@cs': 'Věkový stupeň 1 – věk 1 až 10 let',
                 '"věkový stupeň 2"@cs': 'Věkový stupeň 2 – věk 11 až 20 let',
@@ -219,29 +222,30 @@ define(['ol', 'dc', 'toolbar', 'layermanager', 'SparqlJsonForestry', 'query', 's
                 '"věkový stupeň 16"@cs': 'Věkový stupeň 16 – věk 151 až 160 let',
                 '"věkový stupeň 17"@cs': 'Věkový stupeň 17+ – věk 161 a více let'
             }, 'Vegetation tiers', 'http://nil.uhul.cz/lod/vegetation_tiers/vegetation_tiers.rdf', 'vegetation_tiers'))
-        })]);
+        })
+    ]);
 
 
 
 
 
-        module.value('default_layers', []);
+    module.value('default_layers', []);
 
 
-        module.value('default_view', new ol.View({
-            center: ol.proj.transform([15.2, 49.9], 'EPSG:4326', 'EPSG:5514'),
-            zoom: 2,
-            projection: jtsk,
-            units: "m"
-        }));
+    module.value('default_view', new ol.View({
+        center: ol.proj.transform([15.2, 49.9], 'EPSG:4326', 'EPSG:5514'),
+        zoom: 2,
+        projection: jtsk,
+        units: "m"
+    }));
 
-        module.controller('Main', ['$scope', '$filter', 'Core', 'hs.map.service', 'hs.query.service_infopanel',
-            function($scope, $filter, Core, OlMap, InfoPanelService) {
-                if (console) console.log("Main called");
-                $scope.hsl_path = hsl_path; //Get this from hslayers.js file
-                $scope.Core = Core;
-            }
-        ]);
+    module.controller('Main', ['$scope', '$filter', 'Core', 'hs.map.service', 'hs.query.service_infopanel',
+        function($scope, $filter, Core, OlMap, InfoPanelService) {
+            if (console) console.log("Main called");
+            $scope.hsl_path = hsl_path; //Get this from hslayers.js file
+            $scope.Core = Core;
+        }
+    ]);
 
-        return module;
-    });
+    return module;
+});
