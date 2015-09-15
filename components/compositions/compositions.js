65c5d5e99be1808f0d69b26d1e2c2606d859fe66
Přemysl Vohnout <premek@vohnout.cz>
2015-09-15 14:08:20 +0200

    Compositions and KML

--- components/compositions/compositions.js
+++ components/compositions/compositions.js
@@ -90,20 +90,22 @@ define(['angular', 'ol', 'map'],
                                 layers.push(new_layer);
                                 break;
                             case 'OpenLayers.Layer.Vector':
-                                if (lyr.protocol && lyr.protocol.format.className == 'OpenLayers.Format.KML') {
-                                    var url = lyr.protocol.optoions.url;
+                                if(lyr_def.protocol && lyr_def.protocol.format.className == 'OpenLayers.Format.KML'){
+                                    var url = lyr_def.protocol.options.url;
                                     if (typeof use_proxy === 'undefined' || use_proxy === true) {
                                         url = "/cgi-bin/hsproxy.cgi?toEncoding=utf-8&url=" + encodeURIComponent(url);
                                     }
                                     var src = new ol.source.KML({
-                                        projection: ol.proj.get(lyr.projection),
+                                        projection: ol.proj.get(lyr_def.projection),
                                         url: url,
                                         extractStyles: true
                                     })
                                     var lyr = new ol.layer.Vector({
-                                        title: lyr.title,
-                                        source: src
+                                        from_composition: true,
+                                        source: src,
+                                        title: lyr_def.title
                                     });
+                                    layers.push(lyr);
                                 }
                                 break;
                         }
