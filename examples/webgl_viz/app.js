6cbb7d6f07083ec10a13f6fc2b95f653ac4becfe
Jan Jezek <jezekjan@kma.zcu.cz>
2015-10-06 11:32:47 +0200

    map center changed

--- examples/webgl_viz/app.js
+++ examples/webgl_viz/app.js
@@ -71,8 +71,8 @@ define(['ol',
                 })
             ],
             default_view: new ol.View({
-                center: ol.proj.transform([17.474129, 52.574000], 'EPSG:4326', 'EPSG:3857'), //Latitude longitude    to Spherical Mercator
-                zoom: 4,
+                center: ol.proj.transform([-1.9,52.5], 'EPSG:4326', 'EPSG:3857'), //Latitude longitude    to Spherical Mercator
+                zoom: 11,
                 units: "m"
             })
         });
