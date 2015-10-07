34a95ebb0e723679c2248cbf253733dbf81e7deb
Přemysl Vohnout <premek@vohnout.cz>
2015-10-07 13:02:53 +0200

    WebGL Viz code cleanup

--- examples/webgl_viz/DataLoader.js
+++ examples/webgl_viz/DataLoader.js
@@ -1,60 +1,67 @@
 function DataLoader(visualize) {
     var that = this;
 
-	/**
-	 * Load text file
-	 */
-	 $("#speed_chart").text("Please wait... data are being loaded. This may take a while.");
-	
-	this.loadPosData = function(file) {
+    /**
+     * Load text file
+     */
+    $("#speed_chart").text("Please wait... data are being loaded. This may take a while.");
 
-			var pts = [];
-			var dayes = [];
-			var hours = [];
-			var date = [];
-			var sev = [];
-			var road_surf = [];
-		
-			var j = 0;		
-		/**
-		 * load data
-		 */
-		d3.csv(file, function(error, data) {
-		
-		var dateminmax ;
+    this.loadPosData = function(file) {
 
-			data.forEach(function(val, i) {
-							
-					pts[j++] = parseFloat(val.x);
-					pts[j++] = parseFloat(val.y);
-									
-					var d =  (new Date(val.timestamp*1000));																				
-					//index[i] = rasterer.calc(i);	
-					dayes[i] = d.getDay();
-					
-					hours[i] = d.getHours() + d.getMinutes()/60;		
-					date[i] = Math.round(d.getTime()/(1000*60*60));						
-					dateminmax = getMinMax(date[i], dateminmax)
-					
-					sev[i] = val.accident_severity;
-					road_surf[i] = val.road_surface_conditions;
-				
-			});
-			
-			visualize({pts: pts, dayes: dayes, hours :hours, sev : sev, road_surf: road_surf, date:date, dmm :dateminmax , num : data.length});
-			});
-	}
-	
-	
-	function getMinMax(val, minmax){
-		if (typeof(minmax)=='undefined'){
-			minmax = [];
-			minmax.min = Number.MAX_VALUE;
-			minmax.max = Number.MIN_VALUE;
-		}
-		if (val < minmax.min) {minmax.min = val};
-		if (val > minmax.max) {minmax.max = val};
-		return minmax;
-		
-	}
+        var pts = [];
+        var days = [];
+        var hours = [];
+        var date = [];
+        var sev = [];
+        var road_surf = [];
+
+        var j = 0;
+        /**
+         * load data
+         */
+        d3.csv(file, function(error, data) {
+
+            var dateminmax;
+            data.forEach(function(val, i) {
+
+                pts[j++] = parseFloat(val.x);
+                pts[j++] = parseFloat(val.y);
+
+                var d = (new Date(val.timestamp * 1000));
+                //index[i] = rasterer.calc(i);
+                days[i] = d.getDay();
+
+                hours[i] = d.getHours() + d.getMinutes() / 60;
+                date[i] = Math.round(d.getTime()/(1000*60*60));
+                dateminmax = getMinMax(date[i], dateminmax)
+
+                sev[i] = val.accident_severity;
+                road_surf[i] = val.road_surface_conditions;
+
+            });
+
+            visualize({
+                pts: pts,
+                days: days,
+                hours: hours,
+                sev: sev,
+                road_surf: road_surf,
+                date: date,
+                dmm: dateminmax,
+                num: data.length
+            });
+        });
+    }
+
+    function getMinMax(val, minmax){
+        if (typeof(minmax)=='undefined'){
+            minmax = [];
+            minmax.min = Number.MAX_VALUE;
+            minmax.max = Number.MIN_VALUE;
+        }
+        if (val < minmax.min) {minmax.min = val};
+        if (val > minmax.max) {minmax.max = val};
+        return minmax;
+
+    }
 }
