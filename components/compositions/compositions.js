6710c787b910fc95aca916674a92acfa58b03241
Raitis <raitisbe@gmail.com>
2015-10-13 12:27:44 +0300

    Add informations about composition

--- components/compositions/compositions.js
+++ components/compositions/compositions.js
@@ -11,7 +11,8 @@ define(['angular', 'ol', 'map'],
                 return {
                     templateUrl: hsl_path + 'components/compositions/partials/compositions.html',
                     link: function(scope, element) {
-
+                        $('.mid-pane').prepend($('<div></div>').addClass('composition-info'));
+                        $('.mid-pane').css('margin-top', '0px');
                     }
                 };
             })
@@ -49,6 +50,8 @@ define(['angular', 'ol', 'map'],
                                     OlMap.map.removeLayer(to_be_removed.shift());
                                 }
                             }
+                            $('.composition-info').html($('<a href="#">').html($('<h3>').html(response.data.title)).click(function(){$('.composition-abstract').toggle()}));
+                            $('.composition-info').append($('<div>').html(response.data.abstract).addClass('well composition-abstract'));
                             OlMap.map.getView().fitExtent(me.parseExtent(response.extent || response.data.extent), OlMap.map.getSize());
                             var layers = me.jsonToLayers(response);
                             for (var i = 0; i < layers.length; i++) {
