cc80cc0bb999bed7deba56e6e2570a350813dfc9
Přemysl Vohnout <vohnout@ccss.cz>
2016-02-03 17:09:04 +0100

    Move panel with layer details out of layer folder so it will not be accidentally deleted

--- components/layermanager/layermanager.js
+++ components/layermanager/layermanager.js
@@ -217,6 +217,9 @@ define(['angular', 'app', 'map', 'ol', 'utils'], function(angular, app, map, ol)
              * @param {ol.CollectionEvent} e - Events emitted by ol.Collection instances are instances of this type.
              */
             function layerRemoved(e) {
+                if ($scope.currentlayer.layer == e.element) {
+                    $(".layerpanel").insertAfter($('.hs-lm-mapcontentlist'));
+                }
                 $scope.currentlayer = null;
                 for (var i = 0; i < $scope.layers.length; i++) {
                     if ($scope.layers[i].layer == e.element) {
