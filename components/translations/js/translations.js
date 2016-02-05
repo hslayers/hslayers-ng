113df75f04324fcd1f963d714f9cbaf4c80af99e
Raitis <raitisbe@gmail.com>
2016-02-05 16:35:56 +0200

    fix annotations for translatable strings fix generating of translations.js file from *.po files instead of .*pot some latvian translations

--- components/translations/js/translations.js
+++ components/translations/js/translations.js
@@ -1,42 +1,5 @@
-angular.module('gettext').run(['gettextCatalog', function(gettextCatalog) {
-    /* jshint -W100 */
-    gettextCatalog.setStrings('lv_LV', {
-        "Abstract:": "Abstrakts",
-        "Add": "Pievienot",
-        "Add all": "Pievienot visu",
-        "Add selected": "Pievienot izvēlēto",
-        "Altitude": "Augstums",
-        "Catalogue": "Katalogs",
-        "Clear": "Notīrīt",
-        "Close": "Aizvērt",
-        "Connect": "Savienot",
-        "Dataset:": "Datu kopa:",
-        "Datasources": "Datu avoti",
-        "Eurostat explorer": "Eurostat pētnieks",
-        "Filter by values": "Atlasīt pēc vērtībām",
-        "Folder name": "Mapes nosaukums",
-        "Histogram": "Histogramma",
-        "Image format": "Attēla formāts",
-        "Layer manager": "Slāņu menedžeris",
-        "Legend": "Apzīmējums",
-        "Legend:": "Apzīmējums:",
-        "Measure": "Mērīt",
-        "Not valid url!": "Nav derīgs url!",
-        "Opacity:": "Necaurredzamība:",
-        "Print": "Printēt",
-        "Query format": "Vaicājums formāts",
-        "Remove layer": "Noņemt slāni",
-        "Required!": "Nepieciešams",
-        "Sparql query log": "SPARQL vaicājums žurnāls",
-        "Speed": "Ātrums",
-        "Tile size": "Lauka izmērs",
-        "Title": "Lauks",
-        "Titles": "Lauki",
-        "Type:": "Tips:",
-        "Use tiles": "Izmantot lauku",
-        "Zoom to": "Pietuvināt",
-        "multiple shapes": "vairākas formas"
-    });
-    //gettextCatalog.setCurrentLanguage('lv_LV');
-    /* jshint +W100 */
+angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
+/* jshint -W100 */
+    gettextCatalog.setStrings('lv_LV', {"Abstract":"Apraksts","Abstract:":"Apraksts:","Add":"Pievienot","Add external data":"Pievienot ārējus datu avotus","Add to map":"Pievienot kartei","Baselayers":"Pamatslāņi","Cancel":"Atcelt","Clear":"Notīrīt","Close":"Aizvērt","Color":"Krāsa","Compositions":"Kompozīcijas","Data":"Dati","Datasources":"Datu avoti","Filter:":"Filtrs:","Icon":"Ikona","Info":"Informācija","Keywords":"Atslēgas vārdi","Keywords:":"Atslēgas vārdi:","Layer":"Slānis","Layer manager":"Slāņu pārvaldība","Layers":"Slāņi","Layers:":"Slāņi:","Legend":"Leģenda","Legend:":"Leģenda:"});
+/* jshint +W100 */
 }]);
