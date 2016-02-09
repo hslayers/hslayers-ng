0cb7ef607436260fa77f096f494e1a91f24d6648
Raitis <raitisbe@gmail.com>
2016-02-09 18:07:37 +0200

    simplified infopanel template for spoi example

--- components/translations/js/translations.js
+++ components/translations/js/translations.js
@@ -1,5 +1,30 @@
-angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
-/* jshint -W100 */
-    gettextCatalog.setStrings('lv_LV', {"Abstract":"Apraksts","Abstract:":"Apraksts:","Add":"Pievienot","Add external data":"Pievienot ārējus datu avotus","Add to map":"Pievienot kartei","Baselayers":"Pamatslāņi","Cancel":"Atcelt","Clear":"Notīrīt","Close":"Aizvērt","Color":"Krāsa","Compositions":"Kompozīcijas","Data":"Dati","Datasources":"Datu avoti","Filter:":"Filtrs:","Icon":"Ikona","Info":"Informācija","Keywords":"Atslēgas vārdi","Keywords:":"Atslēgas vārdi:","Layer":"Slānis","Layer manager":"Slāņu pārvaldība","Layers":"Slāņi","Layers:":"Slāņi:","Legend":"Leģenda","Legend:":"Leģenda:"});
-/* jshint +W100 */
+angular.module('gettext').run(['gettextCatalog', function(gettextCatalog) {
+    /* jshint -W100 */
+    gettextCatalog.setStrings('lv_LV', {
+        "Abstract": "Apraksts",
+        "Abstract:": "Apraksts:",
+        "Add": "Pievienot",
+        "Add external data": "Pievienot ārējus datu avotus",
+        "Add to map": "Pievienot kartei",
+        "Baselayers": "Pamatslāņi",
+        "Cancel": "Atcelt",
+        "Clear": "Notīrīt",
+        "Close": "Aizvērt",
+        "Color": "Krāsa",
+        "Compositions": "Kompozīcijas",
+        "Data": "Dati",
+        "Datasources": "Datu avoti",
+        "Filter:": "Filtrs:",
+        "Icon": "Ikona",
+        "Info": "Informācija",
+        "Keywords": "Atslēgas vārdi",
+        "Keywords:": "Atslēgas vārdi:",
+        "Layer": "Slānis",
+        "Layer manager": "Slāņu pārvaldība",
+        "Layers": "Slāņi",
+        "Layers:": "Slāņi:",
+        "Legend": "Leģenda",
+        "Legend:": "Leģenda:"
+    });
+    /* jshint +W100 */
 }]);
