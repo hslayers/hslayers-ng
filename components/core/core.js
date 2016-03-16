81642f972702d85d46f55c8f3e831cfc6a1fd3f5
Raitis <raitisbe@gmail.com>
2016-03-16 12:07:45 +0200

    extra point

--- components/core/core.js
+++ components/core/core.js
@@ -35,7 +35,7 @@ require.config({
         SparqlJson: requirejs.s.contexts._.config.paths.SparqlJson || hsl_path + 'components/layers/hs.source.SparqlJson',
         WFSCapabilities: requirejs.s.contexts._.config.paths.WFSCapabilities || hsl_path + 'components/format/hs.format.WFSCapabilities',
         WfsSource: requirejs.s.contexts._.config.paths.WfsSource || hsl_path + 'components/layers/hs.source.Wfs',
-        'ows.wfs': requirejs.s.contexts._.config.paths.['ows.wfs']|| hsl_path + 'components/ows/ows_wfs',
+        'ows.wfs': requirejs.s.contexts._.config.paths['ows.wfs']|| hsl_path + 'components/ows/ows_wfs',
     },
     shim: {
         'angular': {
