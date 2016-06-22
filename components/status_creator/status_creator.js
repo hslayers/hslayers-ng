e8db2c419bdc2efd2e2693c6d7fea07c1042559e
Přemysl Vohnout <vohnout@ccss.cz>
2016-06-22 15:39:21 +0200

    Generate thumbnail

--- components/status_creator/status_creator.js
+++ components/status_creator/status_creator.js
@@ -586,6 +586,29 @@ define(['angular', 'ol', 'map', 'ngcookies'],
                     }, 0);
                 };
 
+                $scope.generateThumbnail = function() {
+                    if (Core.mainpanel == 'status_creator') {
+                        $('#hs-stc-thumbnail').attr("crossOrigin", "Anonymous");
+                        OlMap.map.once('postcompose', function(event) {
+                            var myCanvas = document.getElementById('my_canvas_id');
+                            var canvas = event.context.canvas;
+                            var canvas2 = document.createElement("canvas");
+                            var width = 256,
+                                height = 256;
+                            canvas2.style.width = width + "px";
+                            canvas2.style.height = height + "px";
+                            canvas2.width = width;
+                            canvas2.height = height;
+                            var ctx2 = canvas2.getContext("2d");
+                            ctx2.drawImage(canvas, canvas.width / 2 - height / 2, canvas.height / 2 - width / 2, width, height, 0, 0, width, height);
+                            $('#hs-stc-thumbnail').attr('src', canvas2.toDataURL('image/png'));
+                            this.thumbnail = canvas2.toDataURL('image/jpeg', 0.8);
+                            $('#hs-stc-thumbnail').width(width).height(height);
+                        }, $scope);
+                        OlMap.map.renderSync();
+                    }
+                }
+
                 $scope.$on('compositions.composition_loaded', function(event, data) {
                     if (angular.isUndefined(data.error)) {
                         if (data.data) {
@@ -620,54 +643,19 @@ define(['angular', 'ol', 'map', 'ngcookies'],
                         $('#stc-download').hide();
                         $('#stc-save, #stc-saveas').hide();
                         $('.stc-tabs li:eq(0) a').tab('show');
-                        $('#hs-stc-thumbnail').attr("crossOrigin", "Anonymous");
-                        OlMap.map.once('postcompose', function(event) {
-                            console.log('thumbnail');
-                            var myCanvas = document.getElementById('my_canvas_id');
-                            var canvas = event.context.canvas;
-                            var canvas2 = document.createElement("canvas");
-                            var width = 256,
-                                height = 256;
-                            canvas2.style.width = width + "px";
-                            canvas2.style.height = height + "px";
-                            canvas2.width = width;
-                            canvas2.height = height;
-                            var ctx2 = canvas2.getContext("2d");
-                            ctx2.drawImage(canvas, canvas.width / 2 - height / 2, canvas.height / 2 - width / 2, width, height, 0, 0, width, height);
-                            $('#hs-stc-thumbnail').attr('src', canvas2.toDataURL('image/png'));
-                            this.thumbnail = canvas2.toDataURL('image/jpeg', 0.8);
-                            $('#hs-stc-thumbnail').width(width).height(height);
-                        }, $scope);
-                        OlMap.map.renderSync();
+                        $scope.generateThumbnail();
                     }
+                });
+
+
+                $scope.$on('map.extent_changed', function(event, data, b) {
+                    $scope.getCurrentExtent();
+                    $scope.generateThumbnail();
+
                 });
 
                 $scope.getCurrentExtent();
 
-                $scope.$on('map.extent_changed', function(event, data, b) {
-                    $scope.getCurrentExtent();
-                    if (Core.mainpanel == 'status_creator') {
-                        $('#hs-stc-thumbnail').attr("crossOrigin", "Anonymous");
-                        OlMap.map.once('postcompose', function(event) {
-                            console.log('thumbnail');
-                            var myCanvas = document.getElementById('my_canvas_id');
-                            var canvas = event.context.canvas;
-                            var canvas2 = document.createElement("canvas");
-                            var width = 256,
-                                height = 256;
-                            canvas2.style.width = width + "px";
-                            canvas2.style.height = height + "px";
-                            canvas2.width = width;
-                            canvas2.height = height;
-                            var ctx2 = canvas2.getContext("2d");
-                            ctx2.drawImage(canvas, canvas.width / 2 - height / 2, canvas.height / 2 - width / 2, width, height, 0, 0, width, height);
-                            $('#hs-stc-thumbnail').attr('src', canvas2.toDataURL('image/png'));
-                            this.thumbnail = canvas2.toDataURL('image/jpeg', 0.8);
-                            $('#hs-stc-thumbnail').width(width).height(height);
-                        }, $scope);
-                        OlMap.map.renderSync();
-                    }
-                });
                 $scope.$emit('scope_loaded', "StatusCreator");
             }
         ]);
