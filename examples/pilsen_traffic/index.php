a49996ef1f392e1a891f2728742e836ed82ffca4
Raitis Berzins <raitisbe@gmail.com>
2017-05-24 16:24:20 +0300

    Pilsen traffic. Use the date, month, hour parameters from the query string to generate metadata tags for social share in php

--- examples/pilsen_traffic/index.php
+++ examples/pilsen_traffic/index.php
@@ -4,6 +4,13 @@
         <meta charset="utf-8">
         <title>Intenzita dopravy v Plzni</title>
         
+        <?php
+            $date = sprintf('%02d', $_GET['date']);
+            $month = sprintf('%02d', (int)($_GET['month']) + 1);
+            $year = $_GET['year'];
+            $hour = sprintf('%02d', $_GET['hour']);
+        ?>
+        
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
@@ -12,16 +19,18 @@
         <link rel="stylesheet" type="text/css" href="pilsen.css">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.4/jspdf.debug.js"></script>
         
-        <meta name="description" content="Předpokládaná dopravní situace v Plzni dne xx.xx.2017 v 10:00." />
+        <?php if (isset($_GET['year'])) { ?>
+        <meta name="description" content="Předpokládaná dopravní situace v Plzni dne <?php echo "$date.$month.$year v $hour"; ?>:00." />
         <!-- Twitter Card data -->
-        <meta name="twitter:card" value="Předpokládaná dopravní situace v Plzni dne xx.xx.2017 v 10:00.">
+        <meta name="twitter:card" value="Předpokládaná dopravní situace v Plzni dne <?php echo "$date.$month.$year v $hour"; ?>:00.">
 
         <!-- Open Graph data -->
         <meta property="og:title" content="Intenzita dopravy v Plzni" />
         <meta property="og:type" content="map" />
         <meta property="og:url" content="<?php echo "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"; ?>" />
         <meta property="og:image" content="<?php echo "http://$_SERVER[HTTP_HOST]" . explode('?', $_SERVER["REQUEST_URI"])[0] . "social-thumbnail.png"; ?>" />
-        <meta property="og:description" content="Předpokládaná dopravní situace v Plzni dne xx.xx.2017 v 10:00." />
+        <meta property="og:description" content="Předpokládaná dopravní situace v Plzni dne <?php echo "$date.$month.$year v $hour"; ?>:00." />
+        <?php } ?>
     </head>
     <body >
         <div hs ng-app="hs" ng-controller="Main" style="position: relative;"></div>
