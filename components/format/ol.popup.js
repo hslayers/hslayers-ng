8096b3c25b83df391f3ae246ec65368f1cbf6276
Jan Shanel <ChcJohnie@gmail.com>
2017-06-12 17:26:19 +0200

    Popup ol polyfill for IE

--- components/format/ol.popup.js
+++ components/format/ol.popup.js
@@ -9,6 +9,32 @@ define(function(require) {
      * @param {Object} opt_options Overlay options
      */
     ol.Overlay.Popup = function(opt_options) {
+        
+        //Object.assign polyfill
+        if (typeof Object.assign != 'function') {
+          Object.assign = function(target, varArgs) { // .length of function is 2
+            'use strict';
+            if (target == null) { // TypeError if undefined or null
+              throw new TypeError('Cannot convert undefined or null to object');
+            }
+
+            var to = Object(target);
+
+            for (var index = 1; index < arguments.length; index++) {
+              var nextSource = arguments[index];
+
+              if (nextSource != null) { // Skip over if undefined or null
+                for (var nextKey in nextSource) {
+                  // Avoid bugs when hasOwnProperty is shadowed
+                  if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                    to[nextKey] = nextSource[nextKey];
+                  }
+                }
+              }
+            }
+            return to;
+          };
+        }
 
         var options = opt_options || {};
 
