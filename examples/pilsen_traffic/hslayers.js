07c702e2a4d78de862fcdabacdc8c4b3e5fcf367
Jan Shanel <ChcJohnie@gmail.com>
2017-06-26 11:38:09 +0200

    Pilsen traffic - hslayers.js cleaning

--- examples/pilsen_traffic/hslayers.js
+++ examples/pilsen_traffic/hslayers.js
@@ -15,23 +15,8 @@ var gitsha = Math.random(); $.ajax({
 require.config({
     urlArgs: 'bust=' + gitsha,
     paths: {
-        toolbar: hsl_path + 'components/toolbar/toolbar',
-        layermanager: hsl_path + 'components/layermanager/layermanager',
-        ows: hsl_path + 'components/ows/ows',
-        'ows.wms': hsl_path + 'components/ows/ows_wms',
-        'ows.wfs': hsl_path + 'components/ows/ows_wfs',
-        'ows.nonwms': hsl_path + 'components/ows/ows_nonwms',
-        'ows.wmsprioritized': hsl_path + 'components/ows/ows_wmsprioritized',
-        query: hsl_path + 'components/query/query',
-        search: hsl_path + 'components/search/search',
-        print: hsl_path + 'components/print/print',
-        permalink: hsl_path + 'components/permalink/permalink',
-        geolocation: hsl_path + 'components/geolocation/geolocation',
-        measure: hsl_path + 'components/measure/measure',
-        legend: hsl_path + 'components/legend/legend',
         app: 'app',
         core: hsl_path + 'components/core/core',
-        api: hsl_path + 'components/api/api',
         pilsentraffic: './pilsentraffic',
         ol: hsl_path + 'node_modules/openlayers/dist/ol-debug',
         calendar: './calendar',
@@ -39,7 +24,6 @@ require.config({
         ngtimeline: hsl_path + 'bower_components/angular-timelinejs3/dist/js/ng-timeline',
         timeline: hsl_path + 'bower_components/TimelineJS3/compiled/js/timeline',
         translations: './translations'
-        //translations: hsl_path + 'components/translations/js/translations'
     },
     shim: {
         d3: {
