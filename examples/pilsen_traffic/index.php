660160b5f3d69b21456ceb22e0c55f2d1dd7431e
Raitis Berzins <raitisbe@gmail.com>
2017-06-27 14:47:49 +0300

    Pilsen traffic. Some other google analythics snippet

--- examples/pilsen_traffic/index.php
+++ examples/pilsen_traffic/index.php
@@ -21,7 +21,8 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.4/jspdf.min.js"></script>
         <script src="https://use.fontawesome.com/700d55ca16.js"></script>
 
-         <!-- Google Tag Manager -->
+        
+<!-- Google Tag Manager -->
         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
@@ -29,6 +30,17 @@
         })(window,document,'script','dataLayer','GTM-MGG4VKH');</script>
         <!-- End Google Tag Manager -->
         
+        <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-101750709-1', 'auto');
+        ga('send', 'pageview');
+
+        </script>
+
         <?php if (isset($_GET['year'])) { ?>
         <meta name="description" content="Předpokládaná dopravní situace v Plzni dne <?php echo "$date.$month.$year v $hour"; ?>:00." />
         <!-- Twitter Card data -->
