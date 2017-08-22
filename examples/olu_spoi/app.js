1a7355af3c0b3950020ce6952b05bf8210709149
Raitis Berzins <raitisbe@gmail.com>
2017-08-22 17:43:05 +0300

    Paint the olu polys according to count of pois inside them

--- examples/olu_spoi/app.js
+++ examples/olu_spoi/app.js
@@ -8,10 +8,9 @@ define(['angular', 'ol', 'sidebar', 'toolbar', 'layermanager', 'SparqlJson', 'ma
             'hs.layermanager',
             'hs.map',
             'hs.query',
-            'hs.search', 'hs.print', 'hs.permalink', 'hs.measure',
-            'hs.legend', 'hs.geolocation', 'hs.core',
+            'hs.search', 'hs.permalink', 'hs.measure',
+            'hs.geolocation', 'hs.core',
             'hs.api',
-            'hs.ows',
             'gettext',
             'hs.sidebar'
         ]);
@@ -66,6 +65,56 @@ define(['angular', 'ol', 'sidebar', 'toolbar', 'layermanager', 'SparqlJson', 'ma
                 return [];
             }
         }
+        
+        
+        function rainbow(numOfSteps, step, opacity) {
+            // based on http://stackoverflow.com/a/7419630
+            // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distiguishable vibrant markers in Google Maps and other apps.
+            // Adam Cole, 2011-Sept-14
+            // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+            var r, g, b;
+            var h = step / (numOfSteps * 1.00000001);
+            var i = ~~(h * 4);
+            var f = h * 4 - i;
+            var q = 1 - f;
+            switch (i % 4) {
+                case 2:
+                    r = f, g = 1, b = 0;
+                    break;
+                case 0:
+                    r = 0, g = f, b = 1;
+                    break;
+                case 3:
+                    r = 1, g = q, b = 0;
+                    break;
+                case 1:
+                    r = 0, g = 1, b = q;
+                    break;
+            }
+            var c = "rgba(" + ~~(r * 235) + "," + ~~(g * 235) + "," + ~~(b * 235) + ", " + opacity + ")";
+            return (c);
+        }
+        
+        var stroke = new ol.style.Stroke({
+            color: '#3399CC',
+            width: 1.25
+        });
+        var olu_style = function(feature, resolution) {
+            var fill = new ol.style.Fill({
+                color: rainbow(30, feature.get('poi_count'), 0.7)
+            });
+            return [
+                new ol.style.Style({
+                    image: new ol.style.Circle({
+                    fill: fill,
+                    stroke: stroke,
+                    radius: 5
+                    }),
+                    fill: fill,
+                    stroke: stroke
+                })
+            ];
+        }
        
         var mercatorProjection = ol.proj.get('EPSG:900913');
 
@@ -105,6 +154,7 @@ define(['angular', 'ol', 'sidebar', 'toolbar', 'layermanager', 'SparqlJson', 'ma
                      var new_lyr = new ol.layer.Vector({
                         title: "Land use parcels",
                         source: spoi_source,
+                        style: olu_style,
                         visible: true,
                         maxResolution: 2.48657133911758
                     });
@@ -121,7 +171,7 @@ define(['angular', 'ol', 'sidebar', 'toolbar', 'layermanager', 'SparqlJson', 'ma
                     var bbox = map.getView().calculateExtent(map.getSize());
                     var ext = ol.proj.transformExtent(bbox, 'EPSG:3857', 'EPSG:4326')
                     var extents = ext[0] + ' ' + ext[1] + ', ' +ext[2] + ' ' + ext[3];
-                    var q = 'https://www.foodie-cloud.org/sparql?default-graph-uri=&query=' + encodeURIComponent('PREFIX geo: <http://www.opengis.net/ont/geosparql#> PREFIX geof: <http://www.opengis.net/def/function/geosparql/> PREFIX virtrdf:	<http://www.openlinksw.com/schemas/virtrdf#> PREFIX poi: <http://www.openvoc.eu/poi#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?o ?use ?wkt FROM <http://w3id.org/foodie/olu#> WHERE {?o geo:hasGeometry ?geometry. ?geometry geo:asWKT ?wkt. FILTER(bif:st_intersects(bif:st_geomfromtext("BOX(' + extents + ')"), ?wkt)). ?o <http://w3id.org/foodie/olu#specificLandUse> ?use.} ORDER BY ?o') + '&should-sponge=&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on';
+                    var q = 'https://www.foodie-cloud.org/sparql?default-graph-uri=&query=' + encodeURIComponent('PREFIX geo: <http://www.opengis.net/ont/geosparql#> PREFIX geof: <http://www.opengis.net/def/function/geosparql/> PREFIX virtrdf: <http://www.openlinksw.com/schemas/virtrdf#> PREFIX poi: <http://www.openvoc.eu/poi#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?o ?use ?wkt (COUNT(*) as ?poi_count) FROM <http://www.sdi4apps.eu/poi.rdf> WHERE { ?Resource poi:class ?POI_Class . ?Resource geo:asWKT ?Coordinates . FILTER(bif:st_intersects (?Coordinates, ?wkt)). { SELECT ?o ?wkt ?use FROM <http://w3id.org/foodie/olu#> WHERE { ?o geo:hasGeometry ?geometry. ?geometry geo:asWKT ?wkt. FILTER(bif:st_intersects(bif:st_geomfromtext("BOX(' + extents + ')"), ?wkt)). ?o <http://w3id.org/foodie/olu#specificLandUse> ?use. } } }') + '&should-sponge=&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on';
                     
                     spoi_source.set('loaded', false);
                     $.ajax({
@@ -131,11 +181,13 @@ define(['angular', 'ol', 'sidebar', 'toolbar', 'layermanager', 'SparqlJson', 'ma
                             if(angular.isUndefined(response.results)) return;
                             var features = [];
                             for (var i = 0; i < response.results.bindings.length; i++) {
-                                var b = response.results.bindings[i];
-                                var g_feature = format.readFeature(b.wkt.value.toUpperCase());
-                                var geom_transformed = g_feature.getGeometry().transform('EPSG:4326', hsMap.map.getView().getProjection());
-                                var feature = new ol.Feature({geometry: geom_transformed, parcel: b.o.value, use: b.use.value});
-                                features.push(feature);
+                                try {
+                                    var b = response.results.bindings[i];
+                                    var g_feature = format.readFeature(b.wkt.value.toUpperCase());
+                                    var geom_transformed = g_feature.getGeometry().transform('EPSG:4326', hsMap.map.getView().getProjection());
+                                    var feature = new ol.Feature({geometry: geom_transformed, parcel: b.o.value, use: b.use.value, poi_count: b.poi_count.value});
+                                    features.push(feature);
+                                } catch(ex){}
                             }
                         spoi_source.clear();
                         spoi_source.addFeatures(features);
@@ -147,6 +199,9 @@ define(['angular', 'ol', 'sidebar', 'toolbar', 'layermanager', 'SparqlJson', 'ma
                 $scope.$on('infopanel.updated', function(event) {
                     if (console) console.log('Attributes', InfoPanelService.attributes, 'Groups', InfoPanelService.groups);
                 });
+                
+                Core.panelEnabled('compositions', false);
+                Core.panelEnabled('status_creator', false);
             }
         ]);
 
