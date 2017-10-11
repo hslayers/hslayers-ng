b19ecea265e572508aabb1d945285438ef4aeb83
Jan Sháněl <ChcJohnie@gmail.com>
2017-10-11 16:04:16 +0200

    Material liberec Measure part1

---
+++ examples/liberecMaterial/materialComponents/panelContents/matMeasure.js
@@ -0,0 +1,53 @@
+/**
+ * @namespace hs.material.search
+ * @memberOf hs
+ */
+define(['angular', 'ol','ngMaterial'],
+
+    function(angular, ol, ngMaterial) {
+        angular.module('hs.material.measure', ['ngMaterial'])
+            
+            .directive('hs.material.measure.directive', function() {
+                return {
+                    templateUrl: hsl_path + 'examples/liberecMaterial/materialComponents/panelContents/matMeasure.html?bust=' + gitsha,
+                    link: function(scope, element) {
+
+                    }
+                };
+            })
+            .controller('hs.material.measure.controller', ['$scope', 'hs.map.service', 'Core', 'hs.measure.service', 
+                function($scope, OlMap, Core, Measure) {
+                    $scope.data = Measure.data;
+
+                    $scope.type = 'distance';
+
+                    $scope.$watch('type', function() {
+                        if (Core.mainpanel == "measure")
+                        Measure.changeMeasureParams($scope.type);
+                    });
+
+                    $scope.clearAll = function() {
+                        Measure.clearMeasurement();
+                        if (!$scope.$$phase) $scope.$digest();
+                    }
+
+                    $scope.clearOne = function(index) {
+                        $scope.data.measurements.splice(index,1);
+                    }
+
+                    $scope.saveToLayer = function() {
+                        //
+                    }
+
+                    $scope.$on('core.mainpanel_changed', function(event) {
+                        if (Core.mainpanel == 'measure') {
+                            Measure.activateMeasuring($scope.type);
+                        } else {
+                            Measure.deactivateMeasuring();
+                        }
+                    });
+
+                    $scope.$emit('scope_loaded', "MaterialMeasure");
+                }
+            ]);
+    })
