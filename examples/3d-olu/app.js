1e89c6b21835c513a8af8dc76d9730e8d1eb2d12
Raitis Berzins <raitisbe@gmail.com>
2017-10-25 13:54:29 +0300

    Labels on villages and peaks

--- examples/3d-olu/app.js
+++ examples/3d-olu/app.js
@@ -248,7 +248,7 @@ define(['ol', 'toolbar', 'layermanager', 'geojson', 'pois', 'sidebar', 'query', 
 
                 function extentChanged() {
                     var bbox = map.getView().calculateExtent(map.getSize());
-                    pois.getPois(map, utils, [[bbox[0], bbox[1]], [bbox[2], bbox[1]], [bbox[2], bbox[3]], [bbox[0], bbox[3]]]);
+                    //pois.getPois(map, utils, [[bbox[0], bbox[1]], [bbox[2], bbox[1]], [bbox[2], bbox[3]], [bbox[0], bbox[3]]]);
                 }
 
                 $rootScope.$on('map.sync_center', function (e, center, bounds) {
