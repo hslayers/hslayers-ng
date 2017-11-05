29c8bfb7b747984a181cbb6108ecb17a9361e954
Raitis Berzins <raitisbe@gmail.com>
2017-11-05 02:40:48 +0200

    minor displacement of character

--- examples/rogainonline/character.js
+++ examples/rogainonline/character.js
@@ -159,7 +159,8 @@ define(['cesium'],
                     },
                     billboard: {
                         image: 'runner.png',
-                        pixelOffset: new Cesium.Cartesian2(0, -6), scaleByDistance: new Cesium.NearFarScalar(100, 0.01, 500, 0.1),
+                        pixelOffset: new Cesium.Cartesian2(0, -16), 
+                        scaleByDistance: new Cesium.NearFarScalar(100, 0.01, 500, 0.1),
                     },
                     position: position_property,
                     orientation: orientation_property
