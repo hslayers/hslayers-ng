d83b6578ce66a6b489f940c612e6b3da9a591723
Raitis Berzins <raitisbe@gmail.com>
2017-11-28 11:26:40 +0200

    Rogaining. Redirect to https if needed

--- examples/rogainonline/index.php
+++ examples/rogainonline/index.php
@@ -1,3 +1,11 @@
+<?php
+if(strpos($_SERVER['HTTP_HOST'], 'ng.hslayers') !== false && (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off")){
+  $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+  header('HTTP/1.1 301 Moved Permanently');
+  header('Location: ' . $redirect);
+  exit();
+}
+?>
 <!DOCTYPE html>
 <html>
 
