1fc9fa6371c2100ebe1a7981d6fff9166f6e3957
SLeitgeb <leitgeb.simon@gmail.com>
2017-12-05 17:28:15 +0100

    Partial layermanager implementation in MD.

--- components/layermanager/layermanager.js
+++ components/layermanager/layermanager.js
@@ -6,13 +6,22 @@
  */
 define(['angular', 'app', 'map', 'ol', 'utils', 'ows.wms', 'dragdroplists', 'status_creator'], function (angular, app, map, ol) {
     angular.module('hs.layermanager', ['hs.map', 'hs.utils', 'hs.ows.wms', 'dndLists', 'hs.status_creator'])
+            
+        .directive('hs.layermanager.directive', function() {
+            return {
+                templateUrl: hsl_path + 'components/layermanager/partials/layermanager.html?bust=' + gitsha,
+                link: function(scope, element) {
+
+                }
+            };
+        })
         /**
          * @module hs.layermanager
          * @name hs.layermanager.directive
          * @ngdoc directive
          * @description Display default HSLayers layer manager panel in application. Contain filter, baselayers, overlay container and settings pane for active layer.
          */
-        .directive('hs.layermanager.directive', function () {
+        .directive('hs.layermanager.directive.deprecated', function () {
             return {
                 templateUrl: hsl_path + 'components/layermanager/partials/layermanager.html?bust=' + gitsha
             };
@@ -24,7 +33,7 @@ define(['angular', 'app', 'map', 'ol', 'utils', 'ows.wms', 'dragdroplists', 'sta
          * @description Directive for displaying list of layers in default HSLayers manager template. Every directive instance contain one folder of folder stucture. For every layer displays current information notes and on click opens layer options panel. Every directive instance is automatically refresh when layermanager.updated fires.
          * Directive has access to contollers data object.
          */
-        .directive('hs.layermanager.layerlistDirective', ['$compile', function ($compile) {
+        .directive('hs.layermanager.layerlistDirective.deprecated', ['$compile', function ($compile) {
             return {
                 templateUrl: hsl_path + 'components/layermanager/partials/layerlist.html?bust=' + gitsha,
                 compile: function compile(element) {
@@ -146,7 +155,7 @@ define(['angular', 'app', 'map', 'ol', 'utils', 'ows.wms', 'dragdroplists', 'sta
             * $compile(el)($scope);
             * ```
          */
-        .directive('hs.layermanager.removeAllDialogDirective', function () {
+        .directive('hs.layermanager.removeAllDialogDirective.deprecated', function () {
             return {
                 templateUrl: hsl_path + 'components/layermanager/partials/dialog_removeall.html?bust=' + gitsha,
                 link: function (scope, element, attrs) {
@@ -160,7 +169,7 @@ define(['angular', 'app', 'map', 'ol', 'utils', 'ows.wms', 'dragdroplists', 'sta
          * @ngdoc directive
          * @description Directive for displaying folder structure in default HS layers template. Used recursively to build full folder structure if it is created in layer manager. Single instance shows layers and subfolders of its position in folder structure.
          */
-        .directive('hs.layermanager.folderDirective', ['$compile', function ($compile) {
+        .directive('hs.layermanager.folderDirective.deprecated', ['$compile', function ($compile) {
             return {
                 templateUrl: hsl_path + 'components/layermanager/partials/folder.html?bust=' + gitsha,
                 compile: function compile(element) {
@@ -1088,7 +1097,241 @@ define(['angular', 'app', 'map', 'ol', 'utils', 'ows.wms', 'dragdroplists', 'sta
      * @ngdoc controller
      * @description Controller for management of deafult HSLayers layer manager template
      */
-    .controller('hs.layermanager.controller', ['$scope', 'hs.map.service', 'config', '$rootScope', 'Core', '$compile', 'hs.utils.service', 'hs.styler.service', 'hs.ows.wms.service_capabilities', 'hs.status_creator.service', 'hs.layermanager.service', 'hs.layermanager.WMSTservice', 'hs.utils.layerUtilsService',
+
+    .controller('hs.layermanager.controller', ['$scope', 'config', 'hs.map.service', 'hs.layermanager.service', '$rootScope', '$mdDialog',
+        function($scope, config, OlMap, LayMan, $rootScope, $mdDialog) {
+
+            $scope.data = LayMan.data;
+            $scope.shiftDown = false;
+
+            $scope.expandLayer = function(layer){
+                if (angular.isUndefined(layer.expanded)) layer.expanded = true;
+                else layer.expanded = !layer.expanded;
+            }
+
+            $scope.expandSettings = function(layer,value){
+                if (angular.isUndefined(layer.opacity)) {
+                    layer.opacity = layer.layer.getOpacity();
+                    layer.maxResolutionLimit = layer.layer.getMaxResolution();
+                    layer.minResolutionLimit = layer.layer.getMinResolution();
+                    layer.maxResolution = layer.maxResolutionLimit;
+                    layer.minResolution = layer.minResolutionLimit;
+                }
+                if (angular.isUndefined(layer.style) && layer.layer.getSource().styleAble) getLayerStyle(layer);
+                layer.expandSettings = value;
+            }
+
+            $scope.setOpacity = function(layer) {
+                layer.layer.setOpacity(layer.opacity);
+                $scope.$emit('compositions.composition_edited');
+            }
+
+            $scope.updateResolution = function(layer) {
+                layer.layer.setMaxResolution(layer.maxResolution);
+                layer.layer.setMinResolution(layer.minResolution);
+            }
+
+            $scope.expandInfo = function(layer,value){
+                layer.expandInfo = value;
+            }
+
+            $scope.changeLayerVisibility = LayMan.changeLayerVisibility;
+
+            $scope.layerOrder = function(layer){
+                return layer.layer.get('position')
+            }
+
+            $scope.changePosition = function(layer,direction,$event) {
+                var index = layer.layer.get('position');
+                var layers = OlMap.map.getLayers();
+                var toIndex = index;
+                if (direction) {// upwards
+                    var max = layers.getLength() - 1;
+                    if (index < max) {
+                        if ($event.shiftKey) toIndex = max;
+                        else toIndex = index+1;
+                    }
+                }
+                else {//downwards
+                    var min;
+                    for (var i = 0; i < layers.getLength(); i++) {
+                        if (layers.item(i).get('base') != true) {
+                            min = i;
+                            break;
+                        }
+                    }
+                    if (index > min) {
+                        if ($event.shiftKey) toIndex = min;
+                        else toIndex = index-1;
+                    }
+                }
+                var moveLayer = layers.item(index);
+                layers.removeAt(index);
+                layers.insertAt(toIndex, moveLayer);
+                LayMan.updateLayerOrder();
+                $rootScope.$broadcast('layermanager.updated'); //Rebuild the folder contents
+            }
+
+            $scope.showRemoveLayerDiag = function(e, layer) {
+                var confirm = $mdDialog.confirm()
+                    .title('Remove layer ' + layer.title)
+                    .textContent('Are you sure about layer removal?')
+                    .ariaLabel('Confirm layer removal')
+                    .targetEvent(e)
+                    .ok('Remove')
+                    .cancel('Cancel');
+      
+                $mdDialog.show(confirm).then(function() {
+                    $scope.removeLayer(layer.layer);
+                }, function() {
+                });
+            }
+
+            $scope.isLayerType = function(layer, type) {
+                switch (type) {
+                    case 'wms':
+                        return isWms(layer);
+                    case 'point':
+                        return layer.getSource().hasPoint;
+                    case 'line':
+                        return layer.getSource().hasLine;
+                    case 'polygon':
+                        return layer.getSource().hasPoly;
+                    default:
+                        return false;
+                }
+            }
+
+            function isWms(layer){
+                return (layer.getSource() instanceof ol.source.TileWMS || layer.getSource() instanceof ol.source.ImageWMS);
+            }
+
+            $scope.setProp = function(layer,property,value) {
+                layer.set(property, value);
+            }
+
+            $scope.layerOpacity = 50;
+
+            function getLayerStyle(wrapper){
+                var layer = wrapper.layer;
+                var source = layer.getSource();
+                wrapper.style = {};
+                if (angular.isUndefined(layer.getStyle)) return;
+                var style = layer.getStyle();
+                if (typeof style == 'function') style = style(source.getFeatures()[0]);
+                if (typeof style == 'object') style = style[0];
+                style = style.clone();
+                if (source.hasPoly) {
+                    wrapper.style.fillColor = style.getFill().getColor();
+                }
+                if (source.hasLine || source.hasPoly) {
+                    wrapper.style.lineColor = style.getStroke().getColor();
+                    wrapper.style.lineWidth = style.getStroke().getColor();
+                }
+                if (source.hasPoint) {
+                    var image = style.getImage();
+                    if (image instanceof ol.style.Circle) wrapper.style.pointType = 'Circle';
+                    else if (image instanceof ol.style.RegularShape) {
+                        wrapper.style.pointPoints = image.getPoints();
+                        wrapper.style.rotation = image.getRotation();
+                        if (angular.isUndefined(image.getRadius2()))wrapper.style.pointType = 'Polygon';
+                        else {
+                            wrapper.style.pointType = 'Star';
+                            wrapper.style.radius2 = image.getRadius2();
+                        }  
+                    }
+                    if (image instanceof ol.style.Circle || image instanceof ol.style.RegularShape) {
+                        wrapper.style.radius = image.getRadius();
+                        wrapper.style.pointFill = image.getFill().getColor();
+                        wrapper.style.pointStroke = image.getStroke().getColor();
+                        wrapper.style.pointWidth = image.getStroke().getWidth();
+                    }
+                    if (angular.isUndefined(wrapper.style.radius2)) wrapper.style.radius2 = wrapper.style.radius / 2;
+                    if (angular.isUndefined(wrapper.style.pointPoints)) wrapper.style.pointPoints = 4;
+                    if (angular.isUndefined(wrapper.style.rotation)) wrapper.style.rotation = Math.PI / 4;
+                }
+                wrapper.style.style = style;
+            }
+
+            $scope.saveStyle = function(layer){
+                setLayerStyle(layer);
+            }
+
+            function setLayerStyle(wrapper){
+                //debugger;
+                var layer = wrapper.layer;
+                var source = layer.getSource();
+                var style = wrapper.style.style;
+                if (source.hasPoly) {
+                    style.setFill(new ol.style.Fill({
+                        color: wrapper.style.fillColor
+                    }));
+                }
+                if (source.hasLine || source.hasPoly) {
+                    style.setStroke(new ol.style.Stroke({
+                        color: wrapper.style.lineColor,
+                        width: wrapper.style.lineWidth
+                    }));
+                }
+                if (source.hasPoint) {
+                    var image;
+                    var stroke = new ol.style.Stroke({
+                        color: wrapper.style.pointStroke,
+                        width: wrapper.style.pointWidth
+                    });
+                    var fill = new ol.style.Fill({
+                        color: wrapper.style.pointFill
+                    });
+                    if (wrapper.style.pointType === 'Circle') {
+                        image = new ol.style.Circle({
+                            stroke: stroke,
+                            fill: fill,
+                            radius: wrapper.style.radius,
+                            rotation: wrapper.style.rotation
+                        });
+                    } 
+                    if (wrapper.style.pointType === 'Polygon') {
+                        image = new ol.style.RegularShape({
+                            stroke: stroke,
+                            fill: fill,
+                            radius: wrapper.style.radius,
+                            points: wrapper.style.pointPoints,
+                            rotation: wrapper.style.rotation
+                        }); 
+                    }
+                    if (wrapper.style.pointType === 'Star') {
+                        image = new ol.style.RegularShape({
+                            stroke: stroke,
+                            fill: fill,
+                            radius1: wrapper.style.radius,
+                            radius2: wrapper.style.radius2,
+                            points: wrapper.style.pointPoints,
+                            rotation: wrapper.style.rotation
+                        });
+                    }
+                    style.setImage(image); 
+                }
+                layer.setStyle(style);
+            }
+
+            $scope.changePointType = function(layer,type) {
+                if (angular.isUndefined(layer.style)) getLayerStyle(layer);
+                layer.style.pointType = type;
+                setLayerStyle(layer);
+            }
+
+            $scope.icons = ["bag1.svg", "banking4.svg", "bar.svg", "beach17.svg", "bicycles.svg", "building103.svg", "bus4.svg", "cabinet9.svg", "camping13.svg", "caravan.svg", "church15.svg", "church1.svg", "coffee-shop1.svg", "disabled.svg", "favourite28.svg", "football1.svg", "footprint.svg", "gift-shop.svg", "gps40.svg", "gps41.svg", "gps42.svg", "gps43.svg", "gps5.svg", "hospital.svg", "hot-air-balloon2.svg", "information78.svg", "library21.svg", "location6.svg", "luggage13.svg", "monument1.svg", "mountain42.svg", "museum35.svg", "park11.svg", "parking28.svg", "pharmacy17.svg", "port2.svg", "restaurant52.svg", "road-sign1.svg", "sailing-boat2.svg", "ski1.svg", "swimming26.svg", "telephone119.svg", "toilets2.svg", "train-station.svg", "university2.svg", "warning.svg", "wifi8.svg"];
+
+            $scope.removeLayer = function (layer) {
+                OlMap.map.removeLayer(layer);
+                $rootScope.$broadcast('layermanager.updated'); //Rebuild the folder contents
+            }
+
+            $scope.$emit('scope_loaded', "MaterialLayerManager");
+        }
+    ])
+
+    .controller('hs.layermanager.controller.deprecated', ['$scope', 'hs.map.service', 'config', '$rootScope', 'Core', '$compile', 'hs.utils.service', 'hs.styler.service', 'hs.ows.wms.service_capabilities', 'hs.status_creator.service', 'hs.layermanager.service', 'hs.layermanager.WMSTservice', 'hs.utils.layerUtilsService',
         function ($scope, OlMap, config, $rootScope, Core, $compile, utils, styler, srv_wms_caps, status_creator, layManService, WMST, layerUtils) {
 
             $scope.Core = Core;
