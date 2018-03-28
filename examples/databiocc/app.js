12f5750f24ea8c303ecb276b907959d687756d21
Raitis Berzins <raitisbe@gmail.com>
2018-03-28 12:34:41 +0300

    Example for wmst layers in Darmstadt codecamp

---
+++ examples/databiocc/app.js
@@ -0,0 +1,135 @@
+'use strict';
+
+define(['ol', 'toolbar', 'layermanager', 'geojson', 'sidebar', 'query', 'search', 'print', 'permalink', 'measure', 'geolocation', 'api', 'cesium', 'ows', 'datasource_selector', 'cesiumjs', 'bootstrap'],
+
+    function (ol, toolbar, layermanager, geojson) {
+        var module = angular.module('hs', [
+            'hs.toolbar',
+            'hs.layermanager',
+            'hs.query',
+            'hs.search', 'hs.print', 'hs.permalink',
+            'hs.datasource_selector',
+            'hs.geolocation',
+            'hs.cesium',
+            'hs.sidebar',
+            'hs.ows'
+        ]);
+
+        module.directive('hs', ['hs.map.service', 'Core', '$compile', '$timeout', function (OlMap, Core, $compile, $timeout) {
+            return {
+                templateUrl: hsl_path + 'hslayers.html',
+                link: function (scope, element) {
+                    $timeout(function () { Core.fullScreenMap(element) }, 0);
+                }
+            };
+        }]);
+
+        function getHostname() {
+            var url = window.location.href
+            var urlArr = url.split("/");
+            var domain = urlArr[2];
+            return urlArr[0] + "//" + domain;
+        };
+
+        module.value('config', {
+            cesiumTimeline: true,
+            terrain_provider: 'https://assets.agi.com/stk-terrain/v1/tilesets/world/tiles',
+            terrain_providers: [{
+                title: 'Local terrain',
+                url: 'http://gis.lesprojekt.cz/cts/tilesets/rostenice_dmp1g/',
+                active: false
+            }, {
+                title: 'EU-DEM',
+                url: 'https://assets.agi.com/stk-terrain/v1/tilesets/world/tiles',
+                active: true
+            }],
+            default_layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.OSM(),
+                    title: "OpenStreetMap",
+                    base: true,
+                    visible: false,
+                    minimumTerrainLevel: 15
+                }),
+                new ol.layer.Tile({
+                    title: "Road segments of Open Transport Map vizualized by their average daily traffic volumes",
+                    source: new ol.source.TileWMS({
+                        url: 'https://intenzitadopravy.plzen.eu/wms-t',
+                        params: {
+                            LAYERS: 'may',
+                            VERSION: '1.3.0',
+                            FORMAT: "image/png",
+                            INFO_FORMAT: "text/html",
+                            time: '2018-03-28T09:00:00.000Z',
+                            minimumTerrainLevel: 12
+                        },
+                        crossOrigin: null
+                    }),
+                    legends: ['http://gis.lesprojekt.cz/wms/transport/open_transport_map?service=WMS&request=GetLegendGraphic&layer=roads__traffic_volumes&version=1.3.0&format=image/png&sld_version=1.1.0'],
+                    maxResolution: 8550,
+                    visible: false,
+                    opacity: 0.7
+                })
+            ],
+            project_name: 'erra/map',
+            datasources: [
+                {
+                    title: "Datasets",
+                    url: "http://otn-dev.intrasoft-intl.com/otnServices-1.0/platform/ckanservices/datasets",
+                    language: 'eng',
+                    type: "ckan",
+                    download: true
+                }, {
+                    title: "Services",
+                    url: "http://cat.ccss.cz/csw/",
+                    language: 'eng',
+                    type: "micka",
+                    code_list_url: 'http://www.whatstheplan.eu/php/metadata/util/codelists.php?_dc=1440156028103&language=eng&page=1&start=0&limit=25&filter=%5B%7B%22property%22%3A%22label%22%7D%5D'
+                }
+            ],
+            hostname: {
+                "default": {
+                    "title": "Default",
+                    "type": "default",
+                    "editable": false,
+                    "url": getHostname()
+                }
+            },
+            'catalogue_url': "/php/metadata/csw",
+            'compositions_catalogue_url': "/php/metadata/csw",
+            status_manager_url: '/wwwlibs/statusmanager2/index.php',
+            default_view: new ol.View({
+                center: ol.proj.transform([1208534.8815206578, 5761821.705531779], 'EPSG:3857', 'EPSG:4326'),
+                zoom: 16,
+                units: "m",
+                projection: 'EPSG:4326'
+            })
+        });
+
+        module.controller('Main', ['$scope', '$compile', '$element', 'Core', 'hs.map.service', 'config', '$rootScope', 'hs.utils.service', '$sce', 
+            function ($scope, $compile, $element, Core, hs_map, config, $rootScope, utils, $sce) {
+                var map;
+
+                if (console) console.log("Main called");
+                $scope.hsl_path = hsl_path; //Get this from hslayers.js file
+                $scope.Core = Core;
+
+                Core.singleDatasources = true;
+                Core.panelEnabled('compositions', true);
+                Core.panelEnabled('status_creator', false);
+                $scope.Core.setDefaultPanel('layermanager');
+
+                $rootScope.$on('map.loaded', function () {
+                    map = hs_map.map;
+                });
+
+                $rootScope.$on('map.sync_center', function (e, center, bounds) {
+                    
+                })
+
+                $scope.$on('infopanel.updated', function (event) { });
+            }
+        ]);
+
+        return module;
+    });
---
+++ examples/databiocc/hslayers.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var hsl_path = '../../';
+var gitsha = Math.random();
+
+require.config({
+    urlArgs: 'bust=' + gitsha,
+    paths: {
+        toolbar: hsl_path + 'components/toolbar/toolbar',
+        layermanager: hsl_path + 'components/layermanager/layermanager',
+        map: hsl_path + 'components/map/map',
+        query: hsl_path + 'components/query/query',
+        search: hsl_path + 'components/search/search',
+        print: hsl_path + 'components/print/print',
+        permalink: hsl_path + 'components/permalink/permalink',
+        geolocation: hsl_path + 'components/geolocation/geolocation',
+        measure: hsl_path + 'components/measure/measure',
+        legend: hsl_path + 'components/legend/legend',
+        app: 'app',
+        core: hsl_path + 'components/core/core',
+        api: hsl_path + 'components/api/api',
+        translations: hsl_path + 'components/translations/js/translations',
+        cesium: hsl_path + 'components/cesium/cesium',
+        cesiumjs: hsl_path + 'node_modules/cesium/Build/Cesium/Cesium',
+        pois: 'poi'
+    },
+    shim: {
+        cesiumjs: {
+            exports: 'Cesium'
+        }
+    }
+});
+
+//http://code.angularjs.org/1.2.1/docs/guide/bootstrap#overview_deferred-bootstrap
+window.name = "NG_DEFER_BOOTSTRAP!";
+
+require(['core'], function(app) {
+    require(['app'], function(app) {
+        var $html = angular.element(document.getElementsByTagName('html')[0]);
+        angular.element().ready(function() {
+            angular.resumeBootstrap([app['name']]);
+        });
+    });
+});
