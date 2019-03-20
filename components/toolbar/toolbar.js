669d8b9b95373f871d38a9c4eaa3e4f3977c5dd0
Raitis Berzins <raitisbe@gmail.com>
2019-03-20 21:27:33 +0200

    Refactoring of toolbar (search+reset) as component

--- components/toolbar/toolbar.js
+++ components/toolbar/toolbar.js
@@ -2,81 +2,85 @@
  * @namespace hs.toolbar
  * @memberOf hs
  */
-define(['angular', 'map', 'core', 'permalink'],
+define(['angular', 'map', 'core', 'permalink'], function (angular) {
+    var module = angular.module('hs.toolbar', ['hs.map', 'hs.core']);
 
-    function(angular) {
-        angular.module('hs.toolbar', ['hs.map', 'hs.core'])
-            /**
-             * @memberof hs.toolbar
-             * @ngdoc directive
-             * @name hs.toolbar.directive
-             * @description Add toolbar to map (search field, full map button a measure button)
-             */
-            .directive('hs.toolbar.directive', function() {
-                return {
-                    templateUrl: hsl_path + 'components/toolbar/partials/toolbar.html?bust=' + gitsha
-                };
-            })
-
+    /**
+     * @memberof hs.toolbar
+     * @ngdoc component
+     * @name hs.toolbar
+     * @description Add toolbar to map (search field, full map button a measure button)
+     */
+    module.component('hs.toolbar', {
+        templateUrl: hsl_path + 'components/toolbar/partials/toolbar.html?bust=' + gitsha,
+        
         /**
          * @memberof hs.toolbar
          * @ngdoc controller
          * @name hs.toolbar.controller
          */
-        .controller('hs.toolbar.controller', ['$scope', 'hs.map.service', 'Core', 'hs.permalink.urlService', '$window', '$compile',
-            function($scope, OlMap, Core, bus, $window, $compile) {
-                $scope.Core = Core;
+        controller: ['$scope', 'hs.map.service', 'Core', 'hs.permalink.urlService', '$window', '$compile',
+            function ($scope, OlMap, Core, bus, $window, $compile) {
                 var collapsed = false;
 
-                /**
-                 * Set current active panel in sidebar
-                 * @memberof hs.toolbar.controller
-                 * @function setMainPanel
-                 * @param {string} which
-                 */
-                $scope.setMainPanel = function(which, by_gui, queryable) {
-                    Core.setMainPanel(which, by_gui, queryable);
-                    if (!$scope.$$phase) $scope.$digest();
-                }
+                angular.extend($scope, {
+                    Core: Core,
 
-                /**
-                 * Change collapsed setting
-                 * @memberof hs.toolbar.controller
-                 * @function collapsed
-                 * @param {boolean} is
-                 */
-                $scope.collapsed = function(is) {
-                    if (arguments.length > 0) {
-                        collapsed = is;
+                    /**
+                     * Set current active panel in sidebar
+                     * @memberof hs.toolbar.controller
+                     * @function setMainPanel
+                     * @param {string} which
+                     */
+                    setMainPanel: function (which, by_gui, queryable) {
+                        Core.setMainPanel(which, by_gui, queryable);
+                        if (!$scope.$$phase) $scope.$digest();
+                    },
+
+                    /**
+                     * Change/read collapsed setting
+                     * @memberof hs.toolbar.controller
+                     * @function collapsed
+                     * @param {boolean} is
+                     */
+                    collapsed: function (is) {
+                        if (arguments.length > 0) {
+                            collapsed = is;
+                        }
+                        return collapsed;
+                    },
+
+                    /**
+                     * Test mobile mode (document width under 800px)
+                     * @memberof hs.toolbar.controller
+                     * @function isMobile
+                     */
+                    isMobile: function () {
+                        if ($(document).width() < 800) {
+                            return "mobile";
+                        } else {
+                            return "";
+                        }
+                    },
+
+                    /**
+                     * True if composition is loaded
+                     * @memberof hs.toolbar.controller
+                     * @property compositionLoaded
+                     */
+                    compositionLoaded: function () {
+                        return angular.isDefined($scope.composition_title);
                     }
-                    return collapsed;
-                }
+                })
 
-                /**
-                 * Test mobile mode (document width under 800px)
-                 * @memberof hs.toolbar.controller
-                 * @function isMobile
-                 */
-                $scope.isMobile = function() {
-                    if ($(document).width() < 800) {
-                        return "mobile";
-                    } else {
-                        return "";
-                    }
-                }
 
-                $scope.$on('core.map_reset', function(event) {
+                $scope.$on('core.map_reset', function (event) {
                     delete $scope.composition_title;
                     delete $scope.composition_abstract;
                     if (!$scope.$$phase) $scope.$digest();
                 });
 
-                $scope.compositionLoaded = function() {
-                    return angular.isDefined($scope.composition_title);
-                }            
-
                 $scope.$emit('scope_loaded', "Toolbar");
-            }
-
-        ]);
-    })
+            }]
+    });
+})
