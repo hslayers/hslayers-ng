31cba0f2041cec021bf94d038747d9c640fefb28
raitisbe <raitisbe@gmail.com>
2019-04-03 13:05:40 +0300

    Remove examples directory

--- examples/olu_spoi/hslayers.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-
-var $html = angular.element(document.getElementsByTagName('html')[0]);
-angular.element().ready(function() {
-//    angular.resumeBootstrap([exports.app['name']]);
-});
--- examples/wirecloud/makewirecloud.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd ../../ && zip -r hslayers.wgt ./components ./css ./extensions ./img ./po ./hslayers.html hslayers-logo.png ./LICENSE ./node_modules/angular/ ./node_modules/angular-gettext/dist/ ./node_modules/angular-loader/ ./node_modules/angular-mocks/ ./node_modules/angular-route/ node_modules/angular-sanitize/ ./node_modules/bootstrap/dist/ ./node_modules/crossfilter/crossfilter.min.js ./node_modules/d3/d3.min.js ./node_modules/jquery/dist/jquery.min.js ./node_modules/ol3/build/ ./node_modules/ol3/css/ol.css node_modules/requirejs/require.js ./node_modules/xml2json/xml2json.js
-cd examples/wirecloud/
-zip -g ../../hslayers.wgt config.xml hslayers.js app.js index.html
-mv ../../hslayers.wgt ./
