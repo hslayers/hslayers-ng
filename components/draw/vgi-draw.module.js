9c30a24550ab52eb7548077e79c0023146109fbd
raitis <raitisbe@gmail.com>
2019-08-23 13:23:30 +0300

    Separate vgi related code out of hs.draw module refs #257

---
+++ components/draw/vgi-draw.module.js
@@ -0,0 +1,34 @@
+import 'components/map/map.module';
+import 'core'
+import 'components/utils/utils.module'
+import 'geolocation';
+import 'draw.module';
+import vgiDrawComponent from './vgi-draw.component';
+
+/**
+* @namespace hs.vgi-draw
+* @memberOf hs
+*/
+
+angular.module('hs.vgi-draw', ['hs.map', 'hs.core', 'hs.utils', 'hs.geolocation', 'hs.save-map', 'hs.draw'])
+    /**
+    * @name hs.vgi-draw.toolbar-button-directive
+    * @ngdoc directive
+    * @memberof hs.vgi-draw
+    * @description Display draw toolbar button in map
+    */
+    .directive('hs.vgiDraw.toolbarButtonDirective', ['config', function (config) {
+        return {
+            template: require('components/draw/partials/toolbar_button_directive.html')
+        };
+    }])
+
+    /**
+    * @name hs.vgi-draw
+    * @ngdoc component
+    * @memberof hs.vgi-draw
+    * @description Component for draw features and sending them to Senslog VGI 
+    * backend. Display draw feature panel in map. Panel contains active layer 
+    * selector, geometry selector and information editor for new features.
+    */
+    .component('hs.vgiDraw', vgiDrawComponent);
