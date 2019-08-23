57ffa31595d451f23677e6c95e397b230b01c697
raitis <raitisbe@gmail.com>
2019-08-23 19:37:33 +0300

    Send and retrieve vec layer features from Layman fixes #256

--- components/save-map/layman.service.js
+++ components/save-map/layman.service.js
@@ -2,6 +2,18 @@ export default ['Core', 'hs.utils.service', '$http', 'config',
     function (Core, utils, $http, config) {
         var me = this;
         angular.extend(me, {
+            /**
+            * @ngdoc method
+            * @function save 
+            * @memberof hs.laymanService
+            * @public
+            * @param {Object} endpoint 
+            * @param {String} compositionJson Json with composition definition
+            * @param {String} compoData Additional fields for composition such 
+            * as title, name
+            * @returns {Promise<Boolean>} Promise result of POST 
+            * @description Save composition to Layman
+            */
             save(compositionJson, endpoint, compoData) {
                 return new Promise((resolve, reject) => {
                     var formdata = new FormData();
@@ -17,7 +29,7 @@ export default ['Core', 'hs.utils.service', '$http', 'config',
                         method: 'POST',
                         data: formdata,
                         transformRequest: angular.identity,
-                        headers: {'Content-Type': undefined}
+                        headers: { 'Content-Type': undefined }
                     })
                         .then(function (response) {
                             resolve(response)
@@ -26,9 +38,151 @@ export default ['Core', 'hs.utils.service', '$http', 'config',
                         });
                 })
             },
-            synchronizeVectorSource(ds, geojson){
-                console.log(geojson);
+
+            /**
+            * @ngdoc method
+            * @function pushVectorSource 
+            * @memberof hs.laymanService
+            * @public
+            * @param {Object} endpoint 
+            * @param {String} geojson Geojson object with features to send to server 
+            * @param {String} description Object containing {name, title, crs} of 
+            * layer to retrieve
+            * @param {Boolean} exists Set to true if you are sure that layer 
+            * exists (has been checked before and result stored somewhere)
+            * @returns {Promise<Boolean>} Promise result of POST/PATCH 
+            * @description Send layer definition and features to Layman
+            */
+            pushVectorSource(endpoint, geojson, description, exists) {
+                return new Promise((resolve, reject) => {
+                    var formdata = new FormData();
+                    formdata.append('file',
+                        new Blob([JSON.stringify(geojson)],
+                            { type: 'application/geo+json' }), 'blob.geojson'
+                    );
+                    formdata.append('name', description.name);
+                    formdata.append('title', description.title);
+                    formdata.append('crs', description.crs);
+                    me.checkIfLayerExists(endpoint, description.name, exists)
+                        .then(exists => {
+                            $http({
+                                url: `${endpoint.url}/rest/${endpoint.user}/layers${exists ? '/' + description.name : ''}?${Math.random()}`,
+                                method: exists ? 'PATCH' : 'POST',
+                                data: formdata,
+                                transformRequest: angular.identity,
+                                headers: { 'Content-Type': undefined }
+                            }).then(function (response) {
+                                resolve(response)
+                            })
+                        })
+                        .catch(err => {
+                            reject(err.data)
+                        });
+                })
+            },
+
+            /**
+            * @ngdoc method
+            * @function pullVectorSource 
+            * @memberof hs.laymanService
+            * @public
+            * @param {Object} endpoint 
+            * @param {String} layerName Object containing {name, title, crs} of 
+            * layer to retrieve
+            * @returns {Promise<Boolean>} Promise which WFS xml (GML3.1) response 
+            * with features for a specified layer
+            * @description Retrieve layers features from server
+            */
+            pullVectorSource(endpoint, layerName) {
+                return new Promise((resolve, reject) => {
+                    me.describeLayer(endpoint, layerName).then(descr => {
+                        if (descr == null) {
+                            resolve();
+                            return;
+                        }
+                        /* When OL will support GML3.2, then we can use WFS 
+                        version 2.0.0. Currently only 3.1.1 is possible */
+                        $http({
+                            url: descr.wfs.url + '?' + utils.paramsToURL({
+                                service: 'wfs',
+                                version: '1.1.0',
+                                request: 'GetFeature',
+                                typeNames: `${endpoint.user}:${descr.name}`,
+                                r: Math.random()
+                            }),
+                            method: 'GET'
+                        })
+                            .then(function (response) {
+                                resolve(response.data);
+                            }, err => resolve(null))
+                    })
+                })
+            },
+
+            /**
+            * @ngdoc method
+            * @function describeLayer 
+            * @memberof hs.laymanService
+            * @public
+            * @param {Object} endpoint 
+            * @param {String} layerName 
+            * @returns {Promise<Boolean>} Promise which returns layers 
+            * description containig name, file, wms, wfs urls etc.
+            * @description Try getting layer description from layman.
+            */
+            describeLayer(endpoint, layerName) {
+                return new Promise((resolve, reject) => {
+                    $http({
+                        url: `${endpoint.url}/rest/${endpoint.user}/layers/${layerName}?${Math.random()}`,
+                        method: 'GET'
+                    })
+                        .then(function (response) {
+                            if (angular.isDefined(response.data.code) &&
+                                response.data.code == 15)
+                                resolve(null);
+                            if (angular.isDefined(response.data.name))
+                                resolve(response.data);
+                        }, err => {
+                            resolve(null)
+                        })
+                })
+            },
+
+            /**
+            * @ngdoc method
+            * @function checkIfLayerExists 
+            * @memberof hs.laymanService
+            * @public
+            * @param {Object} endpoint 
+            * @param {String} layerName 
+            * @param {Boolean} exists Set to true if you are sure that layer 
+            * exists (has been checked before and result stored somewhere)
+            * @returns {Promise<Boolean>} Promise which returns boolean if layer 
+            * exists in Layman
+            * @description Try getting layer description from layman. If it 
+            * succeeds, that means that layer is there and can be updated 
+            * instead of postign a new one
+            */
+            checkIfLayerExists(endpoint, layerName, exists) {
+                return new Promise((resolve, reject) => {
+                    if (angular.isUndefined(exists)) {
+                        me.describeLayer(endpoint, layerName)
+                            .then(description => {
+                                if (description != null &&
+                                    angular.isDefined(description.code) &&
+                                    description.code == 15)
+                                    resolve(false);
+                                else if (angular.isDefined(description.name))
+                                    resolve(true);
+                                else
+                                    resolve(false)
+                            })
+                    } else {
+                        resolve(exists)
+                    }
+                })
             }
+
         });
         return me;
     }]
