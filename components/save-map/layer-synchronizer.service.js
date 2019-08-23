bbfba849f73c655ce225369c1e225d24a4bbde47
raitis <raitisbe@gmail.com>
2019-08-23 19:37:33 +0300

    Send and retrieve vec layer features from Layman fixes #256

--- components/save-map/layer-synchronizer.service.js
+++ components/save-map/layer-synchronizer.service.js
@@ -1,11 +1,14 @@
-import {Vector as VectorSource} from 'ol/source';
-import {  GeoJSON, KML } from 'ol/format';
+import { Vector as VectorSource } from 'ol/source';
+import { GeoJSON, WFS } from 'ol/format';
+import GML3 from 'ol/format/GML3';
 
 export default ['Core', 'hs.utils.service', 'config', 'hs.map.service', 'hs.laymanService',
     function (Core, utils, config, hsMap, laymanService) {
         var me = this;
         angular.extend(me, {
             syncedLayers: [],
+            crs: null,
+            
             init(map) {
                 const layerAdded = (e) => me.addLayer(e.element);
                 map.getLayers().on("add", layerAdded);
@@ -16,7 +19,8 @@ export default ['Core', 'hs.utils.service', 'config', 'hs.map.service', 'hs.laym
                     layerAdded({
                         element: lyr
                     });
-                })
+                });
+                me.crs = map.getView().getProjection().getCode()
             },
 
             /**
@@ -33,28 +37,82 @@ export default ['Core', 'hs.utils.service', 'config', 'hs.map.service', 'hs.laym
             },
 
             /**
-             * Keep track of synchronized vector layers
+             * Keep track of synchronized vector layers by listening to 
+             * VectorSources change events. Initialy also get features from server
              * @memberof hs.layerSynchronizerService
              * @function getLayerDescriptor
              * @param {object} layer Layer to add
              */
             getLayerDescriptor(layer) {
-                if (utils.instOf(layer.getSource(), VectorSource)) {
+                if (utils.instOf(layer.getSource(), VectorSource) &&
+                    layer.get('synchronize') === true) {
                     var layerSource = layer.getSource();
-                    layerSource.on('changefeature', function (e) {
-                        me.synchronize(layer, e.target);
+                    me.pull(layer, layer.getSource());
+                    layerSource.on('change', function (e) {
+                        if(e.target.loading) return;
+                        me.push(layer, e.target);
                     })
                     return layer
                 }
             },
 
-            synchronize(layer, source) {
+            /**
+             * Send features to Layman endpoint as WFS string
+             * @memberof hs.layerSynchronizerService
+             * @function getLayerName
+             * @param {Ol.layer} layer Layer to get Layman friendly name for
+             * @param {Ol.source} source Openlayers VectorSource from which to 
+             * get features
+             */
+            push(layer, source) {
                 var f = new GeoJSON();
                 var geojson = f.writeFeaturesObject(source.getFeatures());
                 (config.datasources || []).filter(ds => ds.type == 'layman').forEach(
                     ds => {
-                        laymanService.synchronizeVectorSource(ds, geojson);
+                        laymanService.pushVectorSource(ds, geojson, {
+                            title: layer.get('title'),
+                            name: me.getLayerName(layer),
+                            crs: me.crs
+                        }).then(response => {
+                        });
                     })
+            },
+
+            /**
+             * Get features from Layman endpoint as WFS string, parse and add 
+             * them to Openlayers VectorSource
+             * @memberof hs.layerSynchronizerService
+             * @function getLayerName
+             * @param {Ol.layer} layer Layer to get Layman friendly name for
+             * @param {Ol.source} source Openlayers VectorSource to store features in
+             */
+            pull(layer, source) {
+                (config.datasources || []).filter(ds => ds.type == 'layman').forEach(
+                    ds => {
+                        laymanService.pullVectorSource(ds, me.getLayerName(layer))
+                        .then(featureString => {
+                            if(featureString){
+                                source.loading = true;
+                                var format = new WFS();
+                                featureString = featureString.replaceAll('urn:x-ogc:def:crs:EPSG:3857', 'EPSG:3857');
+                                source.addFeatures(
+                                    format.readFeatures(featureString)
+                                );
+                                source.loading = false;
+                            }
+                        });
+                    })
+            },
+
+            /**
+             * Get Layman friendly name for layer based on its title by
+             * removing spaces, converting to lowercase
+             * @memberof hs.layerSynchronizerService
+             * @function getLayerName
+             * @param {Ol.layer} layer Layer to get Layman friendly name for
+             */
+            getLayerName(layer) {
+                return layer.get('title').toLowerCase().replaceAll(' ', '')
             },
 
             /**
