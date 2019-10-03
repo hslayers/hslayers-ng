0e55bc2dfe3610fba4227a34b714514232d48210
raitis <raitisbe@gmail.com>
2019-10-03 17:26:55 +0300

    Remove jQuery usage in drag directive refs #187

--- components/drag/drag.directive.js
+++ components/drag/drag.directive.js
@@ -1,5 +1,5 @@
-export default ['$document', '$window', 'config', function($document, $window, config) {
-    return function(scope, element, attr) {
+export default ['$document', '$window', 'config', function ($document, $window, config) {
+    return function (scope, element, attr) {
 
         function isPanel() {
             return (angular.isUndefined(attr.iswindow) || attr.iswindow == "true");
@@ -18,26 +18,28 @@ export default ['$document', '$window', 'config', function($document, $window, c
         scope.unpinned = false;
         scope.drag_panel = element;
         var orig_left, orig_top;
-        $(".card-header:first", element).append($('<button>').attr('type', 'button').addClass('but-title-sm').click(function() {
-                scope.unpinned = true;
-                $(".card-header", element).css('cursor', 'move');
-            })
-            .append($('<span>').addClass('icon-share').attr('aria-hidden', 'true'))
-            .append($('<span>').addClass('sr-only').attr('translate', '').html('Unpin')));
-        element.on('mousedown', function(event) {
+        var header = element[0].querySelector(".card-header");
+        var closeButton = angular.element('<button class="but-title-sm"><span class="icon-share" aria-hidden="true"></span><span class="sr-only" translate>Unpin</span><button>')[0];
+        closeButton.onclick = () => {
+            scope.unpinned = true;
+            header.style.cursor = 'move';
+        };
+        header.appendChild(closeButton);
+        element.on('mousedown', function (event) {
             if (!scope.unpinned && isPanel()) return;
             // Prevent default dragging of selected content
             event.preventDefault();
             if (event.offsetY > 37) return;
-            orig_left = element.offset().left;
-            orig_top = element.offset().top;
+            var rect = element[0].getBoundingClientRect();
+            orig_left = rect.left + document.body.scrollLeft;
+            orig_top = rect.top + document.body.scrollTop;
             startY = event.pageY;
             startX = event.pageX;
             if (element.parent()[0] != document.body) {
                 var w = angular.element($window);
-                element.css("width", element.width() + "px");
+                element.css("width", getComputedStyle(element[0], null).width);
                 scope.original_container = element.parent()[0];
-                element.appendTo($(document.body));
+                document.body.appendChild(element[0]);
                 element.css({
                     top: orig_top + 'px',
                     left: orig_left + 'px',
@@ -51,7 +53,10 @@ export default ['$document', '$window', 'config', function($document, $window, c
         function mousemove(event) {
             y = orig_top + event.pageY - startY;
             x = orig_left + event.pageX - startX;
-            scope[attr.hsDraggableOnmove](x + element.width() / 2, y + element.height() / 2);
+            if (scope[attr.hsDraggableOnmove])
+                scope[attr.hsDraggableOnmove](
+                    x + parseFloat(getComputedStyle(element[0], null).width.replace("px", "")) / 2,
+                    y + parseFloat(getComputedStyle(element[0], null).height.replace("px", "")) / 2);
             element.css({
                 top: y + 'px',
                 left: x + 'px'
