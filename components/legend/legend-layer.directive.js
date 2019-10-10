3549305c8a2573b3df4e07f93128d814110f3db4
raitis <raitisbe@gmail.com>
2019-10-10 14:39:45 +0300

    Dont try to read feature styles for ImageLayer refs #321

--- components/legend/legend-layer.directive.js
+++ components/legend/legend-layer.directive.js
@@ -1,17 +1,23 @@
+import VectorLayer from 'ol/layer/Vector';
 export default ['config', function (config) {
     return {
         template: require('components/legend/partials/layer-directive.html'),
         scope: {
             layer: '<'
         },
-        controller: ['$scope', 'hs.legend.service', 'hs.utils.service', function($scope, service, utils){
-            $scope.styles = service.getStyleVectorLayer($scope.layer.lyr);
-            $scope.geometryTypes = service.getVectorFeatureGeometry($scope.layer.lyr);
-            if($scope.layer.lyr.getSource()){
-                let source = $scope.layer.lyr.getSource();
+        controller: ['$scope', 'hs.legend.service', 'hs.utils.service', function ($scope, service, utils) {
+            var olLayer = $scope.layer.lyr;
+            $scope.styles = [];
+            $scope.geometryTypes = [];
+            if(utils.instOf(olLayer, VectorLayer)){
+                $scope.styles = service.getStyleVectorLayer(olLayer);
+                $scope.geometryTypes = service.getVectorFeatureGeometry(olLayer);
+            }            
+            if (olLayer.getSource()) {
+                let source = olLayer.getSource();
                 var changeHandler = utils.debounce(function (e) {
-                    $scope.styles = service.getStyleVectorLayer($scope.layer.lyr);
-                    $scope.geometryTypes = service.getVectorFeatureGeometry($scope.layer.lyr);
+                    $scope.styles = service.getStyleVectorLayer(olLayer);
+                    $scope.geometryTypes = service.getVectorFeatureGeometry(olLayer);
                 }, 200);
                 source.on('changefeature', changeHandler);
                 source.on('addfeature', changeHandler);
