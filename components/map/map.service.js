8e5a1a7ad3f29a53de7f26a8b1c135570467ac7a
FilipLeitner <leitnerfilip@gmail.com>
2019-11-21 09:21:43 +0100

    add support for EPSG:5514 trasformations

--- components/map/map.service.js
+++ components/map/map.service.js
@@ -11,6 +11,9 @@ import Feature from 'ol/Feature';
 import { Group } from 'ol/layer';
 import { Vector } from 'ol/source';
 import { transform, transformExtent } from 'ol/proj';
+import proj4 from 'proj4';
+import { register } from 'ol/proj/proj4';
+
 
 export default ['config', '$rootScope', 'hs.utils.service', '$timeout', function (config, $rootScope, utils, $timeout) {
 
@@ -106,6 +109,10 @@ export default ['config', '$rootScope', 'hs.utils.service', '$timeout', function
         // me.map.addControl(new ol.control.ScaleLine());
 
         me.repopulateLayers();
+
+        proj4.defs('EPSG:5514', 'PROJCS["S-JTSK / Krovak East North",GEOGCS["S-JTSK",DATUM["System_Jednotne_Trigonometricke_Site_Katastralni",SPHEROID["Bessel 1841",6377397.155,299.1528128,AUTHORITY["EPSG","7004"]],TOWGS84[589,76,480,0,0,0,0],AUTHORITY["EPSG","6156"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4156"]],PROJECTION["Krovak"],PARAMETER["latitude_of_center",49.5],PARAMETER["longitude_of_center",24.83333333333333],PARAMETER["azimuth",30.28813972222222],PARAMETER["pseudo_standard_parallel_1",78.5],PARAMETER["scale_factor",0.9999],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["X",EAST],AXIS["Y",NORTH],AUTHORITY["EPSG","5514"]]');
+        register(proj4);
+
         /**
          * @ngdoc event
          * @name hs.map.service#map.loaded
