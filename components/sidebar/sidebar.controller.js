d57883bef1dd172a269ddbd0aacf6e7144ab9a11
FilipLeitner <leitnerfilip@gmail.com>
2019-12-06 15:07:08 +0100

    Minisidebar fixes
    Rename minisidebar id to hs-minisidebar
    
    Add order parameter

--- components/sidebar/sidebar.controller.js
+++ components/sidebar/sidebar.controller.js
@@ -3,21 +3,21 @@ export default ['$scope', '$timeout', 'hs.map.service', 'Core', 'hs.permalink.ur
         $scope = angular.extend($scope, {
             layoutService,
             buttons: [
-                { panel: 'composition_browser', module: 'hs.compositions', title: gettext('Map Compositions'), description: gettext('List available map compositions'), icon: 'icon-map' },
-                { panel: 'layermanager', module: 'hs.layermanager', title: gettext('Layer Manager'), description: gettext('Manage and style your layers in composition'), icon: 'icon-layers' },
-                { panel: 'legend', module: 'hs.legend', title: gettext('Legend'), description: gettext('Legend'), icon: 'icon-dotlist' },
-                { panel: 'datasource_selector', module: 'hs.datasource_selector', title: gettext('Add layers'), description: gettext('Select data or services for your map composition'), icon: 'icon-database' },
-                { panel: 'feature_crossfilter', module: 'hs.feature_crossfilter.controller', title: gettext('Filter features'), description: gettext('Crossfilter'), icon: 'icon-analytics-piechare' },
-                { panel: 'measure', module: 'hs.measure.controller', title: gettext('Measurements'), description: gettext('Measure distance or area at map'), icon: 'icon-design' },
-                { panel: 'routing', module: 'hs.routing.controller', title: gettext('Routing'), description: gettext(''), icon: 'icon-road' },
-                { panel: 'tracking', module: 'hs.tracking.controller', title: gettext('Tracking'), description: gettext(''), icon: 'icon-screenshot' },
-                { panel: 'print', module: 'hs.print', title: gettext('Print'), description: gettext('Print map'), icon: 'icon-print' },
-                { panel: 'permalink', module: 'hs.permalink', title: gettext('Share map'), description: gettext('Share map'), icon: 'icon-share-alt' },
-                { panel: 'saveMap', module: 'hs.save-map', title: gettext('Save composition'), description: gettext('Save content of map to composition'), icon: 'icon-save-floppy' },
-                { panel: 'sensors', module: 'hs.sensors', title: gettext('Sensors'), description: gettext(''), icon: 'icon-weightscale' },
-                { panel: 'language', module: 'hs.language.controller', title: gettext('Change language'), description: gettext('Change language'), content: function () {return languageService.getCurrentLanguageCode().toUpperCase() } },
-                { panel: 'mobile_settings', module: 'hs.mobile_settings.controller', title: gettext('Application settings'), description: gettext('Specify application user settings'), icon: 'icon-settingsandroid' },
-                { panel: 'info', module: 'hs.query', title: gettext('Info panel'), description: gettext('Display map-query result information'), icon: 'icon-info-sign' }
+                { panel: 'layermanager', module: 'hs.layermanager', order:0, title: gettext('Layer Manager'), description: gettext('Manage and style your layers in composition'), icon: 'icon-layers' },
+                { panel: 'legend', module: 'hs.legend', order:1, title: gettext('Legend'), description: gettext('Legend'), icon: 'icon-dotlist' },
+                { panel: 'info', module: 'hs.query', order:7, title: gettext('Info panel'), description: gettext('Display map-query result information'), icon: 'icon-info-sign' },
+                { panel: 'composition_browser', module: 'hs.compositions', order:3, title: gettext('Map Compositions'), description: gettext('List available map compositions'), icon: 'icon-map' },
+                { panel: 'datasource_selector', module: 'hs.datasource_selector', order:4, title: gettext('Add layers'), description: gettext('Select data or services for your map composition'), icon: 'icon-database' },
+                { panel: 'feature_crossfilter', module: 'hs.feature_crossfilter.controller', order:5, title: gettext('Filter features'), description: gettext('Crossfilter'), icon: 'icon-analytics-piechare' },
+                { panel: 'sensors', module: 'hs.sensors', order:6, title: gettext('Sensors'), description: gettext(''), icon: 'icon-weightscale' },
+                { panel: 'measure', module: 'hs.measure.controller', order:2, title: gettext('Measurements'), description: gettext('Measure distance or area at map'), icon: 'icon-design' },
+                { panel: 'routing', module: 'hs.routing.controller', order:8, title: gettext('Routing'), description: gettext(''), icon: 'icon-road' },
+                { panel: 'tracking', module: 'hs.tracking.controller', order:9, title: gettext('Tracking'), description: gettext(''), icon: 'icon-screenshot' },
+                { panel: 'print', module: 'hs.print', order:10, title: gettext('Print'), description: gettext('Print map'), icon: 'icon-print' },
+                { panel: 'permalink', module: 'hs.permalink', order:11, title: gettext('Share map'), description: gettext('Share map'), icon: 'icon-share-alt' },
+                { panel: 'saveMap', module: 'hs.save-map', order:12, title: gettext('Save composition'), description: gettext('Save content of map to composition'), icon: 'icon-save-floppy' },
+                { panel: 'language', module: 'hs.language.controller', order:13, title: gettext('Change language'), description: gettext('Change language'), content: function () {return languageService.getCurrentLanguageCode().toUpperCase() } },
+                { panel: 'mobile_settings', module: 'hs.mobile_settings.controller', order:14, title: gettext('Application settings'), description: gettext('Specify application user settings'), icon: 'icon-settingsandroid' }
             ],
             visibleButtons: [],
              /**
@@ -78,7 +78,7 @@ export default ['$scope', '$timeout', 'hs.map.service', 'Core', 'hs.permalink.ur
 
 
         if (bus.getParamValue('hs_panel')) {
-            if (angular.isUndefined(document.getElementById("minisidebar")))
+            if(Core.exists('hs.sidebar') && layoutService.minisidebar)
             $scope.setMainPanel(bus.getParamValue('hs_panel'));
         }
 
