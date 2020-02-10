0ffe439eebdea4151f7191d62406cc653fcda5af
Filip Leitner <leitnerfilip@gmail.com>
2020-02-10 10:30:44 +0100

    Directive

--- components/geolocation/geolocation.directive.js
+++ components/geolocation/geolocation.directive.js
@@ -1,39 +1,25 @@
-export default ['hs.map.service', 'hs.geolocation.service', 'Core', 'config', 'hs.layout.service', function (hsMap, Geolocation, Core, config, layoutService) {
-    return {
-        template: Core.isMobile() ? require('components/geolocation/partials/geolocation_cordova.html') : require('components/geolocation/partials/geolocation.html'),
-        link: function link(scope, element, attrs) {
-            if (!Core.puremapApp) {
-                if (Core.isMobile()) {
-                    document.querySelector("#menu").appendChild(element[0]);
-                } else {
+export default ['hs.map.service', 'hs.geolocation.service', 'Core', 'config', 'hs.layout.service',
+    function (hsMap, locationService, Core, config, layoutService) {
+        return {
+            template: require('components/geolocation/partials/geolocation.html'),
+            link: function link(scope, element, attrs) {
+                if (!Core.puremapApp) {
                     hsMap.loaded().then(_ => {
                         layoutService.contentWrapper.querySelector(".ol-overlaycontainer-stopevent").appendChild(element[0]);
                     })
                 }
-            }
-        },
-        controller: ['$scope', 'config', function ($scope, config) {
-            $scope.collapsed = true;
-            $scope.blocateClick = function (e) {
-                //Checking target is needed because follow button is inside locate button (container) 
-                //and because of that follow button triggers click on parent element
-                if (!e.target.classList.contains('locateToggler')) return;
-                $scope.collapsed = !$scope.collapsed;
-                Geolocation.toggleFeatures(!$scope.collapsed);
-                if (Core.isMobile()) {
-                    Geolocation.toggleGps();
-                } else {
-                    Geolocation.geolocation.setTracking(true);
-                }
-            }
+            },
+            controller: ['$scope', 'config', function ($scope, config) {
+                $scope.locationService = locationService;
+                $scope.collapsed = true;
 
-            $scope.geolocationVisible = function () {
-                return (angular.isUndefined(config.locationButtonVisible)
-                    || config.locationButtonVisible) &&
-                    (angular.isUndefined(Core.puremapApp) ||
-                        Core.puremapApp === false);
-            }
-        }],
-        replace: true
-    };
-}]
+                $scope.geolocationVisible = function () {
+                    return (angular.isUndefined(config.locationButtonVisible)
+                        || config.locationButtonVisible) &&
+                        (angular.isUndefined(Core.puremapApp) ||
+                            Core.puremapApp === false);
+                }
+            }],
+
+        };
+    }]
