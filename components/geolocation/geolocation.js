e5a4c123e565321e6d748fd7d2075286c3d0d522
FilipLeitner <44566616+FilipLeitner@users.noreply.github.com>
2020-02-20 23:14:31 +0100

    Geolocation module refactoring (#515)
    * Directive
    
    * module
    
    * service
    
    * html
    
    * delete controller
    
    * Lint
    
    * Add gps icons to iconSubset

--- components/geolocation/geolocation.js
+++ components/geolocation/geolocation.js
@@ -1,31 +1,23 @@
 import geolocationDirective from './geolocation.directive';
 import geolocationService from './geolocation.service';
-import geolocationController from './geolocation.controller';
 
 /**
  * @namespace hs.geolocation
  * @memberOf hs
  */
 angular.module('hs.geolocation', ['hs.map'])
-    /**
+/**
     * @memberof hs.geolocation
     * @ngdoc directive
     * @name hs.geolocation.directive
     * @description Add geolocation tracking panel html template to map, add event listeners through link
     */
-    .directive('hs.geolocation.directive', geolocationDirective)
+  .directive('hs.geolocation.directive', geolocationDirective)
 
-    /**
+/**
     * @memberof hs.geolocation
     * @ngdoc service
     * @name hs.geolocation.service
     * @description Contains geolocation services, for mobile version through navigator.geolocation API, for classic version through OpenLayers ol.Geolocation class
     */
-    .service('hs.geolocation.service', geolocationService)
-
-    /**
-    * @memberof hs.geolocation
-    * @name hs.geolocation.controller
-    * @ngdoc controller
-    */
-    .controller('hs.geolocation.controller', geolocationController);
+  .service('hs.geolocation.service', geolocationService);
