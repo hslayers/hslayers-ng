96ede10bf74124cf577942777860b14533241072
Filip Leitner <leitnerfilip@gmail.com>
2020-02-22 15:01:30 +0100

    lint gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,34 @@
 module.exports = function (grunt) {
 
-    grunt.initConfig({
-        nggettext_extract: {
-            pot: {
-                files: {
-                    'po/template.pot': ['components/**/*.html', 'components/**/*.js']
-                }
-            }
-        },
-        nggettext_compile: {
-            all: {
-                files: {
-                    'components/translations/js/translations.js': ['po/*.po']
-                }
-            }
-        },
-        pkg: grunt.file.readJSON('package.json'),
-        jsdoc: {
-            dist: {
-                src: ['components/**/*.js'],
-                options: {
-                    destination: 'docs',
-                    configure: 'node_modules/angular-jsdoc/common/conf.json',
-                    template: 'node_modules/angular-jsdoc/angular-template',
-                    readme: './README.md'
-                }
-            }
+  grunt.initConfig({
+    nggettext_extract: {
+      pot: {
+        files: {
+          'po/template.pot': ['components/**/*.html', 'components/**/*.js']
         }
-    });
+      }
+    },
+    nggettext_compile: {
+      all: {
+        files: {
+          'components/translations/js/translations.js': ['po/*.po']
+        }
+      }
+    },
+    pkg: grunt.file.readJSON('package.json'),
+    jsdoc: {
+      dist: {
+        src: ['components/**/*.js'],
+        options: {
+          destination: 'docs',
+          configure: 'node_modules/angular-jsdoc/common/conf.json',
+          template: 'node_modules/angular-jsdoc/angular-template',
+          readme: './README.md'
+        }
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-angular-gettext');
-    grunt.loadNpmTasks('grunt-jsdoc');
+  grunt.loadNpmTasks('grunt-angular-gettext');
+  grunt.loadNpmTasks('grunt-jsdoc');
 };
