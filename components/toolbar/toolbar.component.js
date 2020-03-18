7d4e42cef11eec04d8a74f9176294f901eacd9d9
raitis <raitisbe@gmail.com>
2020-03-18 23:56:42 +0200

    Linting of toolbar component

--- components/toolbar/toolbar.component.js
+++ components/toolbar/toolbar.component.js
@@ -1,85 +1,85 @@
 export default {
-    template: require('components/toolbar/partials/toolbar.html'),
-    /**
-     * @memberof hs.toolbar
-     * @ngdoc controller
-     * @name hs.toolbar.controller
-     */
-    controller: ['$scope', 'Core', '$timeout', 'hs.layout.service',
-        function ($scope, Core, $timeout, layoutService) {
-            var collapsed = false;
+  template: require('./partials/toolbar.html'),
+  controller: ['$scope', 'Core', '$timeout', 'hs.layout.service', '$document',
+    function ($scope, Core, $timeout, layoutService, $document) {
+      let collapsed = false;
 
-            angular.extend($scope, {
-                Core: Core,
-                layoutService,
-                
-                measureButtonClicked() {
-                    layoutService.setMainPanel('measure', true);
-                },
+      angular.extend($scope, {
+        Core: Core,
+        layoutService,
 
-                /**
-                 * Change/read collapsed setting
-                 * @memberof hs.toolbar.controller
-                 * @function collapsed
-                 * @param {boolean} is
-                 */
-                collapsed(is) {
-                    if (arguments.length > 0) {
-                        collapsed = is;
-                    }
-                    return collapsed;
-                },
+        measureButtonClicked() {
+          layoutService.setMainPanel('measure', true);
+        },
 
-                /**
-                 * Test mobile mode (document width under 800px)
-                 * @memberof hs.toolbar.controller
-                 * @function isMobile
-                 */
-                isMobile() {
-                    if (document.body.innerWidth < 800) {
-                        return "mobile";
-                    } else {
-                        return "";
-                    }
-                },
+        /**
+         * Change/read collapsed setting
+         * @memberof hs.toolbar.controller
+         * @function collapsed
+         * @returns {Boolean} Collapsed state
+         * @param {boolean} is Value to set collapsed state to
+         */
+        collapsed(is) {
+          if (arguments.length > 0) {
+            collapsed = is;
+          }
+          return collapsed;
+        },
 
-                /**
-                 * True if composition is loaded
-                 * @memberof hs.toolbar.controller
-                 * @property compositionLoaded
-                 */
-                compositionLoaded() {
-                    return angular.isDefined($scope.composition_title);
-                },
+        /**
+         * Test mobile mode (document width under 800px)
+         * @memberof hs.toolbar.controller
+         * @returns {String} Returns if mobile layout should be used (document body less than 800px)
+         * @function isMobile
+         */
+        isMobile() {
+          if ($document[0].body.innerWidth < 800) {
+            return 'mobile';
+          } else {
+            return '';
+          }
+        },
 
-                /**
-                 * Dinamically generates style for placement of toolbar according 
-                 * to panel size and position
-                 * @memberof hs.toolbar.controller
-                 * @function toolbarStyle
-                 */
-                toolbarStyle() {
-                    if (!layoutService.sidebarBottom()){
-                    if (!layoutService.sidebarRight)
-                        return {
-                            marginLeft: layoutService.panelSpaceWidth() + 'px',
-                        }
-                    else
-                        return {
-                            marginRight: layoutService.panelSpaceWidth() + 'px'
-                        }
-                    }
-                }
-            })
+        /**
+         * True if composition is loaded
+         * @memberof hs.toolbar.controller
+         * @function compositionLoaded
+         * @returns {Boolean} Returns if composition_title is set and thus composition is loaded. TODO rename
+         */
+        compositionLoaded() {
+          return angular.isDefined($scope.composition_title);
+        },
+
+        /**
+         * Dinamically generates style for placement of toolbar according
+         * to panel size and position
+         * @memberof hs.toolbar.controller
+         * @function toolbarStyle
+         * @return {Object} Dinamicaly generated CSS style
+         */
+        toolbarStyle() {
+          if (!layoutService.sidebarBottom()) {
+            if (!layoutService.sidebarRight) {
+              return {
+                marginLeft: layoutService.panelSpaceWidth() + 'px'
+              };
+            } else {
+              return {
+                marginRight: layoutService.panelSpaceWidth() + 'px'
+              };
+            }
+          }
+        }
+      });
 
 
-            $scope.$on('core.map_reset', function (event) {
-                $timeout(function () {
-                    delete $scope.composition_title;
-                    delete $scope.composition_abstract;
-                })
-            });
+      $scope.$on('core.map_reset', (event) => {
+        $timeout(() => {
+          delete $scope.composition_title;
+          delete $scope.composition_abstract;
+        });
+      });
 
-            $scope.$emit('scope_loaded', "Toolbar");
-        }]
-}
+      $scope.$emit('scope_loaded', 'Toolbar');
+    }]
+};
