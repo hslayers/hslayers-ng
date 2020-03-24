0d85d8395a6367c6c7bf02c2cc9f8e0c15f5306a
Filip Leitner <leitnerfilip@gmail.com>
2020-03-24 17:14:56 +0100

    allow sublayers to be correctly created on whatever level of WMS structure

--- components/layermanager/layermanager-metadata.service.js
+++ components/layermanager/layermanager-metadata.service.js
@@ -3,6 +3,31 @@ import {WMSCapabilities, WMTSCapabilities} from 'ol/format';
 export default ['$rootScope', 'hs.wmts.getCapabilitiesService', 'hs.wfs.getCapabilitiesService', 'hs.wms.getCapabilitiesService', '$timeout', 'hs.utils.layerUtilsService',
   function ($rootScope, WMTSgetCapabilitiesService, WFSgetCapabilitiesService, WMSgetCapabilitiesService, $timeout, layerUtils) {
     const me = {};
+    /**
+   * @function identifyLayerObject
+   * @memberOf hs.layermanager.metadata.service
+   * @param {Ol.layer} layer Selected layer
+   * @description Recursive callback which identifies object representing added layer in WMS getCapabilities structure.
+   * It is used as reference for sublayer structure, metadata
+   */
+    me.identifyLayerObject = function (layerName, currentLayer) {
+      if (layerName == currentLayer.Name) {
+        return currentLayer;
+      } else {
+        for (const index in currentLayer.Layer) {
+          const node = currentLayer.Layer[index];
+          if (node.Name == layerName) {
+            return node;
+          } else {
+            const result = me.identifyLayerObject(layerName, node);
+            if (result) {
+              return result;
+            }
+          }
+        }
+        return false;
+      }
+    };
 
     /**
      * @function fillMetadata
@@ -11,12 +36,12 @@ export default ['$rootScope', 'hs.wmts.getCapabilitiesService', 'hs.wfs.getCapab
      * @description Async adds hasSublayers parameter if true
      */
     me.fillMetadata = function (layer) {
-      me.queryMetadata(layer).then(()=>{
+      me.queryMetadata(layer).then(() => {
         const subLayers = layer.get('Layer');
         if (angular.isDefined(subLayers) && subLayers.length > 0) {
           layer.hasSublayers = true;
         }
-        $timeout(()=>{
+        $timeout(() => {
           if (!$rootScope.$$phase) {
             $rootScope.$digest();
           }
@@ -32,7 +57,7 @@ export default ['$rootScope', 'hs.wmts.getCapabilitiesService', 'hs.wfs.getCapab
     me.queryMetadata = async function (layer) {
       const url = layerUtils.getURL(layer);
       const metadata = {
-        metainfo: {'OnlineResource': layer.get('Metadata')}
+        metainfo: {'OnlineResource': layer.get('Mtadata')}
       };
       //WMS
       if (layerUtils.isLayerWMS(layer)) {
@@ -40,28 +65,26 @@ export default ['$rootScope', 'hs.wmts.getCapabilitiesService', 'hs.wfs.getCapab
           .then((capabilities_xml) => {
             const parser = new WMSCapabilities();
             const caps = parser.read(capabilities_xml);
-            const layers = caps.Capability.Layer.Layer;
+            const layer_name = (layer.getSource().getParams().LAYERS);
+
+            const layerObject = me.identifyLayerObject(layer_name, caps.Capability.Layer);
             const service = {
               '0': caps.Service
             };
 
-            const layer_name = (layer.getSource().getParams().LAYERS);
-
-            angular.forEach(layers, (service_layer) => {
-              if (layer_name === service_layer.Name) {
-                layer.setProperties(service_layer);
-                if (layer.get('Copyright')) {
-                  layer.set('Attribution', {'OnlineResource': layer.get('Copyright')});
-                }
-                if (layer.get('Metadata')) {
-                  layer.set('MetadataURL', metadata);
-                  return layer;
-                }
-                if (angular.isUndefined(service_layer.MetadataURL)) {
-                  layer.set('MetadataURL', service);
-                }
+            if (layerObject) {
+              layer.setProperties(layerObject);
+              if (layer.get('Copyright')) {
+                layer.set('Attribution', {'OnlineResource': layer.get('Cpyright')});
               }
-            });
+              if (layer.get('Metadata')) {
+                layer.set('MetadataURL', metadata);
+                return layer;
+              }
+              if (angular.isUndefined(layerObject.MetadataURL)) {
+                layer.set('MetadataURL', service);
+              }
+            }
 
             return true;
           })
@@ -70,7 +93,6 @@ export default ['$rootScope', 'hs.wmts.getCapabilitiesService', 'hs.wfs.getCapab
             return e;
           });
         return capabilities;
-
       }
       //WMTS
       else if (layerUtils.isLayerWMTS(layer)) {
@@ -80,9 +102,9 @@ export default ['$rootScope', 'hs.wmts.getCapabilitiesService', 'hs.wfs.getCapab
             const caps = parser.read(capabilities_xml.data);
             layer.setProperties(caps);
             if (layer.get('Copyright')) {
-              layer.set('Attribution', {'OnlineResource': layer.get('Copyright')});
+              layer.set('Attribution', {'OnlineResource': layer.get('Cpyright')});
             } else {
-              layer.set('Attribution', {'OnlineResource': caps.ServiceProvider.ProviderSite});
+              layer.set('Attribution', {'OnlineResource': caps.Servicerovider.ProviderSite});
             }
             if (layer.get('Metadata')) {
               layer.set('MetadataURL', metadata);
@@ -103,9 +125,9 @@ export default ['$rootScope', 'hs.wmts.getCapabilitiesService', 'hs.wfs.getCapab
               const caps = parser.parseFromString(capabilities_xml.data, 'application/xml');
               const el = caps.getElementsByTagNameNS('*', 'ProviderSite');
               if (layer.get('Copyright')) {
-                layer.set('Attribution', {'OnlineResource': layer.get('Copyright')});
+                layer.set('Attribution', {'OnlineResource': layer.get('Cpyright')});
               } else {
-                layer.set('Attribution', {'OnlineResource': el[0].getAttribute('xlink:href')});
+                layer.set('Attribution', {'OnlineResource': el[0].getAttibute('xlink:href')});
               }
               return true;
 
