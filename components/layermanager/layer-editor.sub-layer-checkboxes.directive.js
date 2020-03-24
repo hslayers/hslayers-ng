db807d1a93dfeb370ff71c924706e097ef6e9f8a
Filip Leitner <leitnerfilip@gmail.com>
2020-03-24 17:17:31 +0100

    Lint checkboxes directive

--- components/layermanager/layer-editor.sub-layer-checkboxes.directive.js
+++ components/layermanager/layer-editor.sub-layer-checkboxes.directive.js
@@ -1,35 +1,35 @@
 
-    export default ['hs.layerEditor.sublayerService','hs.layermanager.service',
-    function(subLayerService, LayMan){
-        return {
-                template: require('./partials/sub-layer-checkboxes.html'),
-    
-            controller: ['$scope', function ($scope) {
-                
-                $scope.checkedSubLayers = subLayerService.checkedSubLayers;
-                $scope.withChildren = subLayerService.withChildren;
-                $scope.expanded = false;
+export default ['hs.layerEditor.sublayerService', 'hs.layermanager.service',
+  function(subLayerService, LayMan) {
+    return {
+      template: require('./partials/sub-layer-checkboxes.html'),
 
-                $scope.getSubLayers = function (){
-                   return subLayerService.getSubLayers();
-                }
+      controller: ['$scope', function ($scope) {
 
-                $scope.subLayerIsString= function(subLayer){
-                    return typeof subLayer == 'string'
-                };
+        $scope.checkedSubLayers = subLayerService.checkedSubLayers;
+        $scope.withChildren = subLayerService.withChildren;
+        $scope.expanded = false;
 
-                $scope.toggleExpanded = function(){
-                        $scope.expanded = !$scope.expanded
-                };
-                $scope.subLayerSelected = function (sublayer,state){
-                    if (angular.isDefined(sublayer) && sublayer.Layer){
-                        angular.forEach(sublayer.Layer,function(children){
-                            angular.extend($scope.checkedSubLayers, {[children.Name]:state});
-                        })
-                    }
-                    return subLayerService.subLayerSelected();
-                };
-
-            }]
+        $scope.getSubLayers = function () {
+          return subLayerService.getSubLayers();
         };
-    }]
+
+        $scope.subLayerIsString = function(subLayer) {
+          return typeof subLayer == 'string';
+        };
+
+        $scope.toggleExpanded = function() {
+          $scope.expanded = !$scope.expanded;
+        };
+        $scope.subLayerSelected = function (sublayer, state) {
+          if (angular.isDefined(sublayer) && sublayer.Layer) {
+            angular.forEach(sublayer.Layer, (children) => {
+              angular.extend($scope.checkedSubLayers, {[children.Name]: state});
+            });
+          }
+          return subLayerService.subLayerSelected();
+        };
+
+      }]
+    };
+  }];
