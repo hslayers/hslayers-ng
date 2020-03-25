1612e4b02c1ca66ac89be1f67ce43add9d449bbb
Filip Leitner <leitnerfilip@gmail.com>
2020-03-25 19:31:09 +0100

    Lint layereditor vector-layer service

--- components/layermanager/layer-editor-vector-layer.service.js
+++ components/layermanager/layer-editor-vector-layer.service.js
@@ -1,123 +1,125 @@
-import { Stroke, Fill, Circle, RegularShape, Text, Style } from 'ol/style';
+import {Stroke, Fill, Circle, RegularShape, Text, Style} from 'ol/style';
 import VectorLayer from 'ol/layer/Vector';
-import { Cluster, Vector as VectorSource } from 'ol/source';
-import layerEditorSubLayerCheckboxesDirective from './layer-editor.sub-layer-checkboxes.directive';
-import { Point } from 'ol/geom';
+import {Cluster, Vector as VectorSource} from 'ol/source';
+import {Point} from 'ol/geom';
 
 export default ['hs.map.service', function (hsMap) {
-    let me = {};
-    /**
-     * @function Declutter
-    * @memberOf hs.layerEditor.service
-    * @description Set declutter of features;
-     */
-    me.declutter = function (newValue, layer) {
-        let index = hsMap.map.getLayers().getArray().indexOf(layer);
-        if (newValue == true && !layer.get('cluster')) {
-            hsMap.map.removeLayer(layer);
-            hsMap.map.getLayers().insertAt(index,
-                me.cloneVectorLayer(layer, newValue)
-            );
-        } else {
-            hsMap.map.removeLayer(layer);
-            hsMap.map.getLayers().insertAt(index,
-                me.cloneVectorLayer(layer, false));
-        }
+  const me = {};
+  /**
+ * @function Declutter
+* @memberOf hs.layerEditor.service
+* @description Set declutter of features;
+    */
+  me.declutter = function (newValue, layer) {
+    const index = hsMap.map.getLayers().getArray().indexOf(layer);
+    if (newValue == true && !layer.get('cluster')) {
+      hsMap.map.removeLayer(layer);
+      hsMap.map.getLayers().insertAt(index,
+        me.cloneVectorLayer(layer, newValue)
+      );
+    } else {
+      hsMap.map.removeLayer(layer);
+      hsMap.map.getLayers().insertAt(index,
+        me.cloneVectorLayer(layer, false));
     }
-    me.cloneVectorLayer = function (layer, declutter) {
-        let options = {};
-        layer.getKeys().forEach(k => options[k] = layer.get(k));
-        angular.extend(options, {
-            declutter,
-            source: layer.getSource(),
-            style: layer.getStyleFunction() || layer.getStyle(),
-            maxResolution: layer.getMaxResolution(),
-            minResolution: layer.getMinResolution(),
-            visible: layer.getVisible(),
-            opacity: layer.getOpacity()
-        });
-        return new VectorLayer(options)
-    }
-    /**
+  };
+  me.cloneVectorLayer = function (layer, declutter) {
+    const options = {};
+    layer.getKeys().forEach(k => options[k] = layer.get(k));
+    angular.extend(options, {
+      declutter,
+      source: layer.getSource(),
+      style: layer.getStyleFunction() || layer.getStyle(),
+      maxResolution: layer.getMaxResolution(),
+      minResolution: layer.getMinResolution(),
+      visible: layer.getVisible(),
+      opacity: layer.getOpacity()
+    });
+    return new VectorLayer(options);
+  };
+  /**
   * @function cluster
   * @memberOf hs.layerEditor.service
   * @description Set cluster for layer;
   */
-    me.cluster = function (newValue, layer, distance) {
-        if (!layer.hsOriginalStyle) {
-            layer.hsOriginalStyle = layer.getStyle();
-        }
-        if (newValue == true && !layer.get('declutter')) {
-            var styleCache = {};
-            layer.setSource(me.createClusteredSource(layer, distance));
-            layer.setStyle(function (feature, resolution) {
-                var size = feature.get('features').length;
-                if (size > 1) {
-                    var textStyle = styleCache[size];
-                    if (!textStyle) {
-                        textStyle = new Style({
-                            image: new Circle({
-                                radius: 10,
-                                stroke: new Stroke({
-                                    color: '#fff'
-                                }),
-                                fill: new Fill({
-                                    color: '#3399CC'
-                                })
-                            }),
-                            text: new Text({
-                                text: size.toString(),
-                                fill: new Fill({
-                                    color: '#000'
-                                })
-                            })
-                        });
-                        styleCache[size] = textStyle;
-                    }
-                    return textStyle;
-                } else {
-                    let tmp;
-                    if (typeof layer.hsOriginalStyle == 'function')
-                        tmp = layer.hsOriginalStyle(feature, resolution)
-                    else 
-                        tmp = layer.hsOriginalStyle;
-                    var originalFeature = feature.get('features');
-                    if(tmp.length)
-                        tmp[0].setGeometry(originalFeature[0].getGeometry())
-                    else
-                        tmp.setGeometry(originalFeature[0].getGeometry())
-                    return tmp;
-                }
-
-            })
-        } else {
-            layer.setStyle(function () {
-                if (typeof layer.hsOriginalStyle == 'function')
-                    return layer.hsOriginalStyle();
-                else
-                    return layer.hsOriginalStyle;
+  me.cluster = function (newValue, layer, distance) {
+    if (!layer.hsOriginalStyle) {
+      layer.hsOriginalStyle = layer.getStyle();
+    }
+    if (newValue == true && !layer.get('declutter')) {
+      const styleCache = {};
+      layer.setSource(me.createClusteredSource(layer, distance));
+      layer.setStyle((feature, resolution) => {
+        const size = feature.get('features').length;
+        if (size > 1) {
+          let textStyle = styleCache[size];
+          if (!textStyle) {
+            textStyle = new Style({
+              image: new Circle({
+                radius: 10,
+                stroke: new Stroke({
+                  color: '#fff'
+                }),
+                fill: new Fill({
+                  color: '#3399CC'
+                })
+              }),
+              text: new Text({
+                text: size.toString(),
+                fill: new Fill({
+                  color: '#000'
+                })
+              })
             });
-            layer.setSource(layer.getSource().getSource())
+            styleCache[size] = textStyle;
+          }
+          return textStyle;
+        } else {
+          let tmp;
+          if (typeof layer.hsOriginalStyle == 'function') {
+            tmp = layer.hsOriginalStyle(feature, resolution);
+          } else {
+            tmp = layer.hsOriginalStyle;
+          }
+          const originalFeature = feature.get('features');
+          if (tmp.length) {
+            tmp[0].setGeometry(originalFeature[0].getGeometry());
+          } else {
+            tmp.setGeometry(originalFeature[0].getGeometry());
+          }
+          return tmp;
         }
+
+      });
+    } else {
+      layer.setStyle(() => {
+        if (typeof layer.hsOriginalStyle == 'function') {
+          return layer.hsOriginalStyle();
+        } else {
+          return layer.hsOriginalStyle;
+        }
+      });
+      layer.setSource(layer.getSource().getSource());
     }
-    me.createClusteredSource = function (layer, distance) {
-        return new Cluster({
-            distance: distance,
-            source: layer.getSource(),
-            geometryFunction: function (feature) {
-                switch(feature.getGeometry().getType()){
-                    case 'Point':
-                        return feature.getGeometry()
-                    case 'Polygon':
-                        return feature.getGeometry().getInteriorPoint()
-                    case 'LineString':
-                        return new Point(feature.getGeometry().getFirstCoordinate())
-                    default:        
-                        return null
-                }
-            }
-        });
-    }
-    return me;
+  };
+  me.createClusteredSource = function (layer, distance) {
+    return new Cluster({
+      distance: distance,
+      source: layer.getSource(),
+      geometryFunction: function (feature) {
+        switch (feature.getGeometry().getType()) {
+          case 'Point':
+            return feature.getGeometry();
+          case 'Polygon':
+            return feature.getGeometry().getInteriorPoint();
+          case 'LineString':
+            return new Point(feature.getGeometry().getFirstCoordinate());
+          default:
+            return null;
+        }
+      }
+    });
+  };
+  return me;
 }
-]
+];
