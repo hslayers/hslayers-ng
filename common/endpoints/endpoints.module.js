ab9e8874122aa21c50493087dec0f20375499736
raitis <raitisbe@gmail.com>
2020-04-21 13:01:21 +0300

    Import angular in endpoints module

--- common/endpoints/endpoints.module.js
+++ common/endpoints/endpoints.module.js
@@ -1,11 +1,13 @@
+import angular from 'angular';
 import endpointsService from './endpoints.service';
 
-angular.module('hs.common.endpoints', [])
+angular
+  .module('hs.common.endpoints', [])
 
   /**
-  * @name hs.common.endpointsService
-  * @ngdoc service
-  * @memberOf hs.common.endpoints
-  * @description Service for endpoints
-  */
+   * @name hs.common.endpointsService
+   * @ngdoc service
+   * @memberOf hs.common.endpoints
+   * @description Service for endpoints
+   */
   .factory('hs.common.endpointsService', endpointsService);
