5000c5188189e378f46fc421607213f76c77d991
raitis <raitisbe@gmail.com>
2020-04-29 03:54:58 +0300

    Improve karma test config

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,28 +1,34 @@
-
-var webpackCOnf = require('../scripts/webpack.dev.js');
-var webpack = require('webpack');
+const webpackCOnf = require('../scripts/webpack.dev.js');
+const webpack = require('webpack');
 const process = require('process');
-process.env.CHROME_BIN = require('puppeteer').executablePath()
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 
-module.exports = function(config) {
-    config.set({
-        browsers:   ['ChromeHeadless'],
-        frameworks: ['jasmine'],
-        reporters:  ['mocha'],
+module.exports = function (config) {
+  config.set({
+    browsers: ['ChromeHeadless'],
+    frameworks: ['jasmine'],
+    reporters: ['mocha'],
+    client: {captureConsole: true},
+    browserConsoleLogOptions: {
+      level: 'log',
+      format: '%b %T: %m',
+      terminal: true,
+    },
+    logLevel: config.LOG_INFO,
+    autoWatch:
+      typeof process.env.watch == 'undefined' ? false : process.env.watch,
+    singleRun:
+      typeof process.env.watch == 'undefined' ? true : !process.env.watch,
+    colors: true,
+    port: 9876,
 
-        logLevel: config.LOG_INFO,
-        autoWatch: typeof process.env.watch == 'undefined' ? false :  process.env.watch,
-        singleRun: typeof process.env.watch == 'undefined' ? true : !process.env.watch,
-        colors: true,
-        port: 9876,
-
-        basePath: '',
-        files: ['webpack.karma.context.js'],
-        preprocessors: { 'webpack.karma.context.js': ['webpack', 'sourcemap'] },
-        exclude: [],
-        webpack: webpackCOnf,
-        webpackMiddleware: {
-            noInfo: true
-        }
-    });
+    basePath: '',
+    files: ['webpack.karma.context.js'],
+    preprocessors: {'webpack.karma.context.js': ['webpack', 'sourcemap']},
+    exclude: [],
+    webpack: webpackCOnf,
+    webpackMiddleware: {
+      noInfo: true,
+    },
+  });
 };
