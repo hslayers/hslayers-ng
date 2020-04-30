63fcf5a303463a6c24c152d24038459d4bf045d9
Filip Leitner <leitnerfilip@gmail.com>
2020-04-30 16:01:11 +0200

    change approach for sublayer checkboxes

--- components/layermanager/layer-editor.sub-layer-checkboxes.directive.js
+++ components/layermanager/layer-editor.sub-layer-checkboxes.directive.js
@@ -23,13 +23,26 @@ export default [
           $scope.toggleExpanded = function () {
             $scope.expanded = !$scope.expanded;
           };
+          /**
+           * @function toggleSublayersVisibility
+           * @memberOf hs.layermanager.layer-editor.sub-layer-checkboes
+           * @description Controls state of layer´s sublayers manipulated by input checkboxes
+           * @param {object} sublayer Selected sublayer
+           * @param {object} state New state of sublayer
+           */
           $scope.subLayerSelected = function (sublayer, state) {
             if (angular.isDefined(sublayer) && sublayer.Layer) {
               angular.forEach(sublayer.Layer, (children) => {
                 angular.extend($scope.checkedSubLayers, {
                   [children.Name]: state,
                 });
+                subLayerService.checkedSubLayersTmp[children.Name] = state;
               });
+            }
+            if (angular.isDefined($scope.checkedSubLayers[sublayer.Name])) {
+              subLayerService.checkedSubLayersTmp[sublayer.Name] = state;
+            } else {
+              subLayerService.withChildrenTmp[sublayer.Name] = state;
             }
             return subLayerService.subLayerSelected();
           };
