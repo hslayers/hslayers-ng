abafff75f5b25483a39732232f2e1973f585b993
raitis <raitisbe@gmail.com>
2020-05-21 13:32:23 +0300

    Fix warning when isolating bootstrap

--- scripts/bootstrap-isolate.js
+++ scripts/bootstrap-isolate.js
@@ -1,16 +1,24 @@
-var fs = require('fs');
-var postcss = require('postcss');
-var prefixer = require('postcss-prefix-selector');
+const fs = require('fs');
+const postcss = require('postcss');
+const prefixer = require('postcss-prefix-selector');
 
-var css = fs.readFileSync('node_modules/bootstrap/dist/css/bootstrap.css', 'utf8').toString();
+const css = fs
+  .readFileSync('node_modules/bootstrap/dist/css/bootstrap.css', 'utf8')
+  .toString();
 
-var out = postcss()
-    .use(prefixer({
-        prefix: '.bsi'
-    }))
-    .process(css).then(function (result) {
-        fs.writeFileSync('node_modules/bootstrap/dist/css/bootstrap.isolated.css', result.css);
-        console.log('bootstrap.isolated.css file created in node_modules/bootstrap/dist/css directory');
+const out = postcss()
+  .use(
+    prefixer({
+      prefix: '.bsi',
     })
-
-
+  )
+  .process(css, {from: undefined})
+  .then((result) => {
+    fs.writeFileSync(
+      'node_modules/bootstrap/dist/css/bootstrap.isolated.css',
+      result.css
+    );
+    console.log(
+      'bootstrap.isolated.css file created in node_modules/bootstrap/dist/css directory'
+    );
+  });
