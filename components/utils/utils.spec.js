2b87abc6588824144f6321b242ffb2d5ebb41864
jmacura <macurajan@gmail.com>
2020-05-23 23:45:22 +0200

    Improve removeDuplicates util
    Improve HsUtils.removeDuplicates()
    so it can also simplify arrays with deeper object structure
    Add more test cases for the new functionality

--- components/utils/utils.spec.js
+++ components/utils/utils.spec.js
@@ -15,19 +15,35 @@ describe('utils', () => {
     hsUtils = $injector.get('HsUtilsService');
   }));
 
-  it('removes duplicates from an array', () => {
+  it('remove duplicates from a shallow array', () => {
     const layers = [
       {title: 'villages', features: 10},
       {title: 'villages', features: 10},
+      {title: 'cities', features: 50},
       {title: 'villages', features: 100},
       {title: 'cities', features: 5},
-      {title: 'cities', features: 50},
     ];
     const unique = hsUtils.removeDuplicates(layers, 'title');
     expect(unique.length).toBe(2);
     expect(unique).toEqual([
       {title: 'villages', features: 10},
-      {title: 'cities', features: 5},
+      {title: 'cities', features: 50},
+    ]);
+  });
+
+  it('remove duplicates from a deep array', () => {
+    const layers = [
+      {values: {properties: {title: 'villages', features: 10}}},
+      {values: {properties: {title: 'villages', features: 10}}},
+      {values: {properties: {title: 'cities', features: 50}}},
+      {values: {properties: {title: 'villages', features: 100}}},
+      {values: {properties: {title: 'cities', features: 5}}},
+    ];
+    const unique = hsUtils.removeDuplicates(layers, 'values.properties.title');
+    expect(unique.length).toBe(2);
+    expect(unique).toEqual([
+      {values: {properties: {title: 'villages', features: 10}}},
+      {values: {properties: {title: 'cities', features: 50}}},
     ]);
   });
 });
