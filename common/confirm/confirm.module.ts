d7e65c227d7433543dcbebdc6603b28d642c1127
raitis <raitisbe@gmail.com>
2020-06-09 01:21:29 +0300

    Try rewrite confirm dialog module to Ng9 refs #725

---
+++ common/confirm/confirm.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HsConfirmDialogComponent } from './confirm-dialog.component';
+import { HsConfirmDialog } from './confirm-dialog.service';
+
+@NgModule({
+  declarations: [
+    HsConfirmDialogComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [HsConfirmDialog]
+})
+export class HsConfirmModule { }
