6d895f6dd7a9a7b46272135668d25ca2eef5afa8
raitis <raitisbe@gmail.com>
2020-06-15 17:22:43 +0300

    Initial HsCore serwice rewrite

--- components/core/core.module.ts
+++ components/core/core.module.ts
@@ -1,26 +1,30 @@
-import '../drag/drag.module';
-import '../map/map.module';
-import '../utils/utils.module';
-import 'angular-gettext';
-import * as angular from "angular";
-import '../layout';
-import '../legend';
-import '../print';
-import '../translations/js/translations';
-import coreService from './core.service';
-/**
- * @namespace hs
- * @ngdoc module
- * @module hs.core
- * @name hs.core
- * @description HsCore module for whole HSLayers-NG. HsCore module consists of HsCore service which keeps some app-level settings and mantain app size and panel statuses. TODO
- */
-angular
-  .module('hs.core', ['hs.map', 'gettext', 'hs.drag', 'hs.layout', 'hs.utils', 'hs.legend', 'hs.print'])
   /**
-   * @module hs.core
-   * @name HsCore
-   * @ngdoc service
-   * @description HsCore service of HSL. TODO expand the description
-   */
-  .factory('HsCore', coreService);
+ * @namespace hs.legend
+ * @memberOf hs
+ */
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from '@angular/platform-browser';
+import { HsCoreService } from './core.service';
+import { HsLogService } from './log.service';
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule
+  ],
+  exports: [
+  ],
+  providers: [HsCoreService,
+    {
+      provide: Window, useValue: window
+    },
+    HsLogService],
+  entryComponents: [
+  ]
+})
+export class HsCoreModule {
+}
+
+
