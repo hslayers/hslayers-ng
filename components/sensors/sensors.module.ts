e14a2943f1c18c2c353695f0c692b6c82e3d9bf8
raitis <raitisbe@gmail.com>
2020-07-06 03:13:42 +0300

    Fix minor issues with sensor dialog window refs #790

--- components/sensors/sensors.module.ts
+++ components/sensors/sensors.module.ts
@@ -3,7 +3,7 @@
  * @memberOf hs
  */
 import {BrowserModule} from '@angular/platform-browser';
-import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HsPanelHelpersModule} from '../layout/panel-helpers.module';
 import {HsSensorsComponent} from './sensors.component';
@@ -11,20 +11,21 @@ import {HsSensorsService} from './sensors.service';
 import {HsSensorsUnitDialogComponent} from './sensors-unit-dialog.component';
 import {HsSensorsUnitListItemComponent} from './sensors-unit-list-item.component';
 import { HsSensorsUnitDialogService } from './unit-dialog.service';
+import { FormsModule } from '@angular/forms';
 @NgModule({
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
   declarations: [
     HsSensorsComponent,
     HsSensorsUnitDialogComponent,
     HsSensorsUnitListItemComponent,
   ],
-  imports: [CommonModule, BrowserModule, HsPanelHelpersModule],
+  imports: [CommonModule, BrowserModule, HsPanelHelpersModule, FormsModule],
   exports: [
     HsSensorsComponent,
     HsSensorsUnitDialogComponent,
     HsSensorsUnitListItemComponent,
   ],
   providers: [HsSensorsService, HsSensorsUnitDialogService],
-  entryComponents: [HsSensorsComponent],
+  entryComponents: [HsSensorsComponent, HsSensorsUnitDialogComponent],
 })
 export class HsSensorsModule {}
