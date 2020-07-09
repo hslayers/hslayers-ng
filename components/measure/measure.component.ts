23ec618c871e0884564d544b1b1a4ec962e57975
jmacura <macurajan@gmail.com>
2020-07-09 22:20:30 +0200

    Upgrade measure component to ng9

--- components/measure/measure.component.ts
+++ components/measure/measure.component.ts
@@ -1,80 +1,86 @@
-export const HsMeasureComponent = {
-  template: (HsConfig) => {
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'hs-measure',
+  template: require('./partials/measure.html'),
+})
+export class HsMeasureComponent {
+  /*if (HsConfig.design == 'md') {
+    return require('./partials/measuremd.html');
+  }*/
+  type = 'distance';
+
+  constructor(
+    $scope,
+    private HsLayoutService,
+    private HsMeasureService,
+    $timeout
+  ) {
     'ngInject';
-    if (HsConfig.design == 'md') {
-      return require('./partials/measuremd.html');
-    } else {
-      return require('./partials/measure.html');
-    }
-  },
-  controller: function ($scope, HsLayoutService, HsMeasureService, $timeout) {
-    'ngInject';
-    $scope.data = HsMeasureService.data;
+    $scope.data = this.HsMeasureService.data;
 
     document.addEventListener('keyup', (e) => {
       if (e.keyCode == 17) {
         //ControlLeft
         $timeout(() => {
-          HsMeasureService.switchMultipleMode();
+          this.HsMeasureService.switchMultipleMode();
         }, 0);
       }
     });
 
     $scope.$on('measure.drawStart', () => {
-      HsLayoutService.panelEnabled('toolbar', false);
+      this.HsLayoutService.panelEnabled('toolbar', false);
     });
 
     $scope.$on('measure.drawEnd', () => {
-      HsLayoutService.panelEnabled('toolbar', true);
+      this.HsLayoutService.panelEnabled('toolbar', true);
     });
 
-    $scope.type = 'distance';
-
-    /**
-     * @memberof hs.measure.controller
-     * @function setType
-     * @public
-     * @param {string} type type of measure to use, should be "area" or "distance"
-     * @returns {object} area of polygon with used units
-     * @description Set type of current measurment
-     */
-    $scope.setType = function (type) {
-      $scope.type = type;
-      HsMeasureService.switchMeasureType(type);
-    };
-
-    /**
-     * @memberof hs.measure.controller
-     * @function clearAll
-     * @public
-     * @param {string} type type of measure to use, should be "area" or "distance"
-     * @returns {object} area of polygon with used units
-     * @description Reset sketch and all measurements to start new drawing
-     */
-    $scope.clearAll = function () {
-      HsMeasureService.clearMeasurement();
-    };
-
     $scope.$watch('type', () => {
-      if (HsLayoutService.mainpanel != 'measure') {
+      if (this.HsLayoutService.mainpanel != 'measure') {
         return;
       }
-      HsMeasureService.changeMeasureParams($scope.type);
+      this.HsMeasureService.changeMeasureParams($scope.type);
     });
 
     $scope.$on('core.mainpanel_changed', (event) => {
       if (HsLayoutService.mainpanel == 'measure') {
-        HsMeasureService.activateMeasuring($scope.type);
+        this.HsMeasureService.activateMeasuring($scope.type);
       } else {
-        HsMeasureService.deactivateMeasuring();
+        this.HsMeasureService.deactivateMeasuring();
       }
     });
 
     //Temporary fix when measure panel is loaded as deafult (e.g. reloading page with parameters in link)
-    if (HsLayoutService.mainpanel == 'measure') {
-      HsMeasureService.activateMeasuring($scope.type);
+    if (this.HsLayoutService.mainpanel == 'measure') {
+      this.HsMeasureService.activateMeasuring($scope.type);
     }
 
     $scope.$emit('scope_loaded', 'Measure');
-  },
-};
+  }
+
+  /**
+   * @memberof hs.measure.controller
+   * @function setType
+   * @public
+   * @param {string} type type of measure to use, should be "area" or "distance"
+   * @returns {object} area of polygon with used units
+   * @description Set type of current measurment
+   */
+  setType(type) {
+    this.type = type;
+    this.HsMeasureService.switchMeasureType(type);
+  }
+
+  /**
+   * @memberof hs.measure.controller
+   * @function clearAll
+   * @public
+   * @param {string} type type of measure to use, should be "area" or "distance"
+   * @returns {object} area of polygon with used units
+   * @description Reset sketch and all measurements to start new drawing
+   */
+  clearAll() {
+    this.HsMeasureService.clearMeasurement();
+  }
+}
