52f78e3b53f5809ae414877bd40e03fde461c11e
jmacura <macurajan@gmail.com>
2020-07-09 23:28:39 +0200

    Lint ajs-upgraded-providers

--- ajs-upgraded-providers.ts
+++ ajs-upgraded-providers.ts
@@ -1,132 +1,132 @@
-import { HsMapService } from './components/map/map.service';
-import { HsConfig } from './config.service';
-import { HsLayoutService } from './components/layout/layout.service';
-import { HsUtilsService } from './components/utils/utils.service';
-import { HsLayerUtilsService } from './components/utils/layer-utils.service';
-import { HsDrawService } from './components/draw/draw.service.js';
-import { HsStylerService } from './components/styles/styler.service';
-import { HsLayerSynchronizerService } from './components/save-map/layer-synchronizer.service.js';
-import { HsWmsGetCapabilitiesService } from './common/wms/get-capabilities.service.js';
-import { HsWfsGetCapabilitiesService } from './common/wfs/get-capabilities.service.js';
-import { HsWmtsGetCapabilitiesService } from './common/wmts/get-capabilities.service.js';
-import { HsDimensionService } from './common/dimension.service.js';
+import {HsConfig} from './config.service';
+import {HsDimensionService} from './common/dimension.service';
+import {HsDrawService} from './components/draw/draw.service';
+import {HsLayerSynchronizerService} from './components/save-map/layer-synchronizer.service';
+import {HsLayerUtilsService} from './components/utils/layer-utils.service';
+import {HsLayoutService} from './components/layout/layout.service';
+import {HsMapService} from './components/map/map.service';
+import {HsStylerService} from './components/styles/styler.service';
+import {HsUtilsService} from './components/utils/utils.service';
+import {HsWfsGetCapabilitiesService} from './common/wfs/get-capabilities.service';
+import {HsWmsGetCapabilitiesService} from './common/wms/get-capabilities.service';
+import {HsWmtsGetCapabilitiesService} from './common/wmts/get-capabilities.service';
 
 export function hsMapServiceFactory(i: any) {
-    return i.get('HsMapService');
+  return i.get('HsMapService');
 }
 
 export const HsMapServiceProvider = {
-    provide: HsMapService,
-    useFactory: hsMapServiceFactory,
-    deps: ['$injector']
+  provide: HsMapService,
+  useFactory: hsMapServiceFactory,
+  deps: ['$injector'],
 };
 
 export function hsConfigFactory(i: any) {
-    return i.get('HsConfig');
+  return i.get('HsConfig');
 }
 
 export const HsConfigProvider = {
-    provide: HsConfig,
-    useFactory: hsConfigFactory,
-    deps: ['$injector']
+  provide: HsConfig,
+  useFactory: hsConfigFactory,
+  deps: ['$injector'],
 };
 
 export function hsLayoutServiceFactory(i: any) {
-    return i.get('HsLayoutService');
+  return i.get('HsLayoutService');
 }
 
 export const HsLayoutServiceProvider = {
-    provide: HsLayoutService,
-    useFactory: hsLayoutServiceFactory,
-    deps: ['$injector']
+  provide: HsLayoutService,
+  useFactory: hsLayoutServiceFactory,
+  deps: ['$injector'],
 };
 
 export function hsUtilsServiceFactory(i: any) {
-    return i.get('HsUtilsService');
+  return i.get('HsUtilsService');
 }
 
 export const HsUtilsServiceProvider = {
-    provide: HsUtilsService,
-    useFactory: hsUtilsServiceFactory,
-    deps: ['$injector']
+  provide: HsUtilsService,
+  useFactory: hsUtilsServiceFactory,
+  deps: ['$injector'],
 };
 
 export function hsDrawServiceFactory(i: any) {
-    return i.get('HsDrawService');
+  return i.get('HsDrawService');
 }
 
 export const HsDrawServiceProvider = {
-    provide: HsDrawService,
-    useFactory: hsDrawServiceFactory,
-    deps: ['$injector']
+  provide: HsDrawService,
+  useFactory: hsDrawServiceFactory,
+  deps: ['$injector'],
 };
 
 export function hsStylerServiceFactory(i: any) {
-    return i.get('HsStylerService');
+  return i.get('HsStylerService');
 }
 
 export const HsStylerServiceProvider = {
-    provide: HsStylerService,
-    useFactory: hsStylerServiceFactory,
-    deps: ['$injector']
+  provide: HsStylerService,
+  useFactory: hsStylerServiceFactory,
+  deps: ['$injector'],
 };
 
 export function hsLayerSynchronizerServiceFactory(i: any) {
-    return i.get('HsLayerSynchronizerService');
+  return i.get('HsLayerSynchronizerService');
 }
 
 export const HsLayerSynchronizerServiceProvider = {
-    provide: HsLayerSynchronizerService,
-    useFactory: hsLayerSynchronizerServiceFactory,
-    deps: ['$injector']
+  provide: HsLayerSynchronizerService,
+  useFactory: hsLayerSynchronizerServiceFactory,
+  deps: ['$injector'],
 };
 
 export function hsWmsGetCapabilitiesService(i: any) {
-    return i.get('HsWmsGetCapabilitiesService');
+  return i.get('HsWmsGetCapabilitiesService');
 }
 
 export const HsWmsGetCapabilitiesServiceProvider = {
-    provide: HsWmsGetCapabilitiesService,
-    useFactory: hsWmsGetCapabilitiesService,
-    deps: ['$injector']
+  provide: HsWmsGetCapabilitiesService,
+  useFactory: hsWmsGetCapabilitiesService,
+  deps: ['$injector'],
 };
 
 export function hsWfsGetCapabilitiesService(i: any) {
-    return i.get('HsWfsGetCapabilitiesService');
+  return i.get('HsWfsGetCapabilitiesService');
 }
 
 export const HsWfsGetCapabilitiesServiceProvider = {
-    provide: HsWfsGetCapabilitiesService,
-    useFactory: hsWfsGetCapabilitiesService,
-    deps: ['$injector']
+  provide: HsWfsGetCapabilitiesService,
+  useFactory: hsWfsGetCapabilitiesService,
+  deps: ['$injector'],
 };
 
 export function hsWmtsGetCapabilitiesService(i: any) {
-    return i.get('HsWmtsGetCapabilitiesService');
+  return i.get('HsWmtsGetCapabilitiesService');
 }
 
 export const HsWmtsGetCapabilitiesServiceProvider = {
-    provide: HsWmtsGetCapabilitiesService,
-    useFactory: hsWmtsGetCapabilitiesService,
-    deps: ['$injector']
+  provide: HsWmtsGetCapabilitiesService,
+  useFactory: hsWmtsGetCapabilitiesService,
+  deps: ['$injector'],
 };
 
 export function hsDimensionService(i: any) {
-    return i.get('HsDimensionService');
+  return i.get('HsDimensionService');
 }
 
 export const HsDimensionServiceProvider = {
-    provide: HsDimensionService,
-    useFactory: hsDimensionService,
-    deps: ['$injector']
+  provide: HsDimensionService,
+  useFactory: hsDimensionService,
+  deps: ['$injector'],
 };
 
 export function hsLayerUtilsServiceProvider(i: any) {
-    return i.get('HsLayerUtilsService');
+  return i.get('HsLayerUtilsService');
 }
 
 export const HsLayerUtilsServiceProvider = {
-    provide: HsLayerUtilsService,
-    useFactory: hsLayerUtilsServiceProvider,
-    deps: ['$injector']
+  provide: HsLayerUtilsService,
+  useFactory: hsLayerUtilsServiceProvider,
+  deps: ['$injector'],
 };
