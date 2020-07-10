354e78d89e266d733cb1fc2a5df6bee46e5bfef6
jmacura <macurajan@gmail.com>
2020-07-10 18:52:53 +0200

    Fix measure component display

--- components/measure/measure.module.ts
+++ components/measure/measure.module.ts
@@ -8,12 +8,19 @@ import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {HsMeasureComponent} from './measure.component';
 import {HsMeasureService} from './measure.service';
+import {HsPanelHelpersModule} from '../layout/panel-helpers.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
   declarations: [HsMeasureComponent],
-  imports: [CommonModule, BrowserModule, FormsModule, NgbModule],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    HsPanelHelpersModule,
+    NgbModule,
+  ],
   exports: [HsMeasureComponent],
   providers: [HsMeasureService],
   entryComponents: [HsMeasureComponent],
