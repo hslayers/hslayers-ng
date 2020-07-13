0552509a1228ef6806a85a3cde12fb96326a8602
SLeitgeb <leitgeb.simon@gmail.com>
2020-07-13 14:20:36 +0200

    Modify and polish MD layout

--- components/feature-filter/feature-list.directive.js
+++ components/feature-filter/feature-list.directive.js
@@ -4,6 +4,7 @@
 export default function (HsConfig) {
   'ngInject';
   return {
+	replace: true,
     template: require(`./partials/feature-list-md.html`),
     link: function (scope, element) {},
   };
