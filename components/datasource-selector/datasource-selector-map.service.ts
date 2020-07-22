53b144ee4086a69ef30fae46b175c4164d35eab0
jmacura <macurajan@gmail.com>
2020-07-22 15:33:32 +0200

    Upgrade datasources own services to ng9
    Rewrite datasource_selector.service and
    datasource_selector-map.service to classes
    Upgrade those to ng9

--- components/datasource-selector/datasource-selector-map.service.ts
+++ components/datasource-selector/datasource-selector-map.service.ts
@@ -1,28 +1,55 @@
-import * as angular from 'angular';
 import VectorLayer from 'ol/layer/Vector';
+import {Feature} from 'ol';
 import {Fill, Stroke, Style} from 'ol/style';
+import {HsLogService} from '../core/log.service';
+import {HsMapService} from '../map/map.service';
+import {Injectable} from '@angular/core';
 import {Vector} from 'ol/source';
 import {transform} from 'ol/proj';
 
-/* eslint-disable angular/on-watch */
-/**
- * @param $timeout
- * @param HsMapService
- * @param $log
- */
-export const HsDatasourcesMapService = function ($timeout, HsMapService, $log) {
-  'ngInject';
-  const me = this;
+@Injectable({
+  providedIn: 'root',
+})
+export class HsDatasourcesMapService {
+  extentLayer: VectorLayer = new VectorLayer({
+    title: 'Datasources extents',
+    show_in_manager: false,
+    source: new Vector(),
+    style: function (feature, resolution) {
+      return [
+        new Style({
+          stroke: new Stroke({
+            color: '#005CB6',
+            width: feature.get('highlighted') ? 4 : 1,
+          }),
+          fill: new Fill({
+            color: 'rgba(0, 0, 255, 0.01)',
+          }),
+        }),
+      ];
+    },
+  });
+
+  /**
+   * @param HsMapService
+   * @param HsLogService
+   */
+  constructor(
+    private HsMapService: HsMapService,
+    private HsLogService: HsLogService
+  ) {
+    HsMapService.loaded().then(this.init);
+  }
 
   /**
    * @param evt
    */
-  function mapPointerMoved(evt) {
-    const features = me.extentLayer
+  mapPointerMoved(evt): void {
+    const features = this.extentLayer
       .getSource()
       .getFeaturesAtCoordinate(evt.coordinate);
     let somethingChanged = false;
-    me.extentLayer
+    this.extentLayer
       .getSource()
       .getFeatures()
       .forEach((feature) => {
@@ -40,135 +67,113 @@ export const HsDatasourcesMapService = function ($timeout, HsMapService, $log) {
       });
     }
     if (somethingChanged) {
-      $timeout(() => {}, 0);
+      setTimeout(() => {}, 0);
     }
   }
 
   /**
    * @param map
    */
-  function init(map) {
-    map.on('pointermove', mapPointerMoved);
-    map.addLayer(me.extentLayer);
+  init(map): void {
+    map.on('pointermove', this.mapPointerMoved);
+    map.addLayer(this.extentLayer);
   }
 
-  angular.extend(me, {
-    extentLayer: new VectorLayer({
-      title: 'Datasources extents',
-      show_in_manager: false,
-      source: new Vector(),
-      style: function (feature, resolution) {
-        return [
-          new Style({
-            stroke: new Stroke({
-              color: '#005CB6',
-              width: feature.get('highlighted') ? 4 : 1,
-            }),
-            fill: new Fill({
-              color: 'rgba(0, 0, 255, 0.01)',
-            }),
-          }),
-        ];
-      },
-    }),
+  clearExtentLayer(): void {
+    this.extentLayer.getSource().clear();
+  }
 
-    clearExtentLayer() {
-      me.extentLayer.getSource().clear();
-    },
-
-    /**
-     * @function clearDatasetFeatures
-     * @memberOf HsDatasourceBrowserService
-     * @param {object} dataset Configuration of selected datasource (from app config)
-     * Remove layer extent features from map
-     */
-    clearDatasetFeatures(dataset) {
-      dataset.layers.forEach((val) => {
-        try {
-          if (val.feature) {
-            me.extentLayer.getSource().removeFeature(val.feature);
-          }
-        } catch (ex) {
-          $log.warn(ex);
+  /**
+   * @function clearDatasetFeatures
+   * @memberof HsDatasourceBrowserService
+   * @param {object} dataset Configuration of selected datasource (from app config)
+   * Remove layer extent features from map
+   */
+  clearDatasetFeatures(dataset): void {
+    dataset.layers.forEach((val) => {
+      try {
+        if (val.feature) {
+          this.extentLayer.getSource().removeFeature(val.feature);
         }
-      });
-    },
-
-    /**
-     * @function isZoomable
-     * @memberOf HsDataSourceSelectorMapService
-     * @param {unknown} layer TODO
-     * @returns {boolean} Returns if bbox is specified and thus layer is zoomable
-     * Test if it possible to zoom to layer overview (bbox has to be defined
-     * in metadata of selected layer)
-     */
-    isZoomable(layer) {
-      return layer.bbox !== undefined;
-    },
-
-    /**
-     * @function addExtentFeature
-     * @memberOf HsDataSourceSelectorMapService
-     * @param {ol/Feature} extentFeature Openlayers Feature
-     * @description  Callback function which gets executed when extent feature
-     * is created. It should add the feature to vector layer source
-     */
-    addExtentFeature(extentFeature) {
-      me.extentLayer.getSource().addFeatures([extentFeature]);
-    },
-
-    highlightComposition(composition, state) {
-      if (composition.feature !== undefined) {
-        composition.feature.set('highlighted', state);
+      } catch (ex) {
+        this.HsLogService.warn(ex);
       }
-    },
+    });
+  }
 
-    /**
-     * @function zoomTo
-     * @memberOf HsDataSourceSelectorMapService
-     * @param {string} bbox Bounding box of selected layer
-     * ZoomTo / MoveTo to selected layer overview
-     */
-    zoomTo(bbox) {
-      if (bbox === undefined) {
-        return;
-      }
-      let b = null;
-      if (typeof bbox === 'string') {
-        b = bbox.split(' ');
-      } else if (Array.isArray(bbox)) {
-        b = bbox;
-      }
-      let first_pair = [parseFloat(b[0]), parseFloat(b[1])];
-      let second_pair = [parseFloat(b[2]), parseFloat(b[3])];
-      first_pair = transform(
-        first_pair,
-        'EPSG:4326',
-        HsMapService.map.getView().getProjection()
-      );
-      second_pair = transform(
-        second_pair,
-        'EPSG:4326',
-        HsMapService.map.getView().getProjection()
-      );
-      if (
-        isNaN(first_pair[0]) ||
-        isNaN(first_pair[1]) ||
-        isNaN(second_pair[0]) ||
-        isNaN(second_pair[1])
-      ) {
-        return;
-      }
-      const extent = [
-        first_pair[0],
-        first_pair[1],
-        second_pair[0],
-        second_pair[1],
-      ];
-      HsMapService.map.getView().fit(extent, HsMapService.map.getSize());
-    },
-  });
+  /**
+   * @function isZoomable
+   * @memberof HsDataSourceSelectorMapService
+   * @param {unknown} layer TODO
+   * @returns {boolean} Returns if bbox is specified and thus layer is zoomable
+   * Test if it possible to zoom to layer overview (bbox has to be defined
+   * in metadata of selected layer)
+   */
+  isZoomable(layer): boolean {
+    return layer.bbox !== undefined;
+  }
 
-  HsMapService.loaded().then(init);
-  return me;
+  /**
+   * @function addExtentFeature
+   * @memberof HsDataSourceSelectorMapService
+   * @param {Feature} extentFeature Openlayers Feature
+   * @description Callback function which gets executed when extent feature
+   * is created. It should add the feature to vector layer source
+   */
+  addExtentFeature(extentFeature: Feature): void {
+    this.extentLayer.getSource().addFeatures([extentFeature]);
+  }
+
+  highlightComposition(composition, state): void {
+    if (composition.feature !== undefined) {
+      composition.feature.set('highlighted', state);
+    }
+  }
+
+  /**
+   * @function zoomTo
+   * @memberof HsDataSourceSelectorMapService
+   * @param {string} bbox Bounding box of selected layer
+   * ZoomTo / MoveTo to selected layer overview
+   */
+  zoomTo(bbox): void {
+    if (bbox === undefined) {
+      return;
+    }
+    let b = null;
+    if (typeof bbox === 'string') {
+      b = bbox.split(' ');
+    } else if (Array.isArray(bbox)) {
+      b = bbox;
+    }
+    let first_pair = [parseFloat(b[0]), parseFloat(b[1])];
+    let second_pair = [parseFloat(b[2]), parseFloat(b[3])];
+    first_pair = transform(
+      first_pair,
+      'EPSG:4326',
+      this.HsMapService.map.getView().getProjection()
+    );
+    second_pair = transform(
+      second_pair,
+      'EPSG:4326',
+      this.HsMapService.map.getView().getProjection()
+    );
+    if (
+      isNaN(first_pair[0]) ||
+      isNaN(first_pair[1]) ||
+      isNaN(second_pair[0]) ||
+      isNaN(second_pair[1])
+    ) {
+      return;
+    }
+    const extent = [
+      first_pair[0],
+      first_pair[1],
+      second_pair[0],
+      second_pair[1],
+    ];
+    this.HsMapService.map
+      .getView()
+      .fit(extent, this.HsMapService.map.getSize());
+  }
 }
