7fbe1b3c4a2e5d588b8c71464a5e47e1de9a5b2c
jmacura <macurajan@gmail.com>
2020-07-27 22:37:20 +0200

    Optimize css-loader
    We do not use Modular CSS yet,
    hence it's safe to disable their resolving
    and speed-up the compilation

--- scripts/webpack.dev.js
+++ scripts/webpack.dev.js
@@ -14,40 +14,47 @@ const path = require('path');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
-  watchOptions: { ignored: /node_modules/ },
+  watchOptions: {ignored: /node_modules/},
   optimization: {
     // see https://webpack.js.org/guides/build-performance#avoid-extra-optimization-steps
     removeAvailableModules: false,
     removeEmptyChunks: false,
     // In dev mode we simply want to get a big bundle containing all our js
-    splitChunks: false
+    splitChunks: false,
   },
   module: {
     rules: [
       // Load css files which will be injected in html page at startup <style>...</style>)
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            // We do not yet use Modular CSS, hence it's safe to disable their resolving
+            options: {
+              modules: false,
+            },
+          },
+        ],
       },
       {
-          test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-          use: ['url-loader']
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: ['url-loader'],
       },
       // Load angularJS partials HTML file as URL
       {
         test: /\.html$/,
         exclude: path.resolve(__dirname, '../src/index.html'),
-        use: [
-          'html-loader'
-        ]
+        use: ['html-loader'],
       },
       // Load images as URLs
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: {
-          loader: 'url-loader'
-        }
-      }
-    ]
-  }
+          loader: 'url-loader',
+        },
+      },
+    ],
+  },
 });
