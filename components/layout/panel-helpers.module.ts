b92efd1722b5487b5d80f5285e3b2dd1ab4967b7
raitis <raitisbe@gmail.com>
2020-08-05 18:27:24 +0300

    Lint panelhelpers module

--- components/layout/panel-helpers.module.ts
+++ components/layout/panel-helpers.module.ts
@@ -1,21 +1,14 @@
-import { HsPanelHeaderComponent } from './layout-panel-header.component';
-import { NgModule } from '@angular/core';
+import {HsPanelHeaderComponent} from './layout-panel-header.component';
+import {NgModule} from '@angular/core';
 /**
  * @namespace hs.layout
  * @memberOf hs
  */
 @NgModule({
-  declarations: [
-    HsPanelHeaderComponent
-  ],
-  imports: [
-  ],
-  providers: [
-
-  ],
-  entryComponents: [
-    HsPanelHeaderComponent
-  ],
+  declarations: [HsPanelHeaderComponent],
+  imports: [],
+  providers: [],
+  entryComponents: [HsPanelHeaderComponent],
   exports: [HsPanelHeaderComponent],
 })
 export class HsPanelHelpersModule {}
