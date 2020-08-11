a64ca6834d6e734ecebbd783a1eb35b19120b4ab
raitisbe <raitisbe@gmail.com>
2020-08-11 22:20:19 +0200

    Rename permalink module files to *.ts

--- components/permalink/permalink.module.ts
+++ components/permalink/permalink.module.ts
@@ -1,52 +1,17 @@
-import '../compositions/compositions.module';
-import '../language/language.module';
-import * as angular from 'angular';
-import permalinkComponent from './permalink.component';
-import permalinkShareService from './permalink-share.service';
-import {HsPermalinkUrlService} from './permalink-url.service';
-
-/**
- * @namespace hs.permalink
- * @memberOf hs
- */
-
-angular
-  .module('hs.permalink', [
-    '720kb.socialshare',
-    'hs.core',
-    'hs.map',
-    'hs.save-map',
-    'hs.compositions',
-    'hs.language',
-  ])
-
-  .config(function ($locationProvider) {
-    'ngInject';
-    $locationProvider.html5Mode({
-      enabled: true,
-      requireBase: false,
-    });
-  })
-
-  /**
-   * @ngdoc service
-   * @name HsPermalinkUrlService
-   * @membeof hs.permalink
-   * @description Service responsible for creating permalink URLs. Mantain parameters information about map
-   */
-  .service('HsPermalinkUrlService', HsPermalinkUrlService)
-
-  /**
-   * @ngdoc service
-   * @name HsPermalinkShareService
-   * @membeof hs.permalink
-   * @description Service responsible for sharing background. Mantain correct sharing links on the fly
-   */
-  .factory('HsPermalinkShareService', permalinkShareService)
-
-  /**
-   * @name hs.permalink
-   * @membeof hs.permalink
-   * @description
-   */
-  .component('hs.permalink', permalinkComponent);
+import 'angular-cookies';
+import {BrowserModule} from '@angular/platform-browser';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  NO_ERRORS_SCHEMA,
+  NgModule,
+} from '@angular/core';
+import {HsPanelHelpersModule} from '../layout/panel-helpers.module';
+@NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+  declarations: [],
+  imports: [BrowserModule, HsPanelHelpersModule],
+  exports: [],
+  providers: [],
+  entryComponents: [],
+})
+export class HsPermalinkModule {}
