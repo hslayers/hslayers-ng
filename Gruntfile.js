8b0f434749c9b50500ddf412bcdbf16619a8e547
jmacura <macurajan@gmail.com>
2020-08-14 10:45:05 +0200

    Remove obsolete Grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,21 @@
 module.exports = function (grunt) {
-
   grunt.initConfig({
     nggettext_extract: {
       pot: {
         files: {
-          'po/template.pot': ['components/**/*.html', 'components/**/*.js']
-        }
-      }
+          'po/template.pot': ['components/**/*.html', 'components/**/*.js'],
+        },
+      },
     },
     nggettext_compile: {
       all: {
         files: {
-          'components/translations/js/translations.js': ['po/*.po']
-        }
-      }
+          'components/translations/js/translations.js': ['po/*.po'],
+        },
+      },
     },
     pkg: grunt.file.readJSON('package.json'),
-    jsdoc: {
-      dist: {
-        src: ['components/**/*.js'],
-        options: {
-          destination: 'docs',
-          configure: 'node_modules/angular-jsdoc/common/conf.json',
-          template: 'node_modules/angular-jsdoc/angular-template',
-          readme: './README.md'
-        }
-      }
-    }
   });
 
   grunt.loadNpmTasks('grunt-angular-gettext');
-  grunt.loadNpmTasks('grunt-jsdoc');
 };
--- tasks/gen_doc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-grunt jsdoc --force
