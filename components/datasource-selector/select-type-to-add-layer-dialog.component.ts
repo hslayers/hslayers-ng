e1227b202efb98ad37df1eed1d9a64b8c676cb9d
jmacura <macurajan@gmail.com>
2020-08-17 19:31:30 +0200

    Upgrade SelectTypeToAddLayerDialog component

--- components/datasource-selector/select-type-to-add-layer-dialog.component.ts
+++ components/datasource-selector/select-type-to-add-layer-dialog.component.ts
@@ -1,25 +1,36 @@
-export const HsSelectTypeToAddLayerDialogComponent = {
+import {Component, Input, OnInit} from '@angular/core';
+import {HsDatasourcesService} from './datasource-selector.service';
+
+@Component({
+  selector: 'selector-name',
   template: require('./partials/select-type-to-add-layer-dialog.html'),
-  bindings: {
-    layer: '<',
-    types: '<',
-    endpoint: '<',
-  },
-  controller: function (HsDatasourceBrowserService) {
+})
+export class HsSelectTypeToAddLayerDialogComponent implements OnInit {
+  @Input() layer;
+  @Input() types;
+  @Input() endpoint;
+
+  modalVisible;
+  alertChoose;
+  type;
+
+  constructor(private hsDatasourcesService: HsDatasourcesService) {
     'ngInject';
     this.modalVisible = true;
-    const vm = this;
-    vm.add = function () {
-      if (vm.type === undefined) {
-        vm.alertChoose = true;
-      } else {
-        vm.modalVisible = false;
-        HsDatasourceBrowserService.addLayerToMap(
-          vm.endpoint,
-          vm.layer,
-          vm.type
-        );
-      }
-    };
-  },
-};
+  }
+
+  add(): void {
+    if (this.type === undefined) {
+      this.alertChoose = true;
+    } else {
+      this.modalVisible = false;
+      this.hsDatasourcesService.addLayerToMap(
+        this.endpoint,
+        this.layer,
+        this.type
+      );
+    }
+  }
+
+  ngOnInit(): void {}
+}
