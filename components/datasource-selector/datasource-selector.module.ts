2743d599c91651ceee41d1c6993ec63d48554bea
jmacura <macurajan@gmail.com>
2020-08-18 12:01:54 +0200

    Upgrade Micka directives to ng9 components

--- components/datasource-selector/datasource-selector.module.ts
+++ components/datasource-selector/datasource-selector.module.ts
@@ -4,12 +4,16 @@ import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 
 import {EndpointsWithDatasourcesPipe} from './endpoints-with-datasources.pipe';
+import {HsAdvancedMickaDialogComponent} from './micka/advanced-micka-dialog.component';
 import {HsDatasourcesComponent} from './datasource-selector.component';
 import {HsDatasourcesMapService} from './datasource-selector-map.service';
 import {HsDatasourcesService} from './datasource-selector.service';
 import {HsLaymanBrowserService} from './layman/layman.service';
+import {HsMetadataDialogComponent} from './metadata-dialog.component';
 import {HsMickaBrowserService} from './micka/micka.service';
+import {HsMickaFilterComponent} from './micka/micka-filter.component';
 import {HsMickaFilterService} from './micka/micka-filters.service';
+import {HsMickaSuggestionsDialogComponent} from './micka/micka-suggestions-dialog.component';
 import {HsPanelHelpersModule} from '../layout/panel-helpers.module';
 import {HsSelectTypeToAddLayerDialogComponent} from './select-type-to-add-layer-dialog.component';
 
@@ -17,7 +21,11 @@ import {HsSelectTypeToAddLayerDialogComponent} from './select-type-to-add-layer-
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [
     HsDatasourcesComponent,
+    HsMetadataDialogComponent,
     HsSelectTypeToAddLayerDialogComponent,
+    HsMickaFilterComponent,
+    HsMickaSuggestionsDialogComponent,
+    HsAdvancedMickaDialogComponent,
     EndpointsWithDatasourcesPipe,
   ],
   imports: [BrowserModule, CommonModule, FormsModule, HsPanelHelpersModule],
