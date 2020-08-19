5fde9101bb64a34d8564e3c37250e3d6e9687a99
Filip Leitner <leitnerfilip@gmail.com>
2020-08-19 18:25:49 +0200

    import search module into toolbar

--- components/toolbar/toolbar.module.ts
+++ components/toolbar/toolbar.module.ts
@@ -9,6 +9,7 @@ import {CommonModule} from '@angular/common';
 
 import {FormsModule} from '@angular/forms';
 import {HsDrawModule} from '../draw/draw.module';
+import {HsSearchModule} from '../search/search.module'
 import {HsToolbarComponent} from './toolbar.component';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
@@ -16,7 +17,7 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
   declarations: [HsToolbarComponent],
-  imports: [CommonModule, BrowserModule, FormsModule, NgbModule, HsDrawModule],
+  imports: [CommonModule, BrowserModule, FormsModule, NgbModule, HsDrawModule, HsSearchModule],
   exports: [HsToolbarComponent],
   providers: [],
   entryComponents: [HsToolbarComponent],
