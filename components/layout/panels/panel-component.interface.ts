649c2ed06ef1f49eb3f63fa784f96007bd83875c
raitisbe <raitisbe@gmail.com>
2020-08-21 00:00:35 +0200

    Small cleanup of custom panel interface and class

--- components/layout/panels/panel-component.interface.ts
+++ components/layout/panels/panel-component.interface.ts
@@ -3,5 +3,5 @@ import {ViewRef} from '@angular/core';
 export interface HsPanelComponent {
   viewRef: ViewRef;
   data: any;
-  isVisible();
+  isVisible(): boolean;
 }
--- components/layout/panels/panel-item.ts
+++ components/layout/panels/panel-item.ts
@@ -3,9 +3,3 @@ import {Type} from '@angular/core';
 export class HsPanelItem {
   constructor(public component: Type<any>, public data: any) {}
 }
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
