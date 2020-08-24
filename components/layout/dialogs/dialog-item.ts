1a7ef3f4086698df461d55bcac0a887922fdd742
raitisbe <raitisbe@gmail.com>
2020-08-24 16:49:09 +0200

    Rewrite hover popup dialog and confirm window

--- components/layout/dialogs/dialog-item.ts
+++ components/layout/dialogs/dialog-item.ts
@@ -1,6 +1,15 @@
 import {Type} from '@angular/core';
 
 export class HsDialogItem {
+  resolve: (value?: unknown) => void;
+  reject: (reason?: any) => void;
+  waitResult(): Promise<any> {
+    const promise = new Promise((resolve, reject) => {
+      this.resolve = resolve;
+      this.reject = reject;
+    });
+    return promise;
+  }
   constructor(public component: Type<any>, public data: any) {}
 }
 
