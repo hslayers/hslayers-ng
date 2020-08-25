1166e8e394f90d4919f59221cbc5bad6055c5057
DailisLangovskis <dailis.langovskis@gmail.com>
2020-08-25 10:24:30 +0300

    removed unnecessarry imports from search module

--- components/search/search.module.ts
+++ components/search/search.module.ts
@@ -6,14 +6,11 @@ import {
 } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
-import {HsLayoutModule} from './../layout/layout.module';
 import {HsPanelHelpersModule} from '../layout/panels/panel-helpers.module';
 import {HsSearchComponent} from './search.component';
 import {HsSearchInputComponent} from './search-input.component';
 import {HsSearchResultsComponent} from './search-results.component';
 import {HsSearchService} from './search.service';
-import {HsShareModule} from './../permalink/share.module';
-import {HsStylerModule} from './../styles/styles.module';
 import {TranslateModule, TranslateStore} from '@ngx-translate/core';
 import {limitToPipe} from './limitTo.pipe';
 
@@ -25,15 +22,7 @@ import {limitToPipe} from './limitTo.pipe';
     HsSearchInputComponent,
     limitToPipe,
   ],
-  imports: [
-    HsStylerModule,
-    HsLayoutModule,
-    HsShareModule,
-    FormsModule,
-    CommonModule,
-    HsPanelHelpersModule,
-    TranslateModule,
-  ],
+  imports: [FormsModule, CommonModule, HsPanelHelpersModule, TranslateModule],
   exports: [
     HsSearchComponent,
     HsSearchResultsComponent,
