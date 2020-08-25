cede2b105ee73cd307faa816862c9d1d2c896e07
DailisLangovskis <dailis.langovskis@gmail.com>
2020-08-25 10:25:51 +0300

    refactored geolocation service and component to n9

---
+++ components/geolocation/geolocation.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+import {HsGeolocationService} from './geolocation.service';
+import {HsLayoutService} from '../layout/layout.service';
+@Component({
+  selector: 'hs-geolocation',
+  template: require('./partials/geolocation.html'),
+})
+export class HsGeolocationComponent implements OnInit {
+  collapsed: boolean;
+  constructor(
+    private HsGeolocationService: HsGeolocationService,
+    private HsLayoutService: HsLayoutService
+  ) {}
+  ngOnInit(): void {
+    this.collapsed = true;
+  }
+  geolocationVisible(): boolean {
+    return this.HsLayoutService.componentEnabled('geolocationButton');
+  }
+}
