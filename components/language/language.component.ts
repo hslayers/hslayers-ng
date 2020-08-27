e277ca6b86dcf98dac7527c0b1e2830575869673
DailisLangovskis <dailis.langovskis@gmail.com>
2020-08-27 17:31:39 +0300

    Refactored language module to n9

---
+++ components/language/language.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+import {HsLanguageService} from './language.service';
+@Component({
+  selector: 'hs-language',
+  template: require('./partials/language.html'),
+})
+export class HsLanguageComponent implements OnInit {
+  available_languages: any;
+
+  constructor(private HsLanguageService: HsLanguageService) {}
+
+  ngOnInit(): void {
+    this.available_languages = this.HsLanguageService.listAvailableLanguages();
+  }
+  //$scope.$emit('scope_loaded', 'Language');
+}
