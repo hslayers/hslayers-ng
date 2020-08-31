e5fddc7a5bd52852895d18303159626912a91b4a
DailisLangovskis <dailis.langovskis@gmail.com>
2020-08-31 17:41:46 +0300

    Added translations to language module in 3 languages

--- components/language/language.module.ts
+++ components/language/language.module.ts
@@ -7,14 +7,14 @@ import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {HsLanguageComponent} from './language.component';
 import {HsLanguageService} from './language.service';
-import {TranslateModule} from '@ngx-translate/core';
+import {TranslateModule, TranslateStore} from '@ngx-translate/core';
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
   declarations: [HsLanguageComponent],
   imports: [FormsModule, CommonModule, TranslateModule],
   exports: [HsLanguageComponent],
-  providers: [HsLanguageService],
+  providers: [HsLanguageService, TranslateStore],
   entryComponents: [HsLanguageComponent],
 })
 export class HsLanguageModule {}
