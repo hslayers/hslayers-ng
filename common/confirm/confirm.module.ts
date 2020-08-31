bced02e3c333ce0391c6b82a7b1999a38df2802c
DailisLangovskis <dailis.langovskis@gmail.com>
2020-08-31 20:26:43 +0300

    Added translations to confirm module in 3 languages

--- common/confirm/confirm.module.ts
+++ common/confirm/confirm.module.ts
@@ -2,11 +2,12 @@ import {CommonModule} from '@angular/common';
 import {HsConfirmDialog} from './confirm-dialog.service';
 import {HsConfirmDialogComponent} from './confirm-dialog.component';
 import {NgModule} from '@angular/core';
+import {TranslateModule, TranslateStore} from '@ngx-translate/core';
 
 @NgModule({
   declarations: [HsConfirmDialogComponent],
-  imports: [CommonModule],
-  providers: [HsConfirmDialog],
+  imports: [CommonModule, TranslateModule],
+  providers: [HsConfirmDialog, TranslateStore],
   bootstrap: [HsConfirmDialogComponent],
 })
 export class HsConfirmModule {}
