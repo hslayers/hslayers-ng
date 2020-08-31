a461bee1ec2510eb0701c275c22055238f77a042
DailisLangovskis <dailis.langovskis@gmail.com>
2020-08-31 15:08:30 +0300

    Added translate to print module in 3 languages

--- components/print/print.module.ts
+++ components/print/print.module.ts
@@ -2,13 +2,13 @@
  * @namespace hs.print
  * @memberOf hs
  */
-
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {HsPanelHelpersModule} from '../layout/panels/panel-helpers.module';
 import {HsPrintComponent} from './print.component';
 import {HsPrintService} from './print.service';
+import {TranslateModule, TranslateStore} from '@ngx-translate/core';
 /**
  * @memberof hs.print
  * @ngdoc component
@@ -18,10 +18,11 @@ import {HsPrintService} from './print.service';
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [HsPrintComponent],
-  imports: [CommonModule, FormsModule, HsPanelHelpersModule],
+  imports: [CommonModule, FormsModule, HsPanelHelpersModule, TranslateModule],
   exports: [HsPrintComponent],
   providers: [
     HsPrintService,
+    TranslateStore,
     {
       provide: Window,
       useValue: window,
