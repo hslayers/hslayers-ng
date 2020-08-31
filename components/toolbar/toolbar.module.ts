5f2b8cf99c44d774c18f38202ce962ef797993d7
DailisLangovskis <dailis.langovskis@gmail.com>
2020-08-31 19:26:16 +0300

    added translations to toolbar module in 3 languages

--- components/toolbar/toolbar.module.ts
+++ components/toolbar/toolbar.module.ts
@@ -5,10 +5,11 @@ import {
   NgModule,
 } from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {TranslateModule, TranslateStore} from '@ngx-translate/core';
 
 import {FormsModule} from '@angular/forms';
 import {HsDrawModule} from '../draw/draw.module';
-import {HsSearchModule} from '../search/search.module'
+import {HsSearchModule} from '../search/search.module';
 import {HsToolbarComponent} from './toolbar.component';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
@@ -16,9 +17,16 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
   declarations: [HsToolbarComponent],
-  imports: [CommonModule, FormsModule, NgbModule, HsDrawModule, HsSearchModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgbModule,
+    HsDrawModule,
+    HsSearchModule,
+    TranslateModule,
+  ],
   exports: [HsToolbarComponent],
-  providers: [],
+  providers: [TranslateStore],
   entryComponents: [HsToolbarComponent],
 })
 export class HsToolbarModule {}
