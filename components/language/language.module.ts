36b59e3e931feca9f41336d35ff8dc49e201be1e
DailisLangovskis <dailis.langovskis@gmail.com>
2020-08-31 17:52:23 +0300

    Added missing HsPanelHelpersModule to language module

--- components/language/language.module.ts
+++ components/language/language.module.ts
@@ -7,12 +7,13 @@ import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {HsLanguageComponent} from './language.component';
 import {HsLanguageService} from './language.service';
+import {HsPanelHelpersModule} from '../layout/panels/panel-helpers.module';
 import {TranslateModule, TranslateStore} from '@ngx-translate/core';
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
   declarations: [HsLanguageComponent],
-  imports: [FormsModule, CommonModule, TranslateModule],
+  imports: [FormsModule, CommonModule, TranslateModule, HsPanelHelpersModule],
   exports: [HsLanguageComponent],
   providers: [HsLanguageService, TranslateStore],
   entryComponents: [HsLanguageComponent],
