c9194e69a39eaa6992583ebfe586e841302835ff
raitis <raitisbe@gmail.com>
2020-09-12 15:11:50 +0300

    Optimize get layer for feature

--- components/map/map.spec.ts
+++ components/map/map.spec.ts
@@ -5,6 +5,8 @@
 import '../core/core-ajs.mock';
 import 'angular-mocks';
 import * as angular from 'angular';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
 import VectorLayer from 'ol/layer/Vector';
 import {HsMapService} from './map.service';
 import {Vector as VectorSource} from 'ol/source';
@@ -19,9 +21,32 @@ describe('hs.map', function () {
       .module('hs.utils', [])
       .service('HsUtilsService', function () {
         this.debounce = function () {};
-        this.instOf = function (a, b) {
+        this.instOf = function (obj, klass) {
+          if (this.isFunction(klass)) {
+            return obj instanceof klass;
+          }
+          obj = Object.getPrototypeOf(obj);
+          while (obj !== null) {
+            if (obj.constructor.name === klass) {
+              return true;
+            }
+            obj = Object.getPrototypeOf(obj);
+          }
           return false;
         };
+        this.isFunction = function(functionToCheck){
+          return (
+            functionToCheck &&
+            {}.toString.call(functionToCheck) === '[object Function]'
+          );
+        }
+        this.generateUuid = function(){
+          return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+            const r = (Math.random() * 16) | 0,
+              v = c == 'x' ? r : (r & 0x3) | 0x8;
+            return v.toString(16);
+          });
+        }
       })
       .service('HsLayerUtilsService', function () {});
 
@@ -78,5 +103,18 @@ describe('hs.map', function () {
     });
     const exists = hsMap.layerAlreadyExists(layer2);
     expect(exists).toBe(true);
+  });
+
+  it('find layer for feature', async function () {
+    await hsMap.loaded();
+    const featureLayer = new VectorLayer({
+      title: 'Feature layer',
+      source: new VectorSource({}),
+    });
+    hsMap.map.addLayer(featureLayer);
+    const feature = new Feature({geometry: new Point([0, 0]), name: 'test'});
+    featureLayer.getSource().addFeatures([feature]);
+    const foundLayer = hsMap.getLayerForFeature(feature);
+    expect(foundLayer).toBe(featureLayer);
   });
 });
