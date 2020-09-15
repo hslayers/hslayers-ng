39f0e1b6b0c12bb6ba929f85407ae5d33d33650a
raitis <raitisbe@gmail.com>
2020-09-15 10:33:37 +0300

    Fix inline legend not displaying

--- components/layermanager/layer-editor.service.ts
+++ components/layermanager/layer-editor.service.ts
@@ -1,6 +1,7 @@
 import {HsEventBusService} from '../core/event-bus.service';
 import {HsLayerDescriptor} from './layer-descriptor.interface';
 import {HsLayerEditorVectorLayerService} from './layer-editor-vector-layer.service';
+import {HsLayerSelectorService} from './layer-selector.service';
 import {HsLayerUtilsService} from '../utils/layer-utils.service';
 import {HsLayoutService} from '../layout/layout.service';
 import {HsLegendDescriptor} from '../legend/legend-descriptor.interface';
@@ -26,7 +27,13 @@ export class HsLayerEditorService {
     private HsEventBusService: HsEventBusService,
     private HsLayoutService: HsLayoutService,
     private HsLegendService: HsLegendService,
+    private HsLayerSelectorService: HsLayerSelectorService
   ) {
+    this.HsLayerSelectorService.layerSelected.subscribe((layer) => {
+      this.legendDescriptor = this.HsLegendService.getLayerLegendDescriptor(
+        layer.layer
+      );
+    });
   }
 
   /**
