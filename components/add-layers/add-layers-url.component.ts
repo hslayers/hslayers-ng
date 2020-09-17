95e942f8e51fbcc2ec9b7301e7e36968151bd33b
jmacura <macurajan@gmail.com>
2020-09-18 00:56:49 +0200

    Upgrade addLayers components to ng9

--- components/add-layers/add-layers-url.component.ts
+++ components/add-layers/add-layers-url.component.ts
@@ -1,19 +1,23 @@
-export const HsAddLayersUrlComponent = {
+import {Component} from '@angular/core';
+import {HsHistoryListService} from '../../common/history-list/history-list.service';
+
+@Component({
+  selector: 'hs-add-layers-url',
   template: require('./partials/add-layers-url.directive.html'),
-  scope: {
-    type: '@type',
-    url: '=url',
-    connect: '=connect',
-    field: '=field',
-  },
-  controller: [
-    '$scope',
-    'HsHistoryListService',
-    function ($scope, historyListService) {
-      $scope.items = historyListService.readSourceHistory($scope.what);
-      $scope.historySelected = function (url) {
-        $scope.url = url;
-      };
-    },
-  ],
-};
+})
+export class HsAddLayersUrlComponent {
+  items;
+  what;
+  //type: '@type'; TODO: comes from another scope
+  url;
+  //connect: '=connect'; TODO: comes from another scope
+  //field: '=field'; TODO: some AngularJS stuff?
+
+  constructor(private historyListService: HsHistoryListService) {
+    this.items = this.historyListService.readSourceHistory(this.what);
+  }
+
+  historySelected(url): void {
+    this.url = url;
+  }
+}
