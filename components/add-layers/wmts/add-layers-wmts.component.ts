b3527b333b54cbe92705d15b71826c81023a98d2
jmacura <macurajan@gmail.com>
2020-09-21 16:41:17 +0200

    Upgrade add-layer-wmts component

--- components/add-layers/wmts/add-layers-wmts.component.ts
+++ components/add-layers/wmts/add-layers-wmts.component.ts
@@ -1,11 +1,15 @@
 import * as angular from 'angular';
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 import '../../../common/get-capabilities.module';
+import {HsLayoutService} from '../../layout/layout.service';
+import {HsMapService} from '../../map/map.service';
 import {addAnchors} from '../../../common/attribution-utils';
 
 import WMTSCapabilities from 'ol/format/WMTSCapabilities';
 import WMTSTileGrid from 'ol/tilegrid/WMTS';
+import {HsEventBusService} from '../../core/event-bus.service';
+import {HsLogService} from '../../../common/log/log.service';
 import {Tile} from 'ol/layer';
 import {WMTS} from 'ol/source';
 import {get} from 'ol/proj';
@@ -16,150 +20,162 @@ import {getTopLeft, getWidth} from 'ol/extent';
   template: require('./add-wmts-layer.directive.html'),
 })
 export class HsAddLayersWmtsComponent {
-  constructor($scope, HsMapService, $compile, HsLayoutService, $log) {
-    'ngInject';
-    $scope.map_projection = HsMapService.map
+  capabilities;
+  currentLayer;
+  description;
+  error;
+  imageFormat = '';
+  layerTileMatrix;
+  mapProjection: string;
+  services;
+  style = '';
+  tileMatrixSet = '';
+  title;
+  tileURL;
+  version;
+
+  constructor(
+    private hsEventBusService: HsEventBusService,
+    private hsLayoutService: HsLayoutService,
+    private hsLog: HsLogService,
+    private hsMapService: HsMapService
+  ) {
+    this.mapProjection = this.hsMapService.map
       .getView()
       .getProjection()
       .getCode()
       .toUpperCase();
-    $scope.style = '';
-    $scope.tileMatrixSet = '';
-    $scope.image_format = '';
 
-    $scope.capabilitiesReceived = function (response) {
-      try {
-        const parser = new WMTSCapabilities();
-        $scope.capabilities = parser.read(response);
-        const caps = $scope.capabilities;
-        $scope.title = caps.ServiceIdentification.Title;
-        $scope.tileURL = caps.OperationsMetadata.GetTile.DCP.HTTP.Get[0].href;
-        for (
-          let idx = 0;
-          idx < caps.OperationsMetadata.GetTile.DCP.HTTP.Get.length;
-          idx++
-        ) {
-          if (
-            caps.OperationsMetadata.GetTile.DCP.HTTP.Get[idx].Constraint[0]
-              .AllowedValues.Value[0] == 'KVP'
-          ) {
-            $scope.tileURL =
-              caps.OperationsMetadata.GetTile.DCP.HTTP.Get[idx].href;
-            break;
-          }
+    this.hsEventBusService.owsCapabilitiesReceived.subscribe(
+      ({type: type, response: response}) => {
+        if (type === 'WMTS') {
+          this.capabilitiesReceived(response.data);
         }
-        $scope.description = addAnchors(caps.ServiceIdentification.Abstract);
-        $scope.version = caps.Version || caps.version;
-        $scope.services = caps.Contents;
-      } catch (e) {
-        if (console) {
-          $log.log(e);
-        }
-        $scope.error = e.toString();
-        const previousDialog = HsLayoutService.contentWrapper.querySelector(
-          '.hs-ows-wms-capabilities-error'
-        );
-        if (previousDialog) {
-          previousDialog.parentNode.removeChild(previousDialog);
-        }
-        const el = angular.element(
-          '<div hs.wmts.capabilities_error_directive></div>'
-        );
-        HsLayoutService.contentWrapper
-          .querySelector('.hs-dialog-area')
-          .appendChild(el[0]);
-        $compile(el)($scope);
-        //throw "wmts Capabilities parsing problem";
       }
-    };
+    );
+  }
 
-    $scope.$on('ows_wmts.capabilities_received', (event, response) => {
-      $scope.capabilitiesReceived(response.data);
+  capabilitiesReceived(response): void {
+    try {
+      const parser = new WMTSCapabilities();
+      this.capabilities = parser.read(response);
+      const caps = this.capabilities;
+      this.title = caps.ServiceIdentification.Title;
+      this.tileURL = caps.OperationsMetadata.GetTile.DCP.HTTP.Get[0].href;
+      for (
+        let idx = 0;
+        idx < caps.OperationsMetadata.GetTile.DCP.HTTP.Get.length;
+        idx++
+      ) {
+        if (
+          caps.OperationsMetadata.GetTile.DCP.HTTP.Get[idx].Constraint[0]
+            .AllowedValues.Value[0] == 'KVP'
+        ) {
+          this.tileURL = caps.OperationsMetadata.GetTile.DCP.HTTP.Get[idx].href;
+          break;
+        }
+      }
+      this.description = addAnchors(caps.ServiceIdentification.Abstract);
+      this.version = caps.Version || caps.version;
+      this.services = caps.Contents;
+    } catch (e) {
+      if (console) {
+        this.hsLog.log(e);
+      }
+      this.error = e.toString();
+      const previousDialog = this.hsLayoutService.contentWrapper.querySelector(
+        '.hs-ows-wms-capabilities-error'
+      );
+      if (previousDialog) {
+        previousDialog.parentNode.removeChild(previousDialog);
+      }
+      const el = angular.element(
+        '<div hs.wmts.capabilities_error_directive></div>'
+      );
+      this.hsLayoutService.contentWrapper
+        .querySelector('.hs-dialog-area')
+        .appendChild(el[0]);
+      //FIXME: $compile(el)($scope);
+      //throw "wmts Capabilities parsing problem";
+    }
+  }
+
+  /**
+   * @function setCurrentLayer
+   * @description Opens detailed view for manipulating layer
+   * @param {object} layer - Wrapped layer to edit or view
+   * @param {number} index - Used to position the detail panel after layers li element
+   */
+  setCurrentLayer(layer, index: number): void {
+    if (this.currentLayer == layer) {
+      this.currentLayer = null;
+    } else {
+      this.currentLayer = layer;
+      const wmtsLayerPanel = this.hsLayoutService.contentWrapper.querySelector(
+        '.hs-wmts-layerpanel'
+      );
+      const layerNode = this.hsLayoutService.contentWrapper.querySelector(
+        '#wmtslayer-' + index
+      );
+      if (wmtsLayerPanel) {
+        layerNode.parentNode.insertBefore(
+          wmtsLayerPanel,
+          layerNode.nextSibling
+        );
+      }
+    }
+  }
+
+  /**
+   * @function addLayer
+   * @description Add layer to map
+   * @param {object} layer - Wrapped layer to add
+   */
+  addLayer(layer): void {
+    const projection = get(this.mapProjection);
+    const projectionExtent = projection.getExtent();
+    for (const tileMatrixSetItem of this.services.TileMatrixSet) {
+      if (tileMatrixSetItem.Identifier == this.tileMatrixSet) {
+        this.layerTileMatrix = tileMatrixSetItem;
+      }
+    }
+    const size =
+      getWidth(projectionExtent) / this.layerTileMatrix.TileMatrix[0].TileWidth;
+    const resolutions = new Array(this.layerTileMatrix.TileMatrix.length);
+    const matrixIds = new Array(this.layerTileMatrix.TileMatrix.length);
+    for (let z = 0; z < this.layerTileMatrix.TileMatrix.length; ++z) {
+      // generate resolutions and matrixIds arrays for this WMTS
+      resolutions[z] = size / Math.pow(2, z);
+      matrixIds[z] = z;
+    }
+
+    const dimensions = {};
+
+    angular.forEach(layer.Dimension, (val) => {
+      dimensions[val.name] = val;
     });
 
-    /**
-     * @function setCurrentLayer
-     * @memberOf hs.addLayersWmts.controller
-     * @description Opens detailed view for manipulating layer
-     * @param {object} layer - Wrapped layer to edit or view
-     * @param {number} index - Used to position the detail panel after layers li element
-     */
-    $scope.setCurrentLayer = function (layer, index) {
-      if ($scope.currentLayer == layer) {
-        $scope.currentLayer = null;
-      } else {
-        $scope.currentLayer = layer;
-        const wmtsLayerPanel = HsLayoutService.contentWrapper.querySelector(
-          '.hs-wmts-layerpanel'
-        );
-        const layerNode = HsLayoutService.contentWrapper.querySelector(
-          '#wmtslayer-' + index
-        );
-        if (wmtsLayerPanel) {
-          layerNode.parentNode.insertBefore(
-            wmtsLayerPanel,
-            layerNode.nextSibling
-          );
-        }
-      }
-    };
-
-    /**
-     * @function addLayer
-     * @memberOf hs.addLayersWmts.controller
-     * @description Add layer to map
-     * @param {object} layer - Wrapped layer to add
-     */
-
-    $scope.addLayer = function (layer) {
-      const projection = get($scope.map_projection);
-      const projectionExtent = projection.getExtent();
-      for (let idx = 0; idx < $scope.services.TileMatrixSet.length; idx++) {
-        if (
-          $scope.services.TileMatrixSet[idx].Identifier == $scope.tileMatrixSet
-        ) {
-          $scope.layerTileMatrix = $scope.services.TileMatrixSet[idx];
-        }
-      }
-      const size =
-        getWidth(projectionExtent) /
-        $scope.layerTileMatrix.TileMatrix[0].TileWidth;
-      const resolutions = new Array($scope.layerTileMatrix.TileMatrix.length);
-      const matrixIds = new Array($scope.layerTileMatrix.TileMatrix.length);
-      for (let z = 0; z < $scope.layerTileMatrix.TileMatrix.length; ++z) {
-        // generate resolutions and matrixIds arrays for this WMTS
-        resolutions[z] = size / Math.pow(2, z);
-        matrixIds[z] = z;
-      }
-
-      const dimensions = {};
-
-      angular.forEach(layer.Dimension, (val) => {
-        dimensions[val.name] = val;
-      });
-
-      const new_layer = new Tile({
-        title: layer.Title,
-        source: new WMTS({
-          url: $scope.tileURL,
-          layer: layer.Identifier,
-          projection: projection,
-          matrixSet: 'EPSG:3857',
-          format: $scope.image_format,
-          tileGrid: new WMTSTileGrid({
-            origin: getTopLeft(projectionExtent),
-            resolutions: resolutions,
-            matrixIds: matrixIds,
-          }),
-          style: $scope.style,
-          wrapX: true,
+    const new_layer = new Tile({
+      title: layer.Title,
+      source: new WMTS({
+        url: this.tileURL,
+        layer: layer.Identifier,
+        projection: projection,
+        matrixSet: 'EPSG:3857',
+        format: this.imageFormat,
+        tileGrid: new WMTSTileGrid({
+          origin: getTopLeft(projectionExtent),
+          resolutions: resolutions,
+          matrixIds: matrixIds,
         }),
-        saveState: true,
-        removable: true,
-        dimensions: dimensions,
-      });
+        style: this.style,
+        wrapX: true,
+      }),
+      saveState: true,
+      removable: true,
+      dimensions: dimensions,
+    });
 
-      HsMapService.addLayer(new_layer, true);
-    };
+    this.hsMapService.addLayer(new_layer, true);
   }
 }
