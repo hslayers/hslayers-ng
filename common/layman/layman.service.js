fbb49a4acb9f51b9c4d2a86a0412f6657be619a4
jmacura <macurajan@gmail.com>
2020-09-29 13:31:41 +0200

    Common Layman service refactored

--- common/layman/layman.service.js
+++ common/layman/layman.service.js
@@ -8,40 +8,39 @@ export class HsCommonLaymanService {
       $rootScope,
       authChange: new Subject(),
     });
-    angular.extend(this, {
-      getCurrentUser(endpoint) {
-        const url = `${endpoint.url}/rest/current-user`;
-        return new Promise((resolve, reject) => {
-          this.$http.get(url).then(
-            (res) => {
-              let somethingChanged = false;
-              if (res.data.username) {
-                if (endpoint.user != res.data.username) {
-                  endpoint.user = res.data.username;
-                  somethingChanged = true;
-                  this.authChange.next(endpoint);
-                }
-              } else {
-                if (endpoint.user != endpoint.originalConfiguredUser) {
-                  somethingChanged = true;
-                }
-                endpoint.user = endpoint.originalConfiguredUser;
-              }
-              resolve(somethingChanged);
-            },
-            (e) => {
-              reject(e);
-            }
-          );
-        });
+  }
+
+  async getCurrentUser(endpoint) {
+    const url = `${endpoint.url}/rest/current-user`;
+    console.log(url);
+    return this.$http.get(url).then(
+      (res) => {
+        let somethingChanged = false;
+        if (res.data.username) {
+          if (endpoint.user != res.data.username) {
+            endpoint.user = res.data.username;
+            somethingChanged = true;
+            this.authChange.next(endpoint);
+          }
+        } else {
+          if (endpoint.user != endpoint.originalConfiguredUser) {
+            somethingChanged = true;
+          }
+          endpoint.user = endpoint.originalConfiguredUser;
+        }
+        return somethingChanged;
       },
-    });
+      (e) => {
+        console.warn(e);
+        return e;
+      }
+    );
   }
 
   async getCurrentUserIfNeeded(endpoint) {
     if (
-      angular.isUndefined(endpoint.user) ||
-      ['anonymous', 'browser'].indexOf(endpoint.user) > -1
+      endpoint.user === undefined ||
+      ['anonymous', 'browser'].includes(endpoint.user)
     ) {
       await this.getCurrentUser(endpoint);
     }
