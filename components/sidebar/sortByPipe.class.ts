22420576ec1491e91c68169bdca8fe1078644cc7
Dailis <46564459+DailisLangovskis@users.noreply.github.com>
2020-10-05 19:11:00 +0300

    Remove lodash dep in orderBy pipe (#1105)
    * Added missing loadash dependency after initial install
    
    * Replaced loadash orderby with simple array sort

--- components/sidebar/sortByPipe.class.ts
+++ components/sidebar/sortByPipe.class.ts
@@ -1,22 +1,32 @@
 import {Pipe, PipeTransform} from '@angular/core';
-import {orderBy} from 'lodash';
 
 @Pipe({name: 'sortBy'})
 export class SortByPipe implements PipeTransform {
   transform(value: any[], order = '', column = ''): any[] {
     if (!value || order === '' || !order) {
-      return value;
+      return this.ascendingOrder(value, column);
     } // no array
     if (value.length <= 1) {
       return value;
     } // array with only one item
-    if (!column || column === '') {
+    if (!column || column !== '') {
       if (order === 'asc') {
-        return value.sort();
+        return this.ascendingOrder(value, column);
       } else {
-        return value.sort().reverse();
+        return this.descendingOrder(value, column);
       }
-    } // sort 1d array
-    return orderBy(value, [column], [order]);
+    } else {
+      return value;
+    }
+  }
+  ascendingOrder(array: any, column: any): any {
+    return array.sort((a, b) => {
+      return a[column] - b[column];
+    });
+  }
+  descendingOrder(array: any, column: any): any {
+    return array.sort((a, b) => {
+      return b[column] - a[column];
+    });
   }
 }
