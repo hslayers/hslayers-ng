89fb50916e6432c7208779b912bba9e4003b45db
raitis <raitisbe@gmail.com>
2020-10-15 23:53:24 +0300

    Add composition dialogs to module and fix translate tags

--- components/compositions/compositions.module.ts
+++ components/compositions/compositions.module.ts
@@ -2,14 +2,24 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {HsCompositionsComponent} from './compositions.component';
+import {HsCompositionsDeleteDialogComponent} from './dialogs/delete-dialog.component';
+import {HsCompositionsInfoDialogComponent} from './dialogs/info-dialog.component';
 import {HsCompositionsLayerParserModule} from './layer-parser/layer-parser.module';
+import {HsCompositionsOverwriteDialogComponent} from './dialogs/overwrite-dialog.component';
 import {HsCompositionsService} from './compositions.service';
+import {HsCompositionsShareDialogComponent} from './dialogs/share-dialog.component';
 import {HsPanelHelpersModule} from '../layout/panels/panel-helpers.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {TranslateModule} from '@ngx-translate/core';
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  declarations: [HsCompositionsComponent],
+  declarations: [
+    HsCompositionsComponent,
+    HsCompositionsOverwriteDialogComponent,
+    HsCompositionsDeleteDialogComponent,
+    HsCompositionsShareDialogComponent,
+    HsCompositionsInfoDialogComponent,
+  ],
   imports: [
     CommonModule,
     FormsModule,
@@ -20,6 +30,12 @@ import {TranslateModule} from '@ngx-translate/core';
   ],
   exports: [HsCompositionsComponent],
   providers: [HsCompositionsService],
-  entryComponents: [HsCompositionsComponent],
+  entryComponents: [
+    HsCompositionsComponent,
+    HsCompositionsOverwriteDialogComponent,
+    HsCompositionsDeleteDialogComponent,
+    HsCompositionsShareDialogComponent,
+    HsCompositionsInfoDialogComponent,
+  ],
 })
 export class HsCompositionsModule {}
