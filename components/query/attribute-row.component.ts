9ee5b5e7ad1366670456a6a397889e88ad44f25b
raitis <raitisbe@gmail.com>
2020-10-15 15:13:04 +0300

    Fix readonly attributes in info panel to be dynamic

--- components/query/attribute-row.component.ts
+++ components/query/attribute-row.component.ts
@@ -7,7 +7,7 @@ import {Component, Input} from '@angular/core';
 export class HsQueryAttributeRowComponent {
   @Input() attribute;
   @Input() feature;
-  @Input() readonly;
+  @Input() readonly: boolean;
   @Input() template;
 
   change() {
