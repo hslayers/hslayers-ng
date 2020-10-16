db232b1d60da3a714d1789b5618cac6401375113
DailisLangovskis <dailis.langovskis@gmail.com>
2020-10-16 16:28:55 +0300

    Created feature-table component

---
+++ components/feature-table/feature-table.component.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {Component, OnInit} from '@angular/core';
+import {HsConfig} from '../../config.service';
+import {HsFeatureTableService} from './feature-table.service';
+import {Layer} from 'ol/layer';
+/**
+ * @memberof hs.featureTable
+ * @ngdoc component
+ * @name HsFeatureTableComponent
+ */
+@Component({
+  selector: 'hs-feature-table',
+  template: require('./partials/feature-table.html'),
+})
+export class HsFeatureTableComponent implements OnInit {
+  layers: Layer[] = [];
+  constructor(
+    private HsFeatureTableService: HsFeatureTableService,
+    private HsConfig: HsConfig
+  ) {}
+  ngOnInit(): void {
+    for (const layer of this.HsConfig.layersInFeatureTable) {
+      this.addLayerToTable(layer);
+    }
+  }
+  addLayerToTable(layer: Layer): void {
+    const layerDiscriptor = this.HsFeatureTableService.addLayer(layer);
+    if (layerDiscriptor) {
+      this.layers.push(layerDiscriptor);
+    }
+  }
+}
