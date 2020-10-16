104a21f06739785043c927cb16b5bc84cef46e90
jmacura <macurajan@gmail.com>
2020-10-17 01:13:29 +0200

    Fix Layman loading and cancelling ..the same way as Micka

--- components/datasource-selector/layman/layman.service.ts
+++ components/datasource-selector/layman/layman.service.ts
@@ -1,15 +1,20 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {catchError} from 'rxjs/operators';
+import {Subscription} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
 
 import {HsDatasourceLayerDescriptor} from '../datasource-layer-descriptor.interface';
 import {HsEndpoint} from '../../../common/endpoints/endpoint.interface';
+import {HsLogService} from '../../../common/log/log.service';
 import {HsUtilsService} from '../../utils/utils.service';
 
 @Injectable({providedIn: 'root'})
 export class HsLaymanBrowserService {
+  httpCall: Subscription;
+
   constructor(
     private http: HttpClient,
+    private log: HsLogService,
     private hsUtilsService: HsUtilsService
   ) {}
 
@@ -24,44 +29,57 @@ export class HsLaymanBrowserService {
     let url = `${endpoint.url}/rest/${endpoint.user}/layers`;
     url = this.hsUtilsService.proxify(url);
     endpoint.datasourcePaging.loaded = false;
-    /*if (endpoint.canceler !== undefined) {
-      endpoint.canceler.resolve();
-      delete endpoint.canceler;
-    }*/
+    if (this.httpCall !== undefined) {
+      this.httpCall.unsubscribe();
+      delete this.httpCall;
+    }
     //endpoint.canceler = $q.defer();
-    this.http
+    this.httpCall = this.http
       .get(url, {
         //timeout: endpoint.canceler.promise,
         //dataset: endpoint,
         responseType: 'json',
       })
-      .subscribe(this.datasetsReceived, (e) => {
-        endpoint.datasourcePaging.loaded = true;
-      });
+      .pipe(
+        map((x: any) => {
+          x.dataset = endpoint;
+          return x;
+        })
+      )
+      .subscribe(
+        (data) => this.datasetsReceived(data),
+        (e) => {
+          this.log.error(e);
+          endpoint.datasourcePaging.loaded = true;
+        }
+      );
   }
 
   /**
    * @private
    * @function datasetsReceived
-   * @param {object} j HTTP response containing all the layers
+   * @param {object} data HTTP response containing all the layers
    * @description (PRIVATE) Callback for catalogue http query
    */
-  private datasetsReceived(j): void {
-    const dataset = j.config.dataset;
+  private datasetsReceived(data): void {
+    if (!data.dataset) {
+      this.log.error('Malformed data received');
+      return;
+    }
+    const dataset = data.dataset;
     dataset.loading = false;
     dataset.layers = [];
     dataset.datasourcePaging.loaded = true;
-    if (j.data === null) {
+    if (data.data === null) {
       dataset.datasourcePaging.matched == 0;
     } else {
-      j = j.data;
-      dataset.datasourcePaging.matched = j.length;
-      for (const lyr in j) {
-        if (j[lyr]) {
+      dataset.datasourcePaging.matched = data.matched;
+      for (const lyr in data) {
+        if (data[lyr]) {
           const obj = {
-            title: j[lyr].name,
+            title: data[lyr].name,
             type: ['WMS', 'WFS'],
-            name: j[lyr].name,
+            name: data[lyr].name,
           };
           dataset.layers.push(obj);
         }
@@ -77,26 +95,31 @@ export class HsLaymanBrowserService {
    * @description Fills metadata about layer, because layman layer list API provides
    * just name and uuid
    */
-  fillLayerMetadata(dataset: HsEndpoint, layer): Promise<void> {
+  async fillLayerMetadata(
+    dataset: HsEndpoint,
+    layer: HsDatasourceLayerDescriptor
+  ): Promise<HsDatasourceLayerDescriptor> {
     let url = `${dataset.url}/rest/${dataset.user}/layers/${layer.name}`;
     url = this.hsUtilsService.proxify(url);
-    return new Promise((resolve, reject) => {
-      this.http
+    try {
+      return await this.http
         .get(url, {
           //timeout: dataset.canceler.promise,
           //dataset,
           responseType: 'json',
         })
         .toPromise()
-        .then((j: any) => {
-          layer = {...layer, ...j.data};
+        .then((data: any) => {
+          layer = {...layer, ...data};
           if (layer.thumbnail) {
             layer.thumbnail = dataset.url + layer.thumbnail.url;
           }
-          resolve();
-        })
-        .catch((e) => reject(e));
-    });
+          return layer;
+        });
+    } catch (e) {
+      this.log.error(e);
+      return e;
+    }
   }
 
   /**
@@ -107,19 +130,16 @@ export class HsLaymanBrowserService {
    * in a common format for use in add-layers component
    * @description Gets layer metadata and returns promise which describes layer
    */
-  describeWhatToAdd(
+  async describeWhatToAdd(
     ds: HsEndpoint,
     layer: HsDatasourceLayerDescriptor
   ): Promise<any> {
-    return new Promise((resolve, reject) => {
-      this.fillLayerMetadata(ds, layer).then(() => {
-        resolve({
-          type: layer.type,
-          link: layer.wms.url,
-          layer: layer.name,
-          title: layer.name,
-        });
-      });
-    });
+    const lyr = await this.fillLayerMetadata(ds, layer);
+    return {
+      type: lyr.type,
+      link: lyr.wms.url,
+      layer: lyr.name,
+      title: lyr.name,
+    };
   }
 }
