0d113d92ce4fa9774173ad64ce6312eb3534315b
DailisLangovskis <dailis.langovskis@gmail.com>
2020-10-20 11:00:58 +0300

    Added comments to service funtions

--- components/feature-table/feature-table.service.ts
+++ components/feature-table/feature-table.service.ts
@@ -8,10 +8,10 @@ import {Layer} from 'ol/layer';
   providedIn: 'root',
 })
 export class HsFeatureTableService {
-  sortByAttributeNames: any;
-  sortReverse = false;
-  lastSortValue = '';
-  featureAttributeList: any = [];
+  sortingByButtonList: any; //array filled with unique feature attribute names for sorting
+  sortReverse = false; //trigger for reverse sorting
+  lastSortValue = ''; //last sorting value selected
+  featureAttributeList: any = []; //all feature attributes for html table
   constructor(
     private HsUtilsService: HsUtilsService,
     private HsQueryVectorService: HsQueryVectorService
@@ -57,7 +57,7 @@ export class HsFeatureTableService {
   getFeatureAttributes(layer: Layer): void {
     let features = [];
     const featureAttributes = [];
-    this.sortByAttributeNames = ['name'];
+    this.sortingByButtonList = ['name'];
     if (layer.getSource().getSource) {
       features = layer.getSource().getSource().getFeatures();
     } else {
@@ -115,29 +115,47 @@ export class HsFeatureTableService {
   attributesWithoutFeatureName(attributes: any): any {
     return attributes.filter((attr) => attr.name !== 'name');
   }
+  /**
+   * @param feature layers feature from this.featureAttributeList
+   * @ngdoc method
+   * @name HsFeatureTableService#ceateSortingValueArray
+   * @description Create sorting value array from all layers features
+   */
   ceateSortingValueArray(feature): void {
     if (feature?.attributes && feature.attributes.length > 0) {
       this.getSortingValueNames(feature.attributes);
     }
   }
+  /**
+   * @param attributes features attributes array
+   * @ngdoc method
+   * @name HsFeatureTableService#getSortingValueNames
+   * @description get sorting values from each individual feature attributes, if value does not already exist in the this.sortingByButtonList array
+   */
   getSortingValueNames(attributes: Array<unknown>): void {
     if (Array.isArray(attributes)) {
       for (const attribute of attributes) {
         if (
-          this.sortByAttributeNames.length > 0 &&
-          this.sortByAttributeNames !== undefined
+          this.sortingByButtonList.length > 0 &&
+          this.sortingByButtonList !== undefined
         ) {
-          this.sortByAttributeNames.filter((name) => name === attribute.name)
+          this.sortingByButtonList.filter((name) => name === attribute.name)
             .length == 0
-            ? this.sortByAttributeNames.push(attribute.name)
+            ? this.sortingByButtonList.push(attribute.name)
             : [];
         } else {
-          this.sortByAttributeNames.push(attribute.name);
+          this.sortingByButtonList.push(attribute.name);
         }
       }
     }
   }
-  sortFeaturesBy(valueName) {
+  /**
+   * @param valueName Requested value to sort the feature table list
+   * @ngdoc method
+   * @name HsFeatureTableService#sortFeaturesBy
+   * @description Sort features by requested value
+   */
+  sortFeaturesBy(valueName): void {
     if (
       this.featureAttributeList !== undefined &&
       this.featureAttributeList.length > 1
@@ -150,7 +168,16 @@ export class HsFeatureTableService {
       );
     }
   }
-  sortFeatures(a, b, valueName) {
+  /**
+   * @param a First input feature
+   * @param b second input feature
+   * @param valueName Sorting value
+   * @ngdoc method
+   * @name HsFeatureTableService#sortFeatures
+   * @description Sorting algorithm
+   * @returns {number} Returns each features relative position in the table
+   */
+  sortFeatures(a, b, valueName): number {
     let aFeature, bFeature: any;
     let position: number;
     if (valueName === 'name') {
@@ -158,8 +185,8 @@ export class HsFeatureTableService {
       aFeature = a[valueName];
       bFeature = b[valueName];
     } else {
-      aFeature = this.getSortingValue(a.attributes, valueName); //get requested attribute value
-      bFeature = this.getSortingValue(b.attributes, valueName);
+      aFeature = this.getValue(a.attributes, valueName); //get requested attribute value
+      bFeature = this.getValue(b.attributes, valueName);
     }
     if (aFeature === null) {
       position = 1;
@@ -182,7 +209,15 @@ export class HsFeatureTableService {
     this.lastSortValue = valueName;
     return position;
   }
-  getSortingValue(attributes: any, valueName: string): string | number {
+  /**
+   * @param attributes features attributes
+   * @param valueName Sorting value
+   * @ngdoc method
+   * @name HsFeatureTableService#getValue
+   * @description Get requested features attribute value, which will be used in the sorting algorithm
+   * @returns {number | string} Returns attributes value
+   */
+  getValue(attributes: any, valueName: string): string | number {
     let value = attributes //get requested attribute value
       .filter((attr) => attr.name == valueName)
       .map((attr) => attr.value);
