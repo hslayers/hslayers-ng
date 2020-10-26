6cd9fd6a7429f8edd47564e4ab17a1b90a7474cb
raitis <raitisbe@gmail.com>
2020-10-27 00:18:48 +0200

    Add save function in compositions overwrite dialog

--- components/compositions/dialogs/overwrite-dialog.component.ts
+++ components/compositions/dialogs/overwrite-dialog.component.ts
@@ -2,6 +2,7 @@ import {Component, ViewRef} from '@angular/core';
 import {HsCompositionsService} from '../compositions.service';
 import {HsDialogComponent} from '../../layout/dialogs/dialog-component.interface';
 import {HsDialogContainerService} from '../../layout/dialogs/dialog-container.service';
+import {HsSaveMapManagerService} from '../../save-map/save-map-manager.service';
 @Component({
   selector: 'hs.compositions-overwrite-dialog',
   template: require('./dialog_overwriteconfirm.html'),
@@ -13,7 +14,8 @@ export class HsCompositionsOverwriteDialogComponent
 
   constructor(
     private HsDialogContainerService: HsDialogContainerService,
-    private HsCompositionsService: HsCompositionsService
+    private HsCompositionsService: HsCompositionsService,
+    private HsSaveMapManagerService: HsSaveMapManagerService
   ) {}
 
   close(): void {
@@ -30,6 +32,16 @@ export class HsCompositionsOverwriteDialogComponent
       this.HsCompositionsService.compositionToLoad.url,
       true
     );
+    this.close();
+  }
+
+  /**
+   * @ngdoc method
+   * @public
+   * @description Save currently loaded composition first
+   */
+  save() {
+    this.HsSaveMapManagerService.openPanel(null);
     this.close();
   }
 
