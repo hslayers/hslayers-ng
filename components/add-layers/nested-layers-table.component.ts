51afd9c15dfeff46fe29145cc55f3e1e434ea084
jmacura <macurajan@gmail.com>
2020-11-03 18:52:13 +0100

    Upgrade add-layers templates to ng9

--- components/add-layers/nested-layers-table.component.ts
+++ components/add-layers/nested-layers-table.component.ts
@@ -1,10 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'hs-nested-layers-table',
   template: require('./partials/nested-layers-table.directive.html'),
 })
 export class HsNestedLayersTableComponent {
-  layers;
+  @Input() layers;
   constructor() {}
 }
