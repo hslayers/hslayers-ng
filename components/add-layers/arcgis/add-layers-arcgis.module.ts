4f212ee66ac253f644df9977180def96e49c2002
jmacura <macurajan@gmail.com>
2020-11-03 18:40:43 +0100

    Upgrade add-layers-arcgis files to ng9

--- components/add-layers/arcgis/add-layers-arcgis.module.ts
+++ components/add-layers/arcgis/add-layers-arcgis.module.ts
@@ -4,12 +4,13 @@ import {FormsModule} from '@angular/forms';
 import {TranslateModule} from '@ngx-translate/core';
 
 import {HsAddLayersArcGisComponent} from './add-layers-arcgis.component';
+import {HsAddLayersArcGisService} from './add-layers-arcgis.service';
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [CommonModule, FormsModule, TranslateModule],
-  exports: [],
-  declarations: [/*HsAddLayersArcGisComponent*/],
-  providers: [],
+  exports: [HsAddLayersArcGisComponent],
+  declarations: [HsAddLayersArcGisComponent],
+  providers: [HsAddLayersArcGisService],
 })
 export class HsAddLayersArcGisModule {}
