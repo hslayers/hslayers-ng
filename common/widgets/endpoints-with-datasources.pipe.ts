22ee539bc74438d5b07d424671cc6429048aafbb
jmacura <macurajan@gmail.com>
2020-11-04 22:19:00 +0100

    Enhance endpointsWithDatasources pipe
    extend endpointsWithDatasourcesPipe functionality
    by allowing to also exclude item from an array

--- common/widgets/endpoints-with-datasources.pipe.ts
+++ common/widgets/endpoints-with-datasources.pipe.ts
@@ -4,7 +4,18 @@ import {Pipe, PipeTransform} from '@angular/core';
   name: 'endpointsWithDatasources',
 })
 export class EndpointsWithDatasourcesPipe implements PipeTransform {
-  transform(endpoints: any[], typeToFilter = 'statusmanager'): any[] {
-    return endpoints.filter((ep) => ep.type != typeToFilter);
+  transform(
+    endpoints: any[],
+    typeToFilter = 'statusmanager',
+    filterType = 'exclusive'
+  ): any[] {
+    switch (filterType) {
+      case 'exclusive':
+        return endpoints.filter((ep) => ep.type != typeToFilter);
+      case 'inclusive':
+        return endpoints.filter((ep) => ep.type == typeToFilter);
+      default:
+        return endpoints;
+    }
   }
 }
