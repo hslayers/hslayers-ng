97fccdbd78b6288215e68a0b1258af7ea4f1755a
raitis <raitisbe@gmail.com>
2020-11-11 01:20:52 +0200

    Customizable format of dimension and provide default value

--- components/layermanager/dimensions/dimension.class.ts
+++ components/layermanager/dimensions/dimension.class.ts
@@ -1,16 +1,27 @@
+import moment = require('moment');
 export class HsDimensionDescriptor {
   public type: string;
   public value: any;
   public modelValue: any;
   public label?: string;
   public values?: Array<any>;
+  public format: string;
 
+  //Original dimension object can be shared between multiple layers.
   constructor(public name: string, public originalDimension: any) {
     this.type = this.originalDimension.type;
     this.value = this.originalDimension.value;
     this.modelValue = this.originalDimension.value;
+    if (this.type == 'date' || this.type == 'datetime') {
+      this.modelValue = {
+        year: this.value.getFullYear(),
+        month: this.value.getMonth() + 1,
+        day: this.value.getDate(),
+      };
+    }
     this.label = this.originalDimension.label;
     this.values = this.originalDimension.values;
+    this.format = this.originalDimension.format;
   }
 
   /* This is needed because ng-bootstrap datepicker sets value to
@@ -27,5 +38,11 @@ export class HsDimensionDescriptor {
     } else {
       this.originalDimension.value = this.modelValue;
     }
+    if (this.type == 'datetime' && this.format) {
+      this.originalDimension.value = moment(
+        this.originalDimension.value
+      ).format(this.format);
+    }
+    this.value = this.originalDimension.value;
   }
 }
