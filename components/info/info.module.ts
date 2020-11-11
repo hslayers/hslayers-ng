f564594ea254791a41c8ce9d4b162a6ebb93c142
DailisLangovskis <dailis.langovskis@gmail.com>
2020-11-11 17:47:49 +0200

    Added translations to info component

--- components/info/info.module.ts
+++ components/info/info.module.ts
@@ -7,11 +7,12 @@ import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {HsInfoComponent} from './info.component';
 import {HsPanelHelpersModule} from '../layout/panels/panel-helpers.module';
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
   declarations: [HsInfoComponent],
-  imports: [FormsModule, CommonModule, HsPanelHelpersModule],
+  imports: [FormsModule, CommonModule, HsPanelHelpersModule, TranslateModule],
   exports: [HsInfoComponent],
   providers: [],
   entryComponents: [HsInfoComponent],
