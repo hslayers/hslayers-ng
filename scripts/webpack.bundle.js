6101dbd3675c98b101d138721b8a57f1368a384b
Filip Leitner <leitnerfilip@gmail.com>
2020-11-12 15:11:46 +0100

    Make expected scss overrides file name more specific
    Renamed to hsl-custom.scss

--- scripts/webpack.bundle.js
+++ scripts/webpack.bundle.js
@@ -82,8 +82,10 @@ module.exports = merge(common, {
           {
             loader: 'sass-loader',
             options: {
-              additionalData: fs.existsSync(scssOverridesPath + 'custom.scss')
-                ? `@use "${scssOverridesPath}custom.scss" as *;`
+              additionalData: fs.existsSync(
+                scssOverridesPath + 'hsl-custom.scss'
+              )
+                ? `@use "${scssOverridesPath}hsl-custom.scss" as *;`
                 : '',
             },
           },
