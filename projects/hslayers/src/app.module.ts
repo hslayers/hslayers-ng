50c003e4bfc9b788abe1207987a30bcb842d7c34
raitis <raitisbe@gmail.com>
2020-11-17 14:26:13 +0200

    Add HsLayoutModule import back

--- projects/hslayers/src/app.module.ts
+++ projects/hslayers/src/app.module.ts
@@ -2,9 +2,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HsBootstrapComponent} from './bootstrap.component';
 import {HsCoreModule} from './components/core';
+import { HsLayoutModule } from './components/layout';
 
 @NgModule({
-  imports: [BrowserModule, HsCoreModule],
+  imports: [BrowserModule, HsCoreModule, HsLayoutModule],
   exports: [HsBootstrapComponent],
   declarations: [HsBootstrapComponent],
   entryComponents: [],
