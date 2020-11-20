34fbd3355edd5458038e19a1179cb45476b09372
Filip Leitner <leitnerfilip@gmail.com>
2020-11-20 13:31:39 +0100

    Add advancedForm config param

--- components/save-map/save-map-simple-form.component.ts
+++ components/save-map/save-map-simple-form.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {HsLayerUtilsService} from '../utils/layer-utils.service';
 @Component({
-  selector: 'hs.save-map-simple-form',
+  selector: 'hs-save-map-simple-form',
   template: require('./partials/simpleform.html'),
 })
 export class HsSaveMapSimpleFormComponent {
