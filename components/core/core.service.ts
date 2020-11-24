ab2a7e1e4766370961d6cbcdce1bfa50ac48fc56
raitis <raitisbe@gmail.com>
2020-11-24 18:07:06 +0200

    Linting

--- components/core/core.service.ts
+++ components/core/core.service.ts
@@ -1,11 +1,10 @@
-import {DOCUMENT} from '@angular/common';
 import {HsConfig} from '../../config.service';
 import {HsEventBusService} from './event-bus.service';
 import {HsLayoutService} from '../layout/layout.service';
 import {HsLogService} from '../../common/log/log.service';
 import {HsMapService} from '../map/map.service';
 import {HsUtilsService} from '../utils/utils.service';
-import {Inject, Injectable} from '@angular/core';
+import {Injectable} from '@angular/core';
 
 import {TranslateService} from '@ngx-translate/core';
 
