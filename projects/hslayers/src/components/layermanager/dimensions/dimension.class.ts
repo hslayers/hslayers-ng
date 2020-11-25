e94311930e654880e65892e4f115aba42621b427
raitis <raitisbe@gmail.com>
2020-11-25 11:33:32 +0200

    Add time picker for datetime dimension

--- projects/hslayers/src/components/layermanager/dimensions/dimension.class.ts
+++ projects/hslayers/src/components/layermanager/dimensions/dimension.class.ts
@@ -6,13 +6,25 @@ export class HsDimensionDescriptor {
   public label?: string;
   public values?: Array<any>;
   public format: string;
+  public modelTimeValue: { hour: any; minute: any; };
 
   //Original dimension object can be shared between multiple layers.
   constructor(public name: string, public originalDimension: any) {
     this.type = this.originalDimension.type;
     this.value = this.originalDimension.value;
     this.modelValue = this.originalDimension.value;
-    if (this.type == 'date' || this.type == 'datetime') {
+    if (this.type == 'datetime') {
+      this.modelValue = {
+        year: this.value.getFullYear(),
+        month: this.value.getMonth() + 1,
+        day: this.value.getDate()
+      };
+      this.modelTimeValue = {
+        hour: this.value.getHours(),
+        minute: this.value.getMinutes(),
+      };
+    }
+    if (this.type == 'date') {
       this.modelValue = {
         year: this.value.getFullYear(),
         month: this.value.getMonth() + 1,
@@ -33,12 +45,14 @@ export class HsDimensionDescriptor {
       this.originalDimension.value = new Date(
         this.modelValue.year,
         this.modelValue.month - 1,
-        this.modelValue.day
+        this.modelValue.day,
+        this.modelTimeValue?.hour,
+        this.modelTimeValue?.minute
       );
     } else {
       this.originalDimension.value = this.modelValue;
     }
-    if (this.type == 'datetime' && this.format) {
+    if (['datetime', 'date'].includes(this.type) && this.format) {
       this.originalDimension.value = moment(
         this.originalDimension.value
       ).format(this.format);
