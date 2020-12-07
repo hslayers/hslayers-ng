fd33f554408d227ed78449e71b9d20acc002ad27
DailisLangovskis <dailis.langovskis@gmail.com>
2020-12-07 13:41:18 +0200

    Added hslayers-proxy project

---
+++ projects/hslayers-proxy/src/proxy.js
@@ -0,0 +1,37 @@
+const querystring = require('querystring');
+// Listen on a specific host via the HOST environment variable
+const host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+const port = process.env.PROXY_PORT || 8085;
+
+const cors_proxy = require('cors-anywhere').createServer({
+  originWhitelist: [], // Allow all origins
+});
+const GEONAMES_APIKEY = process.env.HS_GEONAMES_API_KEY || 'hslayersng';
+
+require('http')
+  .createServer((req, res) => {
+    try {
+      req.url = req.url.replace('/proxy', '');
+      if (req.url.indexOf('api.geonames.org/searchJSON') > -1) {
+        const params = querystring.decode(req.url.split('?')[1]);
+        if (
+          typeof params.provider == 'undefined' ||
+          params.provider == 'geonames'
+        ) {
+          req.url = `/http://api.geonames.org/searchJSON?name_startsWith=${encodeURIComponent(
+            params.name_startsWith
+          )}&username=${GEONAMES_APIKEY}`;
+        }
+      }
+      cors_proxy.emit('request', req, res);
+    } catch (ex) {
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.write('Invalid request');
+      res.write(ex);
+      res.end();
+    }
+  })
+  .listen(port, host, () => {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+  });
