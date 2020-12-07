0c0451d0733754770dfb79dccf8e45a2eedc717a
raitis <raitisbe@gmail.com>
2020-12-07 20:47:23 +0200

    Configure angular cli for app project using hmr

---
+++ projects/hslayers-app/src/main.ts
@@ -0,0 +1,17 @@
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
+import {hmrBootstrap} from './hmr';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
+if (environment.hmr) {
+  hmrBootstrap(module, bootstrap);
+} else {
+  bootstrap().catch((err) => console.log(err));
+}
