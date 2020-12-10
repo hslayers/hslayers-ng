2210f50d3ef39aa79c02d2541171100024e7b40e
Filip Leitner <leitnerfilip@gmail.com>
2020-12-10 09:42:58 +0100

    Provide functionality for WMTS adding through add-layers

--- components/add-layers/wmts/add-layers-wmts.component.js
+++ components/add-layers/wmts/add-layers-wmts.component.js
@@ -1,15 +1,24 @@
 import '../../../common/get-capabilities.module';
+import WMTS, {optionsFromCapabilities} from 'ol/source/WMTS';
 import WMTSCapabilities from 'ol/format/WMTSCapabilities';
 import WMTSTileGrid from 'ol/tilegrid/WMTS';
 import {Tile} from 'ol/layer';
-import {WMTS} from 'ol/source';
 import {addAnchors} from '../../../common/attribution-utils';
 import {get} from 'ol/proj';
 import {getTopLeft, getWidth} from 'ol/extent';
+import {transform, transformExtent} from 'ol/proj';
 
 export default {
   template: require('./add-wmts-layer.directive.html'),
-  controller: function ($scope, HsMapService, $compile, HsLayoutService, $log) {
+  controller: function (
+    $scope,
+    HsMapService,
+    $compile,
+    HsLayoutService,
+    $log,
+    HsWmtsGetCapabilitiesService,
+    HsUtilsService
+  ) {
     'ngInject';
     $scope.map_projection = HsMapService.map
       .getView()
@@ -20,30 +29,35 @@ export default {
     $scope.tileMatrixSet = '';
     $scope.image_format = '';
 
+    $scope.connect = function () {
+      try {
+        HsWmtsGetCapabilitiesService.requestGetCapabilities($scope.url);
+      } catch (e) {
+        console.warn(e);
+      }
+      $scope.showDetails = true;
+    };
+
+    $scope.$on('ows.wmts_connecting', (event, url, layer) => {
+      $scope.layerToAdd = layer;
+      $scope.setUrlAndConnect(url);
+    });
+
+    $scope.setUrlAndConnect = function (url) {
+      $scope.url = url;
+      $scope.connect();
+    };
+
     $scope.capabilitiesReceived = function (response) {
       try {
         const parser = new WMTSCapabilities();
-        $scope.capabilities = parser.read(response);
-        const caps = $scope.capabilities;
+        const caps = parser.read(response);
+        $scope.caps = caps;
         $scope.title = caps.ServiceIdentification.Title;
-        $scope.tileURL = caps.OperationsMetadata.GetTile.DCP.HTTP.Get[0].href;
-        for (
-          let idx = 0;
-          idx < caps.OperationsMetadata.GetTile.DCP.HTTP.Get.length;
-          idx++
-        ) {
-          if (
-            caps.OperationsMetadata.GetTile.DCP.HTTP.Get[idx].Constraint[0]
-              .AllowedValues.Value[0] == 'KVP'
-          ) {
-            $scope.tileURL =
-              caps.OperationsMetadata.GetTile.DCP.HTTP.Get[idx].href;
-            break;
-          }
-        }
+
         $scope.description = addAnchors(caps.ServiceIdentification.Abstract);
         $scope.version = caps.Version || caps.version;
-        $scope.services = caps.Contents;
+        $scope.services = caps.Contents.Layer;
       } catch (e) {
         if (console) {
           $log.log(e);
@@ -70,89 +84,100 @@ export default {
       $scope.capabilitiesReceived(response.data);
     });
 
-    /**
-     * @function setCurrentLayer
-     * @memberOf hs.addLayersWmts.controller
-     * @description Opens detailed view for manipulating layer
-     * @param {object} layer - Wrapped layer to edit or view
-     * @param {number} index - Used to position the detail panel after layers li element
-     */
-    $scope.setCurrentLayer = function (layer, index) {
-      if ($scope.currentLayer == layer) {
-        $scope.currentLayer = null;
-      } else {
-        $scope.currentLayer = layer;
-        const wmtsLayerPanel = HsLayoutService.contentWrapper.querySelector(
-          '.hs-wmts-layerpanel'
-        );
-        const layerNode = HsLayoutService.contentWrapper.querySelector(
-          '#wmtslayer-' + index
-        );
-        if (wmtsLayerPanel) {
-          layerNode.parentNode.insertBefore(
-            wmtsLayerPanel,
-            layerNode.nextSibling
-          );
+
+    $scope.checked = function () {
+      for (const layer of $scope.services) {
+        if (layer.checked) {
+          return true;
         }
       }
+      return false;
+    };
+    $scope.changed = function () {
+      $scope.isChecked = $scope.checked();
     };
 
     /**
      * @function addLayer
+     * @param checked
      * @memberOf hs.addLayersWmts.controller
      * @description Add layer to map
      * @param {object} layer - Wrapped layer to add
      */
+    $scope.tryAddLayers = function (checked) {
+      $scope.add_all = checked;
+      $scope.addLayers(checked);
+      return;
+    };
 
-    $scope.addLayer = function (layer) {
-      const projection = get($scope.map_projection);
-      const projectionExtent = projection.getExtent();
-      for (let idx = 0; idx < $scope.services.TileMatrixSet.length; idx++) {
-        if (
-          $scope.services.TileMatrixSet[idx].Identifier == $scope.tileMatrixSet
-        ) {
-          $scope.layerTileMatrix = $scope.services.TileMatrixSet[idx];
+    $scope.addLayers = function (checked) {
+      /**
+       * @param layer
+       */
+      function recurse(layer) {
+        if (!checked || layer.checked) {
+          $scope.addLayer(layer);
+        }
+
+        angular.forEach(layer.Layer, (sublayer) => {
+          recurse(sublayer);
+        });
+      }
+      angular.forEach($scope.services, (layer) => {
+        recurse(layer);
+      });
+    };
+
+    $scope.getPreferedFormat = function (formats) {
+      const prefered = formats.find((format) => format.includes('png'));
+      return prefered ? prefered : formats[0];
+    };
+
+    $scope.getPreferedMatrixSet = function (sets) {
+      const supportedFormats = ['3857', '4326', '5514'];
+      const prefered = sets.filter((set) =>
+        supportedFormats.some((v) => set.TileMatrixSet.includes(v))
+      );
+      const prefer3857 = prefered.find((set) =>
+        set.TileMatrixSet.includes('3857')
+      );
+      return prefer3857 ? prefer3857.TileMatrixSet : prefered[0].TileMatrixSet;
+    };
+
+    $scope.getPreferedInfoFormat = function (formats) {
+      if (formats) {
+        const supportedFormats = ['html', 'xml', 'gml'];
+        const infos = formats.filter(
+          (format) =>
+            format.resourceType == 'FeatureInfo' &&
+            supportedFormats.some((v) => format.format.includes(v))
+        );
+        if (infos.length != 0) {
+          const preferHTML = infos.find((format) =>
+            format.format.includes('html')
+          );
+          return preferHTML ? preferHTML.format : infos[0].format;
         }
       }
-      const size =
-        getWidth(projectionExtent) /
-        $scope.layerTileMatrix.TileMatrix[0].TileWidth;
-      const resolutions = new Array($scope.layerTileMatrix.TileMatrix.length);
-      const matrixIds = new Array($scope.layerTileMatrix.TileMatrix.length);
-      for (let z = 0; z < $scope.layerTileMatrix.TileMatrix.length; ++z) {
-        // generate resolutions and matrixIds arrays for this WMTS
-        resolutions[z] = size / Math.pow(2, z);
-        matrixIds[z] = z;
-      }
+    };
 
-      const dimensions = {};
-
-      angular.forEach(layer.Dimension, (val) => {
-        dimensions[val.name] = val;
-      });
-
-      const new_layer = new Tile({
+    $scope.addLayer = function (layer) {
+      const wmts = new Tile({
         title: layer.Title,
-        source: new WMTS({
-          url: $scope.tileURL,
-          layer: layer.Identifier,
-          projection: projection,
-          matrixSet: 'EPSG:3857',
-          format: $scope.image_format,
-          tileGrid: new WMTSTileGrid({
-            origin: getTopLeft(projectionExtent),
-            resolutions: resolutions,
-            matrixIds: matrixIds,
-          }),
-          style: $scope.style,
-          wrapX: true,
-        }),
-        saveState: true,
-        removable: true,
-        dimensions: dimensions,
+        info_format: $scope.getPreferedInfoFormat(layer.ResourceURL),
+        source: new WMTS({}),
       });
-
-      HsMapService.addLayer(new_layer, true);
+      // Get WMTS Capabilities and create WMTS source base on it
+      const options = optionsFromCapabilities($scope.caps, {
+        layer: layer.Identifier,
+        matrixSet: $scope.getPreferedMatrixSet(layer.TileMatrixSetLink),
+        format: $scope.getPreferedFormat(layer.Format),
+      });
+      // WMTS source for raster tiles layer
+      const wmtsSource = new WMTS(options);
+      // set the data source for raster and vector tile layers
+      wmts.setSource(wmtsSource);
+      HsMapService.addLayer(wmts, true);
     };
   },
 };
