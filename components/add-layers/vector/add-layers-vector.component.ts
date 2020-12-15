cd1d2d4bc0a73c31d6728c514d851804771154c2
DailisLangovskis <dailis.langovskis@gmail.com>
2020-12-15 15:13:29 +0200

    Add files only after add btn click

--- components/add-layers/vector/add-layers-vector.component.ts
+++ components/add-layers/vector/add-layers-vector.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {HsAddLayersVectorService} from './add-layers-vector.service';
 import {HsHistoryListService} from '../../../common/history-list/history-list.service';
 import {HsLayoutService} from '../../layout/layout.service';
+
 @Component({
   selector: 'hs-add-layers-vector',
   template: require('./add-vector-layer.directive.html'),
@@ -12,10 +13,16 @@ export class HsAddLayersVectorComponent {
   extract_styles = false;
   abstract: string;
   url: string;
+  base64url: string;
   name = '';
   advancedPanelVisible = false;
   folder_name = '';
   dropzoneActive = false;
+  features: any[] = [];
+  featureCount = 0;
+  type = '';
+  fileUploadEnabled = false;
+  errorOccured = false;
   constructor(
     public hsAddLayersVectorService: HsAddLayersVectorService,
     public hsHistoryListService: HsHistoryListService,
@@ -34,24 +41,83 @@ export class HsAddLayersVectorComponent {
    */
   async add() {
     const layer = await this.hsAddLayersVectorService.addVectorLayer(
-      '',
-      this.url,
+      this.type,
+      this.url || this.base64url,
       this.name,
       this.title,
       this.abstract,
       this.srs,
-      {extractStyles: this.extract_styles}
+      {extractStyles: this.extract_styles, features: this.features}
     );
     this.hsAddLayersVectorService.fitExtent(layer);
     this.hsLayoutService.setMainPanel('layermanager');
+    this.setToDefault();
     return layer;
   }
   dropZoneState($event: boolean): void {
     this.dropzoneActive = $event;
   }
-  handleFileUpload(fileList: FileList): void {
-    Array.from(fileList).forEach((f) => {
-      this.hsAddLayersVectorService.readUploadedFile(f);
+  handleFileUpload(fileList: FileList): any {
+    this.fileUploadEnabled = true;
+    Array.from(fileList).forEach(async (f) => {
+      const uploadedData = await this.hsAddLayersVectorService.readUploadedFile(
+        f
+      );
+      if (uploadedData !== undefined) {
+        uploadedData.type !== undefined
+          ? (this.type = uploadedData.type)
+          : (this.type = '');
+
+        uploadedData.url !== undefined
+          ? (this.base64url = uploadedData.url)
+          : ((this.url = ''), (this.base64url = ''));
+
+        uploadedData.name !== undefined
+          ? (this.name = uploadedData.name)
+          : (this.name = '');
+
+        uploadedData.title !== undefined
+          ? (this.title = uploadedData.title)
+          : (this.title = '');
+
+        uploadedData.srs !== undefined
+          ? (this.srs = uploadedData.srs)
+          : (this.srs = '');
+
+        uploadedData.abstract !== undefined
+          ? (this.abstract = uploadedData.abstract)
+          : (this.abstract = '');
+
+        if (uploadedData.features !== undefined) {
+          this.features = uploadedData.features;
+          this.featureCount = uploadedData.features.length;
+        } else {
+          this.features = [];
+          this.featureCount = 0;
+        }
+      } else {
+        this.setToDefault();
+        this.errorOccured = true;
+        setTimeout(() => {
+          this.errorOccured = false;
+        }, 3000);
+      }
     });
+  }
+  setToDefault(): void {
+    this.srs = 'EPSG:4326';
+    this.title = '';
+    this.extract_styles = false;
+    this.abstract = '';
+    this.url = '';
+    this.base64url = '';
+    this.name = '';
+    this.advancedPanelVisible = false;
+    this.folder_name = '';
+    this.dropzoneActive = false;
+    this.features = [];
+    this.featureCount = 0;
+    this.type = '';
+    this.fileUploadEnabled = false;
   }
 }
