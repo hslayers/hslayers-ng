f196e09f401b08fc908f9cf71a637346f8e98ec1
jmacura <macurajan@gmail.com>
2020-12-16 15:23:13 +0100

    Fix hs-nested-layers-table dependency

--- components/add-layers/common/nested-layers-table.component.ts
+++ components/add-layers/common/nested-layers-table.component.ts
@@ -2,7 +2,7 @@ import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'hs-nested-layers-table',
-  template: require('./partials/nested-layers-table.directive.html'),
+  template: require('../partials/nested-layers-table.directive.html'),
 })
 export class HsNestedLayersTableComponent {
   @Input() layers;
