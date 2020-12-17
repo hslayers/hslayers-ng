e44c9bcb7b276ba007ec020125e7585b7169072c
raitis <raitisbe@gmail.com>
2020-12-17 13:08:28 +0200

    Port of https://github.com/hslayers/hslayers-ng/pull/1383

--- projects/hslayers/src/components/add-layers/common/add-layers-common.module.ts
+++ projects/hslayers/src/components/add-layers/common/add-layers-common.module.ts
@@ -1,12 +1,15 @@
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {TranslateModule} from '@ngx-translate/core';
+
 import {HsAddLayersTargetPositionComponent} from './add-layers-target-position.component';
 import {HsAddLayersUrlComponent} from './add-layers-url.component';
 import {HsGetCapabilitiesErrorComponent} from './capabilities-error-dialog.component';
 import {HsHistoryListModule} from '../../../common/history-list/history-list.module';
-import {NgModule} from '@angular/core';
-import {TranslateModule} from '@ngx-translate/core';
+import {HsNestedLayersTableComponent} from './nested-layers-table.component';
+
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [CommonModule, FormsModule, TranslateModule, HsHistoryListModule],
@@ -14,12 +17,15 @@ import {TranslateModule} from '@ngx-translate/core';
     HsAddLayersUrlComponent,
     HsAddLayersTargetPositionComponent,
     HsGetCapabilitiesErrorComponent,
+    HsNestedLayersTableComponent,
   ],
   declarations: [
     HsAddLayersUrlComponent,
     HsAddLayersTargetPositionComponent,
     HsGetCapabilitiesErrorComponent,
+    HsNestedLayersTableComponent,
   ],
   providers: [],
+  entryComponents: [HsGetCapabilitiesErrorComponent],
 })
 export class HsAddLayersCommonModule {}
--- projects/hslayers/src/components/add-layers/common/nested-layers-table.component.ts
+++ projects/hslayers/src/components/add-layers/common/nested-layers-table.component.ts
@@ -2,7 +2,7 @@ import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'hs-nested-layers-table',
-  templateUrl: './partials/nested-layers-table.directive.html',
+  templateUrl: './nested-layers-table.directive.html',
 })
 export class HsNestedLayersTableComponent {
   @Input() layers;
