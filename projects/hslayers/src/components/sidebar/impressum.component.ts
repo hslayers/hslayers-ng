f700f649502dbdfbcab5abd5ffb0887feb6758a7
DailisLangovskis <dailis.langovskis@gmail.com>
2020-12-22 15:17:58 +0200

    Try to hide logo after layout size update

--- projects/hslayers/src/components/sidebar/impressum.component.ts
+++ projects/hslayers/src/components/sidebar/impressum.component.ts
@@ -1,6 +1,6 @@
-import {Component} from '@angular/core';
-
 import * as packageJson from '../../package.json';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {HsEventBusService} from '../core/event-bus.service';
 import {HsUtilsService} from '../utils/utils.service';
 
 @Component({
@@ -8,10 +8,28 @@ import {HsUtilsService} from '../utils/utils.service';
   templateUrl: './partials/impressum.html',
 })
 export class HsImpressumComponent {
+  @ViewChild('hslogo') hsLogo: ElementRef;
   version = 'dev';
   logo = '';
   logoDisabled = false;
-  constructor(public hsUtilsService: HsUtilsService) {
+  logoHeight: number;
+  constructor(
+    public hsUtilsService: HsUtilsService,
+    public HsEventBusService: HsEventBusService,
+    public el: ElementRef
+  ) {
     this.version = packageJson.version;
+    this.HsEventBusService.sizeChanges.subscribe(() => {
+      this.getLogoHeight();
+      if (this.el.nativeElement.offsetHeight < this.logoHeight) {
+        this.logoDisabled = true;
+      }
+    });
+  }
+  ngAfterViewInit(): void {
+    this.getLogoHeight();
+  }
+  getLogoHeight(): void {
+    this.logoHeight = this.hsLogo.nativeElement.clientHeight;
   }
 }
