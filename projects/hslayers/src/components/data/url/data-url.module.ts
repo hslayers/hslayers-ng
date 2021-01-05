1143467da76a480fbd3826b69218782410037fda
Filip Leitner <leitnerfilip@gmail.com>
2021-01-05 14:03:57 +0100

    Move arcgis module

--- projects/hslayers/src/components/data/url/data-url.module.ts
+++ projects/hslayers/src/components/data/url/data-url.module.ts
@@ -5,6 +5,7 @@ import {
 } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
+import {HsDataArcGisModule} from './arcgis/data-url-arcgis.module';
 import {HsDataUrlComponent} from './data-url-component';
 import {HsDataUrlWmsModule} from '../url/wms/data-url-wms.module';
 import {TranslateModule} from '@ngx-translate/core';
@@ -12,7 +13,13 @@ import {TranslateModule} from '@ngx-translate/core';
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
   declarations: [HsDataUrlComponent],
-  imports: [CommonModule, FormsModule, TranslateModule, HsDataUrlWmsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    TranslateModule,
+    HsDataUrlWmsModule,
+    HsDataArcGisModule,
+  ],
   exports: [HsDataUrlComponent],
   providers: [],
   entryComponents: [HsDataUrlComponent],
