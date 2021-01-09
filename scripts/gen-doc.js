b5005525857522a9f01fc6c779c00c7be61a7ec3
jmacura <macurajan@gmail.com>
2021-01-09 01:19:43 +0100

    📝 Add custom documentation script
    = boost up the documentation with a common homepage
    parsed from a README file

---
+++ scripts/gen-doc.js
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require('fs');
+const path = require('path');
+// 'handlebars' and 'marked' are dependencies of 'compodoc'
+// so as long as we use compodoc
+// these are installed in node_modules automagically
+const Handlebars = require('handlebars');
+const marked = require('marked');
+
+// Load README.md
+const readmeMd = fs.readFileSync(
+  path.resolve(__dirname, '../README.md'),
+  'utf-8'
+);
+
+// Parse Markdown syntax to HTML syntax
+const readmeHtml = marked(readmeMd);
+
+// Load Handlebars-style template to be filled
+const index = fs.readFileSync(
+  path.resolve(__dirname, './docs_index_template.html'),
+  'utf-8'
+);
+
+// Compile Handlebars-style template to function
+const indexTemplate = Handlebars.compile(index, {
+  preventIndent: true,
+  noEscape: true,
+  strict: true,
+});
+
+// Fill-in the index template with readme
+const indexExtended = indexTemplate({'readme': readmeHtml});
+
+// Save the filled-in index to drive
+fs.writeFileSync(path.resolve(__dirname, '../docs/index.html'), indexExtended);
+
+console.log('Documentation common homepage generated');
