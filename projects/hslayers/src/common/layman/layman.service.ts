a2a57637ee088213884fed5bd905966107af9fdf
raitis <raitisbe@gmail.com>
2021-01-13 14:16:14 +0200

    Small docs fix

--- projects/hslayers/src/common/layman/layman.service.ts
+++ projects/hslayers/src/common/layman/layman.service.ts
@@ -13,7 +13,7 @@ export class HsCommonLaymanService {
    * @description Monitor if authorization state has changed and
    * return true and broadcast authChange event if so .
    * @param endpoint Endpoint definition - usually Layman
-   * @returns true
+   * @returns Promise<boolean> true if authorization state changed (user logged in or out)
    */
   async detectAuthChange(endpoint): Promise<boolean> {
     const url = `${endpoint.url}/rest/current-user`;
