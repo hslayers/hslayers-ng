834964f0b717d8c65758dbcea1577812f853cc34
jmacura <jmacura@users.noreply.github.com>
2021-01-13 23:46:33 +0100

    Fix little TSDoc issues
    Apply suggestions from code review

--- projects/hslayers/src/common/layman/layman.service.ts
+++ projects/hslayers/src/common/layman/layman.service.ts
@@ -10,9 +10,9 @@ export class HsCommonLaymanService {
   constructor(private $http: HttpClient) {}
 
   /**
-   * @description Monitor if authorization state has changed and
+   *  Monitor if authorization state has changed and
    * return true and broadcast authChange event if so .
-   * @param endpoint Endpoint definition - usually Layman
+   * @param endpoint - Endpoint definition - usually Layman
    * @returns Promise<boolean> true if authorization state changed (user logged in or out)
    */
   async detectAuthChange(endpoint): Promise<boolean> {
