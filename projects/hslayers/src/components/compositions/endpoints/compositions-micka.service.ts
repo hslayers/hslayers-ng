73bccc08f493efc8505532464bac49abe263c938
DailisLangovskis <dailis.langovskis@gmail.com>
2021-01-15 20:46:09 +0200

    Added compositions list item component
    Use observables instead of promises
    to return compositionEntries

--- projects/hslayers/src/components/compositions/endpoints/compositions-micka.service.ts
+++ projects/hslayers/src/components/compositions/endpoints/compositions-micka.service.ts
@@ -1,25 +1,31 @@
+import {HsEndpoint} from './../../../common/endpoints/endpoint.interface';
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 
 import Feature from 'ol/Feature';
 import {fromExtent as polygonFromExtent} from 'ol/geom/Polygon';
 
+import {EMPTY, Observable, Subscription} from 'rxjs';
 import {HsCompositionsParserService} from '../compositions-parser.service';
+import {HsLogService} from '../../../common/log/log.service';
 import {HsMapService} from '../../map/map.service';
 import {HsUtilsService} from '../../utils/utils.service';
+import {catchError, map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HsCompositionsMickaService {
+  listLoading: Subscription;
   constructor(
     public HsCompositionsParserService: HsCompositionsParserService,
     private $http: HttpClient,
     public HsMapService: HsMapService,
-    public HsUtilsService: HsUtilsService
+    public HsUtilsService: HsUtilsService,
+    public log: HsLogService
   ) {}
 
-  getCompositionsQueryUrl(endpoint, params, bbox) {
+  getCompositionsQueryUrl(endpoint, params, bbox): string {
     const query = params.query;
     const bboxDelimiter =
       endpoint.url.indexOf('cswClientRun.php') > 0 ? ',' : ' ';
@@ -74,93 +80,117 @@ export class HsCompositionsMickaService {
     tmp = this.HsUtilsService.proxify(tmp);
     return tmp;
   }
-
-  getCompositions(endpoint, params, bbox) {
-    return new Promise((resolve, reject) => {
-      endpoint.listLoading = this.$http
-        .get(this.getCompositionsQueryUrl(endpoint, params, bbox))
-        .subscribe((response: any) => {
-          resolve(response);
-        });
-    });
+  compositionsReceived(
+    endpoint: HsEndpoint,
+    extentLayer: any,
+    response: any
+  ): HsEndpoint {
+    if (!response.records) {
+      return;
+    }
+    endpoint.compositionsPaging.loaded = true;
+    endpoint.compositions = response.records;
+    if (response.records && response.records.length > 0) {
+      endpoint.compositionsPaging.matched = response.matched;
+    } else {
+      endpoint.compositionsPaging.matched = 0;
+    }
+    //TODO: Needs refactoring
+    endpoint.compositionsPaging.next = response.next;
+    const mapExtent = this.HsMapService.getMapExtent();
+    for (const record of endpoint.compositions) {
+      const attributes: any = {
+        record: record,
+        hs_notqueryable: true,
+        highlighted: false,
+        title: record.title || record.name,
+      };
+      record.editable = false;
+      record.endpoint = endpoint;
+      if (record.thumbnail == undefined) {
+        record.thumbnail = endpoint.url + '?request=loadthumb&id=' + record.id;
+      }
+      const extent = this.HsCompositionsParserService.parseExtent(record.bbox);
+      //Check if height or Width covers the whole screen
+      if (
+        extent &&
+        !(
+          (extent[0] < mapExtent[0] && extent[2] > mapExtent[2]) ||
+          (extent[1] < mapExtent[1] && extent[3] > mapExtent[3])
+        )
+      ) {
+        attributes.geometry = polygonFromExtent(extent);
+        attributes.is_hs_composition_extent = true;
+        const newFeature = new Feature(attributes);
+        record.feature = newFeature;
+        extentLayer.getSource().addFeatures([newFeature]);
+      } else {
+        //Composition not in extent
+      }
+    }
+    console.log('ENDING');
+    return endpoint;
   }
 
-  loadList(endpoint, params, bbox, extentLayer): Promise<any> {
-    return new Promise((resolve, reject) => {
-      endpoint.compositionsPaging.loaded = false;
-      if (params.sortBy == undefined || params.sortBy === 'None') {
-        params.sortBy = 'title';
-      }
-      if (params.type == undefined || params.type === 'None') {
-        params.type = 'application';
-      }
-      if (params.theme == undefined || params.theme === 'None') {
-        params.theme = '';
-      }
-      if (params.start == undefined) {
-        params.start = endpoint.compositionsPaging.start;
-      }
-      if (params.limit == undefined || isNaN(params.limit)) {
-        params.limit = endpoint.compositionsPaging.limit;
-      }
-      if (endpoint.listLoading) {
-        endpoint.listLoading.unsubscribe();
-        delete endpoint.listLoading;
-      }
-      this.getCompositions(endpoint, params, bbox).then(
-        (response: any) => {
-          endpoint.compositionsPaging.loaded = true;
-          endpoint.compositions = response.records;
-          if (response.records && response.records.length > 0) {
-            endpoint.compositionsPaging.matched = response.matched;
-          } else {
-            endpoint.compositionsPaging.matched = 0;
-          }
-          //TODO: Needs refactoring
-          endpoint.compositionsPaging.next = response.next;
-          const mapExtent = this.HsMapService.getMapExtent();
-          for (const record of endpoint.compositions) {
-            const attributes: any = {
-              record: record,
-              hs_notqueryable: true,
-              highlighted: false,
-              title: record.title || record.name,
-            };
-            record.editable = false;
-            record.endpoint = endpoint;
-            if (record.thumbnail == undefined) {
-              record.thumbnail =
-                endpoint.url + '?request=loadthumb&id=' + record.id;
-            }
-            const extent = this.HsCompositionsParserService.parseExtent(
-              record.bbox
-            );
-            //Check if height or Width covers the whole screen
-            if (
-              extent &&
-              !(
-                (extent[0] < mapExtent[0] && extent[2] > mapExtent[2]) ||
-                (extent[1] < mapExtent[1] && extent[3] > mapExtent[3])
-              )
-            ) {
-              attributes.geometry = polygonFromExtent(extent);
-              attributes.is_hs_composition_extent = true;
-              const newFeature = new Feature(attributes);
-              record.feature = newFeature;
-              extentLayer.getSource().addFeatures([newFeature]);
-            } else {
-              //Composition not in extent
-            }
-          }
-          resolve();
-        },
-        (err) => {}
+  loadList(
+    endpoint: HsEndpoint,
+    params: any,
+    bbox: any,
+    extentLayer: any
+  ): Observable<any> {
+    params = this.checkForParams(endpoint, params);
+    const url = this.getCompositionsQueryUrl(endpoint, params, bbox);
+    endpoint.compositionsPaging.loaded = false;
+
+    if (endpoint.httpCall) {
+      // dataset.httpCall.unsubscribe();
+      console.log('remove');
+      delete endpoint.httpCall;
+    }
+
+    endpoint.httpCall = this.$http
+      .get(url, {
+        //FIXME: dataset must be passed to datasetsReceived
+        //timeout: dataset.canceler.promise,
+        //dataset,
+        //extentFeatureCreated,
+        responseType: 'json',
+      })
+      .pipe(
+        map((response: any) => {
+          const ep = this.compositionsReceived(endpoint, extentLayer, response);
+          return ep;
+        }),
+        catchError((e) => {
+          this.log.error(e);
+          endpoint.datasourcePaging.loaded = true;
+          return EMPTY;
+        })
       );
-    });
+
+    return endpoint.httpCall;
   }
 
   resetCompositionCounter(endpoint) {
     endpoint.compositionsPaging.start = 0;
     endpoint.compositionsPaging.next = endpoint.compositionsPaging.limit;
+  }
+  checkForParams(endpoint: HsEndpoint, params: any): any {
+    if (params.sortBy == undefined || params.sortBy === 'None') {
+      params.sortBy = 'title';
+    }
+    if (params.type == undefined || params.type === 'None') {
+      params.type = 'application';
+    }
+    if (params.theme == undefined || params.theme === 'None') {
+      params.theme = '';
+    }
+    if (params.start == undefined) {
+      params.start = endpoint.compositionsPaging.start;
+    }
+    if (params.limit == undefined || isNaN(params.limit)) {
+      params.limit = endpoint.compositionsPaging.limit;
+    }
+    return params;
   }
 }
