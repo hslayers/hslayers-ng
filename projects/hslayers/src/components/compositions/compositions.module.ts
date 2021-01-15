d724553634cb327567a12c977f7b7209ae1432dd
DailisLangovskis <dailis.langovskis@gmail.com>
2021-01-15 20:48:59 +0200

    Moved functionality to comps catalogue service

--- projects/hslayers/src/components/compositions/compositions.module.ts
+++ projects/hslayers/src/components/compositions/compositions.module.ts
@@ -1,6 +1,7 @@
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
+import {HsCompositionsCatalogueService} from './compositions-catalogue.service';
 import {HsCompositionsComponent} from './compositions.component';
 import {HsCompositionsDeleteDialogComponent} from './dialogs/delete-dialog.component';
 import {HsCompositionsInfoDialogComponent} from './dialogs/info-dialog.component';
@@ -8,6 +9,7 @@ import {HsCompositionsLayerParserModule} from './layer-parser/layer-parser.modul
 import {HsCompositionsOverwriteDialogComponent} from './dialogs/overwrite-dialog.component';
 import {HsCompositionsService} from './compositions.service';
 import {HsCompositionsShareDialogComponent} from './dialogs/share-dialog.component';
+import {HsCompostionsListItemComponent} from './compositions-list-item.component';
 import {HsLaymanModule} from '../../common/layman/layman.module';
 import {HsPanelHelpersModule} from '../layout/panels/panel-helpers.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
@@ -20,6 +22,7 @@ import {TranslateModule} from '@ngx-translate/core';
     HsCompositionsDeleteDialogComponent,
     HsCompositionsShareDialogComponent,
     HsCompositionsInfoDialogComponent,
+    HsCompostionsListItemComponent,
   ],
   imports: [
     CommonModule,
@@ -31,13 +34,14 @@ import {TranslateModule} from '@ngx-translate/core';
     HsLaymanModule,
   ],
   exports: [HsCompositionsComponent],
-  providers: [HsCompositionsService],
+  providers: [HsCompositionsService, HsCompositionsCatalogueService],
   entryComponents: [
     HsCompositionsComponent,
     HsCompositionsOverwriteDialogComponent,
     HsCompositionsDeleteDialogComponent,
     HsCompositionsShareDialogComponent,
     HsCompositionsInfoDialogComponent,
+    HsCompostionsListItemComponent,
   ],
 })
 export class HsCompositionsModule {}
