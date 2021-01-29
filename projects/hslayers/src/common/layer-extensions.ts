1fa2a15ffd3b86fb0bc9596941b66f2b3f053319
raitis <raitisbe@gmail.com>
2021-01-29 16:20:29 +0200

    Get set abstract using extension functions

--- projects/hslayers/src/common/layer-extensions.ts
+++ projects/hslayers/src/common/layer-extensions.ts
@@ -2,6 +2,7 @@ import Layer from 'ol/layer/Layer';
 
 const TITLE = 'title';
 const NAME = 'name';
+const ABSTRACT = 'name';
 
 export function setTitle(layer: Layer, title: string): void {
   layer.set(TITLE, title);
@@ -19,9 +20,19 @@ export function getName(layer: Layer): string {
   return layer.get(NAME);
 }
 
+export function setAbstract(layer: Layer, abstract: string): void {
+  layer.set(ABSTRACT, abstract);
+}
+
+export function getAbstract(layer: Layer): string {
+  return layer.get(ABSTRACT);
+}
+
 export const HsLayerExt = {
   setTitle,
   getTitle,
   setName,
   getName,
+  setAbstract,
+  getAbstract,
 };
