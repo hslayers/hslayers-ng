d7244cbe29711b181beb331f758a71dc0a2a1df3
DailisLangovskis <dailis.langovskis@gmail.com>
2021-02-03 13:53:51 +0200

    Set and Get feature highlighted property using extensions

--- projects/hslayers/src/common/feature-extensions.ts
+++ projects/hslayers/src/common/feature-extensions.ts
@@ -1,7 +1,7 @@
 import Feature from 'ol/Feature';
 
 const TITLE = 'title';
-
+const HIGHLIGHTED = 'highlighted';
 export function setFeatureTitle(feature: Feature, title: string): void {
   feature.set(TITLE, title);
 }
@@ -10,7 +10,16 @@ export function getFeatureTitle(feature: Feature): string {
   return feature.get(TITLE);
 }
 
+export function setHighlighted(feature: Feature, highlighted: boolean): void {
+  feature.set(HIGHLIGHTED, highlighted);
+}
+
+export function getHighlighted(feature: Feature): boolean {
+  return feature.get(HIGHLIGHTED);
+}
 export const HsFeatureExt = {
   setFeatureTitle,
   getFeatureTitle,
+  setHighlighted,
+  getHighlighted,
 };
