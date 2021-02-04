9e8850b00b751c8cd118b4c01813f6b832c117e0
DailisLangovskis <dailis.langovskis@gmail.com>
2021-02-04 12:08:30 +0200

    Port - Specify folder for added vector and shape layers

--- components/add-layers/vector/add-layers-vector.component.ts
+++ components/add-layers/vector/add-layers-vector.component.ts
@@ -54,7 +54,11 @@ export class HsAddLayersVectorComponent {
       this.title,
       this.abstract,
       this.srs,
-      {extractStyles: this.extract_styles, features: this.features}
+      {
+        extractStyles: this.extract_styles,
+        features: this.features,
+        path: this.folder_name,
+      }
     );
     this.hsAddLayersVectorService.fitExtent(layer);
     this.hsLayoutService.setMainPanel('layermanager');
