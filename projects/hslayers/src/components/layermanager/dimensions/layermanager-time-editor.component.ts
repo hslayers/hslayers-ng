d3b165bda7fe3c725b22f0660eb7aac0855e0860
jmacura <macurajan@gmail.com>
2021-02-07 16:45:19 +0100

    Init time-editor

---
+++ projects/hslayers/src/components/layermanager/dimensions/layermanager-time-editor.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'hs-layermanager-time-editor',
+  templateUrl: 'layermanager-time-editor.component.html',
+})
+export class HsLayerManagerTimeEditorComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
