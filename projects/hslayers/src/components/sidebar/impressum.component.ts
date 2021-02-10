9eb8f52cf84a51749f2d996d78e4681783b30c4f
DailisLangovskis <dailis.langovskis@gmail.com>
2021-02-10 13:26:35 +0200

    Show logo only if margin + element is bigger than 90px

--- projects/hslayers/src/components/sidebar/impressum.component.ts
+++ projects/hslayers/src/components/sidebar/impressum.component.ts
@@ -11,11 +11,13 @@ export class HsImpressumComponent {
   version = 'dev';
   logoDisabled = false;
   freeSpaceHeight: number;
+  elem: any;
   constructor(
     public hsUtilsService: HsUtilsService,
     public HsEventBusService: HsEventBusService,
     public el: ElementRef
   ) {
+    this.elem = this.el.nativeElement;
     this.version = packageJson.version;
     this.HsEventBusService.sizeChanges.subscribe(() => {
       this.getFreeSpaceHeight();
@@ -23,9 +25,9 @@ export class HsImpressumComponent {
   }
   getFreeSpaceHeight(): void {
     const calculatedMargin = parseFloat(
-      getComputedStyle(this.el.nativeElement, null).marginTop.replace('px', '')
+      getComputedStyle(this.elem, null).marginTop.replace('px', '')
     );
-    calculatedMargin > 0 && !isNaN(calculatedMargin)
+    calculatedMargin + this.elem.offsetHeight > 90 && !isNaN(calculatedMargin)
       ? (this.logoDisabled = false)
       : (this.logoDisabled = true);
   }
