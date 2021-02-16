8eec3909a24b5e66bd529f9b66adac366185d0a2
raitis <raitisbe@gmail.com>
2021-02-16 16:44:28 +0200

    Ability to set Hslayers config through attribute

--- projects/hslayers/src/hslayers.component.ts
+++ projects/hslayers/src/hslayers.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {HsConfig} from './config.service';
 
 @Component({
@@ -6,6 +6,12 @@ import {HsConfig} from './config.service';
   templateUrl: './hslayers.html',
   styles: [],
 })
-export class HslayersComponent {
+export class HslayersComponent implements OnInit {
+  @Input() config: HsConfig;
   constructor(public HsConfig: HsConfig) {}
+  ngOnInit(): void {
+    if (this.config) {
+      this.HsConfig.update(this.config);
+    }
+  }
 }
