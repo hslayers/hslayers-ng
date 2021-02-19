11ffa1ea27c66568b21f567a4ac12504e345523e
Filip Leitner <leitnerfilip@gmail.com>
2021-02-19 09:48:14 +0100

    Move adddata url WMS,WMTS,WFS and ArcGis subscriptions to service
    Prevents multiple error handler trigerring

--- projects/hslayers/src/components/add-data/url/arcgis/add-data-url-arcgis.component.ts
+++ projects/hslayers/src/components/add-data/url/arcgis/add-data-url-arcgis.component.ts
@@ -2,12 +2,9 @@ import {Component} from '@angular/core';
 
 import {HsAddDataArcGisService} from './add-data-url-arcgis.service';
 import {HsArcgisGetCapabilitiesService} from '../../../../common/arcgis/get-capabilities.service';
-import {HsDialogContainerService} from '../../../layout/dialogs/dialog-container.service';
 import {HsEventBusService} from '../../../core/event-bus.service';
-import {HsGetCapabilitiesErrorComponent} from '../../common/capabilities-error-dialog.component';
 import {HsHistoryListService} from '../../../../common/history-list/history-list.service';
-import {HsLanguageService} from '../../../language/language.service';
-import {HsLogService} from '../../../../common/log/log.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'hs-add-data-url-arcgis',
@@ -15,95 +12,49 @@ import {HsLogService} from '../../../../common/log/log.service';
 })
 export class HsAddDataArcGisComponent {
   data;
-  showDetails;
   sourceHistory;
-  url;
   layerToSelect: any;
   error: any;
+
+  owsConnectingSubscription: Subscription;
 
   constructor(
     public HsAddDataArcGisService: HsAddDataArcGisService,
     public hsArcgisGetCapabilitiesService: HsArcgisGetCapabilitiesService,
     public hsEventBusService: HsEventBusService,
-    public hsHistoryListService: HsHistoryListService,
-    public HsLanguageService: HsLanguageService,
-    public HsDialogContainerService: HsDialogContainerService,
-    public hsLog: HsLogService
+    public hsHistoryListService: HsHistoryListService
   ) {
     this.data = HsAddDataArcGisService.data;
-    this.hsEventBusService.owsConnecting.subscribe(({type, uri, layer}) => {
-      if (type === 'arcgis') {
-        this.setUrlAndConnect(uri, layer);
-      }
-    });
 
-    this.hsEventBusService.owsCapabilitiesReceived.subscribe(
-      async ({type, response}) => {
-        if (type === 'ArcGIS') {
-          try {
-            await this.HsAddDataArcGisService.capabilitiesReceived(
-              response,
-              this.layerToSelect
-            );
-            if (this.layerToSelect) {
-              this.addLayers(true);
-            }
-          } catch (e) {
-            if (e.status == 401) {
-              this.HsAddDataArcGisService.arcgisCapsParseError.next(
-                'Unauthorized access. You are not authorized to query data from this service'
-              );
-              return;
-            }
-            this.HsAddDataArcGisService.arcgisCapsParseError.next(e);
-          }
-        }
-        if (type === 'error') {
-          this.HsAddDataArcGisService.arcgisCapsParseError.next(
-            response.message
-          );
+    this.owsConnectingSubscription = this.hsEventBusService.owsConnecting.subscribe(
+      ({type, uri, layer}) => {
+        if (type === 'arcgis') {
+          this.setUrlAndConnect(uri, layer);
         }
       }
     );
 
-    this.HsAddDataArcGisService.arcgisCapsParseError.subscribe((e) => {
-      this.hsLog.warn(e);
-      this.url = null;
-      this.showDetails = false;
-
-      this.error = e.toString();
-      if (this.error.includes('property')) {
-        this.error = this.HsLanguageService.getTranslationIgnoreNonExisting(
-          'ADDLAYERS',
-          'serviceTypeNotMatching'
-        );
-      }
-      this.HsDialogContainerService.create(
-        HsGetCapabilitiesErrorComponent,
-        this.error
-      );
-    });
     //TODO: this.sourceHistory = this.HsAddDataArcGisService.sourceHistory;
   }
 
   hasNestedLayers = this.HsAddDataArcGisService.hasNestedLayers;
   getDimensionValues = this.HsAddDataArcGisService.getDimensionValues;
 
-  /**
-   * @function clear
-   * @description Clear Url and hide detailsArcgis
-   */
-  clear(): void {
-    this.url = '';
-    this.showDetails = false;
+  ngOnDestroy() {
+    this.owsConnectingSubscription.unsubscribe();
   }
 
   connect = (layerToSelect): void => {
-    this.hsHistoryListService.addSourceHistory('Arcgis', this.url);
-    this.hsArcgisGetCapabilitiesService.requestGetCapabilities(this.url);
-    this.HsAddDataArcGisService.data.getMapUrl = this.url;
+    this.hsHistoryListService.addSourceHistory(
+      'Arcgis',
+      this.HsAddDataArcGisService.url
+    );
+    this.hsArcgisGetCapabilitiesService.requestGetCapabilities(
+      this.HsAddDataArcGisService.url
+    );
+    this.HsAddDataArcGisService.data.getMapUrl = this.HsAddDataArcGisService.url;
 
-    this.showDetails = true;
+    this.HsAddDataArcGisService.showDetails = true;
   };
 
   /**
@@ -135,7 +86,7 @@ export class HsAddDataArcGisComponent {
    * getCapabilities arrives
    */
   setUrlAndConnect(url: string, layer): void {
-    this.url = url;
+    this.HsAddDataArcGisService.url = url;
     this.connect(layer);
   }
 }
--- projects/hslayers/src/components/add-data/url/wfs/add-data-url-wfs.component.ts
+++ projects/hslayers/src/components/add-data/url/wfs/add-data-url-wfs.component.ts
@@ -1,117 +1,42 @@
-import VectorLayer from 'ol/layer/Vector';
 import {Component} from '@angular/core';
-import {bbox} from 'ol/loadingstrategy';
-import {transformExtent} from 'ol/proj';
+import {Subscription} from 'rxjs';
 
 import {HsAddDataWfsService} from './add-data-url-wfs.service';
-import {HsDialogContainerService} from '../../../layout/dialogs/dialog-container.service';
 import {HsEventBusService} from '../../../core/event-bus.service';
-import {HsGetCapabilitiesErrorComponent} from '../../common/capabilities-error-dialog.component';
-import {HsLanguageService} from '../../../language/language.service';
-import {HsLayoutService} from '../../../layout/layout.service';
-import {HsLogService} from '../../../../common/log/log.service';
-import {HsMapService} from '../../../map/map.service';
-import {HsUtilsService} from '../../../utils/utils.service';
 import {HsWfsGetCapabilitiesService} from '../../../../common/wfs/get-capabilities.service';
+import {HsUtilsService} from '../../../utils/utils.service';
 
 @Component({
   selector: 'hs-add-data-url-wfs',
   templateUrl: './add-data-wfs-layer.directive.html',
 })
 export class HsAddDataWfsComponent {
-  url: string;
-  error: any;
+  owsConnectingSubscription: Subscription;
+
   addAll: boolean;
   isChecked: boolean;
-  mapProjection: any;
   loadingFeatures: boolean;
-  showDetails: boolean;
   title = ''; //FIXME: unused
-  layerToAdd: string;
-
-  folderName = 'WFS';
 
   constructor(
     public HsAddDataWfsService: HsAddDataWfsService,
-    public HsDialogContainerService: HsDialogContainerService,
     public HsEventBusService: HsEventBusService,
-    public HsLayoutService: HsLayoutService,
-    public hsLog: HsLogService,
-    public HsMapService: HsMapService,
     public HsWfsGetCapabilitiesService: HsWfsGetCapabilitiesService,
-    public hsUtilsService: HsUtilsService,
-    public HsLanguageService: HsLanguageService
+    public HsUtilsService: HsUtilsService //used in template
   ) {
-    this.HsEventBusService.olMapLoads.subscribe(() => {
-      this.mapProjection = this.HsMapService.map
-        .getView()
-        .getProjection()
-        .getCode()
-        .toUpperCase();
-    });
-
-    this.HsEventBusService.owsCapabilitiesReceived.subscribe(
-      async ({type, response}) => {
-        if (type === 'WFS') {
-          try {
-            const bbox = await this.HsAddDataWfsService.parseCapabilities(
-              response
-            );
-            if (this.layerToAdd) {
-              for (const layer of this.HsAddDataWfsService.services) {
-                //TODO: If Layman allows layers with different casing,
-                // then remove the case lowering
-                if (
-                  layer.Title.toLowerCase() === this.layerToAdd.toLowerCase()
-                ) {
-                  layer.checked = true;
-                }
-              }
-              this.addLayers(true);
-              this.layerToAdd = null;
-              this.zoomToBBox(bbox);
-            }
-          } catch (e) {
-            if (e.status == 401) {
-              this.HsAddDataWfsService.wfsCapabilitiesError.next(
-                'Unauthorized access. You are not authorized to query data from this service'
-              );
-              return;
-            }
-            this.HsAddDataWfsService.wfsCapabilitiesError.next(e);
-          }
-        }
-        if (type === 'error') {
-          this.HsAddDataWfsService.wfsCapabilitiesError.next(response.message);
+    //Merge subscriptions in order to easily unsubscribe on destroy
+    this.owsConnectingSubscription = this.HsEventBusService.owsConnecting.subscribe(
+      ({type, uri, layer}) => {
+        if (type == 'wfs') {
+          this.HsAddDataWfsService.layerToAdd = layer;
+          this.setUrlAndConnect(uri);
         }
       }
     );
+  }
 
-    this.HsEventBusService.owsConnecting.subscribe(({type, uri, layer}) => {
-      if (type == 'wfs') {
-        this.layerToAdd = layer;
-        this.setUrlAndConnect(uri);
-      }
-    });
-
-    this.HsAddDataWfsService.wfsCapabilitiesError.subscribe((e) => {
-      this.hsLog.warn(e);
-      this.url = null;
-      this.showDetails = false;
-
-      this.error = e.toString();
-      if (this.error.includes('TypeError')) {
-        this.error = this.HsLanguageService.getTranslationIgnoreNonExisting(
-          'ADDLAYERS',
-          'serviceTypeNotMatching'
-        );
-      }
-      this.HsDialogContainerService.create(
-        HsGetCapabilitiesErrorComponent,
-        this.error
-      );
-      //throw "WMS Capabilities parsing problem";
-    });
+  ngOnDestroy() {
+    this.owsConnectingSubscription.unsubscribe();
   }
 
   /**
@@ -119,13 +44,16 @@ export class HsAddDataWfsComponent {
    * @description Clear Url and hide detailsWms
    */
   clear(): void {
-    this.url = '';
-    this.showDetails = false;
+    this.HsAddDataWfsService.url = '';
+    this.HsAddDataWfsService.showDetails = false;
   }
 
   connect = (): void => {
-    this.HsWfsGetCapabilitiesService.requestGetCapabilities(this.url);
-    this.showDetails = true;
+    this.HsWfsGetCapabilitiesService.requestGetCapabilities(
+      this.HsAddDataWfsService.url
+    );
+    this.HsAddDataWfsService.services = [];
+    this.HsAddDataWfsService.showDetails = true;
   };
 
   /**
@@ -134,7 +62,7 @@ export class HsAddDataWfsComponent {
    * @param {string} url Url of requested service
    */
   setUrlAndConnect(url: string): void {
-    this.url = url;
+    this.HsAddDataWfsService.url = url;
     this.connect();
   }
 
@@ -153,6 +81,18 @@ export class HsAddDataWfsComponent {
     this.changed();
   }
 
+  /**
+   * @function addLayers
+   * @description First step in adding layers to the map. Lops through the list of layers and calls addLayer.
+   * @param {boolean} checkedOnly Add all available layers or only checked ones. Checked=false=all
+   */
+  addLayers(checkedOnly: boolean): void {
+    this.HsAddDataWfsService.addAll = checkedOnly;
+    for (const layer of this.HsAddDataWfsService.services) {
+      this.HsAddDataWfsService.addLayersRecursively(layer);
+    }
+  }
+
   checked(): boolean {
     for (const layer of this.HsAddDataWfsService.services) {
       if (layer.checked) {
@@ -164,81 +104,5 @@ export class HsAddDataWfsComponent {
 
   changed(): void {
     this.isChecked = this.checked();
-  }
-
-  /**
-   * @function addLayers
-   * @description First step in adding layers to the map. Lops through the list of layers and calls addLayer.
-   * @param {boolean} checkedOnly Add all available layers or only checked ones. Checked=false=all
-   */
-  addLayers(checkedOnly: boolean): void {
-    this.addAll = checkedOnly;
-    for (const layer of this.HsAddDataWfsService.services) {
-      this.addLayersRecursively(layer);
-    }
-  }
-
-  private addLayersRecursively(layer): void {
-    if (!this.addAll || layer.checked) {
-      this.addLayer(
-        layer,
-        layer.Title.replace(/\//g, '&#47;'),
-        this.hsUtilsService.undefineEmptyString(this.folderName),
-        this.HsAddDataWfsService.srs
-      );
-    }
-    if (layer.Layer) {
-      for (const sublayer of layer.Layer) {
-        this.addLayersRecursively(sublayer);
-      }
-    }
-  }
-
-  /**
-   * @function addLayer
-   * @private
-   * @description (PRIVATE) Add selected layer to map???
-   * @param {object} layer capabilities layer object
-   * @param {string} layerName layer name in the map
-   * @param {string} folder name
-   * @param {OpenLayers.Projection} srs of the layer
-   */
-  private addLayer(layer, layerName: string, folder: string, srs): void {
-    const options = {
-      layer: layer,
-      url: this.HsWfsGetCapabilitiesService.service_url.split('?')[0],
-      strategy: bbox,
-      srs: srs,
-    };
-
-    const new_layer = new VectorLayer({
-      title: layerName,
-      source: this.HsAddDataWfsService.createWfsSource(options),
-      path: folder,
-      renderOrder: null,
-      removable: true,
-    });
-    this.HsMapService.map.addLayer(new_layer);
-    this.HsLayoutService.setMainPanel('layermanager');
-  }
-
-  private zoomToBBox(bbox: any) {
-    if (!bbox) {
-      return;
-    }
-    if (bbox.LowerCorner) {
-      bbox = [
-        bbox.LowerCorner.split(' ')[0],
-        bbox.LowerCorner.split(' ')[1],
-        bbox.UpperCorner.split(' ')[0],
-        bbox.UpperCorner.split(' ')[1],
-      ];
-    }
-    const extent = transformExtent(bbox, 'EPSG:4326', this.mapProjection);
-    if (extent) {
-      this.HsMapService.map
-        .getView()
-        .fit(extent, this.HsMapService.map.getSize());
-    }
   }
 }
