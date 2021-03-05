26c131d0dcdfea85e7610d6554edb9037b50d48a
fzadrazil <fzadr@LAPTOP-FARNK195>
2021-03-05 22:33:49 +0100

    Implement refresing access token Unify the code notation

--- projects/hslayers-server/src/oauth2/session.js
+++ projects/hslayers-server/src/oauth2/session.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
 
 const session = require('express-session');
-const connect_sqlite3 = require('connect-sqlite3');
+const connectSQLite3 = require('connect-sqlite3');
 
-exports.create_express_session = () => {
-  const SQLiteStore = connect_sqlite3(session);
+exports.createExpressSession = () => {
+  const SQLiteStore = connectSQLite3(session);
   const session_store = new SQLiteStore({
     table: 'sessions',
     db: process.env.DB_PATH,
     //dir: './data'
   });
 
-  const session_config = {
+  const sessionConfig = {
     secret: process.env.SESSION_SECRET,
     cookie: {
       maxAge: parseInt(process.env.SESSION_MAX_AGE, 10) * 1000,
@@ -21,6 +21,6 @@ exports.create_express_session = () => {
     store: session_store
   };
 
-  return session(session_config);
+  return session(sessionConfig);
 
 };
