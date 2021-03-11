d7480a23cd7846e1f985532fc6e3599673e9a5b2
fzadrazil <fzadr@LAPTOP-FARNK195>
2021-03-11 17:19:45 +0100

    Redirect all Geoserver Layman requests to hslayers server component.

--- projects/hslayers-server/src/oauth2/util.js
+++ projects/hslayers-server/src/oauth2/util.js
@@ -48,6 +48,26 @@ const getAuthenticationHeaders = (user) => {
   }
 };
 
+exports.handleProxyRes = (proxyRes, req, res) => {
+  this.allowOrigin(proxyRes, req, res);
+
+  var body = [];
+  proxyRes.on('data', function (chunk) {
+    body.push(chunk);
+  });
+  proxyRes.on('end', function () {
+    if (this.headers["content-type"] == "text/xml" || this.headers["content-type"] == "application/json") {
+      let repl = new RegExp(process.env.LAYMAN_BASEURL.trimEnd('/'), "g");
+      let replWith = process.env.OAUTH2_CALLBACK_URL.replace("/callback", "").trimEnd('/');
+      body = Buffer.concat(body).toString().replace(repl, replWith);
+      res.end(body);
+    }
+    else {
+      res.end(Buffer.concat(body));
+    }
+  });
+};
+
 exports.addAuthenticationHeaders = (proxyReq, req, res) => {
   if (req.session.passport && req.session.passport.user) {
     const user = req.session.passport.user;
