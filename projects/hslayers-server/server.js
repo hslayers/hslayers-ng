6f3c39018e74470cdd24a540770ce467d097fa34
raitis <raitisbe@gmail.com>
2021-03-15 14:20:55 +0200

    Require dotenv as early as possible

--- projects/hslayers-server/server.js
+++ projects/hslayers-server/server.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-
+require('dotenv').config();
 require('./src/proxy');
 require('./src/share');
--- projects/hslayers-server/src/share.js
+++ projects/hslayers-server/src/share.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 const express = require('express');
 //const cors = require('cors');
 const share = express();
