214aa8937876394d6af954f5c83e2a3257bac9d9
jmacura <macurajan@gmail.com>
2021-04-01 00:15:28 +0200

    feat: Introduce custom filter pipe
    fixes filtering in Legend panel
    refs #1717

--- projects/hslayers/src/common/widgets/ui-extensions.module.ts
+++ projects/hslayers/src/common/widgets/ui-extensions.module.ts
@@ -2,13 +2,22 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 
 import {EndpointsWithDatasourcesPipe} from './endpoints-with-datasources.pipe';
+import {FilterPipe} from './filter.pipe';
 import {HsUiExtensionsRecursiveDd} from './recursive-dd.component';
 
 @NgModule({
-  declarations: [HsUiExtensionsRecursiveDd, EndpointsWithDatasourcesPipe],
+  declarations: [
+    HsUiExtensionsRecursiveDd,
+    EndpointsWithDatasourcesPipe,
+    FilterPipe,
+  ],
   imports: [CommonModule],
   providers: [EndpointsWithDatasourcesPipe],
   entryComponents: [HsUiExtensionsRecursiveDd],
-  exports: [HsUiExtensionsRecursiveDd, EndpointsWithDatasourcesPipe],
+  exports: [
+    HsUiExtensionsRecursiveDd,
+    EndpointsWithDatasourcesPipe,
+    FilterPipe,
+  ],
 })
 export class HsUiExtensionsModule {}
--- projects/hslayers/src/components/legend/legend.module.ts
+++ projects/hslayers/src/components/legend/legend.module.ts
@@ -1,16 +1,15 @@
-/**
- * @namespace hs.legend
- * @memberOf hs
- */
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {TranslateModule} from '@ngx-translate/core';
+
 import {HsLegendComponent} from './legend.component';
 import {HsLegendLayerComponent} from './legend-layer.component';
 import {HsLegendLayerStaticComponent} from './legend-layer-static.component';
 import {HsLegendLayerVectorComponent} from './legend-layer-vector.component';
 import {HsLegendService} from './legend.service';
 import {HsPanelHelpersModule} from '../layout/panels/panel-helpers.module';
-import {TranslateModule} from '@ngx-translate/core';
+import {HsUiExtensionsModule} from '../../common/widgets/ui-extensions.module';
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [
@@ -19,7 +18,13 @@ import {TranslateModule} from '@ngx-translate/core';
     HsLegendLayerVectorComponent,
     HsLegendLayerStaticComponent,
   ],
-  imports: [CommonModule, HsPanelHelpersModule, TranslateModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    HsPanelHelpersModule,
+    HsUiExtensionsModule,
+    TranslateModule,
+  ],
   exports: [HsLegendComponent, HsLegendLayerComponent],
   providers: [HsLegendService],
   entryComponents: [HsLegendComponent, HsLegendLayerComponent],
