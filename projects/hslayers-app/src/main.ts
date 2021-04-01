19f946e2b23cc317c1cf16f32bf0322c5e9fbf72
raitis <raitisbe@gmail.com>
2021-04-01 14:57:39 +0300

    perf: Disable hmr since it doesn't work in ng9

--- projects/hslayers-app/src/main.ts
+++ projects/hslayers-app/src/main.ts
@@ -3,15 +3,10 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import {AppModule} from './hslayers-app/hslayers-app.module';
 import {environment} from './environments/environment';
-import {hmrBootstrap} from './hmr';
 
 if (environment.production) {
   enableProdMode();
 }
 
 const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
-if (environment.hmr) {
-  hmrBootstrap(module, bootstrap);
-} else {
-  bootstrap().catch((err) => console.log(err));
-}
+bootstrap().catch((err) => console.log(err));
