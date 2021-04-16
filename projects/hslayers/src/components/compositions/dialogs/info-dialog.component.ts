1a038ebc999cc7e55c995755e363bf116f5845e0
Filip Leitner <leitnerfilip@gmail.com>
2021-04-16 07:12:37 +0200

    feat: Add redirect button pointing to Micka for compositions

--- projects/hslayers/src/components/compositions/dialogs/info-dialog.component.ts
+++ projects/hslayers/src/components/compositions/dialogs/info-dialog.component.ts
@@ -2,6 +2,7 @@ import {Component, ViewRef} from '@angular/core';
 import {HsCompositionsService} from '../compositions.service';
 import {HsDialogComponent} from '../../layout/dialogs/dialog-component.interface';
 import {HsDialogContainerService} from '../../layout/dialogs/dialog-container.service';
+
 @Component({
   selector: 'hs-compositions-info-dialog',
   templateUrl: './dialog_info.html',
@@ -9,10 +10,9 @@ import {HsDialogContainerService} from '../../layout/dialogs/dialog-container.se
 export class HsCompositionsInfoDialogComponent implements HsDialogComponent {
   viewRef: ViewRef;
   data: any;
-
   constructor(
     public HsDialogContainerService: HsDialogContainerService,
-    public HsCompositionsService: HsCompositionsService
+    public HsCompositionsService: HsCompositionsService,
   ) {}
 
   close(): void {
