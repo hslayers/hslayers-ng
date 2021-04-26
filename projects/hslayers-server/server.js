bd4c11792e98bb4be52c8faa4493217d08ecc074
fzadrazil <fzadr@LAPTOP-FARNK195>
2021-04-26 19:23:20 +0200

    fix: Check for the hslayers-server.db directory existence

--- projects/hslayers-server/server.js
+++ projects/hslayers-server/server.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
 require('dotenv').config();
+
+const path = require('path');
+const fs = require("fs");
+if (!process.env.DB_PATH)
+  process.env.DB_PATH = 'data/hslayers-server.db';
+let dbdir = path.dirname(process.env.DB_PATH);
+if (!fs.existsSync(dbdir)) {
+  fs.mkdirSync(dbdir);
+}
+
 require('./src/proxy');
 require('./src/share');
 require('./src/layman');
