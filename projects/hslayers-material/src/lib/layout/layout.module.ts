1abfbd2450248138accbfa8a850d154e802aa34b
raitis <raitisbe@gmail.com>
2021-04-26 23:25:38 +0300

    refactor: Extract attributionDialog in separately

--- projects/hslayers-material/src/lib/layout/layout.module.ts
+++ projects/hslayers-material/src/lib/layout/layout.module.ts
@@ -4,8 +4,8 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatIconModule} from '@angular/material/icon';
 
-import {AttributionDialog, HsMatOverlayComponent} from './overlay.component';
 import {HsAddDataModule} from '../../../../hslayers/src/components/add-data/add-data.module';
+import {HsAttributionDialogComponent} from './attribution-dialog.component';
 import {HsCompositionsModule} from '../../../../hslayers/src/components/compositions/compositions.module';
 import {HsConfirmModule} from '../../../../hslayers/src/common/confirm/confirm.module';
 import {HsDialogContainerComponent} from '../../../../hslayers/src/components/layout/dialogs/dialog-container.component';
@@ -23,6 +23,7 @@ import {HsMapHostDirective} from './map-host.directive';
 import {HsMapModule} from '../../../../hslayers/src/components/map/map.module';
 import {HsMatLayerManagerModule} from '../layermanager/layermanager.module';
 import {HsMatLayoutComponent} from './layout.component';
+import {HsMatOverlayComponent} from './overlay.component';
 import {HsMeasureModule} from '../../../../hslayers/src/components/measure/measure.module';
 import {HsPanelHelpersModule} from '../../../../hslayers/src/components/layout/panels/panel-helpers.module';
 import {HsPrintModule} from '../../../../hslayers/src/components/print/print.module';
@@ -42,7 +43,7 @@ import {HsTripPlannerModule} from '../../../../hslayers/src/components/trip_plan
     HsMapHostDirective,
     HsMatLayoutComponent,
     HsMatOverlayComponent,
-    AttributionDialog,
+    HsAttributionDialogComponent,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
