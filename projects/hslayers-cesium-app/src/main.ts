932baa75b81e0f00486acd6355f5841914d33e56
raitis <raitisbe@gmail.com>
2021-05-14 15:50:12 +0300

    build: Disable hmr for cesium-app

--- projects/hslayers-cesium-app/src/main.ts
+++ projects/hslayers-cesium-app/src/main.ts
@@ -3,15 +3,10 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
-import {hmrBootstrap} from './hmr';
 
 if (environment.production) {
   enableProdMode();
 }
 
 const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
-if (environment.hmr) {
-  hmrBootstrap(module, bootstrap);
-} else {
-  bootstrap().catch((err) => console.log(err));
-}
+bootstrap().catch((err) => console.log(err));
