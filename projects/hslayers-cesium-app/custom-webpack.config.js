ae4839db65e626caefbab18c22fcbc2eda2eb5e1
raitis <raitisbe@gmail.com>
2021-05-14 15:19:05 +0300

    build: Setup hslayers-cesium-app build and remove hmr environment

--- projects/hslayers-cesium-app/custom-webpack.config.js
+++ projects/hslayers-cesium-app/custom-webpack.config.js
@@ -1,13 +1,22 @@
+var webpack = require('webpack');
+
 module.exports = {
+  devtool: false,
   node: {
     // Resolve node module use of fs
-    fs: 'empty',
+    fs: "empty",
     Buffer: false,
-    http: 'empty',
-    https: 'empty',
-    zlib: 'empty',
+    http: "empty",
+    https: "empty",
+    zlib: "empty",
   },
   module: {
     unknownContextCritical: false,
   },
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({
+      filename: "[file].map",
+      exclude: ["vendor.js"],
+    }),
+  ],
 };
