2d5223f646df2057ce34298ae59c46f282c5ed7c
SLeitgeb <leitgeb.simon@gmail.com>
2021-06-08 19:28:08 +0200

    Update dependencies, add library exports

--- projects/hslayers-material/src/public-api.ts
+++ projects/hslayers-material/src/public-api.ts
@@ -6,5 +6,7 @@ export * from './lib/hslayers-material.service';
 export * from './lib/hslayers-material.component';
 export * from './lib/hslayers-material.module';
 export * from './lib/layout/map-host.directive';
+export * from './lib/layout/layout.component';
+export * from './lib/layout/layout.module';
 export * from './lib/layout/overlay.component';
 export * from './lib/layout/attribution-dialog.component';
