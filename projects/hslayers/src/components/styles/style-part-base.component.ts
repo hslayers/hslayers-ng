3c2b7b2aaa3bbbe32bea48cbef32cf2d3336cbe9
raitis <raitisbe@gmail.com>
2021-06-14 00:21:20 +0300

    fix: Notify about non-working sld atrributes

--- projects/hslayers/src/components/styles/style-part-base.component.ts
+++ projects/hslayers/src/components/styles/style-part-base.component.ts
@@ -1,10 +1,11 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   template: '<div></div>',
 })
 export class HsStylerPartBaseComponent {
   @Output() changes = new EventEmitter<void>();
+  @Input() warning: string;
 
   emitChange(): void {
     this.changes.emit();
