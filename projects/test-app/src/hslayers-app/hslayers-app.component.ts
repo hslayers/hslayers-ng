6e1993f2848125001c2e83194eca88209fe9f495
Filip Leitner <leitnerfilip@gmail.com>
2021-06-22 13:05:15 +0200

    feat(testapp):Configure test-app proxyPrefix

--- projects/test-app/src/hslayers-app/hslayers-app.component.ts
+++ projects/test-app/src/hslayers-app/hslayers-app.component.ts
@@ -101,6 +101,9 @@ export class HslayersAppComponent {
       ],
     };
     Object.assign(this.HsConfig, {
+      proxyPrefix: window.location.hostname.includes('localhost')
+      ? `${window.location.protocol}//${window.location.hostname}:8085/`
+      : '/proxy/',
       panelsEnabled: {
         tripPlanner: true,
       },
