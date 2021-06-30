f10a689a10bc79d6e23848c9d6900fdb6b5e9e77
Filip Leitner <leitnerfilip@gmail.com>
2021-06-30 09:57:56 +0200

    fix: Styler color unreadable for dark colors
    fixes #1959

--- projects/hslayers/src/components/styles/symbolizers/color-picker.component.ts
+++ projects/hslayers/src/components/styles/symbolizers/color-picker.component.ts
@@ -12,10 +12,31 @@ export class HsColorPickerComponent extends HsStylerPartBaseComponent {
   @Input() symbolizer: MarkSymbolizer | FillSymbolizer | TextSymbolizer;
   @Input() attribute: string;
   @Input() label: string;
+  fontColor = 'white';
   pickerVisible = false;
 
-  onPick($event: ColorEvent) {
-    this.symbolizer[this.attribute] = `rgba(${$event.color.rgb.r}, ${$event.color.rgb.g}, ${$event.color.rgb.b}, ${$event.color.rgb.a})`;
+  onPick($event: ColorEvent): void {
+    this.symbolizer[
+      this.attribute
+    ] = `rgba(${$event.color.rgb.r}, ${$event.color.rgb.g}, ${$event.color.rgb.b}, ${$event.color.rgb.a})`;
+    this.fontColor = this.generateFontColor([
+      $event.color.rgb.r,
+      $event.color.rgb.g,
+      $event.color.rgb.b,
+    ]);
     this.emitChange();
+  }
+
+  generateFontColor(rgbColor: Array<number>): string {
+    const [r, g, b] = rgbColor;
+    const hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b));
+    return hsp > 127.5 ? 'black' : 'white';
+  }
+
+  colorPickerStyle() {
+    return {
+      'background-color': this.symbolizer[this.attribute],
+      'color': this.fontColor,
+    };
   }
 }
