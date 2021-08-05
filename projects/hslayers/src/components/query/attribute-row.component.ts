7832b9107ab6c6e6dc7b9651baefcf9e3e2a7548
DailisLangovskis <dailis.langovskis@gmail.com>
2021-08-05 17:37:05 +0300

    feat(attribute-row): Stringify complex feature object attribute values for display

--- projects/hslayers/src/components/query/attribute-row.component.ts
+++ projects/hslayers/src/components/query/attribute-row.component.ts
@@ -1,19 +1,31 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'hs-query-attribute-row',
   templateUrl: './partials/attribute-row.html',
 })
-export class HsQueryAttributeRowComponent {
+export class HsQueryAttributeRowComponent implements OnInit {
   @Input() attribute;
   @Input() feature;
   @Input() readonly: boolean;
   @Input() template;
 
-  change() {
+  ngOnInit(): void {
+    this.checkAttributeValue();
+  }
+  change(): void {
     if (this.feature?.feature) {
       const feature = this.feature.feature;
       feature.set(this.attribute.name, this.attribute.value);
+    }
+  }
+
+  checkAttributeValue(): void {
+    if (
+      typeof this.attribute.value == 'object' &&
+      !Array.isArray(this.attribute.value)
+    ) {
+      this.attribute.value = JSON.stringify(this.attribute.value);
     }
   }
 }
