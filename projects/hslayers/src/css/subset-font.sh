41f0984b9bacc9dcc612a69c878369778b6c05a7
jmacura <macurajan@gmail.com>
2021-08-13 23:58:10 +0200

    refactor: Remove unused icons
    - icon-share
    - icon-twitter
    - icon-facebook
    - icon-googleplus
    - icon-road

--- projects/hslayers/src/css/subset-font.sh
+++ projects/hslayers/src/css/subset-font.sh
@@ -19,13 +19,9 @@
 # icon-chevron-left U+f489
 # icon-chevron-down U+f48b
 # icon-upload U+f47a
-# icon-share U+f4c9
 # icon-share-alt U+f16c
 # icon-edit U+f47c
 # icon-trash U+f0ce
-# icon-twitter U+f16a
-# icon-facebook U+f140
-# icon-googleplus U+f165
 # icon-download U+f47b
 # icon-pin  U+f20a
 # icon-polygonlasso U+f397
@@ -52,7 +48,6 @@
 # icon-calcminus    U+f572
 # icon-weightscale  U+f782
 # icon-design   U+f53d
-# icon-road U+f249
 # icon-screenshot   U+f109
 # icon-print    U+f125
 # icon-settingsandroid  U+f309
@@ -83,5 +78,5 @@
 # icon-opennewwindow U+f332
 # icon-move U+f322
 # icon-filter U+f05c
-pyftsubset whhg-font/font/webhostinghub-glyphs.ttf --unicodes="U+F1CA,U+f4c2,U+f315,U+f209,U+f00b,U+f000,U+002b,U+f022,U+f2b4,U+f078,U+f488,U+f489,U+f48b,U+f47a,U+f4c9,U+f47c,U+f0ce,U+f16a,U+f140,U+f165,U+f47b,U+f20a,U+f397,U+f1bf,U+f772,U+f1b7,U+00d7,U+f487,U+f19b,U+f23a,U+f36c,U+f316,U+f127,U+f0c5,U+f1b8,U+f1ed,U+f319,U+f692,U+f01b,U+f187,U+f0d2,U+f0c8,U+f571,U+f572,U+f782,U+f53d,U+f249,U+f109,U+f125,U+f309,U+f4d2,U+f30c,U+2212,U+f16c,U+f21e,U+f21f,U+f269,U+f470,U+f310,U+f307,U+f7ac,U+f210,U+f5d5,U+f0a3,U+f4d0,U+f09e,U+f6ec,U+f0dc,U+f301,U+f2fe,U+f675,U+f207,U+f206,U+f253,U+f11b,U+f332,U+f322,U+f05c"  --output-file="whhg-font/font/subset.ttf"
+pyftsubset whhg-font/font/webhostinghub-glyphs.ttf --unicodes="U+F1CA,U+f4c2,U+f315,U+f209,U+f00b,U+f000,U+002b,U+f022,U+f2b4,U+f078,U+f488,U+f489,U+f48b,U+f47a,U+f47c,U+f0ce,U+f47b,U+f20a,U+f397,U+f1bf,U+f772,U+f1b7,U+00d7,U+f487,U+f19b,U+f23a,U+f36c,U+f316,U+f127,U+f0c5,U+f1b8,U+f1ed,U+f319,U+f692,U+f01b,U+f187,U+f0d2,U+f0c8,U+f571,U+f572,U+f782,U+f53d,U+f109,U+f125,U+f309,U+f4d2,U+f30c,U+2212,U+f16c,U+f21e,U+f21f,U+f269,U+f470,U+f310,U+f307,U+f7ac,U+f210,U+f5d5,U+f0a3,U+f4d0,U+f09e,U+f6ec,U+f0dc,U+f301,U+f2fe,U+f675,U+f207,U+f206,U+f253,U+f11b,U+f332,U+f322,U+f05c"  --output-file="whhg-font/font/subset.ttf"
 ttf2eot whhg-font/font/subset.ttf whhg-font/font/subset.eot
