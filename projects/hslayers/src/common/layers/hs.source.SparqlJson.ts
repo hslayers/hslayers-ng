14707a98ab520cd7b61a814a927a758e0257dd18
jmacura <macurajan@gmail.com>
2021-08-18 16:02:25 +0200

    refactor(layers): fix this context & add type-defs

--- projects/hslayers/src/common/layers/hs.source.SparqlJson.ts
+++ projects/hslayers/src/common/layers/hs.source.SparqlJson.ts
@@ -110,7 +110,7 @@ function loadFeatures(
       );
       if (!isNaN(x) && !isNaN(y)) {
         const coord = transform([x, y], 'EPSG:4326', 'EPSG:3857');
-        if (typeof occupied_xy[coord] !== 'undefined') {
+        if (typeof occupied_xy[coord.toString()] !== 'undefined') {
           continue;
         }
         objects[key].geometry = new Point(coord);
@@ -122,7 +122,7 @@ function loadFeatures(
           category_map,
           category_id
         );
-        occupied_xy[coord] = true;
+        occupied_xy[coord.toString()] = true;
         features.push(feature);
       }
     }
@@ -135,7 +135,7 @@ function loadFeatures(
       delete objects[key]['http://www.opengis.net/ont/geosparql#asWKT'];
       const coord = objects[key].geometry.getCoordinates();
 
-      if (typeof occupied_xy[coord] !== 'undefined') {
+      if (typeof occupied_xy[coord.toString()] !== 'undefined') {
         continue;
       }
       const feature = new Feature(objects[key]);
@@ -233,6 +233,10 @@ export class SparqlJson extends Vector<Geometry> {
   loadCounter: number;
   loadTotal: number;
   occupied_xy = {};
+  //What is it good for?
+  styleAble: boolean;
+  //What is it good for?
+  hasPoint: boolean;
   /**
    * Only 'projection' and either 'url' or 'endpointUrl' + 'query' are mandatory
    */
@@ -254,7 +258,7 @@ export class SparqlJson extends Vector<Geometry> {
   }: SparqlOptions) {
     super({
       format: new GeoJSON(),
-      loader: async function (extent, resolution, projection) {
+      loader: async (extent, resolution, projection) => {
         this.set('loaded', false);
         if (!url && !endpointUrl) {
           return;
@@ -453,7 +457,6 @@ export class SparqlJson extends Vector<Geometry> {
           }
           return [tmp];
         },
-      projection: projection,
     });
     this.loadCounter = 0;
     this.loadTotal = 0;
