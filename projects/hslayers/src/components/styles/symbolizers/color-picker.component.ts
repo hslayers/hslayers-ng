b178cd080610b6ba1b53d7c02f0fbed9a91a8242
raitis <raitisbe@gmail.com>
2021-08-23 18:03:27 +0300

    fix: Fix opacity read/write in SLD
    Depending on the symbolizer, the opacity was not written to SLD file or wrong opacity attribute
    (fillOpacity instead of opacity) was used. This also implements workaround for mismatch of opacity
    attribs in geostyler.
    
    fix #2081, #1956

--- projects/hslayers/src/components/styles/symbolizers/color-picker.component.ts
+++ projects/hslayers/src/components/styles/symbolizers/color-picker.component.ts
@@ -11,20 +11,39 @@ import {HsStylerPartBaseComponent} from '../style-part-base.component';
 export class HsColorPickerComponent extends HsStylerPartBaseComponent {
   @Input() symbolizer: MarkSymbolizer | FillSymbolizer | TextSymbolizer;
   @Input() attribute: string;
+  @Input() opacityAttribute?: string;
   @Input() label: string;
   fontColor = 'white';
   pickerVisible = false;
 
   onPick($event: ColorEvent): void {
-    this.symbolizer[
-      this.attribute
-    ] = `rgba(${$event.color.rgb.r}, ${$event.color.rgb.g}, ${$event.color.rgb.b}, ${$event.color.rgb.a})`;
+    this.symbolizer[this.attribute] = $event.color.hex;
+    if (this.opacityAttribute) {
+      this.symbolizer[this.opacityAttribute] = $event.color.rgb.a;
+    }
     this.fontColor = this.generateFontColor([
       $event.color.rgb.r,
       $event.color.rgb.g,
       $event.color.rgb.b,
     ]);
     this.emitChange();
+  }
+
+  addAlpha(color: string, opacity: number): string {
+    if (opacity == undefined) {
+      return color;
+    }
+    const t = {...this.hex2Rgb(color), a: opacity};
+    return `rgba(${t.r}, ${t.g}, ${t.b}, ${t.a})`;
+  }
+
+  hex2Rgb(aRgbHex: string): {r: number; g: number; b: number} {
+    const rgb = aRgbHex.replace('#', '').match(/.{1,2}/g);
+    return {
+      r: parseInt(rgb[0], 16),
+      g: parseInt(rgb[1], 16),
+      b: parseInt(rgb[2], 16),
+    };
   }
 
   generateFontColor(rgbColor: Array<number>): string {
