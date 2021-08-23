b44fe05273a2dee207ccbdb316ac84411df5bcdc
raitis <raitisbe@gmail.com>
2021-08-23 22:30:43 +0300

    refactor: Remove dead search code

--- projects/hslayers/src/components/search/search.component.ts
+++ projects/hslayers/src/components/search/search.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-
 import {Subscription} from 'rxjs';
 
 import {HsEventBusService} from '../core/event-bus.service';
@@ -13,7 +12,6 @@ export class HsSearchComponent implements OnInit, OnDestroy {
   replace = false;
   clearVisible = false;
   searchInputVisible: boolean;
-  query = '';
   searchResultsReceivedSubscription: Subscription;
   constructor(
     public HsSearchService: HsSearchService,
@@ -30,40 +28,8 @@ export class HsSearchComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    if (this.HsShareUrlService.getParamValue('search')) {
-      this.query = this.HsShareUrlService.getParamValue('search');
-      this.queryChanged();
-    }
     window.innerWidth < 767
       ? (this.searchInputVisible = false)
       : (this.searchInputVisible = true);
-  }
-  /**
-   * Handler of search input, request search service and display results div
-   */
-  queryChanged(): void {
-    this.HsSearchService.request(this.query);
-  }
-  //not being used anywhere
-
-  // /**
-  //  * Set property highlighted of result to state
-  //  * @param {object} result Record to highlight
-  //  * @param {string} state To highlight or not to highlight
-  //  */
-  // highlightResult(result: any, state: any): void {
-  //   if (result.feature !== undefined) {
-  //     setHighlighted(result.feature, state);
-  //   }
-  // }
-
-  /**
-   * Remove previous search and search results
-   *
-   */
-  clear(): void {
-    this.query = '';
-    this.clearVisible = false;
-    this.HsSearchService.cleanResults();
   }
 }
