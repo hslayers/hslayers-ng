e9e8a06cdb5e9c1039fc96173a21709086d6f9f8
DailisLangovskis <dailis.langovskis@gmail.com>
2021-08-24 15:50:48 +0300

    test: Fix failed tests

---
+++ projects/hslayers/src/components/permalink/share-url.service.mock.ts
@@ -0,0 +1,68 @@
+import {HS_PRMS, HS_PRMS_BACKWARDS, HS_PRMS_REGENERATED} from './get-params';
+
+export class HsShareUrlServiceMock {
+  constructor() {}
+
+  /**
+   * @param str Parameter string to parse
+   * @returns Parsed parameter object
+   * Parse parameter string from Url into key-value(s) pairs
+   */
+  parse(str: string): any {
+    if (typeof str != 'string') {
+      return {};
+    }
+
+    str = str.trim().replace(/^\?/, '');
+
+    if (!str) {
+      return {};
+    }
+
+    return str
+      .trim()
+      .split('&')
+      .reduce((paramDict, param) => {
+        const parts = param.replace(/\+/g, ' ').split('=');
+        let key = parts[0];
+        let val = parts[1];
+
+        key = decodeURIComponent(key);
+        // missing `=` should be `null`:
+        // http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
+        val = val == undefined ? null : decodeURIComponent(val);
+
+        if (!paramDict.hasOwnProperty(key)) {
+          paramDict[key] = val;
+        } else if (Array.isArray(paramDict[key])) {
+          paramDict[key].push(val);
+        } else {
+          paramDict[key] = [paramDict[key], val];
+        }
+
+        return paramDict;
+      }, {});
+  }
+
+  getParamValue(param: string): string {
+    const tmp = this.parse(location.search);
+    const key = Object.keys(HS_PRMS).find((k) => HS_PRMS[k] == param);
+    if (tmp[param]) {
+      return tmp[param];
+    } else {
+      if (tmp[param] == undefined && HS_PRMS_BACKWARDS[key] != undefined) {
+        return this.getParamValue(HS_PRMS_BACKWARDS[key]);
+      } else {
+        return;
+      }
+    }
+  }
+
+  getParamValues(keys: string[]): any {
+    const tmp = {};
+    for (const key of keys) {
+      tmp[key] = this.getParamValue(key);
+    }
+    return tmp;
+  }
+}
