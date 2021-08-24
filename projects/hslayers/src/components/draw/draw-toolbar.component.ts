ad866a76069d79ee556d4e18768b015aa2316c26
Filip Leitner <leitnerfilip@gmail.com>
2021-08-24 09:39:56 +0200

    feat: Add selection type menu dropdown to draw-toolbar

--- projects/hslayers/src/components/draw/draw-toolbar.component.ts
+++ projects/hslayers/src/components/draw/draw-toolbar.component.ts
@@ -14,6 +14,7 @@ export class HsDrawToolbarComponent {
   drawToolbarExpanded = false;
   layersExpanded: boolean;
   drawTypeExpanded = false;
+  selectionMenuExpanded = false;
   onlyMineFilterVisible = false;
   getTitle = getTitle;
 
