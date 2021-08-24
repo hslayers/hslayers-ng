60f9e390df58b5f45d91bddb94e4107070a409da
Filip Leitner <leitnerfilip@gmail.com>
2021-08-24 09:03:40 +0200

    fix: Mobile view UI fixes

--- projects/test-app/src/hslayers-app/hslayers-app.component.ts
+++ projects/test-app/src/hslayers-app/hslayers-app.component.ts
@@ -101,15 +101,15 @@ export class HslayersAppComponent {
       ],
     };
     Object.assign(this.HsConfig, {
-      datasources: [
-        {
-          title: 'Layman',
-          url: 'http://localhost:8087',
-          user: 'anonymous',
-          type: 'layman',
-          liferayProtocol: 'https',
-        },
-      ],
+      // datasources: [
+      //   {
+      //     title: 'Layman',
+      //     url: 'http://localhost:8087',
+      //     user: 'anonymous',
+      //     type: 'layman',
+      //     liferayProtocol: 'https',
+      //   },
+      // ],
       proxyPrefix: window.location.hostname.includes('localhost')
         ? `${window.location.protocol}//${window.location.hostname}:8085/`
         : '/proxy/',
