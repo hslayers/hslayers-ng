09cf3b8a08ebc56cdc72f0d9744564d241c5dcac
raitis <raitisbe@gmail.com>
2021-08-30 14:00:49 +0300

    build: Upgrade to Angular 12

--- projects/hslayers-cesium/test/test.ts
+++ projects/hslayers-cesium/test/test.ts
@@ -1,7 +1,7 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 import '@angular/localize/init';
-import 'zone.js/dist/zone';
-import 'zone.js/dist/zone-testing';
+import 'zone.js';
+import 'zone.js/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
