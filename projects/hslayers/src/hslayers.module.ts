d1904d21127e0f530d2120486b14ce613d7c495c
raitis <raitisbe@gmail.com>
2021-09-06 17:44:21 +0300

    refactor: Move optional components out of HsLayout

--- projects/hslayers/src/hslayers.module.ts
+++ projects/hslayers/src/hslayers.module.ts
@@ -1,11 +1,42 @@
-import {HsCoreModule} from './components/core/core.module';
-import {HsLayoutModule} from './components/layout/layout.module';
-import {HslayersComponent} from './hslayers.component';
 import {NgModule} from '@angular/core';
 
+import {HsAddDataModule} from './components/add-data/add-data.module';
+import {HsCompositionsModule} from './components/compositions/compositions.module';
+import {HsCoreModule} from './components/core/core.module';
+import {HsDrawModule} from './components/draw/draw.module';
+import {HsFeatureTableModule} from './components/feature-table/feature-table.module';
+import {HsLayerManagerModule} from './components/layermanager/layermanager.module';
+import {HsLayoutModule} from './components/layout/layout.module';
+import {HsLegendModule} from './components/legend/legend.module';
+import {HsMeasureModule} from './components/measure/measure.module';
+import {HsPrintModule} from './components/print/print.module';
+import {HsQueryModule} from './components/query/query.module';
+import {HsSaveMapModule} from './components/save-map/save-map.module';
+import {HsSearchModule} from './components/search/search.module';
+import {HsShareModule} from './components/permalink/share.module';
+import {HsStylerModule} from './components/styles/styles.module';
+import {HsTripPlannerModule} from './components/trip_planner/trip-planner.module';
+import {HslayersComponent} from './hslayers.component';
 @NgModule({
   declarations: [HslayersComponent],
-  imports: [HsCoreModule, HsLayoutModule],
+  imports: [
+    HsCoreModule,
+    HsLayoutModule,
+    HsLayerManagerModule,
+    HsMeasureModule,
+    HsAddDataModule,
+    HsDrawModule,
+    HsLegendModule,
+    HsTripPlannerModule,
+    HsSaveMapModule,
+    HsFeatureTableModule,
+    HsPrintModule,
+    HsShareModule,
+    HsQueryModule,
+    HsSearchModule,
+    HsCompositionsModule,
+    HsStylerModule,
+  ],
   exports: [HslayersComponent],
 })
 export class HslayersModule {}
