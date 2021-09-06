b461c5cbf4eef2d4855ae40233624fc382584d2a
raitis <raitisbe@gmail.com>
2021-09-07 02:04:19 +0300

    test: Run plain decoupled hslayers app

--- projects/decoupling-test-app/src/hslayers-app/hslayers-app.module.ts
+++ projects/decoupling-test-app/src/hslayers-app/hslayers-app.module.ts
@@ -1,24 +1,14 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import {HsAddDataCommonModule} from '../../../hslayers/src/components/add-data/common/add-data-common.module';
-import {HsCompositionsModule} from '../../../hslayers/src/components/compositions/compositions.module';
 import {HsCoreModule} from '../../../hslayers/src/components/core/core.module';
 import {HsLayoutModule} from '../../../hslayers/src/components/layout/layout.module';
-import {HsQueryModule} from '../../../hslayers/src/components/query/query.module';
 import {HslayersAppComponent} from './hslayers-app.component';
 import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
   declarations: [HslayersAppComponent],
-  imports: [
-    BrowserModule,
-    HsCoreModule,
-    TranslateModule,
-    HsAddDataCommonModule,
-    HsCompositionsModule,
-    HsLayoutModule,
-  ],
+  imports: [BrowserModule, HsCoreModule, TranslateModule, HsLayoutModule],
   providers: [],
   bootstrap: [HslayersAppComponent],
 })
