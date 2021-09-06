0194df15e32367f0a1006b5343e387274348ac44
raitis <raitisbe@gmail.com>
2021-09-07 02:16:08 +0300

    refactor: Panel name for sensors with base class

--- projects/hslayers-sensors/src/components/sensors/sensors.component.ts
+++ projects/hslayers-sensors/src/components/sensors/sensors.component.ts
@@ -1,25 +1,28 @@
 import {Component, OnInit, ViewRef} from '@angular/core';
 import {HsLayoutService} from 'hslayers-ng';
 import {HsMapService} from 'hslayers-ng';
-import {HsPanelComponent} from 'hslayers-ng';
+import {HsPanelBaseComponent} from 'hslayers-ng';
 import {HsSensorUnit} from './sensor-unit.class';
 import {HsSensorsService} from './sensors.service';
 @Component({
   selector: 'hs-sensors',
   templateUrl: './partials/panel.html',
 })
-export class HsSensorsComponent implements HsPanelComponent, OnInit {
+export class HsSensorsComponent extends HsPanelBaseComponent implements OnInit {
   viewMode = 'sensors';
   viewExpanded = false;
   query: any = {description: ''};
   viewRef: ViewRef;
   data: {viewMode?: string};
+  name = 'sensors';
 
   constructor(
     public HsMapService: HsMapService,
     public HsSensorsService: HsSensorsService,
     public HsLayoutService: HsLayoutService
-  ) {}
+  ) {
+    super(HsLayoutService);
+  }
   ngOnInit(): void {
     this.HsMapService.loaded().then(() => this.init());
   }
