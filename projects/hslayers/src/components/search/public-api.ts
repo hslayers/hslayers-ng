d78b1ec381626058085ec8030db22bda6acbf1c8
DailisLangovskis <dailis.langovskis@gmail.com>
2021-09-09 20:03:15 +0300

    fix: Added missing exports to public-api files toolbar, measure, search components

--- projects/hslayers/src/components/search/public-api.ts
+++ projects/hslayers/src/components/search/public-api.ts
@@ -4,3 +4,4 @@ export * from './search-results.component';
 export * from './search.component';
 export * from './search.module';
 export * from './search.service';
+export * from './search-toolbar.component';
