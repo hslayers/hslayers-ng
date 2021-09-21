274b834900844d2cae577a7658c1d7ea0c07f68a
DailisLangovskis <dailis.langovskis@gmail.com>
2021-09-22 01:23:20 +0300

    feat: Listen for style changes also
    Change the legend also if the style is being changed

--- projects/hslayers/src/components/legend/legend-layer.component.ts
+++ projects/hslayers/src/components/legend/legend-layer.component.ts
@@ -1,37 +1,52 @@
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+
 import VectorLayer from 'ol/layer/Vector';
-import {Component, Input, OnInit} from '@angular/core';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
+
 import {HsLegendService} from './legend.service';
+import {HsStylerService} from '../styles/styler.service';
 import {HsUtilsService} from '../utils/utils.service';
 
 @Component({
   selector: 'hs-legend-layer-directive',
   templateUrl: './partials/legend-layer.component.html',
 })
-export class HsLegendLayerComponent implements OnInit {
+export class HsLegendLayerComponent implements OnInit, OnDestroy {
   @Input() layer: any;
   styles = [];
   geometryTypes = [];
-
+  private ngUnsubscribe = new Subject();
   constructor(
-    public HsUtilsService: HsUtilsService,
-    public HsLegendService: HsLegendService
-  ) {}
+    public hsUtilsService: HsUtilsService,
+    public hsLegendService: HsLegendService,
+    public hsStylerService: HsStylerService
+  ) {
+    this.hsStylerService.onSet
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((_) => {
+        this.styles = this.hsLegendService.getStyleVectorLayer(this.layer.lyr);
+        this.geometryTypes = this.hsLegendService.getVectorFeatureGeometry(
+          this.layer.lyr
+        );
+      });
+  }
 
   ngOnInit(): void {
     const olLayer = this.layer.lyr;
 
-    if (this.HsUtilsService.instOf(olLayer, VectorLayer)) {
-      this.styles = this.HsLegendService.getStyleVectorLayer(olLayer);
+    if (this.hsUtilsService.instOf(olLayer, VectorLayer)) {
+      this.styles = this.hsLegendService.getStyleVectorLayer(olLayer);
       this.geometryTypes =
-        this.HsLegendService.getVectorFeatureGeometry(olLayer);
+        this.hsLegendService.getVectorFeatureGeometry(olLayer);
     }
     if (olLayer.getSource()) {
       const source = olLayer.getSource();
-      const changeHandler = this.HsUtilsService.debounce(
+      const changeHandler = this.hsUtilsService.debounce(
         (e) => {
-          this.styles = this.HsLegendService.getStyleVectorLayer(olLayer);
+          this.styles = this.hsLegendService.getStyleVectorLayer(olLayer);
           this.geometryTypes =
-            this.HsLegendService.getVectorFeatureGeometry(olLayer);
+            this.hsLegendService.getVectorFeatureGeometry(olLayer);
         },
         200,
         false,
@@ -41,5 +56,10 @@ export class HsLegendLayerComponent implements OnInit {
       source.on('addfeature', changeHandler);
       source.on('removefeature', changeHandler);
     }
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 }
