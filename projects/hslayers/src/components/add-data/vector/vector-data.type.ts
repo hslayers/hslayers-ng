14db9e471e57a55a31529359694d7446164cde5c
DailisLangovskis <dailis.langovskis@gmail.com>
2021-11-11 20:39:19 +0200

    refactor: Clean up vector-file data-object

--- projects/hslayers/src/components/add-data/vector/vector-data.type.ts
+++ projects/hslayers/src/components/add-data/vector/vector-data.type.ts
@@ -1,23 +1,31 @@
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import {Geometry} from 'ol/geom';
 import {Layer} from 'ol/layer';
 import {Source} from 'ol/source';
 
+import {HsLayerDescriptor} from '../../layermanager/layer-descriptor.interface';
+import {accessRightsModel} from '../common/access-rights.model';
+
 export type vectorDataObject = {
-  srs: string;
+  abstract?: string;
+  access_rights?: accessRightsModel;
+  addUnder?: Layer<Source>;
+  base64url?: string;
+  dataType?: string;
+  extract_styles?: boolean;
+  featureCount?: number;
+  features?: Array<any>;
+  folder_name?: string;
+  name?: string;
   // Not possible to save KML to layman yet
-  abstract: string;
-  addUnder: Layer<Source>;
-  base64url: string;
-  dataType: string;
-  errorOccurred: boolean;
-  extract_styles: boolean;
-  featureCount: number;
-  features: Array<any>;
-  folder_name: string;
-  name: string;
-  saveAvailable: boolean;
-  saveToLayman: boolean;
-  showDetails: boolean;
-  title: string;
-  type: string;
-  url: string;
+  saveAvailable?: boolean;
+  saveToLayman?: boolean;
+  showDetails?: boolean;
+  sourceLayer?: VectorLayer<VectorSource<Geometry>>;
+  srs?: string;
+  title?: string;
+  type?: string;
+  url?: string;
+  vectorLayers?: HsLayerDescriptor[];
 };
