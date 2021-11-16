8bef569a7147de7af7b403ed7b684ca75596780d
DailisLangovskis <dailis.langovskis@gmail.com>
2021-11-16 18:03:56 +0200

    fix(add-data): Provide error message
    When adding geotiff to layman fails, display error using toast component

--- projects/hslayers/src/components/save-map/layman-layer-descriptor.interface.ts
+++ projects/hslayers/src/components/save-map/layman-layer-descriptor.interface.ts
@@ -2,6 +2,12 @@ export interface HsLaymanLayerDescriptor {
   wms?: any;
   wfs?: any;
   name?: any;
+  file?: {
+    error?: {
+      code?: number;
+      message?: string;
+    };
+  };
   code?: any;
   exists?: boolean;
 }
