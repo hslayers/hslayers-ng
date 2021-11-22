cb2c09a2379f3558247eccb9d87cb5ef67e0ef68
DailisLangovskis <dailis.langovskis@gmail.com>
2021-11-22 21:37:18 +0200

    feat: Store csv table data also

--- projects/test-statistics-app/src/lib/upload-panel.ts
+++ projects/test-statistics-app/src/lib/upload-panel.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 
 import Papa from 'papaparse';
 import {
@@ -8,8 +8,10 @@ import {
   HsLayoutService,
   HsPanelBaseComponent,
   HsSidebarService,
+  HsUploadComponent,
   HsUploadedFiles,
 } from 'hslayers-ng';
+
 import {HsStatisticsService, Usage} from './statistics.service';
 import {HsStatisticsToMapDialogComponent} from './to-map-dialog.component';
 
@@ -17,13 +19,18 @@ import {HsStatisticsToMapDialogComponent} from './to-map-dialog.component';
   selector: 'hs-statistics-upload',
   templateUrl: './upload-panel.component.html',
 })
-export class HsStatisticsUploadPanelComponent extends HsPanelBaseComponent {
+export class HsStatisticsUploadPanelComponent
+  extends HsPanelBaseComponent
+  implements AfterViewInit
+{
   public title = '';
   name = 'statistics-upload';
   columns: string[];
   uses: Usage;
   rows: any[];
   rowsCollapsed = false;
+  fileInput;
+  @ViewChild(HsUploadComponent) hsUploadComponent: HsUploadComponent;
   constructor(
     public hsStatisticsService: HsStatisticsService,
     public hsConfig: HsConfig,
@@ -43,6 +50,26 @@ export class HsStatisticsUploadPanelComponent extends HsPanelBaseComponent {
         hsLanguageService.getTranslation('SIDEBAR.descriptions.UPLOAD'),
       icon: 'icon-upload',
     });
+    if (!this.rows && !this.columns) {
+      const savedTable = localStorage.getItem('hs_statistics_table');
+      if (savedTable) {
+        this.rows = JSON.parse(savedTable).rows;
+        this.columns = JSON.parse(savedTable).columns;
+        this.setUses();
+      }
+    }
+    this.hsStatisticsService.clearData$.subscribe(() => {
+      this.rows = [];
+      this.columns = [];
+      this.uses = {};
+      this.rowsCollapsed = false;
+      if (this.fileInput?.nativeElement?.value) {
+        this.fileInput.nativeElement.value = '';
+      }
+    });
+  }
+  ngAfterViewInit(): void {
+    this.fileInput = this.hsUploadComponent.getFileInput();
   }
 
   visualizeInMap(): void {
@@ -51,6 +78,10 @@ export class HsStatisticsUploadPanelComponent extends HsPanelBaseComponent {
       columns: this.columns,
       uses: this.uses,
     });
+  }
+
+  dataAvailable(): boolean {
+    return this.columns?.length > 0 && this.rows?.length > 0;
   }
 
   handleFileUpload(evt: HsUploadedFiles): void {
@@ -69,22 +100,29 @@ export class HsStatisticsUploadPanelComponent extends HsPanelBaseComponent {
       }
       const records = Papa.parse(fileContents[0] as string, {header: true});
       this.columns = Object.keys(records.data[0]);
-      this.uses = {};
-      Object.keys(records.data[0]).map((key) => {
-        switch (key) {
-          case 'Novads':
-          case 'Pagasts':
-          case 'Pašvaldība':
-            this.uses[key] = 'location';
-            break;
-          case 'Gads':
-            this.uses[key] = 'time';
-            break;
-          default:
-            this.uses[key] = 'variable';
-        }
-      });
+      this.setUses();
       this.rows = records.data;
+    });
+  }
+
+  setUses(): void {
+    if (!this.columns) {
+      return;
+    }
+    this.uses = {};
+    this.columns.map((key) => {
+      switch (key) {
+        case 'Novads':
+        case 'Pagasts':
+        case 'Pašvaldība':
+          this.uses[key] = 'location';
+          break;
+        case 'Gads':
+          this.uses[key] = 'time';
+          break;
+        default:
+          this.uses[key] = 'variable';
+      }
     });
   }
 }
