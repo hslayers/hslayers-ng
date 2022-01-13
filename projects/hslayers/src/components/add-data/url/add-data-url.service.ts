751055745a43c637d1746a18a6b8fa55da9381e2
raitis <raitisbe@gmail.com>
2022-01-14 00:36:03 +0200

    chore: Return allowAdding=true for arcgis always

--- projects/hslayers/src/components/add-data/url/add-data-url.service.ts
+++ projects/hslayers/src/components/add-data/url/add-data-url.service.ts
@@ -17,7 +17,6 @@ export class HsAddDataUrlService {
   addingAllowed: boolean;
   typeSelected: AddDataUrlType;
   connectFromParams = true;
-  serviceType: AddDataUrlType;
   constructor(
     public hsLog: HsLogService,
     public hsLanguageService: HsLanguageService,
@@ -128,6 +127,6 @@ export class HsAddDataUrlService {
   searchForChecked(services: Array<any>): void {
     this.addingAllowed =
       services.some((service) => service.checked) ||
-      this.serviceType == 'arcgis';
+      this.typeSelected == 'arcgis';
   }
 }
