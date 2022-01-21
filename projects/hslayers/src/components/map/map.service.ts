08b84c8f3e92800a6aa4ec55ca8d95dee9f61573
DailisLangovskis <dailis.langovskis@gmail.com>
2022-01-21 13:13:41 +0200

    fix: Register 3995 projection to proj.def

--- projects/hslayers/src/components/map/map.service.ts
+++ projects/hslayers/src/components/map/map.service.ts
@@ -414,6 +414,10 @@ export class HsMapService {
     );
     proj4.defs('EPSG:4326', '+proj=longlat +datum=WGS84 +no_defs');
     proj4.defs(
+      'EPSG:3995',
+      '+proj=stere +lat_0=90 +lat_ts=71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs'
+    );
+    proj4.defs(
       'EPSG:3031',
       '+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs'
     );
