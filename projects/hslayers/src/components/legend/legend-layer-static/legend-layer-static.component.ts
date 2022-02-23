4de5a7816b14520833c5f431fe46f3969ecbe537
DailisLangovskis <dailis.langovskis@gmail.com>
2022-02-23 19:50:05 +0200

    refactor(legend-layer-static): Moved fillContent function to service for reuse purposes

--- projects/hslayers/src/components/legend/legend-layer-static/legend-layer-static.component.ts
+++ projects/hslayers/src/components/legend/legend-layer-static/legend-layer-static.component.ts
@@ -1,10 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {
-  DomSanitizer,
-  SafeHtml,
-  SafeResourceUrl,
-} from '@angular/platform-browser';
 
+import {HsLegendLayerStaticService} from './legend-layer-static.service';
+import {LayerLegend} from './types/layer-legend.type';
 import {getLegends} from '../../../common/layer-extensions';
 
 @Component({
@@ -13,39 +10,21 @@ import {getLegends} from '../../../common/layer-extensions';
 })
 export class HsLegendLayerStaticComponent implements OnInit {
   @Input() layer: any;
-  lastLegendImage: any;
-  legendType: string;
-  svgContent: SafeHtml;
-  legendImage: SafeResourceUrl;
+  layerLegend: LayerLegend = {};
 
-  constructor(private sanitizer: DomSanitizer) {}
-
-  fillContent(): void {
-    let legendImage = getLegends(this.layer.lyr);
-    if (Array.isArray(legendImage)) {
-      legendImage = legendImage[0];
-    }
-    if (legendImage) {
-      this.lastLegendImage = legendImage;
-      if (legendImage.indexOf('<svg') > -1) {
-        this.legendType = 'svg';
-        this.svgContent = this.sanitizer.bypassSecurityTrustHtml(legendImage);
-      } else {
-        this.legendType = 'image';
-        this.legendImage =
-          this.sanitizer.bypassSecurityTrustResourceUrl(legendImage);
-      }
-    }
-  }
+  constructor(private hsLegendLayerStaticService: HsLegendLayerStaticService) {}
 
   ngOnInit(): void {
     if (getLegends(this.layer.lyr)) {
-      this.fillContent();
+      Object.assign(
+        this.layerLegend,
+        this.hsLegendLayerStaticService.fillContent(this.layer.lyr)
+      );
     }
     this.layer.lyr.on('change', (e) => {
       //TODO: Maybe rewrite this to something more fancy like Observable
-      if (getLegends(this.layer.lyr) != this.lastLegendImage) {
-        this.fillContent();
+      if (getLegends(this.layer.lyr) != this.layerLegend.lastLegendImage) {
+        this.hsLegendLayerStaticService.fillContent(this.layer.lyr);
       }
     });
   }
---
+++ projects/hslayers/src/components/legend/legend-layer-static/legend-layer-static.service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core';
+
+import {DomSanitizer} from '@angular/platform-browser';
+import {Layer} from 'ol/layer';
+import {Source} from 'ol/source';
+
+import {LayerLegend} from './types/layer-legend.type';
+import {getLegends} from '../../../common/layer-extensions';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HsLegendLayerStaticService {
+  constructor(private sanitizer: DomSanitizer) {}
+
+  fillContent(lyr: Layer<Source>): LayerLegend {
+    const layerLegend: LayerLegend = {};
+    let legendImage = getLegends(lyr);
+    if (Array.isArray(legendImage)) {
+      legendImage = legendImage[0];
+    }
+    if (legendImage) {
+      layerLegend.lastLegendImage = legendImage;
+      if (legendImage.indexOf('<svg') > -1) {
+        layerLegend.legendType = 'svg';
+        layerLegend.svgContent =
+          this.sanitizer.bypassSecurityTrustHtml(legendImage);
+      } else {
+        layerLegend.legendType = 'image';
+        layerLegend.legendImage =
+          this.sanitizer.bypassSecurityTrustResourceUrl(legendImage);
+      }
+    }
+    return layerLegend;
+  }
+}
