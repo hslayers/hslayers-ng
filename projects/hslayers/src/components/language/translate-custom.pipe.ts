03070da17749e0839196f3436d85fb2e1d14428e
raitis <raitisbe@gmail.com>
2022-03-09 01:14:16 +0200

    perf: Cache translation pipe

--- projects/hslayers/src/components/language/translate-custom.pipe.ts
+++ projects/hslayers/src/components/language/translate-custom.pipe.ts
@@ -13,8 +13,7 @@ import {TranslatePipe, TranslateService} from '@ngx-translate/core';
 })
 export class TranslateCustomPipe
   extends TranslatePipe
-  implements PipeTransform
-{
+  implements PipeTransform {
   onLangChangeOverridden: boolean;
   constructor(
     private translate2: TranslateService,
@@ -25,6 +24,9 @@ export class TranslateCustomPipe
   }
 
   transform(query: string, ...args: any[]) {
+    if (equals(query, this.lastKey) && equals(args, this.lastParams)) {
+      return this.value;
+    }
     let interpolateParams: any | undefined = undefined;
     if (args[0] && args.length) {
       if (typeof args[0] === 'string' && args[0].length) {
@@ -44,6 +46,12 @@ export class TranslateCustomPipe
         interpolateParams = args[0];
       }
     }
+
+    this.lastKey = query;
+
+    // store the params, in case they change
+    this.lastParams = args;
+
     const onTranslation = (res: string) => {
       this.value = res !== undefined ? res : query;
       this.lastKey = query;
@@ -54,4 +62,54 @@ export class TranslateCustomPipe
       .subscribe(onTranslation);
     return this.value;
   }
+}
+
+export function equals(o1: any, o2: any): boolean {
+  if (o1 === o2) {
+    return true;
+  }
+  if (o1 === null || o2 === null) {
+    return false;
+  }
+  if (o1 !== o1 && o2 !== o2) {
+    return true;
+  } // NaN === NaN
+  let t1 = typeof o1,
+    t2 = typeof o2,
+    length: number,
+    key: any,
+    keySet: any;
+  if (t1 == t2 && t1 == 'object') {
+    if (Array.isArray(o1)) {
+      if (!Array.isArray(o2)) {
+        return false;
+      }
+      if ((length = o1.length) == o2.length) {
+        for (key = 0; key < length; key++) {
+          if (!equals(o1[key], o2[key])) {
+            return false;
+          }
+        }
+        return true;
+      }
+    } else {
+      if (Array.isArray(o2)) {
+        return false;
+      }
+      keySet = Object.create(null);
+      for (key in o1) {
+        if (!equals(o1[key], o2[key])) {
+          return false;
+        }
+        keySet[key] = true;
+      }
+      for (key in o2) {
+        if (!(key in keySet) && typeof o2[key] !== 'undefined') {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+  return false;
 }
