d790b2424b32844e79b9213e6321dac1e2f1909f
raitis <raitisbe@gmail.com>
2022-03-10 18:38:37 +0200

    fix: Add custom translate pipe and service to public-api

--- projects/hslayers/src/components/language/public-api.ts
+++ projects/hslayers/src/components/language/public-api.ts
@@ -1,3 +1,5 @@
 export * from './language.component';
 export * from './language.module';
 export * from './language.service';
+export * from './custom-translate.service';
+export * from './translate-custom.pipe';
