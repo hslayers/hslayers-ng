3ab98a9cb1c4b142e5f9cb2716e1a465a315a34f
DailisLangovskis <dailis.langovskis@gmail.com>
2022-04-14 00:16:22 +0300

    style: Datatypes for save map composition data

---
+++ projects/hslayers/src/components/save-map/types/user-data.type.ts
@@ -0,0 +1,12 @@
+export type UserData = {
+  email?: string;
+  phone?: string;
+  name?: string;
+  address?: string;
+  country?: string;
+  postalCode?: string;
+  city?: string;
+  organization?: string;
+  position?: string;
+  state?: string;
+};
