02ca26a8178cdac81af82fcf9a487e8645a62364
raitis <raitisbe@gmail.com>
2022-04-20 15:40:02 +0300

    fix: Bootstrapping code for cesium-app

--- projects/hslayers-cesium-app/src/app/app.module.ts
+++ projects/hslayers-cesium-app/src/app/app.module.ts
@@ -1,5 +1,5 @@
+import {ApplicationRef, DoBootstrap, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {HsCesiumModule} from 'hslayers-cesium';
@@ -11,4 +11,26 @@ import {HslayersModule} from 'hslayers-ng';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule implements DoBootstrap {
+  constructor() {}
+
+  ngDoBootstrap(appRef: ApplicationRef) {
+    let tries = 0;
+    const waitAppElement = () => {
+      tries++;
+      if (tries > 20) {
+        console.error('<hslayers-cesium-app> element is missing!');
+        return;
+      }
+      if (!document.querySelector('hslayers-cesium-app')) {
+        setTimeout(waitAppElement, 50);
+        return;
+      }
+      document.querySelectorAll('hslayers-cesium-app').forEach((el) => {
+        appRef.bootstrap(AppComponent, el);
+      });
+    };
+
+    setTimeout(waitAppElement, 50);
+  }
+}
