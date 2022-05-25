383a28886261980d116b79fcae0a78a75975c7bc
DailisLangovskis <dailis.langovskis@gmail.com>
2022-05-25 13:46:32 +0300

    fix: Fixed multiple layer removal

--- projects/hslayers/src/common/layman/types/delete-layer-response.type.ts
+++ projects/hslayers/src/common/layman/types/delete-layer-response.type.ts
@@ -6,10 +6,22 @@ export type DeleteAllLayersResponse = {
   title?: string;
   uuid?: string;
   url?: string;
+  code?: number;
+  detail?: any;
+  error?: {
+    message: string;
+  };
+  message?: string;
 };
 
 export type DeleteSingleLayerResponse = {
   name?: string;
   uuid?: string;
   url?: string;
+  code?: number;
+  detail?: any;
+  error?: {
+    message: string;
+  };
+  message?: string;
 };
