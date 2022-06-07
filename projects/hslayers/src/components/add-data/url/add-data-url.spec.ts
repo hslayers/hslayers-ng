c162c21f7f7f48eb558faca9ef927ce379d76824
Filip Leitner <leitnerfilip@gmail.com>
2022-06-07 13:45:12 +0200

    test: Create two add-data-url unit tests

---
+++ projects/hslayers/src/components/add-data/url/add-data-url.spec.ts
@@ -0,0 +1,143 @@
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {FormsModule} from '@angular/forms';
+
+import {CommonModule} from '@angular/common';
+import {HsAddDataUrlComponent} from './add-data-url.component';
+import {HsAddDataVectorModule} from '../vector/vector.module';
+import {HsLanguageModule} from '../../language/language.module';
+import {HsUrlArcGisModule} from './arcgis/arcgis.module';
+import {HsUrlWfsModule} from './wfs/wfs.module';
+import {HsUrlWmsModule} from './wms/wms.module';
+import {HsUrlWmtsModule} from './wmts/wmts.module';
+
+import {HsConfig} from '../../../config.service';
+import {HsConfigMock} from '../../../config.service.mock';
+import {HsLayerUtilsService} from '../../utils/layer-utils.service';
+import {HsLayoutService} from '../../layout/layout.service';
+import {HsLayoutServiceMock} from '../../layout/layout.service.mock';
+import {HsUtilsService} from '../../utils/utils.service';
+import {HsUtilsServiceMock} from '../../utils/utils.service.mock';
+import {lastValueFrom} from 'rxjs';
+import {mockLayerUtilsService} from '../../utils/layer-utils.service.mock';
+
+import serviceEndpoints from '../../../../test/data/service-endpoints.json';
+import {HsMapService} from '../../map/map.service';
+import {HsMapServiceMock} from '../../map/map.service.mock';
+import {HsWmsGetCapabilitiesService} from '../../../common/get-capabilities/wms-get-capabilities.service';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+
+let httpClient;
+let hsWmsGetCapabilitiesService;
+
+describe('HsAddDataUrlComponent', () => {
+  beforeAll(() => {
+    TestBed.resetTestEnvironment();
+    TestBed.initTestEnvironment(
+      BrowserDynamicTestingModule,
+      platformBrowserDynamicTesting(),
+      {
+        teardown: {destroyAfterEach: false},
+      }
+    );
+  });
+
+  let component: HsAddDataUrlComponent;
+  let fixture: ComponentFixture<HsAddDataUrlComponent>;
+  let originalTimeout: number;
+  beforeEach(() => {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+
+    const bed = TestBed.configureTestingModule({
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      declarations: [HsAddDataUrlComponent],
+      imports: [
+        HttpClientModule,
+        CommonModule,
+        FormsModule,
+        HsLanguageModule,
+        HsUrlWmsModule,
+        HsUrlArcGisModule,
+        HsUrlWfsModule,
+        HsAddDataVectorModule,
+        HsUrlWmtsModule,
+      ],
+      providers: [
+        {provide: HsMapService, useValue: new HsMapServiceMock()},
+        {provide: HsConfig, useValue: new HsConfigMock()},
+        {provide: HsUtilsService, useValue: new HsUtilsServiceMock()},
+        {provide: HsLayoutService, useValue: new HsLayoutServiceMock()},
+        {provide: HsLayerUtilsService, useValue: mockLayerUtilsService()},
+      ],
+    });
+    hsWmsGetCapabilitiesService = TestBed.inject(HsWmsGetCapabilitiesService);
+    httpClient = TestBed.inject(HttpClient);
+    //Mock server response
+    hsWmsGetCapabilitiesService.request = async (url) => {
+      const serviceURUL = url.includes('?')
+        ? url.substring(0, url.indexOf('?'))
+        : url;
+      const r = await lastValueFrom(
+        httpClient.get(serviceURUL + '?service=WMS&request=getCapabilities', {
+          responseType: 'text',
+        })
+      );
+      const wrap = {response: r};
+      return wrap;
+    };
+    //bed.compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HsAddDataUrlComponent);
+    fixture.componentInstance.app = 'default';
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  //
+
+  it('Should list WMS capability layers', async () => {
+    component.hsAddDataCommonService.hsAddDataService.selectType(
+      'url',
+      'default'
+    );
+
+    await component.hsAddDataOwsService.connectToOWS(
+      {
+        type: 'wms',
+        uri: serviceEndpoints.wms[1],
+        layer: 'GR_ZM100',
+        style: undefined,
+      },
+      component.app
+    );
+    const wmsService = component.hsAddDataOwsService.hsUrlWmsService;
+    expect(wmsService.get(component.app).data.layers.length).toBe(1);
+  });
+
+  it('Should load dataset metadata record as service', async () => {
+    await component.hsAddDataOwsService.connectToOWS(
+      {
+        type: 'wms',
+        uri: serviceEndpoints.wms[1],
+        layer: 'Random non existent name',
+        style: undefined,
+      },
+      component.app
+    );
+    expect(
+      component.hsAddDataCommonService.hsAddDataService.get(component.app)
+        .dsSelected
+    ).toBe('url');
+  });
+});
