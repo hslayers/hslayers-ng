8acf098df9f18cc3d67a2c3081dba6c71f247931
raitis <raitisbe@gmail.com>
2022-06-15 13:54:10 +0300

    build(statistics): Resolve font conflicts for production

--- projects/test-statistics-app/css/subset-font.sh
+++ projects/test-statistics-app/css/subset-font.sh
@@ -5,6 +5,6 @@
 # npm install ttf2eot -g
 
 # CSS classes - Unicode character mappings:
-# icon-barchartasc U+f5eb
+# statistics-icon-barchartasc U+f5eb
 pyftsubset ../../hslayers/src/css/whhg-font/font/webhostinghub-glyphs.ttf --unicodes="U+f5eb"  --output-file="whhg-font/font/subset.ttf"
 ttf2eot whhg-font/font/subset.ttf whhg-font/font/subset.eot
