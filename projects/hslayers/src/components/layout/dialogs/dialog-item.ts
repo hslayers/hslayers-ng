151830796a6f1420de2be0010290bb472eb6eeef
jmacura <macurajan@gmail.com>
2022-07-27 00:34:26 +0200

    chore: Whitespace & comments clean-up

--- projects/hslayers/src/components/layout/dialogs/dialog-item.ts
+++ projects/hslayers/src/components/layout/dialogs/dialog-item.ts
@@ -15,9 +15,3 @@ export class HsDialogItem {
     this.app = app;
   }
 }
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
