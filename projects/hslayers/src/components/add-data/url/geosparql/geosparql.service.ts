78590d5fd843852dfc374afcc7d5856c265bc500
jmacura <macurajan@gmail.com>
2022-07-30 02:47:20 +0200

    feat(add-data): Verify SPARQL endpoint URL

--- projects/hslayers/src/components/add-data/url/geosparql/geosparql.service.ts
+++ projects/hslayers/src/components/add-data/url/geosparql/geosparql.service.ts
@@ -1,4 +1,8 @@
+import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
+import {lastValueFrom, takeUntil} from 'rxjs';
+
+import {HsAddDataService} from '../../add-data.service';
 import {urlDataObject} from '../types/data-object.type';
 
 class HsUrlGeoSparqlParams {
@@ -20,12 +24,42 @@ export class HsUrlGeoSparqlService {
     [id: string]: any;
   } = {default: new HsUrlGeoSparqlParams()};
 
-  constructor() {}
+  constructor(
+    public httpClient: HttpClient,
+    public hsAddDataService: HsAddDataService
+  ) {}
 
   get(app: string): HsUrlGeoSparqlParams {
     if (this.apps[app ?? 'default'] === undefined) {
       this.apps[app ?? 'default'] = new HsUrlGeoSparqlParams();
     }
     return this.apps[app ?? 'default'];
+  }
+
+  async verifyEndpoint(url: string, app: string = 'default') {
+    try {
+      const r = await lastValueFrom(
+        this.httpClient
+          .get(url, {
+            //TODO: could there be also different accept header?
+            headers: {'Accept': ['application/rdf+xml', 'application/xml']},
+            responseType: 'blob',
+          })
+          .pipe(takeUntil(this.hsAddDataService.cancelUrlRequest))
+      );
+      const blobText = await r.text();
+      const parsedResponse = new DOMParser().parseFromString(
+        blobText,
+        'application/xml'
+      );
+      console.log(parsedResponse);
+      console.log(parsedResponse.activeElement.localName);
+      if (parsedResponse.activeElement.localName === 'RDF') {
+        return true;
+      }
+    } catch {
+      TODO: null;
+    }
+    return false;
   }
 }
