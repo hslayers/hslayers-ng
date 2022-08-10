b3fb761e9b80c893ae5bd056770fcc65f23b0b95
raitis <raitisbe@gmail.com>
2022-08-10 23:00:21 +0300

    fix(statistics): Fix building of statistics app for ng14

--- projects/test-statistics-app/src/bootstrap.ts
+++ projects/test-statistics-app/src/bootstrap.ts
@@ -1,6 +1,11 @@
+import {AppModule} from './app/app.module';
+import {enableProdMode} from '@angular/core';
+import {environment} from './environments/environment';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import {AppModule} from './app/app.module';
+if (environment.production) {
+  enableProdMode();
+}
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
--- projects/test-statistics-app/webpack.config.js
+++ projects/test-statistics-app/webpack.config.js
@@ -1,54 +1,16 @@
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const mf = require("@angular-architects/module-federation/webpack");
-const path = require("path");
-const share = mf.share;
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
-const sharedMappings = new mf.SharedMappings();
-sharedMappings.register(
-  path.join(__dirname, './tsconfig.app.json'),
-  []);
+module.exports = withModuleFederationPlugin({
+  name: 'statistics',
 
-  console.log(sharedMappings.getAliases());
-module.exports = {
-  output: {
-    uniqueName: "statistics",
-    publicPath: "auto"
+  exposes: {
+    './HsStatisticsPanelComponent': './projects/test-statistics-app/src/lib/statistics-panel.component.ts',
   },
-  optimization: {
-    runtimeChunk: false
-  },   
-  resolve: {
-    alias: {
-      ...sharedMappings.getAliases(),
-    }
-  },
-  experiments: {
-    outputModule: true
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-        library: { type: "module" },
 
-        // For remotes (please adjust)
-        name: "statistics",
-        filename: "remoteEntry.js",
-        exposes: {
-            './HsStatisticsPanelComponent': './projects/test-statistics-app/src/lib/statistics-panel.component.ts',
-        },        
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
 
-        shared: share({
-          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/forms": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@ngx-translate/core": { singleton: true, strictVersion: true , requiredVersion: '^14.0.0'},
-          "rxjs": { singleton: true }, 
-          "hslayers-ng": { singleton: true }, 
-          "ol": { singleton: true }, 
-          ...sharedMappings.getDescriptors()
-        })
-        
-    }),
-    sharedMappings.getPlugin()
-  ],
-};
+  sharedMappings: ['hslayers-ng'],
+
+});
