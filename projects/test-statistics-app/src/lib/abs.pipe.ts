d4ed423c91ba62ad54d6e55ca0ed5310a7b5503a
raitis <raitisbe@gmail.com>
2022-08-17 12:20:41 +0300

    refactor: Remove statistics app from hslayers

--- projects/test-statistics-app/src/lib/abs.pipe.ts
+++
@@ -1,8 +0,0 @@
-import {Pipe, PipeTransform} from '@angular/core';
-
-@Pipe({name: 'abs'})
-export class AbsPipe implements PipeTransform {
-  transform(num: number, args?: any): any {
-    return Math.abs(num);
-  }
-}
