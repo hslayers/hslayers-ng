99384812855e6a20d50f1bbe7dd6f21c495a4c6d
Filip Leitner <leitnerfilip@gmail.com>
2022-08-31 10:12:25 +0200

    test(e2e): Migrate cypress configs to v10, fix and add layout tests

---
+++ cypress.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable import/extensions */
+/* eslint-disable @typescript-eslint/no-var-requires */
+import {defineConfig} from 'cypress';
+
+export default defineConfig({
+  fixturesFolder: 'cypress/fixtures',
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.ts')(on, config);
+    },
+    specPattern: 'cypress/tests/**/*.cy.{js,jsx,ts,tsx}',
+    baseUrl: 'http://localhost:4200',
+  },
+});
