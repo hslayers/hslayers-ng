76d48e0965bedb5ba5a7f9795ccccb0fbaaf9296
raitis <raitisbe@gmail.com>
2022-10-14 19:31:54 +0300

    test: Check if all panels open

---
+++ cypress/tests/sidebar.cy.ts
@@ -0,0 +1,39 @@
+describe('Hslayers application', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    //Open layer manager
+    cy.get('.hs-sidebar-item[data-cy="layermanager"]').click();
+    //Turn off all layers
+    cy.get('hs-layermanager-layer-list li').each((layer) => {
+      cy.wrap(layer).find('.d-flex div:first').click();
+    });
+    //Close layer manager
+    cy.get('.hs-sidebar-item[data-cy="layermanager"]:first').click();
+  });
+
+  it('All panels should open', () => {
+    cy.wait(200);
+    cy.get('hs-sidebar .hs-sidebar-item:not(.hs-panel-hidden)').each(
+      (sidebarButton) => {
+        const panelName = sidebarButton.attr('data-cy');
+        if (panelName) {
+          cy.wrap(sidebarButton).click();
+          //Check if panel header visible
+          cy.get(`hs-panel-header[name="${panelName}"]`).should('be.visible');
+          cy.wrap(sidebarButton).click();
+        }
+      }
+    );
+    cy.get('.hs-sidebar-additional-items:first').click();
+    cy.get('hs-mini-sidebar .hs-sidebar-item.hs-panel-hidden').each(
+      (sidebarButton) => {
+        const panelName = sidebarButton.attr('data-cy');
+        if (panelName) {
+          cy.wrap(sidebarButton).click();
+          cy.get(`hs-panel-header[name="${panelName}"]`).should('be.visible');
+          cy.get('.hs-sidebar-additional-items:first').click();
+        }
+      }
+    );
+  });
+});
