aa425c45817c0fe3ae19bca84f67187c28dd4147
raitis <raitisbe@gmail.com>
2022-10-17 23:34:12 +0300

    test: Language tests

---
+++ cypress/tests/language.cy.ts
@@ -0,0 +1,22 @@
+describe('Hslayers application', () => {
+  before(() => {
+    cy.visit('/');
+  });
+
+  it('Language panel should open', () => {
+    cy.get('.hs-sidebar-additional-items:first').click();
+    cy.get(
+      'hs-mini-sidebar .hs-sidebar-item.hs-panel-hidden[data-cy="language"]'
+    ).click();
+    cy.get('hs-language .list-group .btn:last').click();
+    cy.get('hs-language hs-panel-header').should(
+      'have.attr',
+      'ng-reflect-title',
+      'Nomainīt valodu'
+    );
+    cy.get('hs-language hs-panel-header span').should(
+      'contain.text',
+      'Nomainīt valodu'
+    );
+  });
+});
