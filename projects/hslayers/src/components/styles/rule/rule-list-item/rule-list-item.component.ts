a674d1162d6247bf24a0e71ee44f7403041a6530
raitis <raitisbe@gmail.com>
2022-12-09 01:12:23 +0200

    feat: Display legend for each rule in styler panel

--- projects/hslayers/src/components/styles/rule/rule-list-item/rule-list-item.component.ts
+++ projects/hslayers/src/components/styles/rule/rule-list-item/rule-list-item.component.ts
@@ -1,4 +1,8 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
+
+import LegendRenderer from 'geostyler-legend/dist/LegendRenderer/LegendRenderer';
+import {Style as GeoStylerStyle} from 'geostyler-style';
 
 import {HsStylerService} from '../../styler.service';
 
@@ -7,9 +11,33 @@ import {HsStylerService} from '../../styler.service';
   templateUrl: 'rule-list-item.component.html',
   styleUrls: ['../../styler.component.scss'],
 })
-export class HsRuleListItemComponent {
+export class HsRuleListItemComponent implements OnInit {
   @Input() rule: any;
   @Input() app = 'default';
   ruleVisible = false;
-  constructor(public hsStylerService: HsStylerService) {}
+  svg: SafeHtml;
+
+  constructor(
+    public hsStylerService: HsStylerService,
+    private sanitizer: DomSanitizer
+  ) {}
+
+  ngOnInit(): void {
+    this.generateLegend();
+  }
+
+  async generateLegend() {
+    const obj: GeoStylerStyle = {name: '', rules: [this.rule]};
+    const legendOpts: any = {
+      styles: [obj],
+      size: [50, 35],
+      hideRect: true,
+    };
+    const legendRenderer = (LegendRenderer as any).default
+      ? new (LegendRenderer as any).default(legendOpts)
+      : new LegendRenderer(legendOpts);
+    const el = document.createElement('div');
+    await legendRenderer.render(el);
+    this.svg = this.sanitizer.bypassSecurityTrustHtml(el.innerHTML);
+  }
 }
