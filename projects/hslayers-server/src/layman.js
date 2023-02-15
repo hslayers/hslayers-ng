hslayers/hslayers-ng

commit 30b42f86ad3b3780b71074e6a8816cdf7b8cf667

fix(server): Update import type of ES module "got"

projects/hslayers-server/src/layman.js
@@ -8,7 +8,7 @@ const passport = require('passport');
 const OAuth2 = require('passport-oauth2').Strategy;
 const OAuth2Refresh = require('passport-oauth2-middleware').Strategy;
 const sqlite = require('better-sqlite3');
-const got = require('got');
+const got = import('got');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const authnUtil = require('./oauth2/util');

projects/hslayers-server/src/oauth2/util.js
@@ -1,4 +1,4 @@
-const got = require('got');
+const got = import('got');
 require('dotenv').config();
 
 

commit 5707666448178d1f1d7d1c3a973a6ededbf74e55

fix(server): Fix dynamic import of "got" lib
Fixes error "got is not a function" introduced by #3797

projects/hslayers-server/src/layman.js
@@ -8,7 +8,6 @@ const passport = require('passport');
 const OAuth2 = require('passport-oauth2').Strategy;
 const OAuth2Refresh = require('passport-oauth2-middleware').Strategy;
 const sqlite = require('better-sqlite3');
-const got = import('got');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const authnUtil = require('./oauth2/util');
@@ -66,7 +65,8 @@ app.use(authnUtil.addIncomingTimestamp);
 OAuth2.prototype.userProfile = (access_token, done) => {
   (async () => {
     try {
-      const response = await got(process.env.LAYMAN_USER_PROFILE_URL, {
+      const got = await import('got');
+      const response = await got.got(process.env.LAYMAN_USER_PROFILE_URL, {
         responseType: 'json',
         headers: {
           AuthorizationIssUrl: process.env.OAUTH2_AUTH_URL,

projects/hslayers-server/src/oauth2/util.js
@@ -1,4 +1,3 @@
-const got = import('got');
 require('dotenv').config();
 
 
@@ -9,7 +8,8 @@ exports.addIncomingTimestamp = (req, res, next) => {
 
 exports.ensureUsername = async (access_token, profile) => {
   if (!profile['username']) {
-    var response = await got.patch(`${process.env.LAYMAN_USER_PROFILE_URL}?adjust_username=true`, {
+    const got = await import('got');
+    var response = await got.got.patch(`${process.env.LAYMAN_USER_PROFILE_URL}?adjust_username=true`, {
       responseType: 'json',
       headers: {
         'AuthorizationIssUrl': process.env.OAUTH2_AUTH_URL,
@@ -28,8 +28,8 @@ exports.deleteUserSession = async (req) => {
     const user = req.session.passport.user;
 
     try {
-
-      let response = await got.delete(process.env.LAYMAN_USER_PROFILE_URL, {
+      const got = await import('got');
+      let response = await got.got.delete(process.env.LAYMAN_USER_PROFILE_URL, {
         headers: getAuthenticationHeaders(user)
       });
 