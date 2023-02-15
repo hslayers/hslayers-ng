ccab35a538dbd35f43dcfd825a3bcd0006d18275
jmacura <macurajan@gmail.com>
2023-02-15 01:56:37 +0100

    build: Improve version update script
    refs #3786

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build-all:
-	npm run build && npm run build-app && npm run build-cesium && npm run build-cesium-app && cd dist hslayers && sudo npm link && cd ../hslayers-app && sudo npm link && cd ../hslayers-cesium-app && sudo npm link && cd ../hslayers-cesium-app && sudo npm link 
+	npm run build && npm run build-app && npm run build-cesium && npm run build-cesium-app && cd dist/hslayers && sudo npm link && cd ../hslayers-app && sudo npm link && cd ../hslayers-cesium-app && sudo npm link && cd ../hslayers-cesium-app && sudo npm link 
 	
 update-ng:
 	npm run ng update @angular/common @angular/compiler @angular/core @angular/forms @angular/localize @angular/cdk @angular/material @angular/platform-browser @angular/platform-browser-dynamic @angular/compiler-cli  @angular/cli ng-packagr
--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,26 +1,53 @@
 #!/bin/sh
+
+# Get the current working directory path
+cwd=$(pwd)
+
+# Collect inputs
+echo "Which applications/libraries?"
+select app in "all" "hslayers-ng" "hslayers-ng-app" "hslayers-cesium" "hslayers-cesium-app"; do
+    break;
+done
+
 echo "What kind of a release?"
 select yn in "patch" "minor" "major" "premajor" "prerelease"; do
     case $yn in
-        patch ) TYPE="patch"; break;;
-        minor ) TYPE="minor"; break;;
-        major ) TYPE="major"; break;;
-        premajor ) TYPE="premajor --preid=next"; break;;
-        prerelease ) TYPE="prerelease --preid=next --tag next"; break;;
+        patch ) type="patch"; break;;
+        minor ) type="minor"; break;;
+        major ) type="major"; break;;
+        premajor ) type="premajor --preid=next"; break;;
+        prerelease ) type="prerelease --preid=next --tag next"; break;;
     esac
 done
-cd projects/hslayers
-npm version $TYPE
-standard-changelog
-cd ../hslayers-app
-npm version $TYPE
-cd ../hslayers-cesium
-npm version $TYPE
-cd ../hslayers-cesium-app
-npm version $TYPE
-cd ../../
+
+# Bump versions
+if [ "$app" = "all" ] || [ "$app" = "hslayers-ng" ]; then
+    cd $cwd
+    cd ../projects/hslayers
+    npm version $type
+    standard-changelog
+fi;
+if [ "$app" = "all" ] || [ "$app" = "hslayers-ng-app" ]; then
+    cd $cwd
+    cd ../projects/hslayers-app
+    npm version $type
+fi;
+if [ "$app" = "all" ] || [ "$app" = "hslayers-cesium" ]; then
+    cd $cwd
+    cd ../projects/hslayers-cesium
+    npm version $type
+fi;
+if [ "$app" = "all" ] || [ "$app" = "hslayers-cesium-app" ]; then
+    cd $cwd
+    cd ../projects/hslayers-cesium-app
+    npm version $type
+fi;
+
+# Build libs/apps
+cd $cwd
+cd ..
 make build-all
 # Second round of building needed because app building 
-# generates  unnecessary ngcc files in hslayers-ng / hslayers-cesium lib directories
+# generates unnecessary ngcc files in hslayers-ng / hslayers-cesium lib directories
 npm run build
 npm run build-cesium
