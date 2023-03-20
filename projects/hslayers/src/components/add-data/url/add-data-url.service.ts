458776a8ccc32629ad58167d95df7f04afc1d3e7
Filip Leitner <leitnerfilip@gmail.com>
2023-03-20 17:41:03 +0100

    fix: Pass app param to calcAllLayersExtent

--- projects/hslayers/src/components/add-data/url/add-data-url.service.ts
+++ projects/hslayers/src/components/add-data/url/add-data-url.service.ts
@@ -131,7 +131,7 @@ export class HsAddDataUrlService {
     }
     try {
       const layerExtents = layers.map((lyr) => [...lyr?.getExtent()]); //Spread need to not create reference
-      return this.calcCombinedExtent(layerExtents);
+      return this.calcCombinedExtent(layerExtents, app);
     } catch (error) {
       this.hsToastService.createToastPopupMessage(
         'ADDLAYERS.capabilitiesParsingProblem',
@@ -149,8 +149,8 @@ export class HsAddDataUrlService {
   /**
    * For given array of layers (service layer definitions) it calculates a cumulative bounding box which encloses all the layers
    */
-  calcCombinedExtent(extents: number[][]): number[] {
-    const currentMapProj = this.hsMapService.getCurrentProj();
+  calcCombinedExtent(extents: number[][], app: string): number[] {
+    const currentMapProj = this.hsMapService.getCurrentProj(app);
     const bounds = transform([180, 90], 'EPSG:4326', currentMapProj);
 
     return extents.reduce((acc, curr) => {
