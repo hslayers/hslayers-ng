065ac33c210689ea40f827c3ba8af5d23e75edff
Filip Leitner <leitnerfilip@gmail.com>
2023-09-11 18:54:30 +0200

    refactor: Specific UI for uppermost filter addition

--- projects/hslayers/src/components/styles/filters/add-filter-button.component.ts
+++ projects/hslayers/src/components/styles/filters/add-filter-button.component.ts
@@ -1,15 +1,31 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, Output} from '@angular/core';
+import {Rule} from 'geostyler-style';
+
 import {filterType} from './filters.component';
 
 @Component({
   selector: 'hs-add-filter-button',
   templateUrl: './add-filter-button.component.html',
 })
-export class HsAddFilterButtonComponent {
+export class HsAddFilterButtonComponent implements OnChanges {
   @Output() clicks = new EventEmitter();
+  @Input() rule?: Rule;
 
+  /**
+   * Update UI on filter removal
+   */
+  ngOnChanges() {
+    this.setActiveTab(this.activeTab);
+  }
+
+  activeTab: filterType;
   readonly filterOptions: filterType[] = ['AND', 'OR', 'NOT', 'COMPARE'];
   emitClick(type: filterType): void {
     this.clicks.emit({type});
+    this.setActiveTab(type);
+  }
+
+  setActiveTab(type: filterType) {
+    this.activeTab = this.rule?.filter ? type : undefined;
   }
 }
