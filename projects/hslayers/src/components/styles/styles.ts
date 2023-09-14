4d09afc9c2dfc53c47959ab89640e8886557cf6d
Filip Leitner <leitnerfilip@gmail.com>
2023-09-14 12:16:23 +0200

    fix: Use fallback when SLD style parsing fails

--- projects/hslayers/src/components/styles/styles.ts
+++ projects/hslayers/src/components/styles/styles.ts
@@ -5,7 +5,7 @@ export const defaultStyle = `<?xml version="1.0" encoding="UTF-8" standalone="ye
   <NamedLayer>
     <Name/>
     <UserStyle>
-      <Name/>
+      <sld:Name>HSLayers Default Style</sld:Name>
       <Title/>
       <FeatureTypeStyle>
         <Rule>
