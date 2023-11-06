b24816c30087e0b4fb3c6698ccc3226fb7eec3d7
Filip Leitner <leitnerfilip@gmail.com>
2023-11-06 09:51:34 +0100

    feat: Register EPSG:3035 ETRS89-extended / LAEA Europe

--- projects/hslayers/src/components/map/map.service.ts
+++ projects/hslayers/src/components/map/map.service.ts
@@ -497,6 +497,15 @@ export class HsMapService {
     this.repopulateLayers(this.visibleLayersInUrl);
 
     proj4.defs(
+      'EPSG:3035',
+      '+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs',
+    );
+    proj4.defs(
+      'http://www.opengis.net/gml/srs/epsg.xml#3035',
+      proj4.defs('EPSG:3035'),
+    );
+
+    proj4.defs(
       'EPSG:5514',
       '+proj=krovak +lat_0=49.5 +lon_0=24.83333333333333 +alpha=30.28813972222222 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +towgs84=542.5,89.2,456.9,5.517,2.275,5.516,6.96 +units=m +no_defs',
     );
