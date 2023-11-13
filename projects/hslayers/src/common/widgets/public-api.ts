c6337367dd21e0737d886ff804259e040a760f95
jmacura <macurajan@gmail.com>
2023-11-14 00:19:04 +0100

    fix(widgets): Export EPSG Pipe

--- projects/hslayers/src/common/widgets/public-api.ts
+++ projects/hslayers/src/common/widgets/public-api.ts
@@ -1,4 +1,5 @@
-export * from './trackByProperty.pipe';
+export * from './epsg.pipe';
 export * from './filter.pipe';
 export * from './recursive-dd.component';
+export * from './trackByProperty.pipe';
 export * from './ui-extensions.module';
