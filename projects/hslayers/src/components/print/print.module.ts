be0dd773de0a464f39c47631422dd9f8d968e82c
Filip Leitner <leitnerfilip@gmail.com>
2023-12-11 14:42:05 +0100

    fix:Decoupling test app
    build: Improve treeshakability of print component

---
+++ projects/hslayers/src/components/print/print.module.ts
@@ -0,0 +1,35 @@
+import {AsyncPipe, NgClass, NgIf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {NgModule} from '@angular/core';
+
+import {HsPanelHeaderComponent} from '../layout/panels/panel-header/panel-header.component';
+import {HsPrintComponent} from './print.component';
+import {HsPrintImprintStylerComponent} from './imprint-styler/imprint-styler.component';
+import {HsPrintLegendStylerComponent} from './legend-styler/legend-styler.component';
+import {HsPrintScaleStylerComponent} from './scale-styler/scale-styler.component';
+import {HsPrintTextStylerComponent} from './text-styler/text-styler.component';
+import {TranslateCustomPipe} from '../language/translate-custom.pipe';
+
+@NgModule({
+  imports: [
+    HsPrintImprintStylerComponent,
+    HsPrintLegendStylerComponent,
+    HsPrintTextStylerComponent,
+    HsPrintScaleStylerComponent,
+    NgIf,
+    NgClass,
+    AsyncPipe,
+    FormsModule,
+    TranslateCustomPipe,
+    HsPanelHeaderComponent,
+  ],
+  declarations: [HsPrintComponent],
+  exports: [
+    HsPrintComponent,
+    HsPrintImprintStylerComponent,
+    HsPrintLegendStylerComponent,
+    HsPrintTextStylerComponent,
+    HsPrintScaleStylerComponent,
+  ],
+})
+export class PrintModule {}
