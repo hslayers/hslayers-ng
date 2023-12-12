5b494c7f00198756e9aad7da0e301d88ae25c621
Filip Leitner <leitnerfilip@gmail.com>
2023-12-12 10:28:04 +0100

    build(decouplin-app): Optimize chunk number

--- projects/decoupling-test-app/custom-webpack.config.js
+++ projects/decoupling-test-app/custom-webpack.config.js
@@ -3,9 +3,15 @@ var webpack = require('webpack');
 module.exports = {
   devtool: false,
   plugins: [
-      new webpack.SourceMapDevToolPlugin( {
-        filename: '[file].map',
-        exclude: ['vendor.js'],
-      } ) 
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[file].map',
+      exclude: ['vendor.js'],
+    }),
+    // new webpack.optimize.LimitChunkCountPlugin({
+    //   maxChunks: 25,
+    // }),
+    new webpack.optimize.MinChunkSizePlugin({
+      minChunkSize: 50000, // Minimum number of characters
+    })
   ],
 };
