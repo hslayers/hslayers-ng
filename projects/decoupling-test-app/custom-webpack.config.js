b1e6e55c5177c3d2201e1cf89b15fe0c7c0600ac
Filip Leitner <leitnerfilip@gmail.com>
2023-12-13 10:02:25 +0100

    perf(decoupling): Split panel chunks into smaller parts
    In contrast with chunk create by less verbose approach + max chunk number plugin content of these chunks are not mixed

--- projects/decoupling-test-app/custom-webpack.config.js
+++ projects/decoupling-test-app/custom-webpack.config.js
@@ -2,6 +2,11 @@ var webpack = require('webpack');
 
 module.exports = {
   devtool: false,
+  optimization: {
+    splitChunks: {
+      maxSize: 50000
+    }
+  },
   plugins: [
     new webpack.SourceMapDevToolPlugin({
       filename: '[file].map',
