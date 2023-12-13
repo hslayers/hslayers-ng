b6e8c5ec9a33a03b01d40194504f1b1c09eb6291
Filip Leitner <leitnerfilip@gmail.com>
2023-12-13 11:35:27 +0100

    refactor(overlay): Expose GUI overlay creation as a function

--- projects/hslayers/src/components/layout/overlay-panel-container.service.ts
+++ projects/hslayers/src/components/layout/overlay-panel-container.service.ts
@@ -1,11 +1,105 @@
-import {HsPanelContainerService} from './panels/panel-container.service';
 import {Injectable} from '@angular/core';
+
+import {HsConfig} from '../../config.service';
+import {HsPanelContainerService} from './panels/panel-container.service';
+import {HsToolbarPanelContainerService} from '../toolbar/toolbar-panel-container.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HsOverlayPanelContainerService extends HsPanelContainerService {
-  constructor() {
+  constructor(
+    private hsConfig: HsConfig,
+    private hsToolbarPanelContainerService: HsToolbarPanelContainerService,
+  ) {
     super();
+  }
+
+  /**
+   * Create GUI component based on name string.
+   */
+  private async _createGuiComponent(
+    name: string,
+    service: HsPanelContainerService = this,
+  ) {
+    const cName = `Hs${this.capitalizeFirstLetter(name)}Component`;
+    let i;
+    switch (name) {
+      case 'toolbar':
+        i = await import('../toolbar/toolbar.component');
+        break;
+      case 'measureToolbar':
+        i = await import('../measure/measure-toolbar.component');
+        break;
+      case 'searchToolbar':
+        i = await import('../search/search-toolbar.component');
+        break;
+      case 'drawToolbar':
+        i = await import('../draw/draw-toolbar/draw-toolbar.component');
+        break;
+      case 'layerManagerGallery':
+        // eslint-disable-next-line prettier/prettier
+        i = await import('../layer-manager/gallery/layer-manager-gallery.component');
+        break;
+      case 'info':
+        i = await import('../info/info.component');
+        break;
+      case 'geolocation':
+        i = await import('../geolocation/geolocation.component');
+        break;
+      default:
+        console.warn(`Trying to create unidentified GUI component ${name}`);
+        break;
+    }
+    service.create(i[cName], {});
+  }
+
+  /**
+   * Wrapper function which creates GUI overlay components based on value
+   * provided in config.componentsEnabled
+   */
+  createGuiOverlay() {
+    if (this.hsConfig.componentsEnabled.guiOverlay) {
+      /**
+       * TOOLBAR
+       */
+      if (this.hsConfig.componentsEnabled.toolbar) {
+        this._createGuiComponent('toolbar');
+        const enabledToolbarParts = Object.entries(
+          this.hsConfig.componentsEnabled,
+        ).reduce(
+          (acc, [component, isEnabled]) =>
+            isEnabled && component.includes('Toolbar')
+              ? [...acc, component]
+              : acc,
+          [],
+        );
+        for (const toolbar of enabledToolbarParts) {
+          this._createGuiComponent(
+            toolbar,
+            this.hsToolbarPanelContainerService,
+          );
+        }
+      }
+      /**
+       * GUI OVERLAY
+       */
+      if (this.hsConfig.componentsEnabled.basemapGallery) {
+        this._createGuiComponent('layerManagerGallery');
+      }
+      if (this.hsConfig.componentsEnabled.info) {
+        this._createGuiComponent('info');
+      }
+      if (this.hsConfig.componentsEnabled.geolocationButton) {
+        this._createGuiComponent('geolocation');
+      }
+    }
+  }
+
+  /**
+   * NOTE: Duplicity but compiler keeps complaining about circular dependency
+   */
+  private capitalizeFirstLetter(target: string): string {
+    return target.charAt(0).toUpperCase() + target.slice(1);
   }
 }
--- projects/hslayers/src/hslayers.component.ts
+++ projects/hslayers/src/hslayers.component.ts
@@ -1,21 +1,13 @@
 import {Component, Input, OnInit, Type, ViewChild} from '@angular/core';
 
 import {HsConfig, HsConfigObject} from './config.service';
-import {HsDrawToolbarComponent} from './components/draw/draw-toolbar/draw-toolbar.component';
 // import {HsFeatureInfoComponent} from './components/query/query-popup-feature/feature-widgets/feature-info.component';
 import {HsExternalService} from './components/external/external.service';
-import {HsGeolocationComponent} from './components/geolocation/geolocation.component';
-import {HsInfoComponent} from './components/info/info.component';
-import {HsLayerManagerGalleryComponent} from './components/layer-manager/gallery/layer-manager-gallery.component';
 import {HsLayoutComponent} from './components/layout/layout.component';
 import {HsMapSwipeService} from './components/map-swipe/map-swipe.service';
-import {HsMeasureToolbarComponent} from './components/measure/measure-toolbar.component';
 import {HsQueryPopupComponent} from './components/query/query-popup/query-popup.component';
 import {HsQueryPopupService} from './components/query/query-popup.service';
 import {HsQueryPopupWidgetContainerService} from './components/query/query-popup-widget-container.service';
-import {HsSearchToolbarComponent} from './components/search/search-toolbar.component';
-import {HsToolbarComponent} from './components/toolbar/toolbar.component';
-import {HsToolbarPanelContainerService} from './components/toolbar/toolbar-panel-container.service';
 
 import {HsOverlayPanelContainerService} from './components/layout/overlay-panel-container.service';
 import {HsPanelConstructorService} from './components/layout/panels/panel-constructor.service';
@@ -34,7 +26,6 @@ export class HslayersComponent implements OnInit {
   constructor(
     public hsConfig: HsConfig,
     private HsOverlayPanelContainerService: HsOverlayPanelContainerService,
-    private hsToolbarPanelContainerService: HsToolbarPanelContainerService,
     private hsQueryPopupService: HsQueryPopupService,
     private HsMapSwipeService: HsMapSwipeService, //Leave this, need to inject somewhere
     private hsQueryPopupWidgetContainerService: HsQueryPopupWidgetContainerService, //Leave this, need to inject somewhere
@@ -55,17 +46,11 @@ export class HslayersComponent implements OnInit {
      */
     this.HsPanelConstructorService.createActivePanels();
 
-    this.hsToolbarPanelContainerService.create(HsSearchToolbarComponent, {});
-    this.hsToolbarPanelContainerService.create(HsDrawToolbarComponent, {});
-    this.hsToolbarPanelContainerService.create(HsMeasureToolbarComponent, {});
+    /**
+     * Create GUI overlay
+     */
+    this.HsOverlayPanelContainerService.createGuiOverlay();
 
-    this.HsOverlayPanelContainerService.create(HsGeolocationComponent, {});
-    this.HsOverlayPanelContainerService.create(HsInfoComponent, {});
-    this.HsOverlayPanelContainerService.create(
-      HsLayerManagerGalleryComponent,
-      {},
-    );
-    this.HsOverlayPanelContainerService.create(HsToolbarComponent, {});
     this.HsOverlayPanelContainerService.create(HsQueryPopupComponent, {
       service: this.hsQueryPopupService,
     });
