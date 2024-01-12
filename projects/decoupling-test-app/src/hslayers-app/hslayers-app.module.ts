cd23b108694202c1c9d4de3a8552f97f167a1299
Filip Leitner <leitnerfilip@gmail.com>
2024-01-12 13:42:17 +0100

    refactor: Decoupling test app
    Use hslayers component and remove comments about popups

--- projects/decoupling-test-app/src/hslayers-app/hslayers-app.module.ts
+++ projects/decoupling-test-app/src/hslayers-app/hslayers-app.module.ts
@@ -9,23 +9,14 @@ import {NgModule} from '@angular/core';
 //import {HsMeasureModule} from 'hslayers-ng/components/measure/public-api';
 //import {HsDrawModule} from 'hslayers-ng/components/draw/public-api';
 import {FormsModule} from '@angular/forms';
-import {HsLayoutModule} from 'hslayers-ng/components/layout';
 import {HslayersAppComponent} from './hslayers-app.component';
+import {HslayersModule} from 'hslayers-ng';
 import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [HslayersAppComponent],
-  imports: [HttpClientModule, BrowserModule, FormsModule, HsLayoutModule],
+  imports: [HttpClientModule, BrowserModule, FormsModule, HslayersModule],
   providers: [],
   bootstrap: [HslayersAppComponent],
 })
 export class AppModule {}
-
-/***
- *
- * V appke import toho čo je potrbea z knižnice  - nejaky wrapper aby to bolo čo najjednoduchšie??
- * To čo je hslayers-component by teda malo byt až v appke nie v knižnici - vynimka simple apps?
- * Nejaka init podoba kde bude na rýyhclo základ. tj. hslayers componenta ale oklieštená.
- * Mapa + Layermanager?
- *
- */
