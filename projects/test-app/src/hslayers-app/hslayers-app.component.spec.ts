ddff89fcf16dfcce846198282723cadddb6a21c7
Filip Leitner <leitnerfilip@gmail.com>
2024-01-15 08:49:32 +0100

    test: Fix hslayers component test

--- projects/test-app/src/hslayers-app/hslayers-app.component.spec.ts
+++ projects/test-app/src/hslayers-app/hslayers-app.component.spec.ts
@@ -1,7 +1,7 @@
 import {TestBed, waitForAsync} from '@angular/core/testing';
 
-import {HsConfig} from '../../../hslayers/src/config.service';
-import {HsConfigMock} from '../../../hslayers/src/config.service.mock';
+import {HsConfig} from 'hslayers-ng/config';
+import {HsConfigMock} from '../../../hslayers/src/testing/config.service.mock';
 import {HslayersAppComponent} from './hslayers-app.component';
 
 describe('AppComponent', () => {
