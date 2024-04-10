1bad516db0dedea0f1225b9b0de5b1a4882fbb68
Filip Leitner <leitnerfilip@gmail.com>
2024-04-10 11:28:19 +0200

    refactor(types): Do not make unnecessary string case changes for LaymanUser
    Simplifies getAllUsers user array handling

--- projects/hslayers/common/layman/types/layman-user.type.ts
+++ projects/hslayers/common/layman/types/layman-user.type.ts
@@ -1,10 +1,10 @@
 /**
  * Change access rights for everyone
  * @param username - Username of the user.
- * @param screenName - Screen name of the user.
- * @param givenName - Given name of the user.
- * @param familyName - Family name of the user
- * @param middleName - Middle name of the user
+ * @param screen_name - Screen name of the user.
+ * @param given_name - Given name of the user.
+ * @param family_name - Family name of the user
+ * @param middle_name - Middle name of the user
  * @param name - Whole name of the user (given_name + middle_name + family_name).
  * @param hslDisplayName - (Optional) Carefully selected word from other user properties, that is used in Hslayers templates
  * @param read - (Optional) Layman read rights status
@@ -12,10 +12,10 @@
  */
 export type LaymanUser = {
   username: string;
-  screenName: string;
-  givenName: string;
-  familyName: string;
-  middleName: string;
+  screen_name: string;
+  given_name: string;
+  family_name: string;
+  middle_name: string;
   name: string;
   hslDisplayName?: string;
   read?: boolean;
