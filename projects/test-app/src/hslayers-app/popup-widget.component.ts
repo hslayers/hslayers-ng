ec5120ceefb28853ad7142f4007476067f1ef906
jmacura <macurajan@gmail.com>
2024-06-10 15:33:35 +0200

    fix(test-app): Unify file names

--- projects/test-app/src/hslayers-app/popup-widget.component.ts
+++ projects/test-app/src/hslayers-app/popup-widget.component.ts
@@ -5,7 +5,7 @@ import {HsPanelComponent} from 'hslayers-ng/common/panels';
 import {HsQueryPopupWidgetBaseComponent} from 'hslayers-ng/common/query-popup';
 @Component({
   selector: 'popup-widget',
-  templateUrl: './popup-widget.html',
+  templateUrl: './popup-widget.component.html',
 })
 export class PopupWidgetComponent
   extends HsQueryPopupWidgetBaseComponent
