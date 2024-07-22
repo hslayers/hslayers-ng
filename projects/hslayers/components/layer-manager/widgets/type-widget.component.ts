6a588300346d538cd2cfdea030bc044d536b99b7
Filip Leitner <leitnerfilip@gmail.com>
2024-07-22 13:17:49 +0200

    feat(layermanager): Add copy button to URL

--- projects/hslayers/components/layer-manager/widgets/type-widget.component.ts
+++ projects/hslayers/components/layer-manager/widgets/type-widget.component.ts
@@ -1,19 +1,39 @@
-import {Component} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 
-import {HsLanguageService} from 'hslayers-ng/services/language';
 import {HsLayerEditorWidgetBaseComponent} from './layer-editor-widget-base.component';
 import {HsLayerSelectorService} from 'hslayers-ng/services/layer-manager';
+import {HsToastService} from 'hslayers-ng/common/toast';
 
 @Component({
   selector: 'hs-type-widget',
   templateUrl: './type-widget.component.html',
 })
 export class HsTypeWidgetComponent extends HsLayerEditorWidgetBaseComponent {
-  constructor(
-    public HsLanguageService: HsLanguageService,
-    hsLayerSelectorService: HsLayerSelectorService,
-  ) {
+  hsToastService = inject(HsToastService);
+
+  showCheck = false;
+
+  constructor(hsLayerSelectorService: HsLayerSelectorService) {
     super(hsLayerSelectorService);
   }
   name = 'type-widget';
+
+  copyToClipBoard() {
+    if (!navigator.clipboard) {
+      this.hsToastService.createToastPopupMessage(
+        'COMMON.copyToClipboard',
+        'COMMON.copyToClipboardFailure',
+        {
+          toastStyleClasses: 'bg-danger text-white',
+        },
+      );
+      return;
+    }
+    this.showCheck = true;
+    const source = this.hsLayerSelectorService.currentLayer.source;
+    navigator.clipboard.writeText(source);
+    setTimeout(() => {
+      this.showCheck = false;
+    }, 500);
+  }
 }
