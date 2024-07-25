761782ccaf42902e969c3ba21db9f09e1313a379
Filip Leitner <leitnerfilip@gmail.com>
2024-07-25 11:44:41 +0200

    test(layermanager): E2E test for layer-type-switcher widget

---
+++ cypress/tests/layer-editor.cy.ts
@@ -0,0 +1,61 @@
+describe('Hslayers application', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.wait(2000);
+    //Open layer manager
+    cy.get('.hs-sidebar-item[data-cy="layerManager"]').click();
+  });
+
+  it('Should not be possible to change base/thematic type for vector layer', () => {
+    cy.get('[data-test="Points"] span.icon-settingsthree-gears ').click();
+    cy.get('hs-layer-editor form').within(() => {
+      cy.get('hs-layer-type-switcher-widget').should('exist');
+      cy.get('hs-layer-type-switcher-widget div').should('not.exist');
+    });
+  });
+
+  it('Should be possible to find base/thematic type toggle for WMS layer', () => {
+    cy.get('[data-test="EVI"] span.icon-settingsthree-gears ').click();
+    cy.get('hs-layer-editor form').within(() => {
+      cy.get('hs-layer-type-switcher-widget div').should('exist');
+    });
+  });
+
+  it.only('Should be possible to change type of WMS layer back and forth', () => {
+    cy.get('[data-test="EVI"] span.icon-settingsthree-gears ').click();
+
+    //Change to BASE
+    cy.get('hs-layer-editor form hs-layer-type-switcher-widget div')
+      .find('button')
+      .contains('Basemap')
+      .should('exist')
+      .click();
+
+    cy.get('.modal-footer').find('button').contains('Yes').click();
+
+    cy.get('.hs-lm-mapcontentlist .hs-lm-item-title').should(
+      'not.contain',
+      'EVI',
+    );
+
+    cy.get('.galleryButton').click();
+    cy.get('.galleryDropdown .dropdown-item').should('contain', 'EVI');
+
+    //Change to thematic
+    cy.get('.galleryDropdown .dropdown-item')
+      .contains('EVI')
+      .parent()
+      .within(() => {
+        cy.get('div').click();
+        cy.get('label').contains('More').click();
+        cy.get('hs-layer-editor form hs-layer-type-switcher-widget div')
+          .find('button')
+          .contains('Thematic map')
+          .should('exist')
+          .click();
+      });
+
+    cy.get('.modal-footer').find('button').contains('Yes').click();
+    cy.get('.hs-lm-mapcontentlist .hs-lm-item-title').should('contain', 'EVI');
+  });
+});
