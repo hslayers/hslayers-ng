f5a8ea05c9f4944489f375a9648c56a4185de6ee
Filip Leitner <leitnerfilip@gmail.com>
2024-08-09 09:42:21 +0200

    refactor(compositions): Dont use mdAbstract property

--- projects/hslayers/test/data/compositions.ts
+++ projects/hslayers/test/data/compositions.ts
@@ -8,7 +8,7 @@ export const compositionsJson = {
       'serviceType': 'WMC',
       'id': '4e4a629c-d09e-476e-b750-d3e847ac47d9',
       'title': 'Swedish municipalities',
-      'mdAbstract': 'Map displays municipalities in Sweden',
+      'abstract': 'Map displays municipalities in Sweden',
       'link':
         'http://www.whatstheplan.eu/wwwlibs/statusmanager2/index.php?request=load&id=4e4a629c-d09e-476e-b750-d3e847ac47d9',
       'bbox': '11 55 24 68',
@@ -19,7 +19,7 @@ export const compositionsJson = {
       'serviceType': 'WMC',
       'id': '71edfa62-5079-47c9-b227-3c9d6cf8a240',
       'title': 'Livestock and its structure in NUTS regions in year 2003',
-      'mdAbstract':
+      'abstract':
         'The map composition consists of four main componenets. Base layer which is just boarders and names of NUTS regions. Structural diagram map, that shows livestock structure in NUTS regions across Europe; diagram map, that shows the amount of livestock units (see what is livestock unit: http://epp.eurostat.ec.europa.eu/statistics_explained/index.php/Glossary:LSU ) in certain region and finally info-',
       'link':
         'http://www.whatstheplan.eu/wwwlibs/statusmanager2/index.php?request=load&id=71edfa62-5079-47c9-b227-3c9d6cf8a240',
@@ -31,7 +31,7 @@ export const compositionsJson = {
       'serviceType': '',
       'id': '5256456a-3fe8-488b-92d2-0273585671e2',
       'title': 'Complex composition of Europe',
-      'mdAbstract':
+      'abstract':
         'This is a complex composition containing data from many different tables (describing different aspects of life) from Eurostat. The given topics are visualized using different techniques. The data is for the administrative levels of NUTS0-3.',
       'link':
         'http://dev.bnhelp.cz/statusmanager/index.php?request=load&permalink=eu_complex',
@@ -43,7 +43,7 @@ export const compositionsJson = {
       'serviceType': 'WMC',
       'id': '952267f7-048e-4676-bcf3-fbfc58c6d4d6',
       'title': 'Spatial Plans',
-      'mdAbstract':
+      'abstract':
         'Composition shows available spatial plans hormonized according to INSPIRE-HILUCS specifications.',
       'link':
         'http://www.whatstheplan.eu/wwwlibs/statusmanager2/index.php?request=load&id=952267f7-048e-4676-bcf3-fbfc58c6d4d6',
@@ -55,7 +55,7 @@ export const compositionsJson = {
       'serviceType': 'WMC',
       'id': '7ea2beb4-5ecb-47d3-ac0b-ecce712439b3',
       'title': 'Coarse particle pollution in Europe in year 2011',
-      'mdAbstract':
+      'abstract':
         'The composition shows measurements of PM10 (coarse particles) air pollution from different stations around the Europe. The unit of measurements is \u00b5g/m3. The data is from EEA database and is for year 2011.',
       'link':
         'http://www.whatstheplan.eu/wwwlibs/statusmanager2/index.php?request=load&id=7ea2beb4-5ecb-47d3-ac0b-ecce712439b3',
@@ -67,7 +67,7 @@ export const compositionsJson = {
       'serviceType': 'WMC',
       'id': 'fe3d4424-0ec3-440d-82dd-9d2915e1b592',
       'title': 'Cultivated land and its structure in NUTS regions in year 2007',
-      'mdAbstract':
+      'abstract':
         'The map composition consists of four main componenets. Base layer which is just boarders and names of NUTS regions. Structural diagram map, that shows usage of cultivated land in NUTS regions across Europe; diagram map, that shows the amount of cultivated land in certain region and finally info-layers, that are clickable and display exact numbers(amount of cultivated land and structure of usage. ',
       'link':
         'http://www.whatstheplan.eu/wwwlibs/statusmanager2/index.php?request=load&id=fe3d4424-0ec3-440d-82dd-9d2915e1b592',
@@ -79,7 +79,7 @@ export const compositionsJson = {
       'serviceType': 'WMC',
       'id': '91f3dd09-6c32-415b-a189-4a092d752242',
       'title': 'Ecoregions by EEA',
-      'mdAbstract':
+      'abstract':
         'Map shows ecoregions in Europe. The data are taken from database of European Environmental Agency.',
       'link':
         'http://www.whatstheplan.eu/wwwlibs/statusmanager2/index.php?request=load&id=91f3dd09-6c32-415b-a189-4a092d752242',
@@ -91,7 +91,7 @@ export const compositionsJson = {
       'serviceType': 'WMC',
       'id': '11bee4d9-3a2e-4215-96d5-a20286ab9913',
       'title': 'Population distribution in NUTS regions',
-      'mdAbstract':
+      'abstract':
         'The given composition shows distribution of population by NUTS regions. The viewer should get an idea of how many people live in each of NUTS regions and also about how dense they are populated. The map itself consists of three main components :base layer (shows boarders and codes of NUTS regions), choropleth map (that shows density of population) and spheres (volume of sphere is proportional to ',
       'link':
         'http://www.whatstheplan.eu/wwwlibs/statusmanager2/index.php?request=load&id=11bee4d9-3a2e-4215-96d5-a20286ab9913',
@@ -103,7 +103,7 @@ export const compositionsJson = {
       'serviceType': 'WMC',
       'id': '44a62e48-6f86-4d57-9de9-3018f6fee4a5',
       'title': 'Cultivated land and its structure in NUTS regions in year 2005',
-      'mdAbstract':
+      'abstract':
         'The map composition consists of four main componenets. Base layer which is just boarders and names of NUTS regions. Structural diagram map, that shows usage of cultivated land in NUTS regions across Europe; diagram map, that shows the amount of cultivated land in certain region and finally info-layers, that are clickable and display exact numbers(amount of cultivated land and structure of usage. ',
       'link':
         'http://www.whatstheplan.eu/wwwlibs/statusmanager2/index.php?request=load&id=44a62e48-6f86-4d57-9de9-3018f6fee4a5',
@@ -115,7 +115,7 @@ export const compositionsJson = {
       'serviceType': 'WMC',
       'id': '011339ee-8232-40ca-915d-8ff88e5b7c3f',
       'title': 'Livestock and its structure in NUTS regions in year 2005',
-      'mdAbstract':
+      'abstract':
         'The map composition consists of four main componenets. Base layer which is just boarders and names of NUTS regions. Structural diagram map, that shows livestock structure in NUTS regions across Europe; diagram map, that shows the amount of livestock units (see what is livestock unit: http://epp.eurostat.ec.europa.eu/statistics_explained/index.php/Glossary:LSU ) in certain region and finally info-',
       'link':
         'http://www.whatstheplan.eu/wwwlibs/statusmanager2/index.php?request=load&id=011339ee-8232-40ca-915d-8ff88e5b7c3f',
@@ -127,7 +127,7 @@ export const compositionsJson = {
       'serviceType': 'WMC',
       'id': '10b28329-39bd-4c2a-a672-08b269b637f4',
       'title': 'Livestock and its structure in NUTS regions in year 2007',
-      'mdAbstract':
+      'abstract':
         'The map composition consists of four main componenets. Base layer which is just boarders and names of NUTS regions. Structural diagram map, that shows livestock structure in NUTS regions across Europe; diagram map, that shows the amount of livestock units (see what is livestock unit: http://epp.eurostat.ec.europa.eu/statistics_explained/index.php/Glossary:LSU ) in certain region and finally info-',
       'link':
         'http://www.whatstheplan.eu/wwwlibs/statusmanager2/index.php?request=load&id=10b28329-39bd-4c2a-a672-08b269b637f4',
@@ -139,7 +139,7 @@ export const compositionsJson = {
       'serviceType': 'WMC',
       'id': '1e0c7685-fefa-4f13-9d92-717dca6bd778',
       'title': 'Common Database on Designated Areas (CDDA)',
-      'mdAbstract':
+      'abstract':
         'Map shows nationally designated areas (CDDA) in Europe in year 2011. The data are taken from database of European Environmental Agency.',
       'link':
         'http://www.whatstheplan.eu/wwwlibs/statusmanager2/index.php?request=load&id=1e0c7685-fefa-4f13-9d92-717dca6bd778',
@@ -152,7 +152,7 @@ export const compositionsJson = {
       'id': '72f23f99-0697-47ab-b028-d541003f97ee',
       'title':
         'Occupation of people by ISCO (for year 2001) codes and rate of unemployment (for year 2011) in NUTS 1,3 regions',
-      'mdAbstract':
+      'abstract':
         'The following map compositions consists of three main components choropleth map that shows percentage of unemployed people in given region, then also basic map with administrative boarders of NUTS regions and its codes and finally diagram map representing population occupation structure by ISCO classification. The size of each diagram is proportional to number of employed people. The data is take',
       'link':
         'http://www.whatstheplan.eu/wwwlibs/statusmanager2/index.php?request=load&id=72f23f99-0697-47ab-b028-d541003f97ee',
@@ -164,7 +164,7 @@ export const compositionsJson = {
       'serviceType': 'WMC',
       'id': '39499777-997e-48ec-97c3-3fa14947f4f0',
       'title': 'GDP per capita in NUTS regions in year 2010',
-      'mdAbstract':
+      'abstract':
         'The map composition consists of three main componenets. Base layer which is just boarders and names of NUTS regions. Choropleth map, that shows if the GDP per capita in given NUTS region is higher or lower than EU average and by how many percents. For regions that have higher GDP than the average shades of red are used, and for regions that have less GDP than the average shades of blue are used. ',
       'link':
         'http://www.whatstheplan.eu/wwwlibs/statusmanager2/index.php?request=load&id=39499777-997e-48ec-97c3-3fa14947f4f0',
@@ -176,7 +176,7 @@ export const compositionsJson = {
       'serviceType': 'WMC',
       'id': '14acaefb-7305-493c-a41a-344ae1821998',
       'title': 'Cultivated land and its structure in NUTS regions in year 2003',
-      'mdAbstract':
+      'abstract':
         'The map composition consists of four main componenets. Base layer which is just boarders and names of NUTS regions. Structural diagram map, that shows usage of cultivated land in NUTS regions across Europe; diagram map, that shows the amount of cultivated land in certain region and finally info-layers, that are clickable and display exact numbers(amount of cultivated land and structure of usage. ',
       'link':
         'http://www.whatstheplan.eu/wwwlibs/statusmanager2/index.php?request=load&id=14acaefb-7305-493c-a41a-344ae1821998',
