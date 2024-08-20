90d30e6cce933b5b9fb85f6927a07939ee922c45
Filip Leitner <leitnerfilip@gmail.com>
2024-08-20 09:54:55 +0200

    refactor(utils): Extent buffering - check against 4087 bounds

--- projects/hslayers/test/utils/layer-utils.service.spec.ts
+++ projects/hslayers/test/utils/layer-utils.service.spec.ts
@@ -11,7 +11,7 @@ describe('LayerUtilsService', () => {
 
   proj4.defs(
     'EPSG:4087',
-    '+proj=cea +lon_0=0 +lat_ts=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs',
+    '+proj=eqc +lat_ts=0 +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +type=crs',
   );
   proj4.defs(
     'http://www.opengis.net/gml/srs/epsg.xml#4087',
@@ -40,87 +40,85 @@ describe('LayerUtilsService', () => {
     expect(service).toBeTruthy();
   });
 
-  describe('bufferExtent', () => {
-    it('should buffer extent correctly in meters', () => {
-      const extent = [1000, 2000, 3000, 4000];
-      const bufferedExtent = service.bufferExtent(
-        extent,
-        getProjection('EPSG:3857'),
-      );
+  it('should buffer extent correctly in meters', () => {
+    const extent = [1000, 2000, 3000, 4000];
+    const bufferedExtent = service.bufferExtent(
+      extent,
+      getProjection('EPSG:3857'),
+    );
 
-      expect(bufferedExtent[0]).toBeLessThan(1000);
-      expect(bufferedExtent[1]).toBeLessThan(2000);
-      expect(bufferedExtent[2]).toBeGreaterThan(3000);
-      expect(bufferedExtent[3]).toBeGreaterThan(4000);
-    });
+    expect(bufferedExtent[0]).toBeLessThan(1000);
+    expect(bufferedExtent[1]).toBeLessThan(2000);
+    expect(bufferedExtent[2]).toBeGreaterThan(3000);
+    expect(bufferedExtent[3]).toBeGreaterThan(4000);
+  });
 
-    it('should buffer extent correctly with degree units (WGS84)', () => {
-      const extent = [-75, 40, -70, 45];
-      const bufferedExtent = service.bufferExtent(
-        extent,
-        getProjection('EPSG:4326'),
-      );
+  it('should buffer extent correctly with degree units (WGS84)', () => {
+    const extent = [-75, 40, -70, 45];
+    const bufferedExtent = service.bufferExtent(
+      extent,
+      getProjection('EPSG:4326'),
+    );
 
-      expect(bufferedExtent[0]).toBeLessThan(-75);
-      expect(bufferedExtent[1]).toBeLessThan(40);
-      expect(bufferedExtent[2]).toBeGreaterThan(-70);
-      expect(bufferedExtent[3]).toBeGreaterThan(45);
-    });
+    expect(bufferedExtent[0]).toBeLessThan(-75);
+    expect(bufferedExtent[1]).toBeLessThan(40);
+    expect(bufferedExtent[2]).toBeGreaterThan(-70);
+    expect(bufferedExtent[3]).toBeGreaterThan(45);
+  });
 
-    it('should not extend extent beyond WGS84 bounds', () => {
-      const extremeExtent = [-180, -90, 180, 90]; // Edge of WGS84 bounds
-      const bufferedExtent = service.bufferExtent(
-        extremeExtent,
-        getProjection('EPSG:4326'),
-      );
+  it('should not buffer extent beyond WGS84 bounds', () => {
+    const extremeExtent = [-190, -85, 180, 86.7]; // Edge of WGS84 bounds
+    const bufferedExtent = service.bufferExtent(
+      extremeExtent,
+      getProjection('EPSG:4326'),
+    );
 
-      expect(bufferedExtent[0]).toBeGreaterThanOrEqual(-180);
-      expect(bufferedExtent[1]).toBeGreaterThanOrEqual(-90);
-      expect(bufferedExtent[2]).toBeLessThanOrEqual(180);
-      expect(bufferedExtent[3]).toBeLessThanOrEqual(90);
-    });
+    expect(bufferedExtent[0]).toBeGreaterThanOrEqual(-180);
+    expect(bufferedExtent[1]).toBeGreaterThanOrEqual(-90);
+    expect(bufferedExtent[2]).toBeLessThanOrEqual(180);
+    expect(bufferedExtent[3]).toBeLessThanOrEqual(90);
+  });
 
-    it('should buffer extent correctly in EPSG:5514', () => {
-      const extent = [
-        -737118.8198068674, -1010163.8964222762, -701407.4374141559,
-        -982026.5783390203,
-      ];
-      const bufferedExtent = service.bufferExtent(
-        extent,
-        getProjection('EPSG:5514'),
-      );
+  it('should buffer extent correctly in EPSG:5514', () => {
+    const extent = [
+      -737118.8198068674, -1010163.8964222762, -701407.4374141559,
+      -982026.5783390203,
+    ];
+    const bufferedExtent = service.bufferExtent(
+      extent,
+      getProjection('EPSG:5514'),
+    );
 
-      expect(bufferedExtent[0]).toBeLessThan(-737118.8198068674);
-      expect(bufferedExtent[1]).toBeLessThan(-1010163.8964222762);
-      expect(bufferedExtent[2]).toBeGreaterThan(-701407.4374141559);
-      expect(bufferedExtent[3]).toBeGreaterThan(-982026.5783390203);
-    });
+    expect(bufferedExtent[0]).toBeLessThan(-737118.8198068674);
+    expect(bufferedExtent[1]).toBeLessThan(-1010163.8964222762);
+    expect(bufferedExtent[2]).toBeGreaterThan(-701407.4374141559);
+    expect(bufferedExtent[3]).toBeGreaterThan(-982026.5783390203);
+  });
 
-    it('should return small but non-zero buffer for moderate distances (e.g., 3000 meters)', () => {
-      const result = service.getPolynomialBufferFactor(3000);
-      expect(Number(result)).toBeCloseTo(0.01, 2); // Expecting close to .0108
-    });
+  it('should return small but non-zero buffer for moderate distances (e.g., 3000 meters)', () => {
+    const result = service.getPolynomialBufferFactor(3000);
+    expect(Number(result)).toBeCloseTo(0.01, 2); // Expecting close to .0108
+  });
 
-    it('should return decreasing buffer for intermediate distances', () => {
-      const result1 = service.getPolynomialBufferFactor(250);
-      const result2 = service.getPolynomialBufferFactor(1000);
-      // As distance increases, buffer should decrease
-      expect(Number(result1)).toBeGreaterThan(Number(result2));
-    });
+  it('should return decreasing buffer for intermediate distances', () => {
+    const result1 = service.getPolynomialBufferFactor(250);
+    const result2 = service.getPolynomialBufferFactor(1000);
+    // As distance increases, buffer should decrease
+    expect(Number(result1)).toBeGreaterThan(Number(result2));
+  });
 
-    it('should return 0 for distances greater than 4000 meters', () => {
-      const result = service.getPolynomialBufferFactor(4100);
-      expect(result).toBeCloseTo(0, 1);
-    });
+  it('should return 0 for distances greater than 4000 meters', () => {
+    const result = service.getPolynomialBufferFactor(5000);
+    expect(result).toBeCloseTo(0, 1);
+  });
 
-    it('should return correct buffer factor for a small distance close to 0 (e.g., 10 meters)', () => {
-      const result = service.getPolynomialBufferFactor(10);
-      expect(result).toBeCloseTo(0.12, 1);
-    });
+  it('should return correct buffer factor for a small distance close to 0 (e.g., 10 meters)', () => {
+    const result = service.getPolynomialBufferFactor(10);
+    expect(result).toBeCloseTo(0.12, 1);
+  });
 
-    it('should return correct buffer factor for 0 meters', () => {
-      const result = service.getPolynomialBufferFactor(0);
-      expect(result).toBeCloseTo(0.12, 1);
-    });
+  it('should return correct buffer factor for 0 meters', () => {
+    const result = service.getPolynomialBufferFactor(0);
+    expect(result).toBeCloseTo(0.12, 1);
   });
 });
