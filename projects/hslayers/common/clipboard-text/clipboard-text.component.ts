e29bc8fb766337e5136817f79a7709e27f36353a
Filip Leitner <leitnerfilip@gmail.com>
2024-08-22 09:39:01 +0200

    feat(common): Clipboard text component

---
+++ projects/hslayers/common/clipboard-text/clipboard-text.component.ts
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import {Component, inject, input} from '@angular/core';
+
+import {HsToastService} from 'hslayers-ng/common/toast';
+import {TranslateCustomPipe} from 'hslayers-ng/services/language';
+
+@Component({
+  selector: 'hs-clipboard-text',
+  standalone: true,
+  imports: [TranslateCustomPipe],
+  template: `
+    <span class="flex-fill text-truncate">{{ text() }}</span>
+    <button
+      class="btn btn-sm text-secondary"
+      data-toggle="tooltip"
+      [title]="'COMMON.copyToClipboard' | translateHs"
+      (click)="copyToClipBoard()"
+    >
+      <i [class.icon-check]="showCheck" [class.icon-copy]="!showCheck"></i>
+    </button>
+  `,
+  styles: `
+    :host {
+      display: flex;
+      align-items: center;
+      width: 100%;
+    }
+  `,
+})
+export class HsClipboardTextComponent {
+  text = input.required<string>();
+
+  hsToastService = inject(HsToastService);
+  showCheck = false;
+
+  copyToClipBoard() {
+    if (!navigator.clipboard) {
+      this.hsToastService.createToastPopupMessage(
+        'COMMON.copyToClipboard',
+        'COMMON.copyToClipboardFailure',
+        {
+          toastStyleClasses: 'bg-danger text-white',
+        },
+      );
+      return;
+    }
+    this.showCheck = true;
+    navigator.clipboard.writeText(this.text());
+    setTimeout(() => {
+      this.showCheck = false;
+    }, 500);
+  }
+}
