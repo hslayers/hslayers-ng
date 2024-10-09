f1b6fda839f2fbc3e841daaa2f757ed01192136d
Filip Leitner <leitnerfilip@gmail.com>
2024-10-09 13:35:34 +0200

    refactor(draw): Standalone components

--- projects/hslayers/components/draw/draw.component.ts
+++ projects/hslayers/components/draw/draw.component.ts
@@ -1,17 +1,37 @@
+import {AsyncPipe, NgClass} from '@angular/common';
 import {Component, OnInit} from '@angular/core';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 import {BehaviorSubject} from 'rxjs';
 
 import {HsDialogContainerService} from 'hslayers-ng/common/dialogs';
-import {HsDrawLayerMetadataDialogComponent} from './draw-layer-metadata/draw-layer-metadata.component';
-
 import {HsDrawService} from 'hslayers-ng/services/draw';
-import {HsPanelBaseComponent} from 'hslayers-ng/common/panels';
-import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {
+  HsPanelBaseComponent,
+  HsPanelHeaderComponent,
+  HsPanelHelpersModule,
+} from 'hslayers-ng/common/panels';
+import {HsQueryModule} from 'hslayers-ng/components/query';
+import {TranslateCustomPipe} from 'hslayers-ng/services/language';
+
+import {DrawEditComponent} from './draw-edit/draw-edit.component';
+import {DrawPanelComponent} from './draw-panel/draw-panel.component';
 
 @Component({
   selector: 'hs-draw',
   templateUrl: './draw.component.html',
+  standalone: true,
+  imports: [
+    NgClass,
+    AsyncPipe,
+    TranslateCustomPipe,
+    HsPanelBaseComponent,
+    DrawPanelComponent,
+    DrawEditComponent,
+    HsQueryModule,
+    HsPanelHeaderComponent,
+    HsPanelHelpersModule,
+  ],
 })
 export class HsDrawComponent extends HsPanelBaseComponent implements OnInit {
   name = 'draw';
@@ -26,7 +46,10 @@ export class HsDrawComponent extends HsPanelBaseComponent implements OnInit {
   ngOnInit(): void {
     this.HsDrawService.layerMetadataDialog
       .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe(() => {
+      .subscribe(async () => {
+        const {HsDrawLayerMetadataDialogComponent} = await import(
+          './draw-layer-metadata/draw-layer-metadata.component'
+        );
         this.HsDialogContainerService.create(
           HsDrawLayerMetadataDialogComponent,
           {service: this.HsDrawService},
