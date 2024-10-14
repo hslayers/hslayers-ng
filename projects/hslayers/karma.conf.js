36e3e7e4aea03d4fe69344f1bb696b2ab61d8ec7
jmacura <macurajan@gmail.com>
2024-10-14 23:07:58 +0200

    build: Fix ChromeHeadless errors

--- projects/hslayers/karma.conf.js
+++ projects/hslayers/karma.conf.js
@@ -28,7 +28,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun:
       typeof process.env.watch == 'undefined' ? true : !process.env.watch,
     restartOnFileChange: true
