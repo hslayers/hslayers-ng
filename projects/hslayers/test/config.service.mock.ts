a5ac1a46537739979b83d6ef8517d2e91cc2a3bc
Filip Leitner <leitnerfilip@gmail.com>
2025-01-10 11:35:25 +0100

    fix(config): Prevent crash when config update fails
    Test config service

--- projects/hslayers/test/config.service.mock.ts
+++ projects/hslayers/test/config.service.mock.ts
@@ -1,6 +1,7 @@
+import {HsConfig} from 'hslayers-ng/config';
 import {Subject} from 'rxjs';
 
-export class HsConfigMock {
+export class HsConfigMock extends HsConfig {
   reverseLayerList: boolean;
   panelsEnabled = {
     legend: false,
@@ -27,11 +28,37 @@ export class HsConfigMock {
   };
   componentsEnabled = {};
   assetsPath = '/assets';
-  configChanges?: Subject<void> = new Subject();
+  configChanges = new Subject<void>();
   id = 'testappid';
-  constructor() {}
 
-  setAppId(id: string) {
-    this.id = id;
+  symbolizerIcons = [
+    {name: 'favourite', url: 'img/icons/favourite28.svg'},
+    {name: 'gps', url: 'img/icons/gps43.svg'},
+    {name: 'information', url: 'img/icons/information78.svg'},
+    {name: 'wifi', url: 'img/icons/wifi8.svg'},
+  ];
+
+  constructor() {
+    super();
+  }
+
+  override update?(config: any): void {
+    Object.assign(this, config);
+    this.configChanges.next();
+  }
+
+  override updateComponentsEnabled?(config: any): any {
+    return {...this.componentsEnabled, ...config?.componentsEnabled};
+  }
+
+  override updateSymbolizers?(config: any): any {
+    return this.symbolizerIcons.map((val) => ({
+      ...val,
+      url: (config.assetsPath ?? '') + val.url,
+    }));
+  }
+
+  override checkDeprecatedCesiumConfig?(config: any): void {
+    // Mock implementation
   }
 }
