hslayers/hslayers-ng

commit 4c2c3b0e5a51e21fec3882bbd5922ce31437771e

refactor: Make calls of transform() safer
Calling ol.proj.transform() with invalid params now throws and error ->
adding some additional checks to prevent accidental crashes

+ fix linter warnings
+ type defs

projects/hslayers-cesium/src/hscesium-layers.service.ts
@@ -22,6 +22,7 @@ import {
   WebMercatorTilingScheme,
   createWorldTerrainAsync,
 } from 'cesium';
+import {Coordinate} from 'ol/coordinate';
 import {Feature} from 'ol';
 import {GeoJSON, KML} from 'ol/format';
 import {
@@ -41,6 +42,7 @@ import {
   Vector as VectorSource,
   XYZ,
 } from 'ol/source';
+import {get as getProjection, transform} from 'ol/proj';
 
 import {HsConfig} from 'hslayers-ng/config';
 import {HsEventBusService} from 'hslayers-ng/services/event-bus';
@@ -61,7 +63,6 @@ import {
 import {HsCesiumConfig} from './hscesium-config.service';
 import {OlCesiumObjectMapItem} from './ol-cesium-object-map-item.class';
 import {ParamCacheMapItem} from './param-cache-map-item.class';
-import {transform} from 'ol/proj';
 
 function MyProxy({proxy, maxResolution, HsUtilsService, projection}) {
   this.proxy = proxy;
@@ -366,16 +367,13 @@ export class HsCesiumLayersService {
         'PROJCS["WGS 84 / Pseudo-Mercator",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]],PROJECTION["Mercator_1SP"],PARAMETER["central_meridian",0],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["X",EAST],AXIS["Y",NORTH],EXTENSION["PROJ4","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"],AUTHORITY["EPSG","3857"]]';
       const secondProjection =
         'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]';
-
-      const xa = coordinates[0];
-      const ya = coordinates[1];
-      const za = coordinates[2];
-
-      const newCoordinates = transform(
-        [xa, ya],
-        firstProjection,
-        secondProjection,
-      );
+      const [xa, ya, za] = coordinates;
+      let newCoordinates: Coordinate;
+      if (getProjection(firstProjection) && getProjection(secondProjection)) {
+        newCoordinates = transform([xa, ya], firstProjection, secondProjection);
+      } else {
+        newCoordinates = coordinates;
+      }
       return Cartesian3.fromDegrees(
         newCoordinates[0],
         newCoordinates[1],

projects/hslayers/common/layers/hs.source.SparqlJson.ts
@@ -3,7 +3,7 @@ import {Feature} from 'ol';
 import {GeoJSON, WKT} from 'ol/format';
 import {Point} from 'ol/geom';
 import {Vector} from 'ol/source';
-import {transform} from 'ol/proj';
+import {get as getProjection, transform} from 'ol/proj';
 
 export type SparqlOptions = {
   category?: string;
@@ -288,8 +288,10 @@ export class SparqlJson extends Vector {
       '&format=application%2Fsparql-results%2Bjson';
     let first_pair = [extent[0], extent[1]];
     let second_pair = [extent[2], extent[3]];
-    first_pair = transform(first_pair, 'EPSG:3857', 'EPSG:4326');
-    second_pair = transform(second_pair, 'EPSG:3857', 'EPSG:4326');
+    if (getProjection('EPSG:3857') && getProjection('EPSG:4326')) {
+      first_pair = transform(first_pair, 'EPSG:3857', 'EPSG:4326');
+      second_pair = transform(second_pair, 'EPSG:3857', 'EPSG:4326');
+    }
     extent = [...first_pair, ...second_pair];
     let s_extent;
     if (optimization === 'wikibase') {
@@ -362,7 +364,7 @@ export class SparqlJson extends Vector {
         const y = parseFloat(
           entity['http://www.w3.org/2003/01/geo/wgs84_pos#lat'],
         );
-        if (!isNaN(x) && !isNaN(y)) {
+        if (!isNaN(x) && !isNaN(y) && getProjection(proj)) {
           const coord = transform([x, y], 'EPSG:4326', proj);
           if (typeof occupiedXY[coord.toString()] !== 'undefined') {
             continue;
@@ -429,6 +431,7 @@ export class SparqlJson extends Vector {
    * Adam Cole, 2011-Sept-14
    * HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
    * based on http://stackoverflow.com/a/7419630
+   * TODO: try inject(HsUtilsService) and call the rainbow() method from there
    * @param numOfSteps
    * @param step
    * @param opacity
@@ -441,19 +444,29 @@ export class SparqlJson extends Vector {
     const q = 1 - f;
     switch (i % 4) {
       case 2:
-        (r = f), (g = 1), (b = 0);
+        r = f;
+        g = 1;
+        b = 0;
         break;
       case 0:
-        (r = 0), (g = f), (b = 1);
+        r = 0;
+        g = f;
+        b = 1;
         break;
       case 3:
-        (r = 1), (g = q), (b = 0);
+        r = 1;
+        g = q;
+        b = 0;
         break;
       case 1:
-        (r = 0), (g = 1), (b = q);
+        r = 0;
+        g = 1;
+        b = q;
         break;
       default:
-        (r = 0), (g = 0), (b = 0);
+        r = 0;
+        g = 0;
+        b = 0;
     }
     const c =
       'rgba(' +

projects/hslayers/common/utils/extent-utils.ts
@@ -1,7 +1,7 @@
 import Feature from 'ol/Feature';
 import {Geometry} from 'ol/geom';
+import {get as getProjection, transform} from 'ol/proj';
 import {fromExtent as polygonFromExtent} from 'ol/geom/Polygon';
-import {transform} from 'ol/proj';
 
 import {generateUuid} from 'hslayers-ng/services/utils';
 
@@ -64,8 +64,9 @@ export function transformExtentValue(
   }
   let first_pair;
   let second_pair;
-  if (disableTransform) {
-    (first_pair = pairs[0]), (second_pair = pairs[1]);
+  if (disableTransform || !getProjection(mapProjection)) {
+    first_pair = pairs[0];
+    second_pair = pairs[1];
   } else {
     first_pair = transform(pairs[0], 'EPSG:4326', mapProjection);
     second_pair = transform(pairs[1], 'EPSG:4326', mapProjection);

projects/hslayers/common/widgets/recursive-dd.component.ts
@@ -35,7 +35,7 @@ export class HsUiExtensionsRecursiveDdComponent {
    * ZoomTo / MoveTo to selected layer overview
    * @param bbox - Bounding box of selected layer
    */
-  zoomTo(bbox): void {
+  zoomTo(bbox: string | number[]): void {
     if (!bbox) {
       return;
     }

projects/hslayers/components/query/query-wmts.service.ts
@@ -3,7 +3,7 @@ import {Injectable} from '@angular/core';
 import WMTSTileGrid from 'ol/tilegrid/WMTS';
 import {Layer} from 'ol/layer';
 import {WMTS} from 'ol/source';
-import {transform} from 'ol/proj';
+import {get as getProjection, transform} from 'ol/proj';
 
 import {HsMapService} from 'hslayers-ng/services/map';
 import {HsUtilsService} from 'hslayers-ng/services/utils';
@@ -30,11 +30,13 @@ export class HsQueryWmtsService {
   ): Promise<{url: string; format: string}> {
     const source = layer.getSource();
 
-    coordinate = transform(
-      coordinate,
-      this.hsMapService.getCurrentProj(),
-      source.getProjection(),
-    );
+    if (getProjection(source.getProjection())) {
+      coordinate = transform(
+        coordinate,
+        this.hsMapService.getCurrentProj(),
+        source.getProjection(),
+      );
+    }
 
     const tileGrid = source.getTileGrid() as WMTSTileGrid;
     const tileCoord = tileGrid.getTileCoordForCoordAndResolution(

projects/hslayers/services/utils/utils.service.ts
@@ -3,7 +3,7 @@ import {Inject, Injectable, PLATFORM_ID} from '@angular/core';
 import {isPlatformBrowser} from '@angular/common';
 
 import {LineString, Polygon} from 'ol/geom';
-import {ProjectionLike, transform} from 'ol/proj';
+import {ProjectionLike, get as getProjection, transform} from 'ol/proj';
 import {getArea, getDistance} from 'ol/sphere';
 import {lastValueFrom} from 'rxjs';
 
@@ -254,38 +254,39 @@ export class HsUtilsService {
   }
 
   /**
-   * Generates CSS color string (rgba(0, 0, 0, 1)) from given range and value for which to have color
-   * @public
+   * Generates CSS color from given range and value for which to have color
+   * Based on http://stackoverflow.com/a/7419630
+   * This function generates vibrant, "evenly spaced" colours (i.e. no clustering).
+   * This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
+   * Adam Cole, 2011-Sept-14
+   * HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
    * @param numOfSteps - Maximum value which is the last color in rainbow
    * @param step - Current value to get color for
    * @param opacity - Opacity from 0 to 1
-   * @returns CSS color
+   * @returns CSS color string (rgba(0, 0, 0, 1))
    */
   rainbow(numOfSteps: number, step: number, opacity: number | string): string {
-    // based on http://stackoverflow.com/a/7419630
-    // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
-    // Adam Cole, 2011-Sept-14
-    // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
-    let r, g, b;
+    let rgb = [0, 0, 0];
     const h = step / (numOfSteps * 1.00000001);
     const i = ~~(h * 4);
     const f = h * 4 - i;
     const q = 1 - f;
     switch (i % 4) {
       case 2:
-        (r = f), (g = 1), (b = 0);
+        rgb = [f, 1, 0];
         break;
       case 0:
-        (r = 0), (g = f), (b = 1);
+      default:
+        rgb = [0, f, 1];
         break;
       case 3:
-        (r = 1), (g = q), (b = 0);
+        rgb = [1, q, 0];
         break;
       case 1:
-        (r = 0), (g = 1), (b = q);
+        rgb = [0, 1, q];
         break;
-      default:
     }
+    const [r, g, b] = rgb;
     const c =
       'rgba(' +
       ~~(r * 235) +
@@ -455,17 +456,21 @@ export class HsUtilsService {
 
   /**
    * Compute and format line length with correct units (m/km)
-   * @private
    * @param line
    * @returns numeric length of line with used units
    */
   formatLength(line: LineString, sourceProj: ProjectionLike): Measurement {
     let length = 0;
     const coordinates = line.getCoordinates();
+    const sourceProjRegistered = getProjection(sourceProj);
 
     for (let i = 0; i < coordinates.length - 1; ++i) {
-      const c1 = transform(coordinates[i], sourceProj, 'EPSG:4326');
-      const c2 = transform(coordinates[i + 1], sourceProj, 'EPSG:4326');
+      const c1 = sourceProjRegistered
+        ? transform(coordinates[i], sourceProj, 'EPSG:4326')
+        : coordinates[i];
+      const c2 = sourceProjRegistered
+        ? transform(coordinates[i + 1], sourceProj, 'EPSG:4326')
+        : coordinates[i + 1];
       length += getDistance(c1, c2);
     }
 

commit 3307bf25ea7e8b34539ee1f988d9c07d4883f415

fix(cesium): Use valid projection for transform
This fixes silent fail of transform function with invalid projections
introduced in 4c2c3b0

eslint.config.mjs
@@ -76,6 +76,7 @@ export default typescriptEslint.config({
         "@typescript-eslint/no-explicit-any": "warn",
         "@typescript-eslint/no-unused-vars": "warn",
         "valid-jsdoc": "off",
+        "jsdoc/check-param-names": "off",
         "jsdoc/require-returns-type": "off",
         "jsdoc/require-hyphen-before-param-description": "off",
         "jsdoc/require-param": "off",

projects/hslayers-cesium/src/hscesium-camera.service.ts
@@ -25,6 +25,7 @@ export class CesiumCameraServiceParams {
   ellipsoid: any;
   lastSyncedExtentFromOl: any;
 }
+
 @Injectable({
   providedIn: 'root',
 })
@@ -75,7 +76,8 @@ export class HsCesiumCameraService extends CesiumCameraServiceParams {
         this.viewer.camera.positionCartographic.latitude * (180 / Math.PI);
       const position = [lngDeg, latDeg, 0];
       return position;
-    } else if (this.viewer.scene.mode == SceneMode.SCENE3D) {
+    }
+    if (this.viewer.scene.mode == SceneMode.SCENE3D) {
       const ray = this.viewer.camera.getPickRay(
         new Cartesian2(
           this.viewer.canvas.width / 2,
@@ -101,9 +103,8 @@ export class HsCesiumCameraService extends CesiumCameraServiceParams {
           ),
         ];
         return position;
-      } else {
-        return null;
       }
+      return null;
     }
   }
 
@@ -123,9 +124,8 @@ export class HsCesiumCameraService extends CesiumCameraServiceParams {
     );
     if (positionCartesian3) {
       return positionCartesian3;
-    } else {
-      return null;
     }
+    return null;
   }
 
   getViewportPolygon() {
@@ -323,8 +323,6 @@ export class HsCesiumCameraService extends CesiumCameraServiceParams {
 
   /**
    * Calculates the distance from the ground based on resolution and latitude
-
-
    * @deprecated
    */
   calcDistanceForResolution(resolution, latitude) {

projects/hslayers-cesium/src/hscesium-layers.service.ts
@@ -111,21 +111,20 @@ export class HsCesiumLayersService {
           resource.indexOf('bbox=0, 45') > -1
         ) {
           return blank_url;
-        } else {
-          const params = this.HsUtilsService.getParamsFromUrl(resource);
-          const bbox = params.bbox.split(',');
-          const dist = Math.sqrt(
-            Math.pow(bbox[0] - bbox[2], 2) + Math.pow(bbox[1] - bbox[3], 2),
-          );
-          if (this.projection == 'EPSG:3857') {
-            if (dist > 1000000) {
-              return blank_url;
-            }
+        }
+        const params = this.HsUtilsService.getParamsFromUrl(resource);
+        const bbox = params.bbox.split(',');
+        const dist = Math.sqrt(
+          Math.pow(bbox[0] - bbox[2], 2) + Math.pow(bbox[1] - bbox[3], 2),
+        );
+        if (this.projection == 'EPSG:3857') {
+          if (dist > 1000000) {
+            return blank_url;
           }
-          if (this.projection == 'EPSG:4326') {
-            if (dist > 1) {
-              return blank_url;
-            }
+        }
+        if (this.projection == 'EPSG:4326') {
+          if (dist > 1) {
+            return blank_url;
           }
         }
       }
@@ -143,7 +142,7 @@ export class HsCesiumLayersService {
     };
   }
 
-  getProjectFromVersion(version, srs, crs) {
+  getProjectFromVersion(version: string, srs, crs) {
     if (version == '1.1.1') {
       return srs;
     }
@@ -288,9 +287,8 @@ export class HsCesiumLayersService {
   currentMapProjCode() {
     if (this.hsMapService.getMap()) {
       return this.hsMapService.getCurrentProj().getCode();
-    } else {
-      this.hsConfig.default_view.getProjection().getCode();
     }
+    this.hsConfig.default_view.getProjection().getCode();
   }
 
   findCesiumLayer(ol: Layer<Source> | Source): ImageryLayer | DataSource {
@@ -362,17 +360,18 @@ export class HsCesiumLayersService {
   async syncFeatures(ol_source: VectorSource) {
     const tmp_source = new GeoJsonDataSource('tmp');
     GeoJsonDataSource.crsNames['EPSG:3857'] = function (coordinates) {
-      const firstProjection =
-        'PROJCS["WGS 84 / Pseudo-Mercator",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]],PROJECTION["Mercator_1SP"],PARAMETER["central_meridian",0],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["X",EAST],AXIS["Y",NORTH],EXTENSION["PROJ4","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"],AUTHORITY["EPSG","3857"]]';
-      const secondProjection =
-        'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]';
+      //PROJCS["WGS 84 / Pseudo-Mercator",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]],PROJECTION["Mercator_1SP"],PARAMETER["central_meridian",0],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["X",EAST],AXIS["Y",NORTH],EXTENSION["PROJ4","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"],AUTHORITY["EPSG","3857"]]
+      const firstProjection = 'EPSG:3857';
+      const secondProjection = 'EPSG:4326';
       const [xa, ya, za] = coordinates;
-      let newCoordinates: Coordinate;
-      if (getProjection(firstProjection) && getProjection(secondProjection)) {
-        newCoordinates = transform([xa, ya], firstProjection, secondProjection);
-      } else {
-        newCoordinates = coordinates;
+      if (!getProjection(firstProjection) || !getProjection(secondProjection)) {
+        this.hsLog.error('UNDEFINED PROJECTION!');
       }
+      const newCoordinates = transform(
+        [xa, ya],
+        firstProjection,
+        secondProjection,
+      );
       return Cartesian3.fromDegrees(
         newCoordinates[0],
         newCoordinates[1],
@@ -441,7 +440,6 @@ export class HsCesiumLayersService {
           this.hsMapService.proxifyLayerLoader(lyr as Layer<Source>, false);
         }
       }
-
       if (this.HsUtilsService.instOf(lyr, TileLayer)) {
         if (
           this.HsUtilsService.instOf(
@@ -490,7 +488,8 @@ export class HsCesiumLayersService {
         show: olLayer.getVisible(),
         minimumTerrainLevel: getMinimumTerrainLevel(olLayer) || 1,
       });
-    } else if (this.HsUtilsService.instOf(layerSource, XYZ)) {
+    }
+    if (this.HsUtilsService.instOf(layerSource, XYZ)) {
       return new ImageryLayer(
         new UrlTemplateImageryProvider({
           url: (layerSource as XYZ).getUrls()[0],
@@ -499,22 +498,24 @@ export class HsCesiumLayersService {
           show: olLayer.getVisible(),
         },
       );
-    } else if (this.HsUtilsService.instOf(layerSource, TileWMS)) {
+    }
+    if (this.HsUtilsService.instOf(layerSource, TileWMS)) {
       return this.createTileProvider(olLayer);
-    } else if (this.HsUtilsService.instOf(layerSource, ImageWMS)) {
+    }
+    if (this.HsUtilsService.instOf(layerSource, ImageWMS)) {
       return this.createSingleImageProvider(olLayer as ImageLayer<ImageSource>);
-    } else if (this.HsUtilsService.instOf(olLayer, VectorLayer)) {
+    }
+    if (this.HsUtilsService.instOf(olLayer, VectorLayer)) {
       const dataSource = await this.createVectorDataSource(
         olLayer as VectorLayer<VectorSource<Feature>>,
       );
       return dataSource;
-    } else {
-      this.hsLog.error(
-        'Unsupported layer type for layer: ',
-        olLayer,
-        'in Cesium converter',
-      );
     }
+    this.hsLog.error(
+      'Unsupported layer type for layer: ',
+      olLayer,
+      'in Cesium converter',
+    );
   }
 
   async createVectorDataSource(ol_lyr: VectorLayer<VectorSource<Feature>>) {

projects/hslayers-cesium/src/picker.service.ts
@@ -100,8 +100,7 @@ export class HsCesiumPickerService {
       ]);
       this.HsCesiumQueryPopupService.showPopup({pixel: movement.position});
       return;
-    } else {
-      this.HsCesiumQueryPopupService.fillFeatures([]);
     }
+    this.HsCesiumQueryPopupService.fillFeatures([]);
   }
 }