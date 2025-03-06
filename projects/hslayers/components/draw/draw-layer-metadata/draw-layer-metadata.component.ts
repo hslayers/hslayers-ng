e2158d94a5ee411b902d6477cd10a63215fa226f
Filip Leitner <leitnerfilip@gmail.com>
2025-03-06 10:54:11 +0100

    refactor(draw): Use hsCommonLaymanService in draw-layer-metadata

--- projects/hslayers/components/draw/draw-layer-metadata/draw-layer-metadata.component.ts
+++ projects/hslayers/components/draw/draw-layer-metadata/draw-layer-metadata.component.ts
@@ -9,6 +9,7 @@ import {
 } from 'hslayers-ng/common/dialogs';
 import {HsDrawService} from 'hslayers-ng/services/draw';
 import {
+  HsCommonLaymanService,
   HsLaymanModule,
   awaitLayerSync,
   getLaymanFriendlyLayerName,
@@ -25,6 +26,8 @@ import {
   setPath,
   setTitle,
 } from 'hslayers-ng/common/extensions';
+import {VectorSourceEvent} from 'ol/source/Vector';
+import {FeatureLike} from 'ol/Feature';
 
 @Component({
   selector: 'hs-draw-layer-metadata',
@@ -34,90 +37,107 @@ import {
 export class HsDrawLayerMetadataDialogComponent
   implements HsDialogComponent, OnInit
 {
-  @Input() data: {
-    service: HsDrawService;
-  };
+  @Input() data: object;
 
   newLayerPath: string;
-  attributes: Array<any> = [];
-  layer: any;
-  title: any;
+  attributes: Array<{id: number; name: string; value: string}> = [];
+  layer = this.hsDrawService.selectedLayer;
+
+  title: string;
   path: string;
   folderVisible = false;
-  type: string;
-  endpoint: any;
+  type: 'draw' | 'layman' = 'draw';
+
   access_rights: AccessRightsModel = {
     'access_rights.write': 'private',
     'access_rights.read': 'EVERYONE',
   };
   onlyMineFilterVisible = false;
-  tmpFeatures: any;
+  tmpFeatures: FeatureLike[] = [];
+  viewRef: ViewRef;
+
   constructor(
     public HsMapService: HsMapService,
     public HsDialogContainerService: HsDialogContainerService,
+    public hsCommonLaymanService: HsCommonLaymanService,
+    public hsDrawService: HsDrawService,
   ) {}
 
-  viewRef: ViewRef;
   ngOnInit(): void {
-    this.layer = this.data.service.selectedLayer;
-    this.title = getTitle(this.layer);
-    this.path = getPath(this.layer);
-    this.endpoint = this.data.service.laymanEndpoint;
-    this.type = 'draw';
+    this.title = getTitle(this.layer) || '';
+    this.path = getPath(this.layer) || '';
+    this.tmpFeatures = this.layer.getSource().getFeatures();
   }
 
   titleChanged(): void {
-    setTitle(this.layer, this.title);
+    if (this.title) {
+      setTitle(this.layer, this.title);
+    }
   }
 
   confirm(): void {
-    const dic = {};
+    try {
+      const dic = {};
 
-    setName(this.layer, getLaymanFriendlyLayerName(getTitle(this.layer)));
-    const tmpLayer = this.HsMapService.findLayerByTitle('tmpDrawLayer') || null;
-    if (tmpLayer) {
-      this.HsMapService.getMap().removeLayer(tmpLayer);
+      setName(this.layer, getLaymanFriendlyLayerName(getTitle(this.layer)));
+      const tmpLayer =
+        this.HsMapService.findLayerByTitle('tmpDrawLayer') || null;
+      if (tmpLayer) {
+        this.HsMapService.getMap().removeLayer(tmpLayer);
+      }
+
+      this.attributes.forEach((a) => {
+        if (a.name && a.name.trim() !== '') {
+          dic[a.name] = a.value;
+        }
+      });
+
+      let editorConfig = getEditor(this.layer);
+      if (!editorConfig) {
+        editorConfig = {};
+        setEditor(this.layer, editorConfig);
+      }
+      editorConfig.defaultAttributes = dic;
+
+      this.layer.getSource().forEachFeature((f) => {
+        f.setProperties(dic);
+      });
+
+      setAccessRights(this.layer, this.access_rights);
+      this.hsDrawService.changeDrawSource();
+
+      this.hsDrawService.addDrawLayer(this.layer);
+      this.hsDrawService.fillDrawableLayers();
+      this.tmpFeatures = this.layer.getSource().getFeatures();
+
+      //Dispatch add feature event in order to trigger sync
+      awaitLayerSync(this.layer).then(() => {
+        const event = this.getFeatureEvent();
+        this.layer.getSource().dispatchEvent(event);
+        this.hsDrawService.tmpDrawLayer = false;
+        this.HsDialogContainerService.destroy(this);
+      });
+    } catch (error) {
+      console.error('Error in confirm method:', error);
+      this.hsDrawService.tmpDrawLayer = false;
+      this.HsDialogContainerService.destroy(this);
     }
-
-    this.attributes.forEach((a) => {
-      dic[a.name] = a.value;
-    });
-    let editorConfig = getEditor(this.layer);
-    if (!editorConfig) {
-      editorConfig = {};
-      setEditor(this.layer, editorConfig);
-    }
-    editorConfig.defaultAttributes = dic;
-
-    this.layer.getSource().forEachFeature((f) => {
-      f.setProperties(dic);
-    });
-
-    setAccessRights(this.layer, this.access_rights);
-    this.data.service.changeDrawSource();
-
-    this.data.service.addDrawLayer(this.layer);
-    this.data.service.fillDrawableLayers();
-    this.tmpFeatures = this.layer.getSource().getFeatures();
-    //Dispatch add feature event in order to trigger sync
-    awaitLayerSync(this.layer).then(() => {
-      const event = this.tmpFeatures ? this.getEventType() : 'addfeature';
-      this.layer.getSource().dispatchEvent(event);
-    });
-    this.data.service.tmpDrawLayer = false;
-    this.HsDialogContainerService.destroy(this);
   }
 
-  getEventType() {
-    return this.layer.getSource().getFeatures().length > this.tmpFeatures.length
-      ? //Existing layer
-        {type: 'addfeature', feature: this.tmpFeatures}
-      : //New layer
-        'addfeature';
+  /**
+   * Creates an event for the feature event.
+   * If the layer is new, the event is 'addfeature'.
+   * If the layer is existing, the event is a VectorSourceEvent with the features.
+   */
+  getFeatureEvent() {
+    return this.tmpFeatures &&
+      this.layer.getSource().getFeatures().length > this.tmpFeatures.length
+      ? new VectorSourceEvent('addfeatures', undefined, this.tmpFeatures)
+      : 'addfeature';
   }
 
   cancel(): void {
-    this.data.service.selectedLayer = this.data.service.previouslySelected;
+    this.hsDrawService.selectedLayer = this.hsDrawService.previouslySelected;
     this.HsDialogContainerService.destroy(this);
   }
 
@@ -130,7 +150,7 @@ export class HsDrawLayerMetadataDialogComponent
   }
 
   selectLayer(layer): void {
-    this.data.service.selectLayer(layer);
+    this.hsDrawService.selectLayer(layer);
     this.HsDialogContainerService.destroy(this);
   }
 }
