dbfb07843a578de221f9c6fcb5153ebf41c90879
Filip Leitner <leitnerfilip@gmail.com>
2025-03-07 10:10:54 +0100

    refator(auth): Handle error responses

--- projects/hslayers/core/auth.interceptor.ts
+++ projects/hslayers/core/auth.interceptor.ts
@@ -1,8 +1,13 @@
-import {HttpInterceptorFn, HttpResponse} from '@angular/common/http';
-import {map} from 'rxjs/operators';
+import {
+  HttpErrorResponse,
+  HttpInterceptorFn,
+  HttpResponse,
+} from '@angular/common/http';
+import {catchError, map} from 'rxjs/operators';
 import {inject} from '@angular/core';
 
 import {HsCommonLaymanService} from 'hslayers-ng/common/layman';
+import {throwError} from 'rxjs';
 
 export const HsAuthInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(HsCommonLaymanService);
@@ -12,15 +17,29 @@ export const HsAuthInterceptor: HttpInterceptorFn = (req, next) => {
     req.url.includes(authService.layman()?.url)
   ) {
     return next(req).pipe(
-      map((event: HttpResponse<unknown>) => {
-        if (
-          event.body?.['authenticated'] === false &&
-          authService.isAuthenticated()
-        ) {
-          //Unauthorized request sent to backend -> logout
-          authService.logout$.next();
+      map((event) => {
+        // Check for HttpResponse (success case)
+        if (event instanceof HttpResponse) {
+          if (
+            event.body?.['authenticated'] === false &&
+            authService.isAuthenticated()
+          ) {
+            // Unauthorized request sent to backend -> logout
+            authService.logout$.next();
+          }
         }
         return event;
+      }),
+      catchError((error: HttpErrorResponse) => {
+        // Check for HttpErrorResponse (error case)
+        if (error.status === 403) {
+          //Unauthorized access, Unsuccessful OAuth2 authentication/HTTP Header authentication
+          //https://github.com/LayerManager/layman/blob/master/src/layman/error_list.py
+          // --> logout
+          authService.logout$.next();
+        }
+        // Re-throw the error using throwError
+        return throwError(() => error);
       }),
     );
   }
