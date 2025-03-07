d316fb816a1dccf42b2b9a834f21178b1063eac1
Filip Leitner <leitnerfilip@gmail.com>
2025-03-07 09:32:33 +0100

    feat(core): Use auth intreceptor which will log out the user on unauthorized req

--- projects/hslayers/core/hslayers.module.ts
+++ projects/hslayers/core/hslayers.module.ts
@@ -1,6 +1,6 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
-import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
 
 import {HsDialogContainerComponent} from 'hslayers-ng/common/dialogs';
 import {HsLayoutHostDirective} from './layout.directive';
@@ -10,6 +10,7 @@ import {HsPanelHelpersModule} from 'hslayers-ng/common/panels';
 import {HsSidebarModule} from 'hslayers-ng/components/sidebar';
 import {HslayersComponent} from './hslayers.component';
 import {HsToastComponent} from 'hslayers-ng/common/toast';
+import {HsAuthInterceptor} from './auth.interceptor';
 
 @NgModule({
   declarations: [HsMapHostDirective, HslayersComponent, HsLayoutHostDirective],
@@ -22,6 +23,6 @@ import {HsToastComponent} from 'hslayers-ng/common/toast';
     HsDialogContainerComponent,
   ],
   exports: [HslayersComponent],
-  providers: [provideHttpClient(withInterceptorsFromDi())],
+  providers: [provideHttpClient(withInterceptors([HsAuthInterceptor]))],
 })
 export class HslayersModule {}
