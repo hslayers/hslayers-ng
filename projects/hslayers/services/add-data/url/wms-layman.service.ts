aa03c7da7dcc1b29329f992bd9c52a4230535999
unknown <leitnerfilip@gmail.com>
2025-04-16 19:14:26 +0200

    refactor(add-data): Get data from layer descriptor for layman OWS
    Necessary for layers from composition where workspace and layer name might not be known

--- projects/hslayers/services/add-data/url/wms-layman.service.ts
+++ projects/hslayers/services/add-data/url/wms-layman.service.ts
@@ -2,10 +2,10 @@ import {inject, Injectable} from '@angular/core';
 
 import {LayerOptions, WhatToAddDescriptor} from 'hslayers-ng/types';
 import {HsMapService} from 'hslayers-ng/services/map';
-import {setQueryCapabilities} from 'hslayers-ng/common/extensions';
 
 import {HsAddDataService} from '../add-data.service';
 import {HsUrlWmsService} from './wms.service';
+import {HsCommonLaymanLayerService} from 'hslayers-ng/common/layman';
 
 @Injectable({
   providedIn: 'root',
@@ -14,11 +14,20 @@ export class HsAddDataWmsLaymanService {
   hsAddService = inject(HsAddDataService);
   wmsService = inject(HsUrlWmsService);
   mapService = inject(HsMapService);
+  hsCommonLaymanLayerService = inject(HsCommonLaymanLayerService);
 
   /**
    * Creates a WMS layer from a layman layer descriptor
+   * NOTE: If catalogueService.addLayerToMap is furthersimplified whatToAdd could be simplified to simple workspace and layer name strings
+   * currently for the sake of simplicity we have duplicate source of necessary inputs in whatToAdd and desc.
    */
-  getLayer(whatToAdd: WhatToAddDescriptor, options: LayerOptions) {
+  async getLayer(whatToAdd: WhatToAddDescriptor, options: LayerOptions) {
+    const desc = await this.hsCommonLaymanLayerService.describeLayer(
+      whatToAdd.name,
+      whatToAdd.workspace,
+      {useCache: true},
+    );
+
     this.wmsService.data.srs = this.mapService
       .getMap()
       .getView()
@@ -34,22 +43,22 @@ export class HsAddDataWmsLaymanService {
 
     const layer = this.wmsService.getLayer(
       {
-        //l_<uuid> stored in layer prop instead of layer name
-        Name: whatToAdd.layer,
-        title: whatToAdd.title,
-        link: whatToAdd.link,
+        //l_<uuid>
+        Name: desc.wms.name,
+        title: desc.title,
+        link: desc.wms.url,
         //TODO:
         MetadataURL: undefined,
         BoundingBox: [
           {
             crs: 'EPSG:3857',
-            extent: whatToAdd.extent,
+            extent: desc.bounding_box,
           },
         ],
       },
       {
         ...options,
-        layerName: whatToAdd.title.replace(/\//g, '&#47;'),
+        layerName: desc.title.replace(/\//g, '&#47;'),
         // path: 'WMS',
         imageFormat: 'image/png',
         queryFormat: 'application/json',
@@ -59,7 +68,6 @@ export class HsAddDataWmsLaymanService {
         queryable: true,
       },
     );
-    setQueryCapabilities(layer, false);
     return [layer];
   }
 }
