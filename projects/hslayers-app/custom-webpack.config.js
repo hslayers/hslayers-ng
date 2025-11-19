d1a59a6d641509216e04e179f65d6ac5d30d1463
unknown <leitnerfilip@gmail.com>
2025-11-19 12:09:56 +0100

    chore: honor sourceMap compile  options

--- projects/hslayers-app/custom-webpack.config.js
+++ projects/hslayers-app/custom-webpack.config.js
@@ -1,14 +1,19 @@
 var webpack = require('webpack');
 
-module.exports = {
-  devtool: false,
-  plugins: [
-    new webpack.SourceMapDevToolPlugin({
-      filename: '[file].map',
-      exclude: ['vendor.js'],
-    })
-  ],
-  output: {
-    publicPath: 'auto',
+module.exports = (config, options) => {
+  // Only add source maps if Angular config has sourceMap enabled
+  if (options.sourceMap !== false) {
+    config.plugins.push(
+      new webpack.SourceMapDevToolPlugin({
+        filename: '[file].map',
+        exclude: ['vendor.js'],
+      })
+    );
   }
+  // Ensure publicPath is set
+  if (config.output) {
+    config.output.publicPath = config.output.publicPath || 'auto';
+  }
+
+  return config;
 };
