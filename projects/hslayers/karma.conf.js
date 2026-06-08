01c1ac893587f4303f40084e3663ff913e622238
leitnerfilip <leitnerfilip@gmail.com>
2026-06-08 13:02:39 +0200

    test: try fixing missing puppeter in github actions

--- projects/hslayers/karma.conf.js
+++ projects/hslayers/karma.conf.js
@@ -1,6 +1,6 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-process.env.CHROME_BIN = require('puppeteer').executablePath();
+// https://karma-runner.github.io/6.4/config/configuration-file.html
+process.env.CHROME_BIN ??= require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
